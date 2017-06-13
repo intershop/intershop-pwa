2aac663cab3c3584fad1a005f9c01b4ff2c63b78
Stefan Hauke <s.hauke@intershop.de>
2017-06-13 13:11:47 +0200

    split up the family page to header, footer and family-page component, added error-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { FooterComponent } from './footer.component';
+import { FamilyPageComponent } from './pages/family-page.component';
+import { ErrorPageComponent } from './pages/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FamilyPageComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/family-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-family-page',
+  templateUrl: './family-page.component.html',
+  styleUrls: ['./family-page.component.css']
+})
+export class FamilyPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
