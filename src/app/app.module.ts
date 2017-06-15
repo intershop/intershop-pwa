817f774126cd09fd7ac080797481d7800a34eb8a
Susanne Schneider <s.schneider@intershop.de>
2017-06-15 13:23:09 +0200

    integrated ngx-translate package, first translation implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpModule, Http} from '@angular/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { FooterComponent } from './footer.component';
 import { FamilyPageComponent } from './pages/family-page.component';
 import { ErrorPageComponent } from './pages/error-page.component';
+
+// AoT requires an exported function for factories
+export function createTranslateLoader(http: Http) {
+    return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -16,7 +24,15 @@ import { ErrorPageComponent } from './pages/error-page.component';
     ErrorPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [Http]
+        }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
