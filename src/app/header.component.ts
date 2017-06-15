43064591fcb38d2c7019195f02e1fc05ba560e6e
Susanne Schneider <s.schneider@intershop.de>
2017-06-15 13:23:09 +0200

    integrated ngx-translate package, first translation implemented

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  
+  constructor(translate: TranslateService) {
+        // this language will be used as a fallback when a translation isn't found in the current language
+        translate.setDefaultLang('en');
 
-  constructor() { }
+         // the lang to use, if the lang isn't available, it will use the current loader to get them
+        translate.use('en');
+    }
 
   ngOnInit() {
   }
