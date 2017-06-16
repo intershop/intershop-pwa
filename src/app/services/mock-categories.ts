c61ad33c369905f14049d1f76f0773b5e6425bfe
Stefan Hauke <s.hauke@intershop.de>
2017-06-16 09:23:22 +0200

    added a categories service that is used for the main navigation ang gets its categoire from the mock-categories.ts for now

---
+++ src/app/services/mock-categories.ts
@@ -0,0 +1,9 @@
+import { Category } from './category';
+
+export const CATEGORIES: Category[] = [
+    { id: '0', name: 'Cameras'},
+    { id: 'C', name: 'Computers' },
+    { id: 'A', name: 'Home Entertainment' },
+    { id: 'B', name: 'Specials' },
+    { id: 'B', name: 'Brands' }
+];
