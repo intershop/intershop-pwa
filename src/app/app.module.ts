9a558c57818491b51bdb6b39fc43ec7a67d9125e
Stefan Hauke <s.hauke@intershop.de>
2017-06-16 14:02:48 +0200

    added static registration page componet as form example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FamilyPageComponent } from './pages/family-page/family-page.component';
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { ErrorPageComponent } from './pages/error-page/error-page.component';
     HeaderComponent,
     FooterComponent,
     FamilyPageComponent,
-    ErrorPageComponent
+    ErrorPageComponent,
+    RegistrationPageComponent
   ],
   imports: [
     BrowserModule
