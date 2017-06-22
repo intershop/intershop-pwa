a3150b4f8f856b7a0a6ba1836e8a4f9eda42d4a3
Stefan Hauke <s.hauke@intershop.de>
2017-06-22 12:07:18 +0200

    getting categrories by REST API but adding mock categies as fallback

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -4,7 +4,7 @@ import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 import { Category } from './category';
-import { CATEGORIES } from './mock-categories';
+import { MOCK_CATEGORIES } from './mock-categories';
 
 @Injectable()
 export class CategoriesService {
@@ -34,7 +34,7 @@ export class CategoriesService {
   
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
+    return Promise.resolve(MOCK_CATEGORIES);
   }
 
 }
--- src/app/services/mock-categories.ts
+++ src/app/services/mock-categories.ts
@@ -1,7 +1,7 @@
 import { Category } from './category';
 
-export const CATEGORIES: Category[] = [
-    { id: 'B', name: 'Brands' },
+export const MOCK_CATEGORIES: Category[] = [
+    { id: 'B', name: 'Mock' },
     { id: '0', name: 'Cameras'},
     { id: 'C', name: 'Computers' },
     { id: 'A', name: 'Home Entertainment' },
