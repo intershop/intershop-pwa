6185ef95117496ae01a85b0e410e16e8ec1b11f3
Susanne Schneider <s.schneider@intershop.de>
2017-06-22 13:47:23 +0200

    implement TranslateService in root component (not in every single component)

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -8,13 +7,7 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class HeaderComponent implements OnInit {
   
-  constructor(translate: TranslateService) {
-        // this language will be used as a fallback when a translation isn't found in the current language
-        translate.setDefaultLang('en');
-
-         // the lang to use, if the lang isn't available, it will use the current loader to get them
-        translate.use('en');
-    }
+  constructor() { }
 
   ngOnInit() {
   }
