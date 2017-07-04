3eeec69ff4f790e690f7c56de6e9122b42313d92
Danilo Hoffmann <d.hoffmann@intershop.de>
2017-07-04 12:13:27 +0200

    repaired example geb test

--- e2e/geb/groovy/ProofOfConceptPage.groovy
+++ e2e/geb/groovy/ProofOfConceptPage.groovy
@@ -3,6 +3,7 @@ class ProofOfConceptPage extends geb.Page {
     static at = { title == "REST based storefront Proof of Concept" }
     
     static content = {
-        category { $('h1') }
+        category(required:false) { $('h1') }
+        customerInfo { $('ul.customer-info') }
     }
 }
