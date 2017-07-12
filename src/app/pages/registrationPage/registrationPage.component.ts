8e2a570a3b5d650c82e1383f147cbdf9ee51b0d8
Intershop <support@intershop.de>
2017-07-12 17:46:11 +0530

    Registration Page Refactor

--- src/app/pages/registrationPage/registrationPage.component.ts
+++ src/app/pages/registrationPage/registrationPage.component.ts
@@ -1,65 +1,23 @@
-import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms'
-import { Router } from '@angular/router'
-import { CreateAccountModel } from 'app/pages/registrationPage/registrationPage.model';
-import { matchOtherValidator, EmailValidator, PasswordValidator } from 'app/shared/validators';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './registrationPage.component.html'
 })
 
 export class RegistrationPageComponent implements OnInit {
-    public email: any;
-    public invalidCaptcha = true;
+    private isCaptchaValid = false;
+    private isAddressFormValid : boolean = false;
+    private isEmailFormValid : boolean = false;
 
-    userDetails: CreateAccountModel;
-    createAccountForm: FormGroup;
-
-    constructor(private _formbuilder: FormBuilder,
-        private router: Router
-    ) { }
+    constructor(private router: Router) { }
 
     ngOnInit() {
-        this.createAccountForm = this._formbuilder.group({
-            emailDetails: this._formbuilder.group({
-                emailAddress: ['', [Validators.required,
-                  EmailValidator.validate
-                ]],
-                confirmEmailAddress: ['', [Validators.required,
-                matchOtherValidator('emailAddress')
-                ]],
-                password: ['', [Validators.required,
-                  PasswordValidator.validate
-                ]],
-                confirmPassword: ['', [Validators.required,
-                matchOtherValidator('password')]],
-                securityQuestion: ['', [Validators.required]],
-                answer: ['', [Validators.required]],
-                receivePromotions: []
-            }),
-            address: this._formbuilder.group({
-                country: ['', [Validators.required]],
-                firstName: ['', [Validators.required]],
-                lastName: ['', [Validators.required]],
-                line1: ['', [Validators.required]],
-                line2: [],
-                zip: ['', [Validators.required]],
-                city: ['', [Validators.required]],
-                phone: [],
-                preferredLanguage: ['', [Validators.required]],
-                birthday: [],
-               // state: ['', [Validators.required]],
-            }),
-
-        })
-    }
-
-    onValidCaptcha( captchaValidation ){
-      this.invalidCaptcha = captchaValidation;
+       
     }
 
     save() {
-        console.log(this.createAccountForm.value);
+        //console.log(this.createAccountForm.value);
     }
 
     cancelClicked() {
