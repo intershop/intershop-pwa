540fd86598c1e3811ecfc9cc99218ee4442509c5
Intershop <support@intershop.de>
2017-07-14 17:04:09 +0530

    recaptcha changes

--- src/app/pages/registrationPage/registrationPage.module.ts
+++ src/app/pages/registrationPage/registrationPage.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RecaptchaModule } from 'ng2-recaptcha';
+import { RecaptchaModule } from 'ng-recaptcha';
 import { registrationPageRoute } from 'app/pages/registrationPage/registrationPage.routes';
 import { RegistrationPageComponent } from 'app/pages/registrationPage/registrationPage.component';
 import {CaptchaComponent} from './captcha/captcha.component';
