9578106432cc590eb3e4e0cf69017e0352f5bc3d
Intershop <support@intershop.de>
2017-07-14 13:09:20 +0530

    Registration Page HTML and CSS changes

--- src/app/shared/validators/password.validator.ts
+++ src/app/shared/validators/password.validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl } from '@angular/forms';
 
 export class PasswordValidator {
     public static validate(c: AbstractControl) {
-        let PASSWORD_REGEXP = /(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9!@#$%^&*()_+}{?><:"\S]{7,})$/;
+        let PASSWORD_REGEXP = /(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9!@#$%^&*()_+}{?><:"\S]{2,})$/;
         if (!c.value) {
             return null;
         } else {
