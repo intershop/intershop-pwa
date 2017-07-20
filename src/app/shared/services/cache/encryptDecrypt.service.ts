3dfc3666709250481f1c209817ebbb19a61b3b4d
Intershop <support@intershop.de>
2017-07-20 14:27:23 +0530

    add jsDoc comments and remove unwanted code

--- src/app/shared/services/cache/encryptDecrypt.service.ts
+++ src/app/shared/services/cache/encryptDecrypt.service.ts
@@ -1,9 +1,18 @@
-
 export class EncryptDecryptService {
-    encrypt(dataToEncrypt) {
-        return btoa(String(dataToEncrypt)); // string => encrypted string
-    }
 
+    /**
+     * Encrpt secure data
+     * @param  {any} dataToEncrypt
+     */
+    encrypt(dataToEncrypt: any) {
+        return btoa(String(dataToEncrypt)); 
+    };
+
+    
+    /**
+     * Decrypt secure data
+     * @param  {string} dataToDecrypt
+     */
     decrypt(dataToDecrypt: string) {
         return new Uint8Array(atob(dataToDecrypt).split(',').map(t => Number(t)))
     }
--- src/app/shared/services/customErrorHandler.ts
+++ src/app/shared/services/customErrorHandler.ts
@@ -2,19 +2,30 @@ import {ErrorHandler} from '@angular/core';
 import {Observable} from 'rxjs/Rx';
 
 export class CustomErrorHandler extends ErrorHandler {
+
+  
+  /**
+   * Constructor
+   */
   constructor() {
-    // The true paramter tells Angular to rethrow exceptions, so operations like 'bootstrap' will result in an error
-    // when an error happens. If we do not rethrow, bootstrap will always succeed.
     super(true);
   }
 
-  handleError(error) {
-    // send the error to the server
-    // delegate to the default handler
+  
+  /**
+   * handle error
+   * @param  {any} error
+   */
+  handleError(error:any) {
     console.log(error.message);
 
   }
 
+  /**
+   * Handle api errors
+   * @param  {any} error
+   * @returns Observable
+   */
   handleApiErros(error: any): Observable<any> {
     const errorMessage = error.status + ' and ' + error.statusText;
     console.log(errorMessage);
