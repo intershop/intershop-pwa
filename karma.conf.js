f3fdf2edb8a3e44d4140b9fdd1428d8129c36a78
Danilo Hoffmann <d.hoffmann@intershop.de>
2017-07-24 12:04:54 +0200

    added PhantomJS as browser for karma

--- karma.conf.js
+++ karma.conf.js
@@ -53,7 +53,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'PhantomJS'],
     singleRun: false
   });
 };
