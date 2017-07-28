a06c185d189f00760cf4b632231a97012daece65
Intershop <support@intershop.de>
2017-07-28 09:46:56 +0530

    Added spec files.

---
+++ src/app/shared/services/api.service.spec.ts
@@ -0,0 +1,68 @@
+import {Headers, Http, Response, URLSearchParams} from '@angular/http';
+
+import {ApiService} from './api.service';
+import {Observable} from 'rxjs/Observable';
+
+describe('API service test', () => {
+  let mockJwtService = null;
+  let mockHttpService = null;
+  let apiService: ApiService;
+
+  beforeEach(() => {
+    mockJwtService = {
+      getToken: () => {
+        return '123token456'
+      }
+    }
+
+    mockHttpService = {
+      get: (url: string, options: {}): Observable<any> => {
+        return Observable.of('{"type":"get"}');
+      },
+      put: (path: string, body: {}): Observable<any> => {
+        return Observable.of('{"type":"put"}');
+      },
+      post: (path: string, body: {}): Observable<any> => {
+        return Observable.of('{"type":"post"}');
+      },
+      delete: (path: string): Observable<any> => {
+        return Observable.of('{"type":"delete"}');
+      },
+
+    };
+    apiService = new ApiService(mockHttpService, mockJwtService);
+  });
+
+  it('should return an observable on calling of GET().', () => {
+    let returnVal;
+    apiService.get('', null).subscribe((res) => {
+      returnVal = res;
+    });
+    expect(returnVal.type).toEqual('get');
+  });
+
+  it('should return an observable on calling of PUT().', () => {
+    let returnVal;
+    apiService.put('', null).subscribe((res) => {
+      returnVal = res;
+    });
+    expect(returnVal.type).toEqual('put');
+  });
+
+  it('should return an observable on calling of POST().', () => {
+    let returnVal;
+    apiService.post('', null).subscribe((res) => {
+      returnVal = res;
+    });
+    expect(returnVal.type).toEqual('post');
+  });
+
+  it('should return an observable on calling of DELETE().', () => {
+    let returnVal;
+    apiService.delete('').subscribe((res) => {
+      returnVal = res;
+    });
+    expect(returnVal.type).toEqual('delete');
+  });
+
+});
