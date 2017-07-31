88d6490908b066d17c08a8bf509b5f14072653db
Stefan Hauke <s.hauke@intershop.de>
2017-07-31 17:30:55 +0200

    added ngx-bootstrap as dependency and used it for wishlistPage Popover and languageSwitch/miniCart Collapse - removed dependency for ng-bootstrap and ngx-popover - set a Popover default configuration  { placement: 'top', triggers: 'hover', container: 'body' }

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { PopoverConfig } from 'ngx-bootstrap/popover';
+
+export function getPopoverConfig(): PopoverConfig {
+  return Object.assign(new PopoverConfig(), { placement: 'top', triggers: 'hover', container: 'body' });
+}
 
 @Component({
   selector: 'is-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [
+    { provide: PopoverConfig, useFactory: getPopoverConfig }
+  ]
 })
 export class AppComponent {
   title = 'app';
@@ -14,4 +22,5 @@ export class AppComponent {
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('en');
   }*/
+
 }
--- src/app/pages/wishlistPage/wishlistPage.module.ts
+++ src/app/pages/wishlistPage/wishlistPage.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { WishListPageComponent } from './wishlistPage.component';
 import { WishlistPageRoutes } from './wishlistPage.routes';
-import {PopoverModule} from 'ngx-popover';
+import { PopoverModule } from 'ngx-bootstrap/popover';
 
 @NgModule({
     imports: [
-        CommonModule,
-        RouterModule.forChild(WishlistPageRoutes),
-        PopoverModule
-        ],
+      CommonModule,
+      RouterModule.forChild(WishlistPageRoutes),
+      PopoverModule
+    ],
     declarations: [WishListPageComponent],
     providers: []
 })
