3cf71c1227684dadceef06cf1e1b6a2ca13c3a0b
Intershop <support@intershop.de>
2017-08-01 17:23:32 +0530

    Added ng-bootstrap popover.

--- src/app/pages/wishlistPage/wishlistPage.module.ts
+++ src/app/pages/wishlistPage/wishlistPage.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { WishListPageComponent } from './wishlistPage.component';
-import { WishlistPageRoutes } from './wishlistPage.routes';
-import {PopoverModule} from 'ngx-popover';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {WishListPageComponent} from './wishlistPage.component';
+import {WishlistPageRoutes} from './wishlistPage.routes';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        RouterModule.forChild(WishlistPageRoutes),
-        PopoverModule
-        ],
-    declarations: [WishListPageComponent],
-    providers: []
+  imports: [
+    CommonModule,
+    RouterModule.forChild(WishlistPageRoutes),
+    NgbModule.forRoot(),
+  ],
+  declarations: [WishListPageComponent],
+  providers: []
 })
 
 export class WishlistPageModule {
