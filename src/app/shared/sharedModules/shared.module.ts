c45fd0881b6997e4d54a2aefbb1562b88cee438a
Intershop <support@intershop.de>
2017-08-01 17:08:58 +0530

    Translate service added in shared module.

--- src/app/shared/sharedModules/shared.module.ts
+++ src/app/shared/sharedModules/shared.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { InstanceService } from '../services/instance.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
+  exports: [TranslateModule],
     providers: [
         InstanceService
     ]
