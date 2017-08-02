a6696c80de194920cc679cdfa74e87ab0c753f24
Intershop <support@intershop.de>
2017-08-02 12:28:48 +0530

    create .ts files in place of .json for language dictionary.

---
+++ src/assets/i18n/de.ts
@@ -0,0 +1,11 @@
+export const de = {
+  'product.compare.link': 'vergleichen',
+  'header.wishlists.text': 'Wunschliste',
+  'product.items.label.none': 'no list item',
+  'product.items.label.singular': 'one list item',
+  'product.items.label.plural': '{{count}} list items',
+  'common.header.contact_no.text': '<small>1300</small>  032 032',
+  'js.message.constructor': 'localized message from constructor',
+  'js.message.somewhere': 'localized message from somewhere in javascript'
+};
+
