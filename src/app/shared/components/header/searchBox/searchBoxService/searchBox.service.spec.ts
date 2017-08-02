046d1a2eeb1385f52b3bf9f4319627b10f7fd483
Intershop <support@intershop.de>
2017-08-02 19:59:06 +0530

    SearchBox and HeaderSlot Component Tests

--- src/app/shared/components/header/searchBox/searchBoxService/searchBox.service.spec.ts
+++ src/app/shared/components/header/searchBox/searchBoxService/searchBox.service.spec.ts
@@ -1,45 +1,31 @@
-// import { TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
-// import { Subject } from 'rxjs/Subject';
+import { TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import { SearchBoxService } from './searchBox.service';
+import { SuggestedElement } from './searchBox.model';
 
-// import { SearchBoxService } from './searchBox.service';
-// import { SuggestedElement } from './searchBox.model';
-// import { Observable } from 'rxjs/Observable';
+describe('Search Box Service', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [SearchBoxService]
+        });
+    });
 
+    it('searchresults should not be undefined when data is available in the service', inject([SearchBoxService], (searchBoxService: SearchBoxService) => {
+        let searchResults;
+        SearchBoxService.searchEntries('g').subscribe((results) => {
+            searchResults = results;
+        });
 
-// describe('Search Box Service', () => {
+        expect(searchResults).not.toBeNull();
+    }));
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({
-//             providers: [SearchBoxService]
-//         });
+    it('searchresults should be blanck array when data is not available in the service', inject([SearchBoxService], (searchBoxService: SearchBoxService) => {
+        let searchResults;
+        SearchBoxService.searchEntries('Test').subscribe((results) => {
+            searchResults = results;
+        });
 
-//     });
-
-//   /*  it('searchresults should not be undefined', fakeAsync(inject([SearchBoxService], (searchBoxService: SearchBoxService) => {
-//         let searchResults;
-//         let searchTerm$ = new Subject<string>();
-//         searchTerm$.next('n');
-
-//         SearchBoxService.search(searchTerm$).subscribe((results: SuggestedElement[]) => {
-//             searchResults = results.length > 0 ? results : [];
-//         });
-
-//         tick(500);
-//         expect(searchResults).not.toBeUndefined();
-//     })));*/
-
-
-// /*    it('searchresults should not be undefined', fakeAsync( () => {
-//         let searchResults;
-//         let searchTerm$ = new Subject<string>();
-//         searchTerm$.next('n');
-
-//         SearchBoxService.search(searchTerm$).subscribe((results: SuggestedElement[]) => {
-//             searchResults = results.length > 0 ? results : [];
-//         });
-
-//         tick(500);
-//         expect(searchResults).not.toBeUndefined();
-//     }));*/
-
-// });
+        expect(searchResults).toEqual([ ]);
+    }));
+});
