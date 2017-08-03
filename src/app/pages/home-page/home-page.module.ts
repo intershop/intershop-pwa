028f61e80cbd3030da3149d39f6ac3fd73aa6ea5
Stefan Hauke <s.hauke@intershop.de>
2017-08-03 16:31:42 +0200

    added a Bootstrap carousel with image teasers to the homepage

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { RouterModule } from '@angular/router';
 import { HomePageRoute } from './home-page.routes';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(HomePageRoute),
+    CarouselModule.forRoot()
   ],
   declarations: [HomePageComponent]
 })
