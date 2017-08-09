1a58136531634c2e9a5bfa15618c9ba791da390e
Stefan Hauke <s.hauke@intershop.de>
2017-08-09 15:43:13 +0200

    clean up after rsystems branch merge, re-added chages at language switch, wishlist page, product compare and error page and removed obsolete/formerly deleted dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { PopoverModule } from 'ngx-bootstrap/popover';
 import { translateFactory } from '../shared/lang-switcher/custom-translate-loader';
+
 @NgModule({
   declarations: [
     AppComponent
