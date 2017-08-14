4882b6e61617622868681d3b8472b7d168d9e9ca
Intershop <support@intershop.de>
2017-08-14 19:50:30 +0530

    refactor code to create configuration core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,67 @@
+import { NgModule, Optional, SkipSelf, ModuleWithProviders } from '@angular/core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { PopoverModule } from 'ngx-bootstrap/popover';
+import { CacheService, CacheLocalStorage, CacheStorageAbstract } from 'ng2-cache/ng2-cache';
+
+import {
+  ApiService, InstanceService, JwtService, EncryptDecryptService,
+  DataEmitterService, CacheCustomService
+} from '../shared/services';
+import { HeaderModule } from '../shared/components/header/header.module';
+import { FooterModule } from '../shared/components/footer/footer.module';
+import { translateFactory } from '../../shared/lang-switcher/custom-translate-loader';
+
+@NgModule({
+  imports: [
+    FooterModule,
+    HeaderModule,
+    BsDropdownModule.forRoot(),
+    CarouselModule.forRoot(),
+    CollapseModule.forRoot(),
+    ModalModule.forRoot(),
+    PopoverModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateFactory,
+        deps: [HttpClient]
+      }
+    }),
+  ],
+  declarations: [],
+  providers: [
+    ApiService,
+    DataEmitterService,
+    EncryptDecryptService,
+    JwtService,
+    CacheCustomService,
+    CacheService,
+    { provide: CacheStorageAbstract, useClass: CacheLocalStorage },
+  ],
+  exports: [
+    FooterModule,
+    HeaderModule
+  ]
+})
+export class CoreModule {
+
+  public static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: CoreModule,
+      providers: [
+      ]
+    };
+  }
+
+  constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
---
+++ src/app/shared/services/index.ts
@@ -0,0 +1,6 @@
+export * from './api.service';
+export * from './data-emitter.service';
+export * from './instance.service';
+export * from './jwt.service';
+export * from './cache/cache-custom.service';
+export * from './cache/encrypt-decrypt.service';
