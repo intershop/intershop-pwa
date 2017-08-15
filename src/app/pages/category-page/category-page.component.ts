df97229b7d9b8b57904355ae0e0dee9497bb7edb
Kristin Stepper <k.stepper@intershop.de>
2017-08-15 14:05:38 +0200

    gebTest lint fixes

--- src/app/pages/category-page/category-page.component.ts
+++ src/app/pages/category-page/category-page.component.ts
@@ -1,24 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'is-category-page',
   templateUrl: './category-page.component.html'
 })
-export class CategoryPageComponent implements OnInit {
-  
-  families = [
-        {"name":'Camcorders', "id":832 },
-        {"name":'Digital Cameras', "id":833 },
-        {"name":'Webcams', "id":834 }
-    ];
 
-  
+export class CategoryPageComponent implements OnInit {
+
+  families = [
+    { 'name': 'Camcorders', 'id': 832 },
+    { 'name': 'Digital Cameras', 'id': 833 },
+    { 'name': 'Webcams', 'id': 834 }
+  ];
 
   ngOnInit() {
-   
-  }
 
-  
+  }
 
 }
