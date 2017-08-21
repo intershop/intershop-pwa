6e3deae0a89304c76ee70592876ee5eb6c630d03
Stefan Hauke <s.hauke@intershop.de>
2017-08-21 09:23:58 +0200

    ISREST-60: cleaned up the solution for changing a context CSS class in the app.component.html depending on the current page route

--- src/app/shared/directives/style-wrapper.directive.ts
+++ src/app/shared/directives/style-wrapper.directive.ts
@@ -1,35 +1,38 @@
-import { Directive, HostBinding, Renderer, ElementRef, OnInit } from '@angular/core';
+import { Directive, HostBinding, OnInit } from '@angular/core';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 
+/**
+ * The <is-style-wrapper> directive dynamicaly adds a context specific CSS class based on a setting in the route handling, e.g. 'homepage' or 'errorpage'
+ */
 @Directive({
-    selector: '[is-style-wrapper]'
+  selector: '[is-style-wrapper]'
 })
 export class StyleWrapperDirective implements OnInit {
 
-    @HostBinding('class') classesString: string = '';
-    constructor(private router: Router,
-        private route: ActivatedRoute) {
-    }
+  @HostBinding('class') classesString: string = '';
+  constructor(private router: Router,
+    private route: ActivatedRoute) {
+  }
 
-    public ngOnInit(): void {
-        this.router.events
-            .filter((event) => event instanceof NavigationEnd)
-            .map(() => this.route)
-            .map((route) => {
-                while (route.firstChild) { route = route.firstChild };
-                return route;
-            })
-            .filter((route) => route.outlet === 'primary')
-            .mergeMap((route) => route.data)
-            .subscribe((event) => {
-                this.addClass(event['className'] || '');
-            });
-    };
+  public ngOnInit(): void {
+    this.router.events
+      .filter((event) => event instanceof NavigationEnd)
+      .map(() => this.route)
+      .map((route) => {
+        while (route.firstChild) { route = route.firstChild };
+        return route;
+      })
+      .filter((route) => route.outlet === 'primary')
+      .mergeMap((route) => route.data)
+      .subscribe((event) => {
+        this.addClass(event['className'] || '');
+      });
+  };
 
-    private addClass(className: string) {
-        this.classesString = className;
-    }
+  private addClass(className: string) {
+    this.classesString = className;
+  }
 }
