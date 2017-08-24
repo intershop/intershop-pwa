0a4d97e70ab056929c14168b95466641ec67656b
Intershop <support@intershop.de>
2017-08-24 15:15:45 +0530

    ISREST-18:Renamed HeaderNaviagtionService to CategoryService and made corresponding changes

--- src/app/shared/components/header/header-navigation/header-navigation.component.ts
+++ src/app/shared/components/header/header-navigation/header-navigation.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core'
-import { HeaderNavigationCategoryModel } from './header-navigation-service/header-navigation-category.model';
-import { HeaderNavigationSubcategoryModel } from './header-navigation-service/header-navigation-subcategory.model';
-import { HeaderNavigationService } from './header-navigation-service/header-navigation.service';
+import { CategoryModel } from './category-service/category.model';
+import { SubcategoryModel } from './category-service/subcategory.model';
+import { CategoryService } from './category-service/category.service';
 import { CacheCustomService } from '../../../../shared/services/cache/cache-custom.service';
 
 @Component({
@@ -10,15 +10,15 @@ import { CacheCustomService } from '../../../../shared/services/cache/cache-cust
 })
 
 export class HeaderNavigationComponent implements OnInit {
-  categories: HeaderNavigationCategoryModel;
-  subCategories: HeaderNavigationSubcategoryModel;
+  categories: CategoryModel;
+  subCategories: SubcategoryModel;
 
-  constructor(private headerNavigationService: HeaderNavigationService, private cacheService: CacheCustomService) {
+  constructor(private categoryService: CategoryService, private cacheService: CacheCustomService) {
 
   }
 
   ngOnInit() {
-    this.headerNavigationService.getCategories().subscribe(data => {
+    this.categoryService.getCategories().subscribe(data => {
       this.categories = data;
     })
   }
@@ -27,7 +27,7 @@ export class HeaderNavigationComponent implements OnInit {
     if (this.cacheService.cacheKeyExists(categoryId)) {
       this.subCategories = this.cacheService.getCachedData(categoryId);
     } else {
-      this.headerNavigationService.getSubCategories(categoryId).subscribe(data => {
+      this.categoryService.getSubCategories(categoryId).subscribe(data => {
         this.subCategories = data;
         this.cacheService.storeDataToCache(this.subCategories, categoryId);
       })
