1c5b787b2a23ce37627733b2628e75f4e4b9935c
Stefan Hauke <s.hauke@intershop.de>
2017-08-24 13:10:14 +0200

    ISREST-36: fixed app.component.spec.ts tests that were broken by adding the CollapseModule to the FooterComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,6 @@ import { Component, Directive, Injectable, Input } from '@angular/core';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { AppComponent } from './app.component';
 import { TranslateService } from '@ngx-translate/core';
-import { FooterComponent } from './shared/components/footer/footer.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockComponent } from './shared/components/mock.component';
 
@@ -14,7 +13,9 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent, FooterComponent, MockComponent({ selector: 'is-header', template: 'Header Component' }),
+        AppComponent,
+        MockComponent({ selector: 'is-header', template: 'Header Component' }),
+        MockComponent({ selector: 'is-footer', template: 'Footer Component' })
       ],
       providers: [
         TranslateService
--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
 
 export class FooterComponent {
 
-  // TODO: the Bootstrap collapsibles in the footer currently miss the behavior of being collapesd on XS screen size but not on larger screens
+  // TODO: the content of the footer needs to come from the Intershop CMS
+  // TODO: the Bootstrap collapsibles in the footer currently miss the behavior of being collapsed on XS screen size but not on larger screens
   // TODO: the Javascript functionality of 'RetailShop.onSubmitNewsletterSignIn' of 'global.js' is still missing - submitting the form and presenting the result in a dialog
 
 }
