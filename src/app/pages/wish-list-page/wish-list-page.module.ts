c5e91442cd25a4580ca47c97ddd7636ea465d293
Stefan Hauke <s.hauke@intershop.de>
2017-08-28 09:55:54 +0200

    Fixed (manually reset) the unintended changes made by "Merge branch 'develop' of gitlab.intershop.de:rest-based-storefront/proof-of-concept into develop" (b874c992) and the according Revert commit (a02aa4d2) that did not lead to the intended result.

--- src/app/pages/wish-list-page/wish-list-page.module.ts
+++ src/app/pages/wish-list-page/wish-list-page.module.ts
@@ -5,15 +5,20 @@ import { PopoverModule } from 'ngx-bootstrap/popover';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { WishListPageComponent } from './wish-list-page.component';
 import { WishlistPageRoutes } from './wish-list-page.routes';
+import { WishListService } from './wish-list-service/wish-list-service';
+import { SharedModule } from '../../shared/shared-modules/shared.module';
 
 @NgModule({
   imports: [
     RouterModule.forChild(WishlistPageRoutes),
     PopoverModule,
-    ModalModule
+    ModalModule,
+    SharedModule
   ],
   declarations: [WishListPageComponent],
-  providers: []
+  providers: [
+    WishListService
+  ]
 })
 
 export class WishlistPageModule {
