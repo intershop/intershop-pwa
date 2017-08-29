4c2c0713f9a156b91a3f4e9f693588c788ce4ff9
Intershop <support@intershop.de>
2017-08-29 17:08:37 +0530

    ISREST-10:Header Component ISML changes

--- src/app/shared/components/header/mini-cart/mini-cart.component.spec.ts
+++ src/app/shared/components/header/mini-cart/mini-cart.component.spec.ts
@@ -1,8 +1,8 @@
-import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { DebugElement, NO_ERRORS_SCHEMA, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MiniCartComponent } from './mini-cart.component';
-import { DataEmitterService } from '../../../services/data-emitter.service';
+import { GlobalState } from '../../../../shared/services';
 
 describe('Mini Cart Component', () => {
     let fixture: ComponentFixture<MiniCartComponent>,
@@ -10,19 +10,18 @@ describe('Mini Cart Component', () => {
         element: HTMLElement,
         debugEl: DebugElement;
 
-    class DataEmitterServiceStub {
-        public miniCartEmitter = new Observable((observer) => {
-            observer.next({ name: 'testItem', salePrice: { value: 20 } });
-            observer.complete();
-        });
-    };
+    class GlobalStateStub {
+        subscribeCachedData(key, callBack: Function) {
+
+        }
+    }
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [
                 MiniCartComponent
             ],
-            providers: [{ provide: DataEmitterService, useClass: DataEmitterServiceStub }],
+            providers: [{ provide: GlobalState, useClass: GlobalStateStub }],
             imports: [],
             schemas: [NO_ERRORS_SCHEMA]
         }).compileComponents();
@@ -33,19 +32,27 @@ describe('Mini Cart Component', () => {
         element = fixture.nativeElement;
     });
 
-    it('should check mini cart has item in it', () => {
-        component.ngOnInit();
-
-        expect(component.cartLength).toBeGreaterThan(0);
+    it('should create the component', () => {
+        const app = debugEl.componentInstance;
+        expect(app).toBeTruthy();
     });
 
-    it('should confirm that cartLength and cartPrice is shown on template', () => {
-        fixture.detectChanges();
-
-        const cartPrice = element.getElementsByClassName('mini-cart-price')[0].textContent;
-        const cartLength = element.getElementsByClassName('js-cart-count')[0].nextElementSibling.textContent;
-
-        expect(cartPrice).toEqual('$ 20');
-        expect(cartLength).toEqual('1 items');
+    it('should call calculateCartValues and verify if the correct calculations are made', () => {
+        const cartItems = [
+            {
+                salePrice: {
+                    'value': 20
+                }
+            },
+            {
+                salePrice: {
+                    'value': 40
+                }
+            },
+        ]
+        component.calculateCartValues(cartItems);
+        expect(component.cartPrice).toEqual(60);
+        expect(component.cartLength).toEqual(2);
     });
+
 });
