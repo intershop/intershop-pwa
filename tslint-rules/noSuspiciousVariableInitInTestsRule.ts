b99e2b2d4f3adab2f30ca6ec226f96302ff1c0a6
Danilo Hoffmann <d.hoffmann@intershop.de>
2017-09-06 14:41:27 +0200

    ISREST-61: collect helper methods

--- tslint-rules/noSuspiciousVariableInitInTestsRule.ts
+++ tslint-rules/noSuspiciousVariableInitInTestsRule.ts
@@ -1,6 +1,7 @@
 import * as ts from 'typescript';
 import * as Lint from 'tslint';
 import { forEachToken, getNextToken, getChildOfKind } from 'tsutils';
+import { RuleHelpers } from './ruleHelpers';
 
 class NoSuspiciousVariableInitInTestsWalker extends Lint.RuleWalker {
 
@@ -16,12 +17,8 @@ class NoSuspiciousVariableInitInTestsWalker extends Lint.RuleWalker {
             //     return;
             // }
             // console.log('####' + sourceFile.fileName);
-            const statements = sourceFile.statements.filter(
-                (stmt) => stmt.kind === ts.SyntaxKind.ExpressionStatement && stmt.getFirstToken().getText() === 'describe');
-            if (statements.length && statements[0].getChildAt(0)) {
-                const describeStatement: ts.Node = statements[0].getChildAt(0).getChildAt(2);
-                const describeBody = describeStatement.getChildAt(2).getChildAt(4).getChildAt(1);
-
+            const describeBody = RuleHelpers.getDescribeBody(sourceFile);
+            if (describeBody) {
                 for (let i = 0; i < describeBody.getChildCount(); i++) {
                     const child: ts.Node = describeBody.getChildAt(i);
                     if (child.kind === ts.SyntaxKind.VariableStatement) {
@@ -37,15 +34,15 @@ class NoSuspiciousVariableInitInTestsWalker extends Lint.RuleWalker {
                 }
             }
 
-            const missingReinit = this.interestingVariables.filter(node => this.correctlyReinitializedVariables.indexOf(this.extractVariableNameInDeclaration(node)) < 0);
-            missingReinit.forEach(key => this.addFailureAtNode(key, 'variable "' + this.extractVariableNameInDeclaration(key) + '" is not re-initialized in beforeEach'));
+            const missingReinit = this.interestingVariables.filter(node => this.correctlyReinitializedVariables.indexOf(RuleHelpers.extractVariableNameInDeclaration(node)) < 0);
+            missingReinit.forEach(key => this.addFailureAtNode(key, 'variable "' + RuleHelpers.extractVariableNameInDeclaration(key) + '" is not re-initialized in beforeEach'));
         }
     }
 
     private checkVariableStatementsInBeforeEach(node: ts.Node) {
         node.getChildren().forEach(child => {
             const statement = child.getChildAt(0);
-            // this.dump(statement);
+            // RuleHelpers.dumpNode(statement);
             if (statement.getChildCount() > 2 && statement.getChildAt(1).kind === ts.SyntaxKind.EqualsToken) {
                 const varName = statement.getChildAt(0).getText();
                 // console.log('found assignment on "' + varName + '" in beforeEach');
@@ -61,59 +58,19 @@ class NoSuspiciousVariableInitInTestsWalker extends Lint.RuleWalker {
     }
 
     private checkVariableStatementInDescribe(statement: ts.Node) {
-        // this.dump(statement);
-        const newKeywordFound = this.getNextTokenOfKind(statement, ts.SyntaxKind.NewKeyword);
+        // helpers.dumpNode(statement);
+        const newKeywordFound = RuleHelpers.getNextChildTokenOfKind(statement, ts.SyntaxKind.NewKeyword);
         if (newKeywordFound) {
             this.addFailureAtNode(statement, 'Complex statements should only be made in beforeEach.');
         }
-        const letKeywordFound = this.getNextTokenOfKind(statement, ts.SyntaxKind.LetKeyword);
-        const assignmentFound = this.getNextTokenOfKind(statement, ts.SyntaxKind.EqualsToken);
+        const letKeywordFound = RuleHelpers.getNextChildTokenOfKind(statement, ts.SyntaxKind.LetKeyword);
+        const assignmentFound = RuleHelpers.getNextChildTokenOfKind(statement, ts.SyntaxKind.EqualsToken);
         if (letKeywordFound && assignmentFound) {
             this.addFailureAtNode(statement, 'Statement should be const statement or re-initialized in beforeEach');
         } else if (letKeywordFound) {
-            const varName = this.extractVariableNameInDeclaration(statement);
+            const varName = RuleHelpers.extractVariableNameInDeclaration(statement);
             // console.log('interested in "' + varName + '"');
             this.interestingVariables.push(statement);
-        }
-    }
-
-    private extractVariableNameInDeclaration(statement: ts.Node): string {
-        return statement.getChildAt(1).getFirstToken().getText();
-    }
-
-    private getNextTokenOfKind(node: ts.Node, kind: ts.SyntaxKind): ts.Node {
-        let pointer: ts.Node = node.getFirstToken();
-        while (pointer && pointer.kind !== kind) {
-            if (pointer === node.getLastToken()) {
-                return null;
-            }
-            pointer = getNextToken(pointer);
-        }
-        return pointer;
-    }
-
-    private dump(node: ts.Node, dumpTokens: boolean = false) {
-        if (node) {
-            console.log('----------------------------------------');
-            console.log('type: ' + node.kind);
-            console.log('text: ' + node.getText());
-            console.log('child count: ' + node.getChildCount());
-            for (let index = 0; index < node.getChildCount(); index++) {
-                const c: ts.Node = node.getChildAt(index);
-                console.log('child #' + index + ' ' + c.kind + ': ' + c.getText());
-            }
-            if (dumpTokens) {
-                let pointer = node.getFirstToken();
-                while (pointer !== node.getLastToken()) {
-                    console.log(pointer.kind + ':' + pointer.getText());
-                    pointer = getNextToken(pointer);
-                }
-                if (pointer) {
-                    console.log(pointer.kind + ':' + pointer.getText());
-                }
-            }
-        } else {
-            console.log(node);
         }
     }
 }
