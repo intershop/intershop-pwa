048117a5b8c2b2ca0881fc2020955220e0495506
Stefan Hauke <s.hauke@intershop.de>
2017-09-08 17:05:26 +0200

    set production environment ip address to fit 'intershop7-devenv' default docker configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   needMock: false,
-  rest_url: 'http://10.131.60.148:9091/INTERSHOP/rest/WFS/inSPIRED-inTRONICS-Site/-/',
-  base_url: 'http://10.131.60.148:9091'
+  rest_url: 'http://192.168.99.100:8081/INTERSHOP/rest/WFS/inSPIRED-inTRONICS-Site/-/',
+  base_url: 'http://192.168.99.100:8081'
 };
-
