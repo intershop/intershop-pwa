00a33734f7d074d734d92ce0b6ea4ad49fe860b3
Stefan Hauke <s.hauke@intershop.de>
2017-09-21 16:24:31 +0200

    ISREST-86: Moved Category Navigation Component to an own shopping module so it can be imported and used on the FamilyPage and CategoryPage

---
+++ src/app/modules/shopping.module.ts
@@ -0,0 +1,16 @@
+import { CategoryNavigationComponent } from './../components/category-navigation/category-navigation.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    CategoryNavigationComponent
+  ],
+  exports: [
+    CategoryNavigationComponent
+  ]
+})
+export class ShoppingModule { }
