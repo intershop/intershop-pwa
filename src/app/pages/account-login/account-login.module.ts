f2428ae26d167aa257d5fa0e2fb4bc8b10ce6809
Intershop <support@intershop.de>
2017-09-21 13:38:03 +0530

    Introduced localize router module and used localize pipe for url translation.

--- src/app/pages/account-login/account-login.module.ts
+++ src/app/pages/account-login/account-login.module.ts
@@ -4,10 +4,12 @@ import { SharedModule } from '../../modules/shared.module';
 import { AccountLoginComponent } from './account-login.component';
 import { AccountLoginRoute } from './account-login.routes';
 import { SimpleRegistrationComponent } from './simple-registration/simple-registration.component';
+import { LocalizeRouterModule } from '../../services/routes-parser-locale-currency/localize-router.module';
 
 @NgModule({
   imports: [
     RouterModule.forChild(AccountLoginRoute),
+    LocalizeRouterModule.forChild(AccountLoginRoute),
     SharedModule
   ],
   declarations: [AccountLoginComponent, SimpleRegistrationComponent],
