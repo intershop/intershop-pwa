465519bbdedd820ec41342c80685ed92aef54001
Intershop <support@intershop.de>
2017-09-25 14:34:06 +0530

    global form error handling test cases

--- src/app/directives/form-validation.directive.spec.ts
+++ src/app/directives/form-validation.directive.spec.ts
@@ -1,52 +1,63 @@
 import { Component, ElementRef, OnInit, Renderer2, ViewChild, ViewContainerRef, Input, DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormValidationDirective } from "./form-validation.directive";
-import { FormGroup, FormBuilder, Validators, FormsModule } from "@angular/forms";
-import { By } from '@angular/platform-browser';
-describe('Form Validation Directive', () => {
-    @Component({
-        template: ` <form #form name="LoginUserForm" [formGroup]="loginForm" class="form-horizontal bv-form" >
-                    <input class="form-control" formControlName="userName" name="ShopLoginForm_Login" />
-                    </form>
-                        `
-    })
-    class MockComponent {
-        @ViewChild('form') formValidationDirective: FormValidationDirective;
-        @Input('formGroup') formGroup: FormGroup;
-        loginForm: FormGroup;
-        constructor(private formBuilder: FormBuilder) {
-            this.loginForm = this.formBuilder.group({
-                userName: ['', Validators.required]
-            });
-        }
+import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { By, BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
+@Component({
+    template: ` <form name="loginForm" [formGroup]="loginForm" >
+                <input class="form-control" formControlName="userName" name="userName" />
+                <button type="submit" class="btn btn-primary" >Submit</button>
+                </form>
+                    `
+})
+class MockComponent {
+    loginForm: FormGroup;
+    constructor(private formBuilder: FormBuilder) {
+        this.loginForm = this.formBuilder.group({
+            userName: ['', Validators.required]
+        });
     }
-
+}
+describe('Form Validation Directive', () => {
     let fixture: ComponentFixture<MockComponent>;
     let component: MockComponent;
-    let element: HTMLElement;
-    let formValidationDirective: DebugElement[];
-    let formGroupDirective: DebugElement[];  
-
-    beforeEach(async(() => {
+    let formValidationElement: DebugElement;
+    let formValidationDirective: FormValidationDirective;
+    beforeEach(() => {
         TestBed.configureTestingModule({
-            imports:[FormsModule],
-            declarations: [FormValidationDirective, MockComponent]
-        }).compileComponents().then(() => {
-            fixture = TestBed.createComponent(MockComponent);
-            component = fixture.componentInstance;
-            element = fixture.nativeElement;
-            formValidationDirective = fixture.debugElement.queryAll(By.directive(FormValidationDirective));
-            formGroupDirective = fixture.debugElement.queryAll(By.directive(FormGroup));
-            
+            imports: [
+                CommonModule,
+                BrowserModule,
+                FormsModule,
+                ReactiveFormsModule
+            ],
+            declarations: [
+                MockComponent,
+                FormValidationDirective
+
+            ]
         });
-    }));
-    xit('form validation directive test case', () => {
-        debugger;
-        console.log(element);
-        fixture.debugElement.query(By.css('form')).triggerEventHandler('submit', null);
-        expect(formValidationDirective[0].injector.get(FormValidationDirective).markAsDirty(formGroupDirective[0].injector.get(FormGroup))).toHaveBeenCalled();
     });
 
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MockComponent);
+        component = fixture.componentInstance;
+        formValidationElement = fixture.debugElement.query(By.directive(FormValidationDirective));
+        formValidationDirective = formValidationElement.injector.get(FormValidationDirective) as FormValidationDirective;
+        fixture.detectChanges();
+    })
+
+    it('should return invalid form', () => {
+        formValidationDirective.onSubmit();
+        expect(component.loginForm.invalid).toBe(true);
+    });
+
+    it('should return valid form', () => {
+        component.loginForm.controls["userName"].setValue("test");
+        formValidationDirective.onSubmit();
+        expect(formValidationDirective.formGroup.valid).toBe(component.loginForm.valid);
+    });
 });
 
 
