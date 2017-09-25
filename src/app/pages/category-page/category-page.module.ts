1e8c05d5352b555fbed9bb188a720946854411f5
Intershop <support@intershop.de>
2017-09-25 15:51:36 +0530

    Merged with current Develop branch.

--- src/app/pages/category-page/category-page.module.ts
+++ src/app/pages/category-page/category-page.module.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ShoppingModule } from '../../modules/shopping.module';
 import { CategoryPageComponent } from './category-page.component';
 import { CategoryPageRoute } from './category-page.routes';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(CategoryPageRoute)
+    RouterModule.forChild(CategoryPageRoute),
+    ShoppingModule
   ],
   declarations: [CategoryPageComponent]
 })
--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -5,6 +5,4 @@ export * from './jwt.service';
 export * from './cache/cache-custom.service';
 export * from './cache/encrypt-decrypt.service';
 export * from './custom-error-handler';
-export * from './global.state';
-export * from './cross-tab-communicator';
 export * from './mock-api.service';
