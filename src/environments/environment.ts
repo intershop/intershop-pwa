d06972f72955bb1cc0a14137cabbeb76679222e0
Stefan Hauke <s.hauke@intershop.de>
2017-09-25 09:27:12 +0200

    Removed 'typecript' dependency, kept only the devDependency. Reverted changes to the environment.ts URLs.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   platformId: '',
   production: false,
   needMock: true,
-  rest_url: 'https://localhost:8444/INTERSHOP/rest/WFS/inSPIRED-inTRONICS-Site/-',
-  base_url: 'https://localhost:8444',
+  rest_url: 'https://localhost:80/INTERSHOP/rest/WFS/inSPIRED-inTRONICS-Site/-',
+  base_url: 'https://localhost:80',
 
   locales: [
     { 'lang': 'en_US', 'currency': 'USD', value: 'English', displayValue: 'en' },
