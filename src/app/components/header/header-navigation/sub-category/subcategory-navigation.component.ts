44b5fab64a06819845f67e9876d370b003d2461d
Intershop <support@intershop.de>
2017-09-26 17:21:22 +0530

    Change in main-navigation less file

--- src/app/components/header/header-navigation/sub-category/subcategory-navigation.component.ts
+++ src/app/components/header/header-navigation/sub-category/subcategory-navigation.component.ts
@@ -5,30 +5,7 @@ import { LocalizeRouterService } from '../../../../services/routes-parser-locale
 @Component({
   selector: 'is-subcategory-navigation',
   templateUrl: './subcategory-navigation.component.html',
-  styles: [`
-  ul.category-level1 > li.dropdown{
-    background:none;
-  }
-  @media (max-width: 767px){
-    ul.category-level1 > li.open ul{
-     display:block;
-    }
-     ul.category-level1 > li > a {
-      width:85%;
-      float:left;
-      clear:initial;
-      }
-       ul.category-level1 > li a.dropdown-toggle{
-         color:#959595;
-        width:15%;
-        float:left;
-        border-left: 1px solid #ccc;
-      }
-      ul.category-level1 > li a.dropdown-toggle > span.glyphicon-plus:before{
-        content:"+";
-      }
-    }
-  `]
+  
 })
 
 export class SubCategoryNavigationComponent {
