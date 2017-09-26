fe21ad0cec11d9a9eef977c12c99ca27079bab98
Stefan Hauke <s.hauke@intershop.de>
2017-09-26 16:54:28 +0200

    ISREST-79: reenabled the tslint "directive-selector" rule and reworked the form-validation directive to comply with this rule - applied general code formatting rules

--- src/app/directives/form-validation.directive.spec.ts
+++ src/app/directives/form-validation.directive.spec.ts
@@ -5,59 +5,57 @@ import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 import { BrowserModule, By } from '@angular/platform-browser';
 import { FormValidationDirective } from './form-validation.directive';
 @Component({
-    template: ` <form name="loginForm" [formGroup]="loginForm" >
+  template: `
+              <form isFormValidation name="loginForm" [formGroup]="loginForm" >
                 <input class="form-control" formControlName="userName" name="userName" />
                 <button type="submit" class="btn btn-primary" >Submit</button>
-                </form>
-                    `
+              </form>
+            `
 })
 class MockComponent {
-    loginForm: FormGroup;
-    constructor(private formBuilder: FormBuilder) {
-        this.loginForm = this.formBuilder.group({
-            userName: ['', Validators.required]
-        });
-    }
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', Validators.required]
+    });
+  }
 }
 describe('Form Validation Directive', () => {
-    let fixture: ComponentFixture<MockComponent>;
-    let component: MockComponent;
-    let formValidationElement: DebugElement;
-    let formValidationDirective: FormValidationDirective;
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [
-                CommonModule,
-                BrowserModule,
-                FormsModule,
-                ReactiveFormsModule
-            ],
-            declarations: [
-                MockComponent,
-                FormValidationDirective
-
-            ]
-        });
+  let fixture: ComponentFixture<MockComponent>;
+  let component: MockComponent;
+  let formValidationElement: DebugElement;
+  let formValidationDirective: FormValidationDirective;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule
+      ],
+      declarations: [
+        MockComponent,
+        FormValidationDirective
+      ]
     });
+  });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(MockComponent);
-        component = fixture.componentInstance;
-        formValidationElement = fixture.debugElement.query(By.directive(FormValidationDirective));
-        formValidationDirective = formValidationElement.injector.get(FormValidationDirective) as FormValidationDirective;
-        fixture.detectChanges();
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MockComponent);
+    component = fixture.componentInstance;
+    formValidationElement = fixture.debugElement.query(By.directive(FormValidationDirective));
+    formValidationDirective = formValidationElement.injector.get(FormValidationDirective) as FormValidationDirective;
+    fixture.detectChanges();
+  });
 
-    it('should return invalid form', () => {
-        formValidationDirective.onSubmit();
-        expect(component.loginForm.invalid).toBe(true);
-    });
+  it('should return invalid form', () => {
+    formValidationDirective.onSubmit();
+    expect(component.loginForm.invalid).toBe(true);
+  });
 
-    it('should return valid form', () => {
-        component.loginForm.controls['userName'].setValue('test');
-        formValidationDirective.onSubmit();
-        expect(formValidationDirective.formGroup.valid).toBe(component.loginForm.valid);
-    });
+  it('should return valid form', () => {
+    component.loginForm.controls['userName'].setValue('test');
+    formValidationDirective.onSubmit();
+    expect(formValidationDirective.formGroup.valid).toBe(component.loginForm.valid);
+  });
 });
-
-
