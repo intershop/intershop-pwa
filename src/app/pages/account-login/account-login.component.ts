9d4164f0eb9add6a53b43c058a57b69deda93ad1
Stefan Hauke <s.hauke@intershop.de>
2017-09-26 16:45:57 +0200

    ISREST-79: reverted tslint changes regarding "ordered imports" and not helpfull formatting changes and fixed the according linting issues

--- src/app/pages/account-login/account-login.component.ts
+++ src/app/pages/account-login/account-login.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CustomValidators } from 'ng2-validation';
 import { GlobalConfiguration } from '../../configurations/global.configuration';
 import { AccountLoginService } from '../../services/account-login';
 import { UserDetail } from '../../services/account-login/account-login.model';
 import { CacheCustomService } from '../../services/cache/cache-custom.service';
 import { LocalizeRouterService } from '../../services/routes-parser-locale-currency/localize-router.service';
-import { CustomValidators } from 'ng2-validation';
+
 @Component({
   templateUrl: './account-login.component.html'
 })
--- src/app/pages/account-login/simple-registration/simple-registration.component.ts
+++ src/app/pages/account-login/simple-registration/simple-registration.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CustomValidators } from 'ng2-validation';
 import { GlobalConfiguration } from '../../../configurations/global.configuration';
 import { UserDetail } from '../../../services/account-login/account-login.model';
 import { LocalizeRouterService } from '../../../services/routes-parser-locale-currency/localize-router.service';
 import { SimpleRegistrationService } from './simple-registration.service';
-import { CustomValidators } from 'ng2-validation';
+
 @Component({
   selector: 'is-simple-registration',
   templateUrl: './simple-registration.component.html',
