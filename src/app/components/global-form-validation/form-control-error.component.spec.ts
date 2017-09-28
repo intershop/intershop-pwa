a547b4ddc050576c5055603b5d12e6ac69d82bf7
Stefan Hauke <s.hauke@intershop.de>
2017-09-28 12:44:38 +0200

    Fixed formatting style guide issues and reverted unintended changes.

--- src/app/components/global-form-validation/form-control-error.component.spec.ts
+++ src/app/components/global-form-validation/form-control-error.component.spec.ts
@@ -5,33 +5,33 @@ import { BrowserModule, By } from '@angular/platform-browser';
 import { FormControlErrorComponent } from './form-control-error.component';
 
 describe('Form control error component', () => {
-    let fixture: ComponentFixture<FormControlErrorComponent>;
-    let component: FormControlErrorComponent;
-    let smallElement: DebugElement;
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [
-                BrowserModule,
-            ],
-            declarations: [
-                FormControlErrorComponent
-            ]
-        }).compileComponents();
-    });
+  let fixture: ComponentFixture<FormControlErrorComponent>;
+  let component: FormControlErrorComponent;
+  let smallElement: DebugElement;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+      ],
+      declarations: [
+        FormControlErrorComponent
+      ]
+    }).compileComponents();
+  });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(FormControlErrorComponent);
-        component = fixture.componentInstance;
-        smallElement = fixture.debugElement.query(By.css('small'));
-    });
-    it('should not create form control error', () => {
-        expect(smallElement).toBeFalsy();
-    });
-    it('should create form control error', () => {
-        component.messagesList = ['error1'];
-        fixture.detectChanges();
-        smallElement = fixture.debugElement.query(By.css('small'));
-        expect(smallElement).toBeTruthy();
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormControlErrorComponent);
+    component = fixture.componentInstance;
+    smallElement = fixture.debugElement.query(By.css('small'));
+  });
+  it('should not create form control error', () => {
+    expect(smallElement).toBeFalsy();
+  });
+  it('should create form control error', () => {
+    component.messagesList = ['error1'];
+    fixture.detectChanges();
+    smallElement = fixture.debugElement.query(By.css('small'));
+    expect(smallElement).toBeTruthy();
+  });
 
 });
--- src/app/components/global-form-validation/form-group-validation.component.ts
+++ src/app/components/global-form-validation/form-group-validation.component.ts
@@ -5,47 +5,45 @@ import * as _ from 'lodash';
 import { FormControlErrorComponent } from './form-control-error.component';
 
 @Component({
-    selector: 'is-form-group-validation',
-    template: `
-      <ng-content></ng-content>
-    `
+  selector: 'is-form-group-validation',
+  template: `<ng-content></ng-content>`
 })
+
 export class FormGroupValidationComponent {
-    @ContentChild(FormControlName) formControlName: FormControlName;
-    @HostBinding('class.has-error') get hasErrors() {
-        if (this.formControlName && this.formControlName.dirty) {
-            this.showError(this.getErrorList());
-            return this.formControlName.invalid;
-        }
+  @ContentChild(FormControlName) formControlName: FormControlName;
+  @HostBinding('class.has-error') get hasErrors() {
+    if (this.formControlName && this.formControlName.dirty) {
+      this.showError(this.getErrorList());
+      return this.formControlName.invalid;
     }
-    @HostBinding('class.has-success') get hasSuccess() {
-        return this.formControlName ? (this.formControlName.valid && this.formControlName.dirty) : false;
-    }
+  }
+  @HostBinding('class.has-success') get hasSuccess() {
+    return this.formControlName ? (this.formControlName.valid && this.formControlName.dirty) : false;
+  }
 
-    @ContentChild('dynamicError', { read: ViewContainerRef }) dynamicError: ViewContainerRef;
-    @Input() errorMessages: object;
+  @ContentChild('dynamicError', { read: ViewContainerRef }) dynamicError: ViewContainerRef;
+  @Input() errorMessages: object;
 
-    constructor(private translate: TranslateService, private cfResolver: ComponentFactoryResolver) { }
+  constructor(private translate: TranslateService, private cfResolver: ComponentFactoryResolver) { }
 
-    getErrorList() {
-        return _.reduce(this.formControlName.errors, (result, value, key) => {
-            const localizedString = (this.errorMessages[key]) ? this.errorMessages[key] : this.formControlName.errors['customError'];
-            this.translate.get(localizedString).subscribe(data => {
-                result.push(data);
-            }).unsubscribe();
-            return result;
-        }, []);
+  getErrorList() {
+    return _.reduce(this.formControlName.errors, (result, value, key) => {
+      const localizedString = (this.errorMessages[key]) ? this.errorMessages[key] : this.formControlName.errors['customError'];
+      this.translate.get(localizedString).subscribe(data => {
+        result.push(data);
+      }).unsubscribe();
+      return result;
+    }, []);
+  }
+  showError(errorList) {
+    if (this.dynamicError) {
+      this.dynamicError.clear();
+      const factory = this.cfResolver.resolveComponentFactory(FormControlErrorComponent);
+      const componentRef = this.dynamicError.createComponent(factory);
+      const formControlMessages: FormControlErrorComponent = componentRef.instance;
+      formControlMessages.messagesList = errorList;
+      formControlMessages.formControl = this.formControlName;
+      componentRef.changeDetectorRef.detectChanges();
     }
-    showError(errorList) {
-        if (this.dynamicError) {
-            this.dynamicError.clear();
-            const factory = this.cfResolver.resolveComponentFactory(FormControlErrorComponent);
-            const componentRef = this.dynamicError.createComponent(factory);
-            const formControlMessages: FormControlErrorComponent = componentRef.instance;
-            formControlMessages.messagesList = errorList;
-            formControlMessages.formControl = this.formControlName;
-            componentRef.changeDetectorRef.detectChanges();
-        }
-    }
+  }
 }
-
