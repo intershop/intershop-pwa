5bf1e942ef52a91a025980638f70ffb2f2c04e94
Intershop <support@intershop.de>
2017-10-10 11:28:38 +0530

    ISREST-13 Redirect user to login page if not logged-in.

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { AccountLoginService } from './account-login/account-login.service';
 import { JwtService } from './jwt.service';
 import { LocalizeRouterService } from './routes-parser-locale-currency/localize-router.service';
 
@@ -7,17 +8,23 @@ import { LocalizeRouterService } from './routes-parser-locale-currency/localize-
 export class AuthGuard implements CanActivate {
 
   constructor(private jwtService: JwtService,
-              private localize: LocalizeRouterService) {
+              private localize: LocalizeRouterService,
+              private accountLoginService: AccountLoginService) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (!!this.jwtService.getToken()) {
-      return true;
+    if (!this.accountLoginService.isAuthorized()) {
+      // not logged in so redirect to login page with the return url
+      this.localize.navigateToRoute('/login', { queryParams: { returnUrl: state.url } });
+      return false;
     }
 
-    // not logged in so redirect to login page with the return url
-    this.localize.navigateToRoute('/login', { queryParams: { returnUrl: state.url } });
-    return false;
+    return true;
+    /*if (!!this.jwtService.getToken()) {
+      return true;
+    }*/
+
+
   }
 }
 
