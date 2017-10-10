c0d623e266bee8e01cce42b3acc8548e0f745417
Intershop <support@intershop.de>
2017-10-10 18:20:08 +0530

    merged changes from header navigation

--- src/app/components/category-navigation/category-navigation.component.spec.ts
+++ src/app/components/category-navigation/category-navigation.component.spec.ts
@@ -1,25 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NO_ERRORS_SCHEMA } from '@angular/core/';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs/Observable';
+import { anything, instance, mock, verify, when } from 'ts-mockito/lib/ts-mockito';
+import { Category } from '../../services/categories/categories.model';
+import { CategoriesService } from '../../services/categories/categories.service';
+import { LocalizeRouterService } from '../../services/routes-parser-locale-currency/localize-router.service';
+import { MockComponent } from '../mock.component';
 import { CategoryNavigationComponent } from './category-navigation.component';
+import { SubCategoryNavigationComponent } from './subcategory-navigation/subcategory-navigation.component';
 
-describe('Category Navigation Component', () => {
+fdescribe('Category Navigation Component', () => {
   let component: CategoryNavigationComponent;
   let fixture: ComponentFixture<CategoryNavigationComponent>;
+  let categoriesServiceMock: CategoriesService;
+  const category: Category = new Category();
 
   beforeEach(async(() => {
+    categoriesServiceMock = mock(CategoriesService);
     TestBed.configureTestingModule({
-      declarations: [ CategoryNavigationComponent ]
-    })
-    .compileComponents();
+      declarations: [CategoryNavigationComponent,
+        MockComponent({ selector: 'is-subcategory-navigation', template: 'Subcategory Navigation Template' })],
+      imports: [RouterTestingModule],
+      providers: [
+        { provide: CategoriesService, useFactory: () => instance(categoriesServiceMock) }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(CategoryNavigationComponent);
+      component = fixture.componentInstance;
+      when(categoriesServiceMock.getCurrentRootCategory()).thenReturn(Observable.of(category));
+      fixture.detectChanges();
+    });
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CategoryNavigationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getCurrentRootCategory', () => {
+    verify(categoriesServiceMock.getCurrentRootCategory()).once();
+    expect(component.rootCategory).toEqual(category);
   });
 });
