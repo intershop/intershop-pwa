f7a52f363e628ee1a988f93fbdc9dd749b7ed8b1
Intershop <support@intershop.de>
2017-10-11 13:02:37 +0530

    Specs

--- src/app/services/products/products.service.spec.ts
+++ src/app/services/products/products.service.spec.ts
@@ -1,5 +1,5 @@
 import { Observable } from 'rxjs/Rx';
-import { instance, mock, when } from 'ts-mockito';
+import { instance, mock, when, anything, verify } from 'ts-mockito';
 import { ApiService } from '../api.service';
 import { ProductListService } from './products.service';
 
@@ -10,14 +10,11 @@ describe('ProuctList Service', () => {
     productListService = new ProductListService(instance(apiService));
   });
 
-  it('should call getProductList method', () => {
+  it('should call getProductList method and confirm apiService.get method is called once', () => {
     const products = ['Product1', 'Product2'];
     const apiUrl = 'categories/Cameras-Camcorders/584/products';
-    when(apiService.get(apiUrl)).thenReturn(Observable.of(products));
-    let productList;
-    productListService.getProductList(apiUrl).subscribe((data) => {
-      productList = data;
-    });
-    expect(productList).toBe(products);
+    when(apiService.get(anything(), anything(), anything(), anything(), anything())).thenReturn(Observable.of(products));
+    productListService.getProductList(apiUrl);
+    verify(apiService.get(anything(), anything(), anything(), anything(), anything())).once();
   });
 });
