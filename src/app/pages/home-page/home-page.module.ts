d906196078a5850f99c2f178ab6d1dc937abb463
Intershop <support@intershop.de>
2017-10-16 14:28:09 +0530

    Review comments updated.

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { LocalizeRouterModule } from '../../services/routes-parser-locale-currency/localize-router.module';
 import { HomePageComponent } from './home-page.component';
-import { HomePageRoute } from './home-page.routes';
 
 @NgModule({
   imports: [
-    RouterModule.forChild(HomePageRoute),
-    LocalizeRouterModule.forChild(HomePageRoute),
     CarouselModule
   ],
   declarations: [HomePageComponent]
