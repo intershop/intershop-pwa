ef9756e8139bbb3e05e8b479a7e3b60071c98f7f
Intershop <support@intershop.de>
2017-10-25 13:58:02 +0530

    Product Row component's price test fixed

--- src/app/components/product-tile/product-tile.component.ts
+++ src/app/components/product-tile/product-tile.component.ts
@@ -114,12 +114,12 @@ export class ProductTileComponent implements OnInit {
       }
     }
 
-    // if (this.mockData.listPrice.value) {
-    //   this.displayCondition = true;
-    //   if (this.mockData.salePrice.value && !(this.mockData.listPrice.value > this.mockData.salePrice.value)) {
-    //     this.displayCondition = false;
-    //   }
-    // }
+    if (this.mockData.listPrice.value) {
+      this.displayCondition = true;
+      if (this.mockData.salePrice.value && !(this.mockData.listPrice.value > this.mockData.salePrice.value)) {
+        this.displayCondition = false;
+      }
+    }
 
 
     if (this.mockData.listPrice.range && !(this.mockData.listPrice.range.minimumPrice === this.mockData.listPrice.range.maximumPrice)) {
