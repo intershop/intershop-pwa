56ba27dc90345b8b8c1dc670c2ad37fb2146577b
Stefan Hauke <s.hauke@intershop.de>
2017-11-08 18:00:40 +0100

    ISREST-86: reworked the category navigation component - moved the logic to the categories.service - provided Components data via Input (category as input instead of from route - route still needs to be used for the category path for now) - restructured the html content and logic to be more like the ISML counterpart

--- src/app/services/categories/categories.service.ts
+++ src/app/services/categories/categories.service.ts
@@ -2,6 +2,7 @@ import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { forkJoin } from 'rxjs/observable/forkJoin';
 import { ApiService } from '../api.service';
 import { Category } from './categories.model';
 
@@ -32,11 +33,43 @@ export class CategoriesService implements Resolve<Category> {
 
   /**
    * REST API - Get info on (sub-)category
-   * @param categoryPath  The category id path for the category of interest.
-   * @returns             Category information.
+   * @param categoryId  The category id path for the category of interest.
+   * @returns           Category information.
    */
-  getCategory(categoryPath: string): Observable<Category> {
-    return this.apiService.get('categories/' + categoryPath, null, null, false);
+  getCategory(categoryId: string): Observable<Category> {
+    return this.apiService.get('categories/' + categoryId, null, null, false);
+  }
+
+
+  // TODO: this method should become obsolete as soon as the categrory REST call will return the category path too
+  /**
+   * Helper function to get the category path for a given category with the help of the current route.
+   * @param category        The category the category path should be gotten for.
+   * @param activatedRoute  The currently activated route that is used to determine the category path.
+   * @returns               A Category array that represents the categroy path from root to the category.
+   */
+  getCategoryPath(category: Category, activatedRoute: ActivatedRouteSnapshot): Observable<Category[]> {
+    const observableArray: Observable<Category>[] = [];
+    let categoryId = '';
+
+    for (const urlSegment of activatedRoute.url) {
+      categoryId = categoryId + urlSegment.path;
+      observableArray.push(this.getCategory(categoryId));
+      categoryId = categoryId + '/';
+    }
+
+    return forkJoin(observableArray);
+    // TODO: optimazation option: do not get the category itself again but use the provided category as input for the array (wraped in an Observable)
+  }
+
+  /**
+   * Helper function to compare two categories
+   * @param category1  The first category to be compared with the second.
+   * @param category2  The second category to be compared with the first.
+   * @returns          True if the categories are the equal, false if not.
+   */
+  isCategoryEqual(category1: Category, category2: Category): boolean {
+    return category1 && category2 && category1.id === category2.id;
   }
 
   /**
