b7c0527bbec0d28b369dfcddff8a87231b589bd2
Stefan Hauke <s.hauke@intershop.de>
2017-11-16 09:36:57 +0100

    ISREST-14: change the language switch links to reflect the language change in the navigation target (href)

--- src/app/components/header/language-switch/language-switch.component.ts
+++ src/app/components/header/language-switch/language-switch.component.ts
@@ -10,12 +10,14 @@ import { CurrentLocaleService } from '../../../services/locale/current-locale.se
 
 export class LanguageSwitchComponent implements OnInit {
 
-  // TODO: is this default realy a property of the language switch component or a global configuration
+  // TODO: is this default really a property of the language switch component or a global configuration
   lang: any;
   localizationArray: any;
 
-  constructor(private currentLocaleService: CurrentLocaleService, public router: Router) {
-  }
+  constructor(
+    private currentLocaleService: CurrentLocaleService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
     // TODO: discussion whether this information is coming from a service or is configured at development time
