7557597bae5bef4068f32277c9f9f546735e64cc
Stefan Hauke <s.hauke@intershop.de>
2017-11-16 09:57:47 +0100

    ISREST-14: changed value and displayName in locales objects to be more intuitive

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,8 +10,8 @@ export const environment = {
   base_url: 'http://192.168.99.100:8081',
 
   locales: [
-    { 'lang': 'en_US', 'currency': 'USD', value: 'English', displayValue: 'en' },
-    { 'lang': 'de_DE', 'currency': 'EUR', value: 'German', displayValue: 'de' },
-    { 'lang': 'fr_FR', 'currency': 'EUR', value: 'French', displayValue: 'fr' }
+    { 'lang': 'en_US', 'currency': 'USD', value: 'en', displayName: 'English' },
+    { 'lang': 'de_DE', 'currency': 'EUR', value: 'de', displayName: 'German' },
+    { 'lang': 'fr_FR', 'currency': 'EUR', value: 'fr', displayName: 'French' }
   ]
 };
