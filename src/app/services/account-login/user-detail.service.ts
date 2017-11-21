66df4b17be8dff49601d186036c592da4d21109b
Intershop <support@intershop.de>
2017-11-21 12:16:25 +0530

    ISREST-90: Session cookie implementation for global-state-aware.service

--- src/app/services/account-login/user-detail.service.ts
+++ src/app/services/account-login/user-detail.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie';
 import { GlobalStateAwareService } from '../base-services/global-state-aware.service';
 import { UserDetail } from './account-login.model';
 
 @Injectable()
 export class UserDetailService extends GlobalStateAwareService<UserDetail> {
 
-  constructor() {
-    super('currentUserDetail', true, true);
+  constructor(cookieService: CookieService) {
+    super('currentUserDetail', true, true, null, cookieService);
   }
 
   setUserDetail(newUserDetail: UserDetail): void {
