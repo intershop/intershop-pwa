870126e4ed4a3a01c666f682c5018eadfbba8ea8
Stefan Hauke <s.hauke@intershop.de>
2017-11-22 12:30:56 +0100

    ISREST-184: moved category resolver code from CategoriesService to a standalone CategoryResolver and used it for category routing

---
+++ src/app/shared/resolvers/category.resolver.ts
@@ -0,0 +1,21 @@
+import { HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Category } from '../../models/categories.model';
+import { CategoriesService } from '../services/categories/categories.service';
+
+@Injectable()
+export class CategoryResolver implements Resolve<Category> {
+
+  constructor(
+    private categoriesService: CategoriesService
+  ) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Category> {
+    const categoryPath = route.url.map(x => x.path).join('/');
+    // TODO: redirect to /error when category not found
+    return this.categoriesService.getCategory(categoryPath);
+  }
+
+}
