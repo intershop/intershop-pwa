a5115a299c28e654d4aa46dc4db80e56b2c4c657
Danilo Hoffmann <d.hoffmann@intershop.de>
2017-11-22 11:19:27 +0100

    ISREST-45: using flags for update, refactored script

--- .gitlab-ci-deploy.sh
+++ .gitlab-ci-deploy.sh
@@ -1,13 +1,31 @@
-set -e
 set -x
+set -e
 
 IMAGE="${CI_REGISTRY}/${CI_PROJECT_PATH}-prod:${CI_BUILD_REF}"
 SERVICE="poc"
 
-if ! docker service ps poc ; then
-  docker service create -d --replicas $SERVICE_REPLICAS --publish mode=host,target=4000,published=4321 --name $SERVICE --env ICM_BASE_URL=$ICM_BASE_URL --env LOGGING=true $IMAGE
+if ! docker service ps $SERVICE ; then
+  docker service create -d \
+      --replicas $SERVICE_REPLICAS \
+      --publish mode=host,target=4000,published=4321 \
+      --name $SERVICE \
+      --env ICM_BASE_URL=$ICM_BASE_URL \
+      --env LOGGING=true \
+      $IMAGE
+else
+  docker service update -d \
+      --image $IMAGE \
+      --env-add ICM_BASE_URL=$ICM_BASE_URL \
+      --env-add LOGGING=true \
+      --force \
+      --replicas $SERVICE_REPLICAS \
+      --update-delay 0s \
+      --update-parallelism 0 \
+      --update-order stop-first \
+      --update-failure-action continue \
+      $SERVICE
+
 fi
 
-docker service update -d --image $IMAGE --env-add ICM_BASE_URL=$ICM_BASE_URL --env-add LOGGING=true --force --replicas $SERVICE_REPLICAS $SERVICE
-
+set +x
 echo "successfully deployed service with image $IMAGE"
