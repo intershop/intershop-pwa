498db8b143440269e7b425d4f37bee37034847a0
Danilo Hoffmann <d.hoffmann@intershop.de>
2017-12-08 12:06:48 +0100

    ISREST-151: refactored mapping to category object in CategoryPageComponent

--- src/app/shopping/pages/category-page/category-page.component.ts
+++ src/app/shopping/pages/category-page/category-page.component.ts
@@ -24,8 +24,8 @@ export class CategoryPageComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.route.data.subscribe((data: { category: Category }) => {
-      this.category = data.category;
+    this.route.data.map(data => data.category).subscribe((category: Category) => {
+      this.category = category;
       // TODO: only category should be needed once the REST call returns the categoryPath as part of the category
       this.categoriesService.getCategoryPath(this.category, this.route.snapshot).subscribe((categoryPath: Category[]) => {
         this.categoryPath = categoryPath;
