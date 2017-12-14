54c38889d5931b925bb5fac13d5acccb8d103752
Stefan Hauke <s.hauke@intershop.de>
2017-12-14 15:36:37 +0100

    added extra feature shared modules for shared functionalities that can be used by the feature pages modules, the feature module itself imports only the routing

--- src/app/shopping/shopping.module.ts
+++ src/app/shopping/shopping.module.ts
@@ -1,32 +1,9 @@
 import { NgModule } from '@angular/core';
-import { SharedModule } from '../shared/shared.module';
-import { CategoryNavigationComponent } from './components/category//category-navigation/category-navigation.component';
-import { CategoryListComponent } from './components/category/category-list/category-list.component';
-import { ProductListComponent } from './components/product/product-list/product-list.component';
-import { ProductRowComponent } from './components/product/product-row/product-row.component';
-import { ProductTileComponent } from './components/product/product-tile/product-tile.component';
-import { DisableIconDirective } from './directives/disable-icon.directive';
+import { ShoppingRoutingModule } from './shopping-routing.module';
 
 @NgModule({
   imports: [
-    SharedModule,
-    // ShoppingRoutingModule // TODO: remove this import since it leads to recursive routes with category page (see Augury)
-  ],
-  declarations: [
-    CategoryListComponent,
-    CategoryNavigationComponent,
-    DisableIconDirective,
-    ProductListComponent,
-    ProductRowComponent,
-    ProductTileComponent
-  ],
-  exports: [
-    CategoryListComponent,
-    CategoryNavigationComponent,
-    DisableIconDirective,
-    ProductListComponent,
-    ProductRowComponent,
-    ProductTileComponent
+    ShoppingRoutingModule
   ]
 })
 
