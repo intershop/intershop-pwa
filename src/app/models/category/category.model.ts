6a0929b8411df80f0602872a4cb3adf386f087e5
Sebastian Glaß <s.glass@intershop.de>
2017-12-19 11:02:02 +0100

    ISREST-151: moved equals methods to Category.model

--- src/app/models/category/category.model.ts
+++ src/app/models/category/category.model.ts
@@ -1,7 +1,7 @@
 import { Image } from '../image.model';
 
 export class Category {
-  
+
   public type: string;
   public hasOnlineProducts: boolean;
   public hasOnlineSubCategories: boolean;
@@ -14,4 +14,15 @@ export class Category {
 
   constructor(public id: string, public name: string) { }
 
+  /**
+   * Helper function to compare two categories
+   * @param category2  The second category to be compared with the first.
+   * @returns          True if the categories are equal, false if not.
+   *                   'equal' means
+   *                   - both categories are defined
+   *                   - the id of the categories is the same
+   */
+  equals(category2: Category): boolean {
+    return !!category2 && this.id === category2.id;
+  }
 }
