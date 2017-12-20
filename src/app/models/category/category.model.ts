0cd2383b4d8cdb8a79e926380bef65d80b2f1de7
Sebastian Glaß <s.glass@intershop.de>
2017-12-20 11:23:25 +0100

    ISREST-151: fixed namings due review

--- src/app/models/category/category.model.ts
+++ src/app/models/category/category.model.ts
@@ -16,13 +16,13 @@ export class Category {
 
   /**
    * Helper function to compare two categories
-   * @param category2  The second category to be compared with the first.
+   * @param category   The category to be compared with the object.
    * @returns          True if the categories are equal, false if not.
    *                   'equal' means
    *                   - both categories are defined
    *                   - the id of the categories is the same
    */
-  equals(category2: Category): boolean {
-    return !!category2 && this.id === category2.id;
+  equals(category: Category): boolean {
+    return !!category && this.id === category.id;
   }
 }
