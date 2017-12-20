63dd4552c8f28c1c60848c8890d2b86c6823a077
Silke Grüber <s.grueber@intershop.de>
2017-12-20 14:14:34 +0100

    ISREST-194: tests for various select boxes added, provide CountryService and RegionService temporarily in SharedModule, minor improvements

--- src/app/registration/components/form-controls/select-language/select-language.component.spec.ts
+++ src/app/registration/components/form-controls/select-language/select-language.component.spec.ts
@@ -1,6 +1,9 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core/';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl, FormGroup } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs/Observable';
+import { anything, instance, mock, when } from 'ts-mockito';
 import { SelectLanguageComponent } from './select-language.component';
 
 describe('Select Language Component', () => {
@@ -9,10 +12,18 @@ describe('Select Language Component', () => {
   let element: HTMLElement;
 
   beforeEach(async(() => {
+    const translateServiceMock = mock(TranslateService);
+    when(translateServiceMock.get(anything())).thenCall((data) => {
+      if (data === 'labelKey') {
+        return Observable.of('LabelName');
+      } else {
+        return Observable.of(null);
+      }
+    });
     TestBed.configureTestingModule({
       declarations: [SelectLanguageComponent],
       providers: [
-        { provide: TranslateService }
+        { provide: TranslateService, useFactory: () => instance(translateServiceMock) }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     })
@@ -20,6 +31,11 @@ describe('Select Language Component', () => {
         fixture = TestBed.createComponent(SelectLanguageComponent);
         component = fixture.componentInstance;
         element = fixture.nativeElement;
+
+        const form = new FormGroup({
+          preferredLanguage: new FormControl('en_US')
+        });
+        component.form = form;
       });
   }));
 
@@ -27,4 +43,17 @@ describe('Select Language Component', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
   });
+
+  it('should set default values properly on creation', () => {
+    fixture.detectChanges();
+    expect(component.controlName).toEqual('preferredLanguage', 'control Name should be <preferredLanguage>');
+    expect(component.label).toEqual('Preferred Language', 'label should be <Preferred Language>');
+  });
+
+  it('should get and display languages on creation', () => {
+    fixture.detectChanges();
+    expect(component.options.length).toEqual(3, '3 languages are in the options array'); // ToDo: languages are retrieved from a service
+    expect(element.querySelector('select[data-testing-id=preferredLanguage]')).toBeTruthy('language select is rendered');
+  });
+
 });
--- src/app/registration/components/form-controls/select-security-question/select-security-question.component.spec.ts
+++ src/app/registration/components/form-controls/select-security-question/select-security-question.component.spec.ts
@@ -1,6 +1,9 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core/';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl, FormGroup } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs/Observable';
+import { anything, instance, mock, when } from 'ts-mockito';
 import { SelectSecurityQuestionComponent } from './select-security-question.component';
 
 describe('Select Security Question Component', () => {
@@ -9,10 +12,18 @@ describe('Select Security Question Component', () => {
   let element: HTMLElement;
 
   beforeEach(async(() => {
+    const translateServiceMock = mock(TranslateService);
+    when(translateServiceMock.get(anything())).thenCall((data) => {
+      if (data === 'labelKey') {
+        return Observable.of('LabelName');
+      } else {
+        return Observable.of(null);
+      }
+    });
     TestBed.configureTestingModule({
       declarations: [SelectSecurityQuestionComponent],
       providers: [
-        { provide: TranslateService }
+        { provide: TranslateService, useFactory: () => instance(translateServiceMock) }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     })
@@ -20,11 +31,28 @@ describe('Select Security Question Component', () => {
         fixture = TestBed.createComponent(SelectSecurityQuestionComponent);
         component = fixture.componentInstance;
         element = fixture.nativeElement;
+
+        const form = new FormGroup({
+          securityQuestion: new FormControl()
+        });
+        component.form = form;
       });
   }));
 
   it('should be created', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
+  });
+
+  it('should set default values properly on creation', () => {
+    fixture.detectChanges();
+    expect(component.controlName).toEqual('securityQuestion', 'control Name should be <securityQuestion>');
+    expect(component.label).toEqual('Security Question', 'label should be <Security Question>');
+  });
+
+  it('should get and display security questions on creation', () => {
+    fixture.detectChanges();
+    expect(component.options.length).toEqual(5, '5 questions are in the options array'); // ToDo: questions are retrieved from a service
+    expect(element.querySelector('select[data-testing-id=securityQuestion]')).toBeTruthy('security question select is rendered');
   });
 });
