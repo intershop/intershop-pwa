6777af8475a0dacf45137e4b3dd4cb0baa715c92
Stefan Hauke <s.hauke@intershop.de>
2018-01-03 23:40:44 +0100

    ISREST-206: initial 'Product Images Component' and 'Product Price Component' - used product price component on product tile, product row and product page components - used product images component on product page component

---
+++ src/app/shopping/components/product/product-price/product-price.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ProductPriceComponent } from './product-price.component';
+
+describe('Product Price Component', () => {
+  let component: ProductPriceComponent;
+  let fixture: ComponentFixture<ProductPriceComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProductPriceComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductPriceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  xit('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
