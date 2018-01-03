5c09bbaab221513f39bef14903a20c8df180ddc6
Stefan Hauke <s.hauke@intershop.de>
2018-01-03 15:21:54 +0100

    ISREST-216: initial product page component with product routing, product resolver and product service for API connection

---
+++ src/app/shopping/resolvers/product.resolver.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Product } from '../../models/product.model';
+import { ProductsService } from '../services/products/products.service';
+
+@Injectable()
+export class ProductResolver implements Resolve<Product> {
+
+  constructor(
+    private productsService: ProductsService
+  ) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> {
+    const sku = route.paramMap.get('sku');
+    // TODO: redirect to /error when product not found
+    return this.productsService.getProduct(sku);
+  }
+
+}
