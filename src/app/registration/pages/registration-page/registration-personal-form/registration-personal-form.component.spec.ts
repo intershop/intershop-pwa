cf7ea88135dcb94a5d624e3cf4fcfab1566edba0
Silke Grüber <s.grueber@intershop.de>
2018-01-09 14:51:38 +0100

    #ISREST-194: tests for registration page extended, tests for registration page sub forms added

--- src/app/registration/pages/registration-page/registration-personal-form/registration-personal-form.component.spec.ts
+++ src/app/registration/pages/registration-page/registration-personal-form/registration-personal-form.component.spec.ts
@@ -1,5 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core/';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormGroup } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 import { RegistrationPersonalFormComponent } from './registration-personal-form.component';
 
@@ -20,6 +21,9 @@ describe('RegistrationPersonalFormComponent', () => {
         fixture = TestBed.createComponent(RegistrationPersonalFormComponent);
         component = fixture.componentInstance;
         element = fixture.nativeElement;
+
+        const parentForm = new FormGroup({});
+        component.parentForm = parentForm;
       });
   }));
 
@@ -27,4 +31,18 @@ describe('RegistrationPersonalFormComponent', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
   });
+
+  it('should throw an error if input parameter parentForm is not set', () => {
+    component.parentForm = null;
+    expect(function() { fixture.detectChanges(); }).toThrow();
+  });
+
+  it('should throw an error if control preferredLanguage does not exist', () => {
+    expect(function() { fixture.detectChanges(); }).toThrow();
+  });
+
+  it('should throw an error if control birthday does not exist', () => {
+    expect(function() { fixture.detectChanges(); }).toThrow();
+  });
+
 });
