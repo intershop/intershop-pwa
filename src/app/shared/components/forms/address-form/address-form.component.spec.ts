f62fa59a9ed680c3c689c52de7e7425ea4ef59f6
Silke Grüber <s.grueber@intershop.de>
2018-01-09 13:02:14 +0100

    #ISREST-194: address form test added

--- src/app/shared/components/forms/address-form/address-form.component.spec.ts
+++ src/app/shared/components/forms/address-form/address-form.component.spec.ts
@@ -1,7 +1,7 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core/';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
-import { SharedModule } from '../../../shared.module';
 import { AddressFormComponent } from './address-form.component';
 
 describe('Address Form Component', () => {
@@ -12,11 +12,10 @@ describe('Address Form Component', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AddressFormComponent],
+
       providers: [
-        { provide: TranslateService }
-      ],
-      imports: [
-        SharedModule,
+        { provide: TranslateService },
+        FormBuilder
       ],
       schemas: [NO_ERRORS_SCHEMA]
     })
@@ -24,6 +23,10 @@ describe('Address Form Component', () => {
         fixture = TestBed.createComponent(AddressFormComponent);
         component = fixture.componentInstance;
         element = fixture.nativeElement;
+
+        const parentForm = new FormGroup({
+        });
+        component.parentForm = parentForm;
       });
   }));
 
@@ -31,4 +34,48 @@ describe('Address Form Component', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
   });
+
+  it('should throw an error if input parameter parentForm is not set', () => {
+    component.parentForm = null;
+    expect(function() { fixture.detectChanges(); }).toThrow();
+  });
+
+  it('should create an address form on creation', () => {
+    expect(component.addressForm).toBeUndefined('address form has not been created before init');
+    fixture.detectChanges();
+    expect(component.addressForm.get('countryCode')).toBeTruthy('address form contains a country code control');
+    expect(component.addressForm.get('firstName')).toBeTruthy('address form contains a firstName control');
+    expect(component.addressForm.get('lastName')).toBeTruthy('address form contains a lastName control');
+    expect(component.addressForm.get('addressLine1')).toBeTruthy('address form contains a addressLine1 control');
+    expect(component.addressForm.get('addressLine2')).toBeTruthy('address form contains a addressLine2 control');
+    expect(component.addressForm.get('postalCode')).toBeTruthy('address form contains a postalCode control');
+    expect(component.addressForm.get('city')).toBeTruthy('address form contains a city control');
+    expect(component.addressForm.get('phoneHome')).toBeTruthy('address form contains a phoneHome control');
+  });
+
+  it('should render country specific address forms if country changes', () => {
+    fixture.detectChanges();
+    expect(element.querySelector('ish-address-default')).toBeTruthy('default address template is rendered if country is empty');
+
+    component.addressForm.get('countryCode').setValue('DE');
+    fixture.detectChanges();
+    expect(element.querySelector('ish-address-de')).toBeTruthy('German address template is rendered if country is DE');
+
+    component.addressForm.get('countryCode').setValue('FR');
+    fixture.detectChanges();
+    expect(element.querySelector('ish-address-fr')).toBeTruthy('French address template is rendered if country is FR');
+
+    component.addressForm.get('countryCode').setValue('GB');
+    fixture.detectChanges();
+    expect(element.querySelector('ish-address-gb')).toBeTruthy('British address template is rendered if country is GB');
+
+    component.addressForm.get('countryCode').setValue('US');
+    fixture.detectChanges();
+    expect(element.querySelector('ish-address-us')).toBeTruthy('US address template is rendered if country is US');
+
+    component.addressForm.get('countryCode').setValue('BG');
+    fixture.detectChanges();
+    expect(element.querySelector('ish-address-default')).toBeTruthy('Default address template is rendered if country has no specific address template ');
+  });
+
 });
