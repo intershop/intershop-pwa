749bbfb9a6c959e44eb2f0a75ae2c6534a2d0d06
Ferdinand Malcher <ferdinand@malcher.media>
2018-01-09 17:26:56 +0100

    added tests for FormVisualizeErrorsDirective

---
+++ src/app/shared/directives/form-visualize-errors.directive.spec.ts
@@ -0,0 +1,77 @@
+import { CommonModule } from '@angular/common';
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { FormValidationDirective } from './form-validation.directive';
+import { FormVisualizeErrorsDirective } from './form-visualize-errors.directive';
+
+@Component({
+  template: `<div [ishVisualizeErrors]="control"></div>`
+})
+class TestComponent {
+  control = new FormControl('', Validators.required);
+}
+
+describe('FormVisualizeErrorsDirective', () => {
+  let fixture: ComponentFixture<TestComponent>;
+  let component: TestComponent;
+
+  const errorClass = 'has-error';
+  const successClass = 'has-success';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+      ],
+      declarations: [
+        TestComponent,
+        FormVisualizeErrorsDirective
+      ]
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+  });
+
+  function getClasses() {
+    return fixture.debugElement.query(By.css('div')).classes;
+  }
+
+  it('should set none of the classes when form is pristine', () => {
+    component.control.setValue('');
+    fixture.detectChanges();
+    expect(getClasses()[errorClass]).toBeFalsy();
+    expect(getClasses()[successClass]).toBeFalsy();
+
+    component.control.setValue('a');
+    fixture.detectChanges();
+    expect(getClasses()[errorClass]).toBeFalsy();
+    expect(getClasses()[successClass]).toBeFalsy();
+  });
+
+  it('should set error CSS class when form is invalid and dirty', () => {
+    component.control.setValue('');
+    component.control.markAsDirty();
+    fixture.detectChanges();
+
+    expect(component.control.invalid).toBeTruthy();
+    expect(getClasses()[errorClass]).toBeTruthy();
+    expect(getClasses()[successClass]).toBeFalsy();
+  });
+
+  it('should set success CSS class when form is valid and dirty', () => {
+    component.control.setValue('a');
+    component.control.markAsDirty();
+    fixture.detectChanges();
+
+    expect(component.control.valid).toBeTruthy();
+    expect(getClasses()[errorClass]).toBeFalsy();
+    expect(getClasses()[successClass]).toBeTruthy();
+  });
+});
