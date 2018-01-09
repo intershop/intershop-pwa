11f0807c878778774d6408626c946d49089fbad7
Ferdinand Malcher <ferdinand@malcher.media>
2018-01-09 17:09:40 +0100

    revised form errors display
    - removed formGroupValidationComponent, which was the wrapper component that did dynamic component spawning
    - also removed this wherever it was used
    - introduced new FormControlErrorComponent, which shows errors where they need to be
    - added tests for FormControlErrorComponent
    
    - used ishVisualizeErrors directive in all places
    
    Caution:
    This breaks validation for the birthday picker! The birthday picker could be refactored to a custom form control, so no need now to fix this.

--- src/app/shared/components/global-form-validation/form-control-error.component.spec.ts
+++ src/app/shared/components/global-form-validation/form-control-error.component.spec.ts
@@ -1,18 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { inject, TestBed } from '@angular/core/testing';
 import { ComponentFixture } from '@angular/core/testing';
+import { FormControl, Validators } from '@angular/forms';
 import { BrowserModule, By } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs/Observable';
+import { anything, instance, mock, when } from 'ts-mockito/lib/ts-mockito';
 import { FormControlErrorComponent } from './form-control-error.component';
 
-describe('Form control error component', () => {
+describe('FormControlErrorComponent', () => {
   let fixture: ComponentFixture<FormControlErrorComponent>;
   let component: FormControlErrorComponent;
+  let translateService: TranslateService;
+
+  beforeEach(() => {
+    translateService = mock(TranslateService);
+
+    when(translateService.get('requiredkey'))
+      .thenReturn(Observable.of('requiredmessage'));
+
+    when(translateService.get('lengthkey'))
+      .thenReturn(Observable.of('lengthmessage'));
+  });
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        BrowserModule,
-      ],
-      declarations: [
-        FormControlErrorComponent
+      imports: [BrowserModule],
+      declarations: [FormControlErrorComponent],
+      providers: [
+        { provide: TranslateService, useFactory: () => instance(translateService) }
       ]
     }).compileComponents();
   });
@@ -20,20 +36,78 @@ describe('Form control error component', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FormControlErrorComponent);
     component = fixture.componentInstance;
+
+    component.control = new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]);
+    component.messages = {
+      required: 'requiredkey',
+      minlength: 'lengthkey'
+    };
   });
 
-  function searchErrorDisplay() {
-    fixture.detectChanges();
-    return fixture.debugElement.query(By.css('small'));
+  function getErrorDisplay() {
+    return fixture.debugElement.queryAll(By.css('small'));
   }
 
-  it('should not create form control error when initialized', () => {
-    expect(component.messagesList).toBeFalsy();
-    expect(searchErrorDisplay()).toBeFalsy();
+  it('should have errors, but should not show them when control is pristine', () => {
+    component.control.setValue('');
+    fixture.detectChanges();
+
+    expect(component.control.errors.required).toBeTruthy();
+    expect(getErrorDisplay().length).toBe(0);
   });
 
-  it('should create form control error when error is set', () => {
-    component.messagesList = ['error1'];
-    expect(searchErrorDisplay()).toBeTruthy();
+  it('should display errors when control is dirty', () => {
+    component.control.setValue('a');
+    component.control.markAsDirty();
+    fixture.detectChanges();
+    const elements = getErrorDisplay();
+
+    expect(component.control.errors.minlength).toBeTruthy();
+    expect(elements.length).toBe(1);
+    expect(elements[0].nativeElement.textContent).toContain('lengthmessage');
+  });
+
+  it('should show hidden existing error when marked as dirty', () => {
+    component.control.setValue('');
+    expect(component.control.errors.required).toBeTruthy();
+    fixture.detectChanges();
+    expect(getErrorDisplay().length).toBe(0);
+
+    component.control.markAsDirty();
+    fixture.detectChanges();
+    expect(getErrorDisplay().length).toBe(1);
+  });
+
+
+  function getIcon() {
+    return fixture.debugElement.query(By.css('i.glyphicon'));
+  }
+
+  it('should display contextual icons', () => {
+    let icon;
+
+    // form invalid and pristine
+    component.control.setValue('');
+    fixture.detectChanges();
+    expect(getIcon()).toBeFalsy();
+
+    // form invalid and dirty
+    component.control.markAsDirty();
+    fixture.detectChanges();
+    icon = getIcon();
+    expect(icon).toBeTruthy();
+    expect(icon.classes['glyphicon-remove']).toBeTruthy();
+    expect(icon.classes['glyphicon-ok']).toBeFalsy();
+
+    // form valid and dirty
+    component.control.setValue('abcd');
+    fixture.detectChanges();
+    icon = getIcon();
+    expect(icon).toBeTruthy();
+    expect(icon.classes['glyphicon-remove']).toBeFalsy();
+    expect(icon.classes['glyphicon-ok']).toBeTruthy();
   });
 });
--- src/app/shared/components/global-form-validation/form-control-error.component.ts
+++ src/app/shared/components/global-form-validation/form-control-error.component.ts
@@ -1,16 +1,48 @@
-// NEEDS_WORK: needs review and evaluation
-import { Component, Input } from '@angular/core';
-import { FormControlName } from '@angular/forms';
+import { Component, DoCheck, Input, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs/Observable';
+import { combineLatest } from 'rxjs/observable/combineLatest';
+import { from } from 'rxjs/observable/from';
+import { map, mergeAll, switchMap } from 'rxjs/operators';
+
+import { FormErrorMessages } from './form-error-messages.interface';
+
 
 @Component({
   selector: 'ish-form-control-error',
-  template: `
-            <i class="form-control-feedback glyphicon" [ngClass]="{'glyphicon-remove':formControl?.invalid,'glyphicon-ok':formControl?.valid}"></i>
-            <small *ngFor="let error of messagesList" class="help-block">{{error}}</small>
-            `
+  templateUrl: './form-control-error.component.html'
 })
+export class FormControlErrorComponent implements DoCheck {
+  @Input() messages: FormErrorMessages = {};
+  @Input() control: AbstractControl;
 
-export class FormControlErrorComponent {
-  @Input() messagesList: string[];
-  @Input() formControl: FormControlName;
+  errors: Observable<string>[];
+
+  constructor(private translate: TranslateService) {}
+
+  ngDoCheck() {
+    if (this.control.dirty) {
+      this.errors = this.getErrorList();
+    }
+  }
+
+  get iconClasses(): { [key: string]: boolean } {
+    return {
+      'glyphicon-remove': this.control.invalid,
+      'glyphicon-ok': this.control.valid
+    };
+  }
+
+  getErrorList(): Observable<string>[] {
+    if (!this.control.errors) { return []; }
+
+    return Object.keys(this.control.errors)
+      .map(key => (this.messages && key in this.messages && this.messages[key]) ?
+        this.messages[key] :
+        this.control.errors['customError']
+      )
+      .filter(locString => !!locString)
+      .map(locString => this.translate.get(locString));
+  }
 }
