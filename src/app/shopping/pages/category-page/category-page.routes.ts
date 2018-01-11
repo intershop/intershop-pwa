5a0b161250e17b7429c7775e9191d344ceeae7fd
Stefan Hauke <s.hauke@intershop.de>
2018-01-11 16:54:17 +0100

    ISREST-207: introduced a CategoryPathResolver and used it for the CategoryPage routing instead of doing it in the ngOnInit of the CategorypageComponent

--- src/app/shopping/pages/category-page/category-page.routes.ts
+++ src/app/shopping/pages/category-page/category-page.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
+import { CategoryPathResolver } from '../../resolvers/category-path.resolver';
 import { CategoryResolver } from '../../resolvers/category.resolver';
 import { CategoryPageComponent } from './category-page.component';
 
 export const categoryPageRoutes: Routes = [
-  { path: '**', component: CategoryPageComponent, resolve: { category: CategoryResolver } }
+  {
+    path: '**', component: CategoryPageComponent,
+    resolve: {
+      category: CategoryResolver,
+      categoryPath: CategoryPathResolver
+    }
+  }
 ];
