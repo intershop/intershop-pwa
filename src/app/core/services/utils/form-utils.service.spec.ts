512216194bf28fb5c642e77dbb21a73bd2de9cf8
Ferdinand Malcher <ferdinand@malcher.media>
2018-01-17 11:44:46 +0100

    Tests for FormUtilsService (markAsDirtyRecursive)

---
+++ src/app/core/services/utils/form-utils.service.spec.ts
@@ -0,0 +1,45 @@
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormUtilsService } from './form-utils.service';
+
+describe('Suggest Service', () => {
+  let formUtils: FormUtilsService;
+
+  beforeEach(() => {
+    formUtils = new FormUtilsService();
+  });
+
+  describe('markDirtyRecursive', () => {
+    it('should mark flat form group dirty', () => {
+      const group = new FormGroup({
+        ctrl: new FormControl('')
+      });
+
+      const ctrl = group.get('ctrl');
+      expect(ctrl.dirty).toBeFalsy();
+      formUtils.markAsDirtyRecursive(group);
+      expect(ctrl.dirty).toBeTruthy();
+    });
+
+    it('should mark nested form group dirty', () => {
+      const group = new FormGroup({
+        foo: new FormGroup({
+          ctrlA: new FormControl(''),
+          grp: new FormGroup({
+            ctrlB: new FormControl('')
+          })
+        })
+      });
+
+      const ctrlA = group.get('foo.ctrlA');
+      const ctrlB = group.get('foo.grp.ctrlB');
+
+      expect(ctrlA.dirty).toBeFalsy();
+      expect(ctrlB.dirty).toBeFalsy();
+
+      formUtils.markAsDirtyRecursive(group);
+
+      expect(ctrlA.dirty).toBeTruthy();
+      expect(ctrlB.dirty).toBeTruthy();
+    });
+  });
+});
