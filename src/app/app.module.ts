6ada9549cd079b6a67a5d9aae085fe8fde540c29
Stefan Hauke <s.hauke@intershop.de>
2018-01-18 14:30:42 +0100

    ISREST-188: ngrx Router Store setup with Custom Router State Serializer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
+import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
 import { MetaReducer, StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'; // not used in production
 import { TranslateService } from '@ngx-translate/core';
@@ -17,6 +18,7 @@ import { RestStateAggregatorInterceptor } from './core/interceptors/rest-state-a
 import { CurrentLocaleService } from './core/services/locale/current-locale.service';
 import { getICMApplication, getICMBaseURL, getICMServerURL, getRestEndPoint, ICM_APPLICATION, ICM_BASE_URL, ICM_SERVER_URL, REST_ENDPOINT } from './core/services/state-transfer/factories';
 import { StatePropertiesService } from './core/services/state-transfer/state-properties.service';
+import { CustomSerializer, reducers } from './core/store';
 import { MockInterceptor } from './mocking/interceptors/mock.interceptor';
 import { RegistrationModule } from './registration/registration.module';
 import { ShoppingModule } from './shopping/shopping.module';
@@ -25,7 +27,6 @@ import { ShoppingModule } from './shopping/shopping.module';
 // In theory the platformBrowserDynamic method in main.ts could handle this but this breaks server-side rendering.
 // tslint:disable-next-line: do-not-import-environment
 import { environment } from '../environments/environment';
-
 
 export const metaReducers: MetaReducer<any>[] = !environment.production ? [storeFreeze] : [];
 
@@ -46,11 +47,13 @@ export const metaReducers: MetaReducer<any>[] = !environment.production ? [store
     AccountModule,
     // AppRoutingModule needs to be imported last since it handles the '**' route that would otherwise overwrite any route that comes after it
     AppRoutingModule,
-    StoreModule.forRoot({}, { metaReducers }),
+    StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([]),
+    StoreRouterConnectingModule,
     !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [
+    { provide: RouterStateSerializer, useClass: CustomSerializer },
     { provide: REST_ENDPOINT, useFactory: getRestEndPoint(), deps: [StatePropertiesService] },
     { provide: ICM_BASE_URL, useFactory: getICMBaseURL(), deps: [StatePropertiesService] },
     { provide: ICM_APPLICATION, useFactory: getICMApplication(), deps: [StatePropertiesService] },
