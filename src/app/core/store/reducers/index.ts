5ea82d0381356233a81b9c1b4befeca155a24412
Stefan Hauke <s.hauke@intershop.de>
2018-01-18 14:30:42 +0100

    ISREST-188: ngrx Router Store setup with Custom Router State Serializer

---
+++ src/app/core/store/reducers/index.ts
@@ -0,0 +1,34 @@
+import { ActivatedRouteSnapshot, Params, RouterStateSnapshot } from '@angular/router';
+import { routerReducer, RouterReducerState, RouterStateSerializer } from '@ngrx/router-store';
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
+
+export interface RouterStateUrl {
+  url: string;
+  queryParams: Params;
+  params: Params;
+}
+
+export interface State {
+  routerReducer: RouterReducerState<RouterStateUrl>;
+}
+
+export const reducers: ActionReducerMap<State> = {
+  routerReducer: routerReducer,
+};
+
+export const getRouterState = createFeatureSelector<RouterReducerState<RouterStateUrl>>('routerReducer');
+
+export class CustomSerializer implements RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    const { url } = routerState;
+    const { queryParams } = routerState.root;
+
+    let state: ActivatedRouteSnapshot = routerState.root;
+    while (state.firstChild) {
+      state = state.firstChild;
+    }
+    const { params } = state;
+
+    return { url, queryParams, params };
+  }
+}
