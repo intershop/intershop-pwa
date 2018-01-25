b4c3b33ae4f71b221b68305f047664a8ae6a91a3
Dobromir Simeonov <d.simeonov@intershop.de>
2018-01-25 17:00:35 +0100

    ISREST-234 - add implementation of profile settings page

--- src/app/account/pages/profile-settings-page/profile-settings-page.component.spec.ts
+++ src/app/account/pages/profile-settings-page/profile-settings-page.component.spec.ts
@@ -1,13 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { anyFunction, instance, mock, when } from 'ts-mockito';
+import { AccountLoginService } from '../../../core/services/account-login/account-login.service';
+import { MockComponent } from '../../../mocking/components/mock.component';
+import { CustomerFactory } from '../../../models/customer/customer.factory';
+import { CustomerData } from '../../../models/customer/customer.interface';
+import { Customer } from '../../../models/customer/customer.model';
 import { ProfileSettingsPageComponent } from './profile-settings-page.component';
 
 describe('Profile Settings Page Component', () => {
   let component: ProfileSettingsPageComponent;
   let fixture: ComponentFixture<ProfileSettingsPageComponent>;
+  const accountLoginServiceMock = mock(AccountLoginService);
+  const userData = {
+    'firstName': 'Patricia',
+    'lastName': 'Miller',
+    'title': '',
+    'credentials': {
+      'login': ''
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ProfileSettingsPageComponent]
+      declarations: [
+        ProfileSettingsPageComponent,
+        MockComponent({ selector: 'ish-breadcrumb', template: 'Breadcrumb Component', inputs: ['trailText'] })
+      ],
+      providers: [
+        { provide: AccountLoginService, useFactory: () => instance(accountLoginServiceMock) },
+      ],
+      imports: [
+        TranslateModule.forRoot()
+      ]
     })
       .compileComponents();
   }));
@@ -15,10 +40,12 @@ describe('Profile Settings Page Component', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProfileSettingsPageComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    when(accountLoginServiceMock.subscribe(anyFunction())).thenCall((callback: (d: Customer) => void) => callback(CustomerFactory.fromData(userData as CustomerData)));
   });
 
   it('should be created', () => {
+    expect(function() { fixture.detectChanges(); }).not.toThrow();
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
