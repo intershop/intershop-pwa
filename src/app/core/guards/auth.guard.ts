bdd9763aac3794d216403279a14b6000eb363bff
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-02-07 17:20:12 +0100

    ISREST-188: auth guard fetches state from store

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
-import { AccountLoginService } from '../services/account-login/account-login.service';
+import { CanActivate } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { map, take, tap, withLatestFrom } from 'rxjs/operators';
+import { getRouterURL, getUserAuthorized, Go, State } from '../store';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
   constructor(
-    private router: Router,
-    private accountLoginService: AccountLoginService
+    private store: Store<State>
   ) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (!this.accountLoginService.isAuthorized()) {
-      // not logged in so redirect to login page with the return url
-      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-      return false;
-    }
-    return true;
+  canActivate(): Observable<boolean> {
+    return this.store.select(getUserAuthorized).pipe(
+      take(1),
+      withLatestFrom(this.store.select(getRouterURL)),
+      tap(([authorized, url]) => {
+        if (!authorized) {
+          // not logged in so redirect to login page with the return url
+          this.store.dispatch(new Go({ path: ['/login'], query: { returnUrl: url } }));
+        }
+      }),
+      map(([authorized, url]) => authorized)
+    );
   }
 }
