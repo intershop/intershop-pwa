34d9863c3ce886b0f5b79bdea4ca4654599dc526
Ferdinand Malcher <ferdinand@malcher.media>
2018-02-07 16:34:30 +0100

    Move product list filter to separate (dumb) component

---
+++ src/app/shopping/components/product/product-list-filter/product-list-filter.component.ts
@@ -0,0 +1,60 @@
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ViewMode } from '../../../../models/types';
+import { SelectOption } from '../../../../shared/components/form-controls/select/select-option.interface';
+
+@Component({
+  selector: 'ish-product-list-filter',
+  templateUrl: './product-list-filter.component.html'
+})
+
+export class ProductListFilterComponent implements OnInit, OnChanges {
+
+  @Input() itemCount: number;
+  @Input() sortBy: string;
+  @Input() viewMode: ViewMode = 'list';
+  @Output() sortChange = new EventEmitter<string>();
+  @Output() viewModeChange = new EventEmitter<string>();
+
+  sortForm: FormControl;
+
+  sortOptions: SelectOption[] = [
+    { value: 'default', label: 'Default Sorting' },
+    { value: 'name-asc', label: 'Name asc' },
+    { value: 'name-desc', label: 'Name desc' },
+    { value: 'SalesRankUnitsIndex-desc', label: 'Topsellers' },
+    { value: 'ArrivalDate-desc', label: 'Newest Arrivals' },
+  ];
+
+  itemCountPluralMapping = {
+    '=0': 'No Items',
+    '=1': '1 List Item',
+    'other': '# List Items'
+  };
+
+
+  ngOnInit() {
+    this.sortForm = new FormControl(this.sortBy);
+    this.sortForm.valueChanges.subscribe(this.sortChange);
+  }
+
+
+  ngOnChanges(c: SimpleChanges) {
+    if (c.sortBy && this.sortForm) {
+      this.sortForm.setValue(this.sortBy, { emitEvent: false });
+    }
+  }
+
+  get listView() {
+    return this.viewMode === 'list';
+  }
+
+  get gridView() {
+    return this.viewMode === 'grid';
+  }
+
+  setViewMode(mode: ViewMode) {
+    this.viewMode = mode;
+    this.viewModeChange.emit(mode);
+  }
+}
