0570fd1d1a653af4f0548552a1c0095a69354df4
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-02-07 17:18:59 +0100

    ISREST-188: removing state handling in account login service

--- src/app/core/store/effects/login.effects.ts
+++ src/app/core/store/effects/login.effects.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { empty } from 'rxjs/observable/empty';
 import { of } from 'rxjs/observable/of';
 import { catchError, map, switchMap } from 'rxjs/operators';
 import { AccountLoginService } from '../../../core/services/account-login/account-login.service';
@@ -23,15 +22,6 @@ export class LoginEffects {
         map(customer => new loginActions.LoginUserSuccess(customer)),
         catchError(error => of(new loginActions.LoginUserFail(error)))
       );
-    })
-  );
-
-  @Effect({ dispatch: false })
-  logoutUser$ = this.actions$.pipe(
-    ofType(loginActions.LOGOUT_USER),
-    switchMap(() => {
-      this.accountLoginService.logout();
-      return empty();
     })
   );
 
