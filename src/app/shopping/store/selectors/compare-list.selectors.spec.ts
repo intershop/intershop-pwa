28c4ca1962ba3052644989edeb83fa2df20a3c30
Ferdinand Malcher <ferdinand@malcher.media>
2018-02-13 18:22:16 +0100

    Add tests for CompareList selectors

---
+++ src/app/shopping/store/selectors/compare-list.selectors.spec.ts
@@ -0,0 +1,31 @@
+import { CompareListState } from '../reducers/compare-list.reducer';
+import * as fromSelectors from './compare-list.selectors';
+
+describe('CompareListSelectors', () => {
+
+  describe('getCompareList', () => {
+    it('should return the SKU list when queried', () => {
+      const state: CompareListState = {
+        skus: ['123', '456']
+      };
+      const result = fromSelectors.getCompareList.projector(state);
+      const expected = state.skus;
+
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe('isInCompareList', () => {
+    it('should say that SKU is in the list if it is', () => {
+      const list = ['123', '456'];
+      const result = fromSelectors.isInCompareList('123').projector(list);
+      expect(result).toBe(true);
+    });
+
+    it('should say that SKU is not in the list if it isn\'t', () => {
+      const list = ['123', '456'];
+      const result = fromSelectors.isInCompareList('789').projector(list);
+      expect(result).toBe(false);
+    });
+  });
+});
