b254a7c4d28e7419846e617dd52570af28dd00cd
Ferdinand Malcher <ferdinand@malcher.media>
2018-02-14 17:35:05 +0100

    Update log operator to use correct type

--- src/app/dev-utils/operators.ts
+++ src/app/dev-utils/operators.ts
@@ -1,11 +1,12 @@
 import { Observable } from 'rxjs/Observable';
 import { delay, tap } from 'rxjs/operators';
 
-export const log = (message?: string) =>
-  tap(e => console.log(message || '', e));
-
-
 export const randomDelay = <T>(min = 1000, max = 10000) =>
   (source: Observable<T>) => source.pipe(
     delay(Math.floor(Math.random() * (max - min + 1)) + min)
+  );
+
+export const log = <T>(message?: string) =>
+  (source: Observable<T>) => source.pipe(
+    tap(e => console.log(message || '', e))
   );
