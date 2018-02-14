aed4418256b2f46b43af7561248907b3b52aa815
Ferdinand Malcher <ferdinand@malcher.media>
2018-02-14 11:26:22 +0100

    Do not load products for category when product is selected

--- src/app/shopping/store/effects/categories.effects.ts
+++ src/app/shopping/store/effects/categories.effects.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { of } from 'rxjs/observable/of';
-import { catchError, concatMap, filter, map, mergeMap, switchMap, take } from 'rxjs/operators';
+import { catchError, concatMap, filter, map, mergeMap, switchMap, take, withLatestFrom } from 'rxjs/operators';
 import { CategoriesService } from '../../../core/services/categories/categories.service';
 import { ProductsService } from '../../services/products/products.service';
 import * as categoriesReducers from '../../store/reducers/categories.reducer';
 import * as categoriesActions from '../actions/categories.actions';
 import * as productsActions from '../actions/products.actions';
 import * as categoriesSelectors from '../selectors/categories.selectors';
+import * as productsSelectors from '../selectors/products.selectors';
+import { log } from '../../../dev-utils/log.operator';
 
 @Injectable()
 export class CategoriesEffects {
@@ -53,8 +55,10 @@ export class CategoriesEffects {
 
   @Effect()
   loadProductsForCategory$ = this.store.select(categoriesSelectors.getSelectedCategory).pipe(
-    filter(c => c && c.hasOnlineProducts && !c.productSkus),
-    concatMap(c => this.productsService.getProductSkuListForCategory(c.uniqueId)),
+    withLatestFrom(this.store.select(productsSelectors.getSelectedProductId)),
+    filter(([c, selectedProductSku]) => !selectedProductSku),
+    filter(([c, sku]) => !sku && c && c.hasOnlineProducts && !c.productSkus),
+    concatMap(([c, sku]) => this.productsService.getProductSkuListForCategory(c.uniqueId)),
     switchMap(res => [
       new categoriesActions.SetProductSkusForCategory(res.categoryUniqueId, res.skus),
       ...res.skus.map(sku => new productsActions.LoadProduct(sku)),
