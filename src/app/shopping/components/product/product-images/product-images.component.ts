a5dbfba7e68b60778c9069708f2cdac4749a51cf
Stefan Hauke <s.hauke@intershop.de>
2018-02-14 18:09:17 +0100

    ISREST-206: review changes for product images component - disabled automatic cycling for product images - used component methods instead of logic in templates for setActiveSlide action and isActiveSlide check - consistently used ngClass for dynamically setting CSS classes - syncronized thumbnail indicator with carousel `[(activeSlide)]` - added ChangeDetectionStrategy.OnPush optimization

--- src/app/shopping/components/product/product-images/product-images.component.ts
+++ src/app/shopping/components/product/product-images/product-images.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Product } from '../../../../models/product/product.model';
 
 @Component({
   selector: 'ish-product-images',
-  templateUrl: './product-images.component.html'
+  templateUrl: './product-images.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class ProductImagesComponent {
+
   @Input() product: Product;
+
   activeSlide = 0;
+
+  setActiveSlide(slideIndex: number) {
+    console.log('setActiveSlide');
+    this.activeSlide = slideIndex;
+  }
+
+  isActiveSlide(slideIndex: number): boolean {
+    console.log('isActiveSlide');
+    return this.activeSlide === slideIndex;
+  }
+
 }
