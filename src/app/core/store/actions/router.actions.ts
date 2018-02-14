ef0c1079e8f112a64bfeff3b229a88e5d847e174
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-02-14 11:21:37 +0100

    ISREST-188: enum for router actions

--- src/app/core/store/actions/router.actions.ts
+++ src/app/core/store/actions/router.actions.ts
@@ -1,12 +1,14 @@
 import { NavigationExtras } from '@angular/router';
 import { Action } from '@ngrx/store';
 
-export const GO = '[Router] Go';
-export const BACK = '[Router] Back';
-export const FORWARD = '[Router] Forward';
+export enum RouterActionTypes {
+  Go = '[Router] Go',
+  Back = '[Router] Back',
+  Forward = '[Router] Forward',
+}
 
 export class Go implements Action {
-  readonly type = GO;
+  readonly type = RouterActionTypes.Go;
   constructor(
     public payload: {
       path: any[];
@@ -17,11 +19,11 @@ export class Go implements Action {
 }
 
 export class Back implements Action {
-  readonly type = BACK;
+  readonly type = RouterActionTypes.Back;
 }
 
 export class Forward implements Action {
-  readonly type = FORWARD;
+  readonly type = RouterActionTypes.Forward;
 }
 
-export type RouterAction = Go | Back | Forward;
+export type RouterActions = Go | Back | Forward;
