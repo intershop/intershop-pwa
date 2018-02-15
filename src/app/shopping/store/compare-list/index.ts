73129dc69581f8be75ad37b656c82715ac14f4a7
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-02-15 15:43:42 +0100

    Revert "removed "no-barrel-files" to support the suggested ngrx code style of Todd Motto that relies on index.ts files, removed outdated/removed tslint rules"
    This reverts commit 775f9e03647a5e763d6a6274463f4598627fb94d.
    
    Ignoring rule for store API

--- src/app/shopping/store/compare-list/index.ts
+++ src/app/shopping/store/compare-list/index.ts
@@ -1,3 +1,5 @@
+// tslint:disable no-barrel-files
+// API to access ngrx compare-list state
 export * from './compare-list.actions';
 export * from './compare-list.selectors';
 export { ShoppingState } from '../shopping.state';
--- src/app/shopping/store/products/index.ts
+++ src/app/shopping/store/products/index.ts
@@ -1,3 +1,5 @@
+// tslint:disable no-barrel-files
+// API to access ngrx products state
 export * from './products.actions';
 export * from './products.selectors';
 export { ShoppingState } from '../shopping.state';
