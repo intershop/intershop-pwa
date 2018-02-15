05ca7206b85ec7cbcbb2b67680e2a7640e97c3a7
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-02-15 17:20:43 +0100

    ISREST-188: added test for UserReducer

---
+++ src/app/core/store/user/user.reducer.spec.ts
@@ -0,0 +1,87 @@
+import { Customer } from '../../../models/customer/customer.model';
+import { LoginUser, LoginUserFail, LoginUserSuccess, LogoutUser } from './user.actions';
+import { initialState, userReducer } from './user.reducer';
+
+describe('User Reducer', () => {
+  const customer = {
+    id: 'dummy'
+  } as Customer;
+
+  describe('initialState', () => {
+    it('should not have a user when unmodified', () => {
+      expect(initialState.customer).toBeNull();
+    });
+
+    it('should not have an error when unmodified', () => {
+      expect(initialState.error).toBeFalsy();
+    });
+
+    it('should not be authorized when unmodified', () => {
+      expect(initialState.authorized).toBe(false);
+    });
+  });
+
+  describe('reducer', () => {
+    it('should return initial state when undefined state is supplied', () => {
+      const newState = userReducer(undefined, {} as any);
+
+      expect(newState).toEqual(initialState);
+    });
+
+    it('should return initial state when undefined action is supplied', () => {
+      const newState = userReducer(initialState, {} as any);
+
+      expect(newState).toEqual(initialState);
+    });
+  });
+
+  describe('Actions', () => {
+    it('should set initial state when LoginUser action is reduced', () => {
+      const newState = userReducer(initialState, new LoginUser({ userName: 'dummy' }));
+
+      expect(newState).toEqual(initialState);
+    });
+
+    it('should set initial when LoginUser action is reduced', () => {
+      const newState = userReducer(initialState, new LoginUser({ userName: 'dummy' }));
+
+      expect(newState).toEqual(initialState);
+    });
+
+    it('should set error when LoginUserFail action is reduced', () => {
+      const error = { message: 'error' } as any;
+      const newState = userReducer(initialState, new LoginUserFail(error));
+
+      expect(newState).toEqual({ ...initialState, error });
+    });
+
+    it('should set error when CreateUserFail action is reduced', () => {
+      const error = { message: 'error' } as any;
+      const newState = userReducer(initialState, new LoginUserFail(error));
+
+      expect(newState).toEqual({ ...initialState, error });
+    });
+
+    it('should set customer and authorized when LoginUserSuccess action is reduced', () => {
+      const newState = userReducer(initialState, new LoginUserSuccess(customer));
+
+      expect(newState).toEqual({ ...initialState, customer, authorized: true });
+    });
+
+    it('should unset authorized and customer when reducing LogoutUser', () => {
+      const oldState = { ...initialState, customer, authorized: true };
+
+      const newState = userReducer(oldState, new LogoutUser());
+
+      expect(newState).toEqual({ ...initialState, customer: null, authorized: false });
+    });
+
+    it('should unset authorized and customer when reducing LoginUser', () => {
+      const oldState = { ...initialState, customer, authorized: true };
+
+      const newState = userReducer(oldState, new LoginUser({ userName: 'dummy' }));
+
+      expect(newState).toEqual({ ...initialState, customer: null, authorized: false });
+    });
+  });
+});
