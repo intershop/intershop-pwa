ff588c26e68693ad5b2464357490085449049b56
Ferdinand Malcher <ferdinand@malcher.media>
2018-02-20 12:49:30 +0100

    Use select as pipeable operator in Effects

--- src/app/shopping/store/products/products.effects.ts
+++ src/app/shopping/store/products/products.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { of } from 'rxjs/observable/of';
 import { catchError, filter, map, mergeMap } from 'rxjs/operators';
 import { ProductsService } from '../../services/products/products.service';
@@ -30,7 +30,8 @@ export class ProductsEffects {
   );
 
   @Effect()
-  selectedProduct$ = this.store.select(productsSelectors.getSelectedProductId).pipe(
+  selectedProduct$ = this.store.pipe(
+    select(productsSelectors.getSelectedProductId),
     filter(id => !!id),
     map(id => new productsActions.LoadProduct(id)),
   );
