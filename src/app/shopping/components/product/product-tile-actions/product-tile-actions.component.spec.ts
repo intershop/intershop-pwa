d1b15edec795b742073570140b706613d64a22f6
Stefan Hauke <s.hauke@intershop.de>
2018-02-23 14:20:25 +0100

    ISREST-32: cleaned up tests and code of product tile actions component and container and aded addToCart dummy method

--- src/app/shopping/components/product/product-tile-actions/product-tile-actions.component.spec.ts
+++ src/app/shopping/components/product/product-tile-actions/product-tile-actions.component.spec.ts
@@ -1,25 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { TranslateModule } from '@ngx-translate/core';
 import { ProductTileActionsComponent } from './product-tile-actions.component';
 
-describe('ProductTileActionsComponent', () => {
+describe('Product Tile Actions Component', () => {
   let component: ProductTileActionsComponent;
   let fixture: ComponentFixture<ProductTileActionsComponent>;
+  let element: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ProductTileActionsComponent]
-    })
-      .compileComponents();
+      imports: [
+        TranslateModule.forRoot()
+      ],
+      declarations: [
+        ProductTileActionsComponent
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductTileActionsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    element = fixture.nativeElement;
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+    expect(element).toBeTruthy();
+    expect(function() { fixture.detectChanges(); }).not.toThrow();
   });
 });
