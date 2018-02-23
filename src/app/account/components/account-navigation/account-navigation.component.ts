ef00d7661f70ee01d000c1979bc10e644806ede2
Radoslav Aladzhov <r.aladzhov@intershop.com>
2018-02-23 18:22:03 +0200

    ISREST-238: Implemented mobile behavior, moved navigation logic in the component

--- src/app/account/components/account-navigation/account-navigation.component.ts
+++ src/app/account/components/account-navigation/account-navigation.component.ts
@@ -1,8 +1,42 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+const MOBILE_VIEW_WIDTH = 992;
 
 @Component({
   selector: 'ish-account-navigation',
   templateUrl: './account-navigation.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AccountNavigationComponent {}
+export class AccountNavigationComponent implements OnInit {
+  isMobileView = false;
+  currentPath: string;
+
+  /**
+   * Manages the Account Navigation items.
+   */
+  navigationItems = [
+    { link: '/account/overview', localizationKey: 'account.my_account.link' },
+    { link: '/account/orders', localizationKey: 'account.order_history.link' },
+    { link: '/account/profile', localizationKey: 'account.profile.link' },
+    { link: '/logout', localizationKey: 'account.navigation.logout.link' },
+  ];
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.isMobileView = window.innerWidth < MOBILE_VIEW_WIDTH;
+    this.currentPath = location.pathname;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  mobileViewHandler(event) {
+    this.isMobileView = event.target.innerWidth < MOBILE_VIEW_WIDTH;
+  }
+
+  navigateTo(link) {
+    if (link != null) {
+      this.router.navigate([link]);
+    }
+  }
+}
