710d3fa904a889ea53d9d688c79a7cb1687e3924
Stefan Hauke <s.hauke@intershop.de>
2018-02-23 14:34:44 +0100

    ISREST-32: reworked ProductRowComponent to use a ProductRowActionsContainer for addToCart - cleaned up tests - new components: ProductRowActionsContainer, ProductRowActionsComponent

---
+++ src/app/shopping/containers/product-row-actions/product-row-actions.container.spec.ts
@@ -0,0 +1,31 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent } from '../../../dev-utils/mock.component';
+import { ProductRowActionsContainerComponent } from './product-row-actions.container';
+
+describe('Product Row Actions Container', () => {
+  let component: ProductRowActionsContainerComponent;
+  let fixture: ComponentFixture<ProductRowActionsContainerComponent>;
+  let element: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        ProductRowActionsContainerComponent,
+        MockComponent({ selector: 'ish-product-row-actions', template: 'Product Row Actions Component' }),
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductRowActionsContainerComponent);
+    component = fixture.componentInstance;
+    component = fixture.componentInstance;
+    element = fixture.nativeElement;
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+    expect(element).toBeTruthy();
+    expect(function() { fixture.detectChanges(); }).not.toThrow();
+  });
+});
