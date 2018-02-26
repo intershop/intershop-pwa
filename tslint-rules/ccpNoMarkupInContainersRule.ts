c4c361b3a793efc15eb28323ba5d9bff533d6982
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-02-26 12:21:21 +0100

    added tslint rules for container-component-pattern

---
+++ tslint-rules/ccpNoMarkupInContainersRule.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+import * as Lint from 'tslint';
+import { SourceFile } from 'typescript';
+
+const MESSAGE = 'Container templates should not contain markup.';
+
+class CCPNoMarkupInContainersWalker extends Lint.RuleWalker {
+
+  patterns: string[];
+
+  constructor(sourceFile: SourceFile, options: Lint.IOptions) {
+    super(sourceFile, options);
+    this.patterns = options['ruleArguments'][0]['patterns'];
+  }
+
+  public visitSourceFile(sourceFile: SourceFile) {
+    if (sourceFile.fileName.match(/.*\/containers\/(?!.*(routes|module|spec).ts$).*.ts/)) {
+      // console.log('####' + sourceFile.fileName);
+
+      const fileName = sourceFile.fileName;
+      const templateName = fileName.substring(0, fileName.length - 2) + 'html';
+
+      try {
+        const template = fs.readFileSync(templateName, 'utf8');
+        this.patterns.forEach(pattern => {
+          if (template.search(pattern) >= 0) {
+            const message = `${MESSAGE} (found '${pattern}')`;
+            this.addFailureAtNode(sourceFile, message);
+          }
+        });
+      } catch (err) {}
+    }
+  }
+}
+
+/**
+ * Implementation of the ccp-no-markup-in-containers rule.
+ */
+export class Rule extends Lint.Rules.AbstractRule {
+
+  public apply(sourceFile: SourceFile): Lint.RuleFailure[] {
+    return this.applyWithWalker(new CCPNoMarkupInContainersWalker(sourceFile, this.getOptions()));
+  }
+}
