83abd58d41b366641acf6c310611589d0370ac58
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-02-26 13:36:53 +0100

    update usage of rxjs-tslint-rules, replace dollarObservable rule by official

--- src/app/core/directives/style-wrapper.directive.ts
+++ src/app/core/directives/style-wrapper.directive.ts
@@ -1,8 +1,6 @@
 import { Directive, HostBinding, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 /**
  * The <ishStyleWrapper> directive dynamicaly adds a context specific CSS class based on a setting in the route handling, e.g. 'homepage' or 'errorpage'
@@ -21,18 +19,18 @@ export class StyleWrapperDirective implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.router.events
-      .filter((event) => event instanceof NavigationEnd)
-      .map(() => this.route)
-      .map((route) => {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.route),
+      map((route) => {
         while (route.firstChild) { route = route.firstChild; }
         return route;
-      })
-      .filter((route) => route.outlet === 'primary')
-      .mergeMap((route) => route.data)
-      .subscribe((event) => {
-        this.addClass(event['className'] || '');
-      });
+      }),
+      filter((route) => route.outlet === 'primary'),
+      mergeMap((route) => route.data)
+    ).subscribe((event) => {
+      this.addClass(event['className'] || '');
+    });
   }
 
   private addClass(className: string) {
