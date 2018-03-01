e60640f610d6326f32c05dcfbec2f2a0eeab0020
Ferdinand Malcher <ferdinand@malcher.media>
2018-03-01 14:11:03 +0100

    Remove SimpleRegistration

--- src/app/core/configurations/injection-keys.ts
+++ src/app/core/configurations/injection-keys.ts
@@ -4,6 +4,5 @@ import { Locale } from '../../models/locale/locale.interface';
 export const NEED_MOCK = new InjectionToken<boolean>('needMock');
 export const MUST_MOCK_PATHS = new InjectionToken<string[]>('mustMockPaths');
 export const AVAILABLE_LOCALES = new InjectionToken<Locale[]>('availableLocales');
-export const USE_SIMPLE_ACCOUNT = new InjectionToken<boolean>('useSimpleAccount');
 export const USER_REGISTRATION_LOGIN_TYPE = new InjectionToken<string>('userRegistrationLoginType');
 export const USER_REGISTRATION_SUBSCRIBE_TO_NEWSLETTER = new InjectionToken<boolean>('userRegistrationSubscribeToNewsletter');
