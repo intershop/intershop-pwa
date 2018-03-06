dc7228975a6be5cd509b12452d45d12138501a2d
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-03-06 14:56:26 +0100

    using category.uniqueId instead of generateCategoryRoute for links to categories

--- src/app/core/components/header/header-navigation/header-navigation.component.spec.ts
+++ src/app/core/components/header/header-navigation/header-navigation.component.spec.ts
@@ -1,26 +1,16 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs/observable/of';
-import { anything, instance, mock, when } from 'ts-mockito';
-import { CategoriesService } from '../../../services/categories/categories.service';
 import { HeaderNavigationComponent } from './header-navigation.component';
 
 describe('Header Navigation Component', () => {
   let fixture: ComponentFixture<HeaderNavigationComponent>;
   let component: HeaderNavigationComponent;
   let element: HTMLElement;
-  let categoriesServiceMock: CategoriesService;
 
   beforeEach(async(() => {
-    categoriesServiceMock = mock(CategoriesService);
-    when(categoriesServiceMock.getTopLevelCategories(anything())).thenReturn(of([]));
-
     TestBed.configureTestingModule({
       declarations: [
         HeaderNavigationComponent
-      ],
-      providers: [
-        { provide: CategoriesService, useFactory: () => instance(categoriesServiceMock) }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
