dd306907a6179c472bd0fc207eeeb845114eb4ee
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-03-07 12:29:41 +0100

    using @angular/common/http/testing interface for api.service.spec

--- src/app/core/services/api.service.spec.ts
+++ src/app/core/services/api.service.spec.ts
@@ -1,65 +1,86 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { Store } from '@ngrx/store';
+import { Action, Store } from '@ngrx/store';
 import { empty } from 'rxjs/observable/empty';
-import { of } from 'rxjs/observable/of';
-import { anything, instance, mock, verify, when } from 'ts-mockito/lib/ts-mockito';
+import { anything, capture, instance, mock, verify, when } from 'ts-mockito/lib/ts-mockito';
+import { ErrorActionTypes, InternalServerError } from '../store/error';
 import { CoreState } from '../store/user';
 import { ApiService } from './api.service';
 import { ApiServiceErrorHandler } from './api.service.errorhandler';
 import { ICM_SERVER_URL, REST_ENDPOINT } from './state-transfer/factories';
 
 describe('ApiService', () => {
-  const BASE_URL = 'http://www.example.org/';
-  let httpClientMock: HttpClient;
+  const BASE_URL = 'http://www.example.org';
   let apiService: ApiService;
-  let storeMock: Store<CoreState>;
+  let storeMock$: Store<CoreState>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    httpClientMock = mock(HttpClient);
-    storeMock = mock(Store);
-    when(storeMock.pipe(anything())).thenReturn(empty());
+    storeMock$ = mock(Store);
+    when(storeMock$.pipe(anything())).thenReturn(empty());
 
     TestBed.configureTestingModule({
+      imports: [
+        // https://angular.io/guide/http#testing-http-requests
+        HttpClientTestingModule
+      ],
       providers: [
-        { provide: HttpClient, useFactory: () => instance(httpClientMock) },
         { provide: REST_ENDPOINT, useValue: BASE_URL },
         { provide: ICM_SERVER_URL, useValue: BASE_URL },
-        { provide: Store, useFactory: () => instance(storeMock) },
-        { provide: ApiServiceErrorHandler },
+        { provide: Store, useFactory: () => instance(storeMock$) },
+        ApiServiceErrorHandler,
         ApiService
       ]
     });
 
     apiService = TestBed.get(ApiService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should call the httpClient.get method when apiService.get method is called.', () => {
-    when(httpClientMock.get(anything(), anything())).thenReturn(of(new ArrayBuffer(3)));
-    verify(httpClientMock.get(anything(), anything())).never();
-    apiService.get('');
-    verify(httpClientMock.get(anything(), anything())).once();
+    apiService.get('data').subscribe(data => expect(data).toBeTruthy());
+
+    const req = httpTestingController.expectOne(`${BASE_URL}/data`);
+    expect(req.request.method).toEqual('GET');
+  });
+
+  it('should create Error Action if  httpClient.get throws Error.', () => {
+    const statusText = 'ERROAAR';
+    apiService.get('data').subscribe(data => expect(data).toBeTruthy());
+    const req = httpTestingController.expectOne(`${BASE_URL}/data`);
+
+    req.flush('err', { status: 500, statusText });
+
+    verify(storeMock$.dispatch(anything())).once();
+    const [action] = capture(storeMock$.dispatch).last();
+    expect((<Action>action).type).toEqual(ErrorActionTypes.InternalServerError);
+    expect((<InternalServerError>action).error.statusText).toEqual(statusText);
   });
 
   it('should call the httpClient.put method when apiService.put method is called.', () => {
-    when(httpClientMock.put(anything(), anything(), anything())).thenReturn(of(new ArrayBuffer(3)));
-    verify(httpClientMock.put(anything(), anything(), anything())).never();
-    apiService.put('');
-    verify(httpClientMock.put(anything(), anything(), anything())).once();
+    apiService.put('data').subscribe(data => expect(data).toBeTruthy());
+
+    const req = httpTestingController.expectOne(`${BASE_URL}/data`);
+    expect(req.request.method).toEqual('PUT');
   });
 
 
   it('should call the httpClient.post method when apiService.post method is called.', () => {
-    when(httpClientMock.post(anything(), anything(), anything())).thenReturn(of(new ArrayBuffer(3)));
-    verify(httpClientMock.post(anything(), anything(), anything())).never();
-    apiService.post('');
-    verify(httpClientMock.post(anything(), anything(), anything())).once();
+    apiService.post('data').subscribe(data => expect(data).toBeTruthy());
+
+    const req = httpTestingController.expectOne(`${BASE_URL}/data`);
+    expect(req.request.method).toEqual('POST');
   });
 
   it('should call the httpClient.delete method when apiService.delete method is called.', () => {
-    when(httpClientMock.delete(anything())).thenReturn(of(new ArrayBuffer(3)));
-    verify(httpClientMock.delete(anything())).never();
-    apiService.delete('');
-    verify(httpClientMock.delete(anything())).once();
+    apiService.delete('data').subscribe(data => expect(data).toBeTruthy());
+
+    const req = httpTestingController.expectOne(`${BASE_URL}/data`);
+    expect(req.request.method).toEqual('DELETE');
   });
 });
