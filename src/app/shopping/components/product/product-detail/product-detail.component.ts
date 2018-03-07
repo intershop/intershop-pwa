bfb9999057a320e18e2741017cfd866d82bfa16a
Stefan Hauke <s.hauke@intershop.de>
2018-03-07 14:58:16 +0100

    ISREST-216: added manufacturer/brand information and SKU to product detail component and cleaned up no longer needed comments

--- src/app/shopping/components/product/product-detail/product-detail.component.ts
+++ src/app/shopping/components/product/product-detail/product-detail.component.ts
@@ -5,6 +5,7 @@ import { Product } from '../../../../models/product/product.model';
   selector: 'ish-product-detail',
   templateUrl: './product-detail.component.html'
 })
+
 export class ProductDetailComponent {
 
   @Input() product: Product;
