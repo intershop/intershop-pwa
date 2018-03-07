a396c26d3de4d2306227521cbb4b968be4e3647a
Sebastian Glaß <s.glass@intershop.de>
2018-03-07 08:19:21 +0100

    Put errorGroup to enums

--- src/app/core/store/error/error.reducer.ts
+++ src/app/core/store/error/error.reducer.ts
@@ -1,5 +1,5 @@
-import { ROUTER_NAVIGATION, RouterAction } from '@ngrx/router-store';
-import { HttpError } from './error.actions';
+import { RouterAction } from '@ngrx/router-store';
+import { ErrorGroupTypes, HttpError } from './error.actions';
 export interface ErrorState {
   current: Error | null;
   type: String;
@@ -16,19 +16,9 @@ export function generalErrorReducer(
 ): ErrorState {
   const httpAction = action as HttpError;
   switch (httpAction.errorGroup) {
-    case '5XX': {
+    case ErrorGroupTypes.Http5XXError: {
       return { ...state, current: httpAction.error, type: httpAction.type };
-    }
-  }
-  if (action.type === ROUTER_NAVIGATION) {
-    const routerAction = action as RouterAction<any>;
-    if (routerAction.payload.routerState.url !== '/error') {
-      // TODO: something user friendly
-      return initialState;
     }
   }
   return state;
 }
-
-
-
