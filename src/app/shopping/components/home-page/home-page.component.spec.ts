05616fa7bdf92a3af68ec08fd7a2d010f2737106
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-03-08 18:24:05 +0100

    container-component-pattern for home-page

---
+++ src/app/shopping/components/home-page/home-page.component.spec.ts
@@ -0,0 +1,28 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { HomePageComponent } from './home-page.component';
+
+describe('Home Page Component', () => {
+  let component: HomePageComponent;
+  let fixture: ComponentFixture<HomePageComponent>;
+  let element: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [CarouselModule.forRoot()],
+      declarations: [HomePageComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomePageComponent);
+    component = fixture.componentInstance;
+    element = fixture.nativeElement;
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+    expect(element).toBeTruthy();
+    expect(() => fixture.detectChanges()).not.toThrow();
+  });
+});
