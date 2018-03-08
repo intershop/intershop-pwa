6b3719bd1e35cb6fb417332e85a5dca5ef8a9e4e
Intershop <support@intershop.de>
2018-03-08 18:56:16 +0530

    ISREST-225 Added Product Quantity Component

---
+++ src/app/shopping/components/product/product-quantity/product-quantity.component.ts
@@ -0,0 +1,43 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+
+import { Product } from '../../../../models/product/product.model';
+import { SelectOption } from '../../../../shared/components/form-controls/select';
+
+@Component({
+  selector: 'ish-product-quantity',
+  templateUrl: './product-quantity.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductQuantityComponent implements OnInit {
+  @Input() product: Product;
+  @Input() parentForm: FormGroup;
+  @Input() controlName: string;
+  @Input() type?: string;
+  @Input() class?: string;
+
+  readonly selectType = 'select';
+  isTypeSelect: boolean;
+  // TODO: to be removed when rest reaponse is available for max quantity
+  maxOrderQuantityOptions = 10;
+
+  ngOnInit() {
+    this.parentForm.addControl(this.controlName,
+      new FormControl(this.product.minOrderQuantity, this.getValidations()));
+  }
+
+  getValidations(): ValidatorFn {
+    if (this.type !== this.selectType) {
+      return Validators.compose([Validators.required,
+      Validators.min(this.product.minOrderQuantity), Validators.pattern('^[0-9]*')]);
+    }
+  }
+
+  get quantityOptions(): SelectOption[] {
+    return Array.from({ length: this.maxOrderQuantityOptions },
+      (v, k) => ({
+        label: (this.product.minOrderQuantity + k).toString(),
+        value: (this.product.minOrderQuantity + k).toString()
+      } as SelectOption));
+  }
+}
