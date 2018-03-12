b626eb82f51b670b19d550a30fd8640397df36f0
Intershop <support@intershop.de>
2018-03-12 11:24:48 +0530

    ISREST-247 Added Product Detail Actions Component

---
+++ src/app/shopping/components/product/product-detail-actions/product-detail-actions.component.spec.ts
@@ -0,0 +1,78 @@
+import { CommonModule, Location } from '@angular/common';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import * as using from 'jasmine-data-provider';
+import { instance, mock } from 'ts-mockito/lib/ts-mockito';
+import { Product } from '../../../../models/product/product.model';
+import { ProductDetailActionsComponent } from './product-detail-actions.component';
+
+describe('Product Detail Actions Component', () => {
+  let component: ProductDetailActionsComponent;
+  let fixture: ComponentFixture<ProductDetailActionsComponent>;
+  let product: Product;
+  let translate: TranslateService;
+  let element: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        TranslateModule.forRoot(),
+      ],
+      providers: [
+        TranslateService,
+        { provide: Location, useFactory: () => instance(mock(Location)) },
+      ],
+      declarations: [ProductDetailActionsComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductDetailActionsComponent);
+    component = fixture.componentInstance;
+    translate = TestBed.get(TranslateService);
+    translate.setDefaultLang('en');
+    translate.use('en');
+    product = new Product('sku');
+    product.availability = true;
+    element = fixture.nativeElement;
+    component.product = product;
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+    expect(element).toBeTruthy();
+    expect(function() { fixture.detectChanges(); }).not.toThrow();
+  });
+
+  describe('link rendering', () => {
+    function dataProvider() {
+      return [
+        { className: '.glyphicon-send', linkName: 'email to friend', localeKey: 'product.email_a_friend.link', localeValue: 'Email a friend' },
+        { className: '.glyphicon-print', linkName: 'print page', localeKey: 'product.print_page.link', localeValue: 'Print Page' },
+        { className: '.glyphicon-equalizer', linkName: 'compare', localeKey: 'product.compare.link', localeValue: 'Compare' }
+      ];
+    }
+    using(dataProvider, (dataSlice) => {
+      it(`should show "${dataSlice.linkName}" link when product inforamtion is available`, () => {
+        translate.set(dataSlice.localeKey, dataSlice.localeValue);
+        fixture.detectChanges();
+        expect(element.querySelector(dataSlice.className).nextElementSibling.textContent).toContain(dataSlice.localeValue);
+      });
+    });
+
+    it('should not show "compare" link when product inforamtion is available and productMaster = true', () => {
+      component.product.productMaster = true;
+      fixture.detectChanges();
+      expect(element.querySelector('a[data-testing-id=\'compare-sku\']')).toBeFalsy();
+    });
+  });
+
+  it('should emit "product to compare" event when compare link is clicked', () => {
+    component.productToCompare.subscribe((data) =>
+      expect(data).toBeFalsy());
+    fixture.detectChanges();
+    (<HTMLElement>element.querySelector('a[data-testing-id=\'compare-sku\']')).click();
+  });
+});
