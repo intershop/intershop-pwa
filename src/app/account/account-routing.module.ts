c4a0841684af56648297dcc77c6f745bd8e7a42b
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-03-15 13:26:52 +0100

    moving lazy loaded subpages to feature routing

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -7,6 +7,14 @@ const routes: Routes = [
     path: 'account',
     loadChildren: 'app/account/containers/account-page/account-page.module#AccountPageModule',
     canActivate: [AuthGuard]
+  },
+  {
+    path: 'account/profile',
+    loadChildren: 'app/account/containers/profile-settings-page/profile-settings-page.module#ProfileSettingsPageModule'
+  },
+  {
+    path: 'account/orders',
+    loadChildren: 'app/account/containers/order-history-page/order-history-page.module#OrderHistoryPageModule'
   }
 ];
 
