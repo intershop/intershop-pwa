eb0651b6cbd2fec89246f188e8a46f0a9ae40916
Intershop <support@intershop.de>
2018-03-16 19:35:52 +0530

    ISREST-254 Refactor Header Navigation to use Component Property for Toggling

--- src/app/core/components/header/header-navigation/header-navigation.component.ts
+++ src/app/core/components/header/header-navigation/header-navigation.component.ts
@@ -10,10 +10,22 @@ export class HeaderNavigationComponent {
 
   @Input() categories: Category[];
 
+  openedCategories = [];
+
   subMenuShow(submenu) {
     submenu.classList.add('hover');
   }
+
   subMenuHide(submenu) {
     submenu.classList.remove('hover');
+  }
+
+  isOpened(category: Category): boolean {
+    return this.openedCategories.includes(category.uniqueId);
+  }
+
+  toggleOpen(category: Category): void {
+    const index = this.openedCategories.findIndex(id => id === category.uniqueId);
+    (index > -1) ? this.openedCategories.splice(index, 1) : this.openedCategories.push(category.uniqueId);
   }
 }
