aeb040420feb3408fdab5b8055f300c98830b0e3
Stefan Hauke <s.hauke@intershop.de>
2018-03-28 15:10:40 +0200

    moved error-page component into shopping/components/error folder

--- src/app/shopping/containers/error-page/error-page.module.ts
+++ src/app/shopping/containers/error-page/error-page.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../../shared/shared.module';
-import { ErrorPageComponent } from '../../components/error-page/error-page.component';
+import { ErrorPageComponent } from '../../components/error/error-page/error-page.component';
 import { ErrorPageContainerComponent } from './error-page.container';
 import { errorPageRoutes } from './error-page.routes';
 
