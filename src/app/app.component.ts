ab0910778e426447c9766d49472a11dd80d16c26
Ferdinand Malcher <ferdinand@malcher.media>
2018-03-28 14:56:58 +0200

    Move storage listener to dedicated service
    and don't pollute the AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { fromEvent } from 'rxjs/observable/fromEvent';
-import { map } from 'rxjs/operators';
-import { CoreState } from './core/store/core.state';
-import { Storage } from './core/store/local-storage-sync/local-storage-sync.reducer';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'ish-root',
@@ -11,18 +6,9 @@ import { Storage } from './core/store/local-storage-sync/local-storage-sync.redu
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
 
   // DEBUG: prints the configured routes for routing analysis
   // constructor(private router: Router) { console.log('ROUTES: ', this.router.config); }
 
-  constructor(
-    private store: Store<CoreState>,
-  ) { }
-
-  ngOnInit() {
-    fromEvent(window, 'storage').pipe(
-      map((e: StorageEvent) => new Storage(e.key))
-    ).subscribe(this.store);
-  }
 }
