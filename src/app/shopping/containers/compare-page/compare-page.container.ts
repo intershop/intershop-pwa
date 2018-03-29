c5d8ca4eac3e3e9995ad8f3388af5c0698df13e3
Intershop <support@intershop.de>
2018-03-29 20:08:50 +0530

    feature/ISREST-253 Added Product compare list component

--- src/app/shopping/containers/compare-page/compare-page.container.ts
+++ src/app/shopping/containers/compare-page/compare-page.container.ts
@@ -1,24 +1,41 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-import { CoreState } from '../../../core/store/core.state';
+import { Pagination } from '../../../models/pagination/pagination.interface';
 import { Product } from '../../../models/product/product.model';
-import { getCompareProducts } from '../../store/compare';
+import { getCompareProductsByCurrentPageAndItemsPerPage, getCompareProductsCount } from '../../store/compare';
+import * as fromCompare from '../../store/compare/compare.actions';
+import { ShoppingState } from '../../store/shopping.state';
 
 @Component({
   selector: 'ish-compare-page-container',
   templateUrl: './compare-page.container.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ComparePageContainerComponent implements OnInit {
-
   compareProducts$: Observable<Product[]>;
-
+  compareProductsCount$: Observable<Number>;
+  itemsPerPage = 2;
   constructor(
-    private store: Store<CoreState>
+    private store: Store<ShoppingState>
   ) { }
 
   ngOnInit() {
-    this.compareProducts$ = this.store.pipe(select(getCompareProducts));
+    const initialPage = 1;
+    this.compareProducts$ = this.store.pipe(select(getCompareProductsByCurrentPageAndItemsPerPage(initialPage, this.itemsPerPage)));
+    this.compareProductsCount$ = this.store.pipe(select(getCompareProductsCount));
+  }
+
+  onPageChanged(event: Pagination): void {
+    this.compareProducts$ = this.store.pipe(select(getCompareProductsByCurrentPageAndItemsPerPage(event.currentPage, event.itemsPerPage)));
+  }
+
+  addToCart({ sku, quantity }) {
+    console.log('[ComparePageContainer] Add to Cart: SKU: ' + sku + ', Quantity: ' + quantity);
+    // TODO: dispatch add to cart action // this.store.dispatch(new AddToCart(sku, quantity));
+  }
+
+  removeProductCompare(sku: string) {
+    this.store.dispatch(new fromCompare.RemoveFromCompare(sku));
   }
 }
