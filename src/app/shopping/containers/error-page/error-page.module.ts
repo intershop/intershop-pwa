36050a31018aa81f4490ccedafe26f5bcb18e984
Sebastian Glaß <s.glass@intershop.de>
2018-04-03 12:17:12 +0200

    moved server-error-page to new error folder

--- src/app/shopping/containers/error-page/error-page.module.ts
+++ src/app/shopping/containers/error-page/error-page.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../../shared/shared.module';
 import { ErrorPageComponent } from '../../components/error/error-page/error-page.component';
-import { ServerErrorPageComponent } from '../../components/server-error-page/server-error-page.component';
+import { ServerErrorPageComponent } from '../../components/error/server-error-page/server-error-page.component';
 import { ErrorPageContainerComponent } from './error-page.container';
 import { errorPageRoutes } from './error-page.routes';
 
