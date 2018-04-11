f8f8fbde8be775614ce990829fe04b9dab77c9b0
Derk Sacklowski <derk.sacklowski@flyacts.com>
2018-04-11 17:10:51 +0200

    ISREST-260 - renamed mini-cart, added line items
    - renamed mini-cart container and component to mini-basket
    - added line item data to cart
    - added line item data matching products to shopping state
    - make the commit Prettier (result of 'npm run format' with Prettier formatting)

--- src/app/checkout/store/basket/basket.actions.ts
+++ src/app/checkout/store/basket/basket.actions.ts
@@ -1,11 +1,15 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Action } from '@ngrx/store';
+import { BasketItem } from '../../../models/basket/basket-item.model';
 import { Basket } from '../../../models/basket/basket.model';
 
 export enum BasketActionTypes {
   LoadBasket = '[Checkout] Load Basket',
   LoadBasketFail = '[Checkout] Load Basket Fail',
   LoadBasketSuccess = '[Checkout] Load Basket Success',
+  LoadBasketItems = '[Chekout] Load Basket Items',
+  LoadBasketItemsFail = '[Chekout] Load Basket Items Fail',
+  LoadBasketItemsSuccess = '[Chekout] Load Basket Items Success',
   AddItemToBasket = '[Checkout] Add Item To Basket',
   AddItemToBasketFail = '[Checkout] Add Item To Basket Fail',
   AddItemToBasketSuccess = '[Checkout] Add Item To Basket Success',
@@ -26,6 +30,21 @@ export class LoadBasketSuccess implements Action {
   constructor(public payload: Basket) {}
 }
 
+export class LoadBasketItems implements Action {
+  readonly type = BasketActionTypes.LoadBasketItems;
+  constructor(public payload: string) {}
+}
+
+export class LoadBasketItemsFail implements Action {
+  readonly type = BasketActionTypes.LoadBasketItemsFail;
+  constructor(public payload: HttpErrorResponse) {}
+}
+
+export class LoadBasketItemsSuccess implements Action {
+  readonly type = BasketActionTypes.LoadBasketItemsSuccess;
+  constructor(public payload: BasketItem[]) {}
+}
+
 export class AddItemToBasket implements Action {
   readonly type = BasketActionTypes.AddItemToBasket;
   constructor(public payload: { sku: string; quantity: number }) {}
@@ -44,6 +63,9 @@ export type BasketAction =
   | LoadBasket
   | LoadBasketFail
   | LoadBasketSuccess
+  | LoadBasketItems
+  | LoadBasketItemsFail
+  | LoadBasketItemsSuccess
   | AddItemToBasket
   | AddItemToBasketFail
   | AddItemToBasketSuccess;
