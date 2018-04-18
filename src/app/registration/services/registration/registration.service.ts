f1e70795a8efe80389bf7d195d96f971ef027e70
Stefan Hauke <s.hauke@intershop.de>
2018-04-18 18:04:43 +0200

    added RegistrationService code documentation

--- src/app/registration/services/registration/registration.service.ts
+++ src/app/registration/services/registration/registration.service.ts
@@ -6,10 +6,18 @@ import { ApiService } from '../../../core/services/api.service';
 import { LoginCredentials } from '../../../models/credentials/credentials.model';
 import { Customer } from '../../../models/customer/customer.model';
 
+/**
+ * The Registration Service handles the registration related interaction with the 'customers' REST API.
+ */
 @Injectable()
 export class RegistrationService {
   constructor(private apiService: ApiService) {}
 
+  /**
+   * Sign in an existing user with the given login credentials (login, password).
+   * @param loginCredentials  The users login credentials {login: 'foo', password. 'bar'}.
+   * @returns                 The logged in user data.
+   */
   signinUser(loginCredentials: LoginCredentials): Observable<Customer> {
     const headers = new HttpHeaders().set(
       'Authorization',
@@ -18,6 +26,11 @@ export class RegistrationService {
     return this.apiService.get<Customer>('customers/-', null, headers);
   }
 
+  /**
+   * Create a new user with the given customer data.
+   * @param newCustomer  The user data to create a new user.
+   * @returns            The created user data.
+   */
   createUser(newCustomer: Customer): Observable<Customer> {
     return this.apiService.post<Customer>('customers', newCustomer).pipe(
       // TODO:see #IS-22750 - user should actually be logged in after registration
