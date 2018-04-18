12c8178a27c1f315f468b844a906506f75ffbc0a
Stefan Hauke <s.hauke@intershop.de>
2018-04-18 12:30:16 +0200

    added CategoryHelper getCategoryPath(uniqueId: string) method to handle the category uniqueId to category REST API path convertion and introduced a uniqueIdSeparator as a central static property that could be changed

--- src/app/models/category/category.helper.ts
+++ src/app/models/category/category.helper.ts
@@ -1,14 +1,29 @@
 import { Category } from './category.model';
 
 export class CategoryHelper {
+  static uniqueIdSeparator = '.';
+
   /**
    * @returns          True if the categories are equal, false if not.
    *                   'equal' means
    *                   - both categories are defined
-   *                   - the id of the categories is the same
+   *                   - the uniqueId of the categories is the same
    */
   static equals(self: Category, category: Category): boolean {
     return !!self && !!category && self.uniqueId === category.uniqueId;
+  }
+
+  /**
+   * Converts a given uniqueId of a category in a REST API category path.
+   * @example
+   * 'A.B.C' -> 'A/B/C'
+   */
+  static getCategoryPath(uniqueId: string): string {
+    if (!uniqueId) {
+      return undefined;
+    }
+    const regEx = new RegExp(`\\${CategoryHelper.uniqueIdSeparator}`, 'g');
+    return uniqueId.replace(regEx, '/');
   }
 
   /**
