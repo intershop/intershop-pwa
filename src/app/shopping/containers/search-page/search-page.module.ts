25735f9483294bd4c81b1feaf1f74df2ca4ed5c1
Susanne Schneider <s.schneider@intershop.de>
2018-04-18 14:01:00 +0200

    ISREST-240: rename SearchPageComponent to SearchResultComponent, documentation for compodoc

--- src/app/shopping/containers/search-page/search-page.module.ts
+++ src/app/shopping/containers/search-page/search-page.module.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 import { SharedSearchModule } from '../../../shared/shared-search.module';
 import { SharedModule } from '../../../shared/shared.module';
 import { SearchNoResultComponent } from '../../components/search/search-no-result/search-no-result.component';
-import { SearchPageComponent } from '../../components/search/search-page/search-page.component';
+import { SearchResultComponent } from '../../components/search/search-result/search-result.component';
 import { ProductsService } from '../../services/products/products.service';
 import { ShoppingSharedModule } from '../../shopping-shared.module';
 import { SearchPageContainerComponent } from './search-page.container';
@@ -12,6 +12,6 @@ import { searchPageRoutes } from './search-page.routes';
 @NgModule({
   imports: [RouterModule.forChild(searchPageRoutes), SharedModule, ShoppingSharedModule, SharedSearchModule],
   providers: [ProductsService],
-  declarations: [SearchPageContainerComponent, SearchPageComponent, SearchNoResultComponent],
+  declarations: [SearchPageContainerComponent, SearchResultComponent, SearchNoResultComponent],
 })
 export class SearchPageModule {}
