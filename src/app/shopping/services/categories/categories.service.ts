b82bb8e319f796e1de435bd79bf9f289b08f3f68
Stefan Hauke <s.hauke@intershop.de>
2018-04-18 12:44:00 +0200

    clean up of the CategoriesService
    - no longer using serviceIdentifier property (this pattern was abandoned)
    - handled the categoryPath translation via CategoryHelper method

--- src/app/shopping/services/categories/categories.service.ts
+++ src/app/shopping/services/categories/categories.service.ts
@@ -6,45 +6,47 @@ import { map } from 'rxjs/operators';
 import { ApiService } from '../../../core/services/api.service';
 import { CategoryData } from '../../../models/category/category.interface';
 import { CategoryMapper } from '../../../models/category/category.mapper';
-import { Category } from '../../../models/category/category.model';
+import { Category, CategoryHelper } from '../../../models/category/category.model';
 
+/**
+ * The Categories Service handles the interaction with the 'categories' REST API.
+ */
 @Injectable()
 export class CategoriesService {
-  private serviceIdentifier = 'categories';
-
   constructor(private apiService: ApiService) {}
 
   /**
-   * REST API - Get top level categories
+   * Get the full Category data for the given unique category ID.
+   * @param categoryUniqueId  The unique category id for the category of interest (encodes the category path).
+   * @returns                 The Category information.
+   */
+  getCategory(categoryUniqueId: string): Observable<Category> {
+    if (!categoryUniqueId) {
+      return ErrorObservable.create('getCategory() called without categoryUniqueId');
+    }
+
+    return this.apiService
+      .get<CategoryData>(`categories/${CategoryHelper.getCategoryPath(categoryUniqueId)}`, null, null, false)
+      .pipe(map(categoryData => CategoryMapper.fromData(categoryData, categoryUniqueId)));
+  }
+
+  /**
+   * Get the sorted top level categories (e.g. for main navigation creation) with sorted sub categories up to the given depth.
    * @param limit  The number of levels to be returned (depth) in hierarchical view.
-   * @returns      List of top level categories.
+   * @returns      A Sorted list of top level categories with sub categories.
    */
   getTopLevelCategories(limit: number): Observable<Category[]> {
     let params = new HttpParams().set('imageView', 'NO-IMAGE');
     if (limit > 0) {
       params = params.set('view', 'tree').set('limit', limit.toString());
     }
+
     return this.apiService
-      .get<CategoryData[]>(this.serviceIdentifier, params, null, true)
+      .get<CategoryData[]>('categories', params, null, true)
       .pipe(
         map(categoriesData =>
           categoriesData.map(categoryData => CategoryMapper.fromData(categoryData, categoryData.id))
         )
       );
-  }
-
-  /**
-   * REST API - Get info on (sub-)category
-   * @param categoryUniqueId  The unique category id for the category of interest (encodes the category path).
-   * @returns                 Category information.
-   */
-  getCategory(categoryUniqueId: string): Observable<Category> {
-    if (!categoryUniqueId) {
-      return ErrorObservable.create('getCategory() called without categoryUniqueId');
-    }
-    const categoryResourceIdentifier = categoryUniqueId.replace(/\./g, '/');
-    return this.apiService
-      .get<CategoryData>(this.serviceIdentifier + '/' + categoryResourceIdentifier, null, null, false)
-      .pipe(map(categoryData => CategoryMapper.fromData(categoryData, categoryUniqueId)));
   }
 }
