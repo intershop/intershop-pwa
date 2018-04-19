0902b285730355b5e4a76531d7312ddb1b43c65f
Stefan Hauke <s.hauke@intershop.de>
2018-04-19 09:20:26 +0200

    ISREST-240: added documentation for the SearchNoResultComponent and simplified the 'search.noresult.guidelines' localizable text

--- src/app/shopping/components/search/search-no-result/search-no-result.component.ts
+++ src/app/shopping/components/search/search-no-result/search-no-result.component.ts
@@ -1,9 +1,21 @@
 import { Component, Input } from '@angular/core';
 
+/**
+ * The Search No Result Component informs the user that no result has been found for his search and provides an input field for a new search.
+ * It uses the {@link SearchBoxContainerComponent}.
+ *
+ * @example
+ * <ish-search-no-result
+ *               [searchTerm]="searchTerm"
+ * ></ish-search-no-result>
+ */
 @Component({
   selector: 'ish-search-no-result',
   templateUrl: './search-no-result.component.html',
 })
 export class SearchNoResultComponent {
+  /**
+   * The search term leading to no result.
+   */
   @Input() searchTerm: string;
 }
