81deb9210a052736cfef507ddecc5f0a46fac6bd
Silke Grüber <s.grueber@intershop.de>
2018-04-20 14:06:19 +0200

    ISREST-258: finalize basket page (layout)

--- src/app/checkout/containers/basket-page/basket-page.container.spec.ts
+++ src/app/checkout/containers/basket-page/basket-page.container.spec.ts
@@ -1,22 +1,35 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Store } from '@ngrx/store';
+import { combineReducers, Store, StoreModule } from '@ngrx/store';
 import { TranslateModule } from '@ngx-translate/core';
-import { instance, mock } from 'ts-mockito/lib/ts-mockito';
+import { Basket } from '../../../models/basket/basket.model';
+import { MockComponent } from '../../../utils/dev/mock.component';
+import { LoadBasket, LoadBasketSuccess } from '../../store/basket/basket.actions';
+import { CheckoutState } from '../../store/checkout.state';
+import { checkoutReducers } from '../../store/checkout.system';
 import { BasketPageContainerComponent } from './basket-page.container';
 
 describe('Basket Page Container', () => {
   let component: BasketPageContainerComponent;
   let fixture: ComponentFixture<BasketPageContainerComponent>;
   let element: HTMLElement;
+  let store$: Store<CheckoutState>;
 
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        declarations: [BasketPageContainerComponent],
-        imports: [TranslateModule.forRoot()],
-        schemas: [NO_ERRORS_SCHEMA],
-        providers: [{ provide: Store, useFactory: () => instance(mock(Store)) }],
+        declarations: [
+          BasketPageContainerComponent,
+          MockComponent({ selector: 'ish-shopping-basket', template: 'Shopping Basket Component', inputs: ['basket'] }),
+          MockComponent({ selector: 'ish-shopping-basket-empty', template: 'Shopping Basket Empty Component' }),
+          MockComponent({ selector: 'ish-recently-viewed-container', template: 'Recently Viewed Container' }),
+          MockComponent({ selector: 'ish-loading', template: 'Loading Component' }),
+        ],
+        imports: [
+          TranslateModule.forRoot(),
+          StoreModule.forRoot({
+            checkout: combineReducers(checkoutReducers),
+          }),
+        ],
       }).compileComponents();
     })
   );
@@ -25,11 +38,38 @@ describe('Basket Page Container', () => {
     fixture = TestBed.createComponent(BasketPageContainerComponent);
     component = fixture.componentInstance;
     element = fixture.nativeElement;
+
+    store$ = TestBed.get(Store);
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
     expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+  it('should render loading component if there is no basket', () => {
+    store$.dispatch(new LoadBasket('BASKET_ID'));
+    fixture.detectChanges();
+    expect(element.querySelector('ish-loading')).toBeTruthy();
+  });
+
+  it('should render empty basket component if the basket has no line items', () => {
+    const basket = { id: 'dummy', lineItems: [] } as Basket;
+    store$.dispatch(new LoadBasketSuccess(basket));
+    fixture.detectChanges();
+    expect(element.querySelector('ish-shopping-basket-empty')).toBeTruthy();
+  });
+
+  it('should render shopping basket component if there is a basket with line items', () => {
+    const basket = { id: 'dummy', lineItems: [{ sku: '123' }] } as Basket;
+    store$.dispatch(new LoadBasketSuccess(basket));
+    fixture.detectChanges();
+    expect(element.querySelector('ish-shopping-basket')).toBeTruthy();
+  });
+
+  it('should render recently viewed items after init', () => {
+    fixture.detectChanges();
+    expect(element.querySelector('ish-recently-viewed-container')).toBeTruthy();
   });
 });
--- src/app/checkout/containers/basket-page/basket-page.container.ts
+++ src/app/checkout/containers/basket-page/basket-page.container.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-
 import { Observable } from 'rxjs/Observable';
 import { Basket } from '../../../models/basket/basket.model';
-import { getCurrentBasket } from '../../store/basket';
+import { getBasketLoading, getCurrentBasket } from '../../store/basket';
 import { CheckoutState } from '../../store/checkout.state';
 
 @Component({
@@ -13,10 +12,12 @@ import { CheckoutState } from '../../store/checkout.state';
 })
 export class BasketPageContainerComponent implements OnInit {
   basket$: Observable<Basket>;
+  basketLoading$: Observable<boolean>;
 
   constructor(private store: Store<CheckoutState>) {}
 
   ngOnInit() {
     this.basket$ = this.store.pipe(select(getCurrentBasket));
+    this.basketLoading$ = this.store.pipe(select(getBasketLoading));
   }
 }
