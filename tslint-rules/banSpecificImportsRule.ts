8293698b9bf652d8ac44fdaee5f1f7bf57425330
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-04-25 12:04:36 +0200

    added tslint rule ban-specific-imports and banned router actions from ngrx-router

---
+++ tslint-rules/banSpecificImportsRule.ts
@@ -0,0 +1,60 @@
+import * as Lint from 'tslint';
+import { ImportDeclaration, SourceFile, SyntaxKind } from 'typescript';
+import { RuleHelpers } from './ruleHelpers';
+
+interface ImportPattern {
+  import: string;
+  from: string;
+  message: string;
+  filePattern: string;
+}
+
+class BanSpecificImportsWalker extends Lint.RuleWalker {
+  patterns: ImportPattern[] = [];
+
+  constructor(sourceFile: SourceFile, options: Lint.IOptions) {
+    super(sourceFile, options);
+
+    this.patterns = options.ruleArguments as ImportPattern[];
+  }
+
+  visitImportDeclaration(importStatement: ImportDeclaration) {
+    const fromStringToken = RuleHelpers.getNextChildTokenOfKind(importStatement, SyntaxKind.StringLiteral);
+    const fromStringText = fromStringToken.getText();
+
+    this.patterns.forEach(pattern => {
+      if (new RegExp(pattern.filePattern).test(importStatement.getSourceFile().fileName)) {
+        if (new RegExp(pattern.from).test(fromStringText)) {
+          const importList = importStatement
+            .getChildAt(1)
+            .getChildAt(0)
+            .getChildAt(1);
+
+          if (pattern.import) {
+            importList
+              .getChildren()
+              .filter(token => token.kind === SyntaxKind.ImportSpecifier)
+              .filter(token => new RegExp(pattern.import).test(token.getText()))
+              .forEach(token =>
+                this.addFailureAtNode(
+                  token,
+                  pattern.message || `Using '${token.getText()}' from ${fromStringText} is banned.`
+                )
+              );
+          } else {
+            this.addFailureAtNode(importStatement, pattern.message || `Importing from ${fromStringText} is banned.`);
+          }
+        }
+      }
+    });
+  }
+}
+
+/**
+ * Implementation of the ban-specific-imports rule.
+ */
+export class Rule extends Lint.Rules.AbstractRule {
+  apply(sourceFile: SourceFile): Lint.RuleFailure[] {
+    return this.applyWithWalker(new BanSpecificImportsWalker(sourceFile, this.getOptions()));
+  }
+}
