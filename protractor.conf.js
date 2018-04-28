4d3302cd68917a25180cac167ee67e3d3b85530c
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-04-28 20:28:19 +0200

    moving protractor files one level up

--- protractor.conf.js
+++ protractor.conf.js
@@ -7,7 +7,7 @@ require('jasmine-reporters');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './e2e/protractor/**/*.e2e-spec.ts'
+    './e2e/**/*.e2e-spec.ts'
   ],
   capabilities: {
     'browserName': 'chrome',
@@ -25,7 +25,7 @@ exports.config = {
   },
   onPrepare() {
     require('ts-node').register({
-      project: 'e2e/protractor/tsconfig.e2e.json'
+      project: 'e2e/tsconfig.e2e.json'
     });
     jasmine.getEnv().addReporter(new SpecReporter({ 
       spec: { 
