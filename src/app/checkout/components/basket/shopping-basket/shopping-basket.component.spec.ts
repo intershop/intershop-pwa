1b2d9c48ac49a5e0f6a9e5dae4b944665f9bb1f1
Silke Grüber <s.grueber@intershop.de>
2018-05-03 14:55:53 +0200

    ISREST-259: tests added for shopping basket component

--- src/app/checkout/components/basket/shopping-basket/shopping-basket.component.spec.ts
+++ src/app/checkout/components/basket/shopping-basket/shopping-basket.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
-import { PopoverModule } from 'ngx-bootstrap/popover';
+
 import { FormsSharedModule } from '../../../../forms/forms-shared.module';
 import { Basket } from '../../../../models/basket/basket.model';
 import { MockComponent } from '../../../../utils/dev/mock.component';
@@ -20,8 +20,13 @@ describe('Shopping Basket Component', () => {
           ShoppingBasketComponent,
           MockComponent({ selector: 'ish-modal-dialog', template: 'Modal Component' }),
           MockComponent({ selector: 'ish-product-image', template: 'Product Image Component', inputs: ['product'] }),
+          MockComponent({
+            selector: 'ish-basket-item-description',
+            template: 'Basket Item Description Component',
+            inputs: ['pli'],
+          }),
         ],
-        imports: [TranslateModule.forRoot(), PopoverModule.forRoot(), RouterTestingModule, FormsSharedModule],
+        imports: [TranslateModule.forRoot(), RouterTestingModule, FormsSharedModule],
         providers: [FormBuilder],
       }).compileComponents();
     })
@@ -33,7 +38,15 @@ describe('Shopping Basket Component', () => {
     element = fixture.nativeElement;
     component.basket = {
       id: '4711',
-      lineItems: [],
+      lineItems: [
+        {
+          id: '4712',
+          quantity: { value: 10000 },
+          product: { sku: '4713' },
+          singleBasePrice: { value: 3, currencyMnemonic: 'USD' },
+          price: { value: 3, currencyMnemonic: 'USD' },
+        },
+      ],
     } as Basket;
   });
 
@@ -42,5 +55,43 @@ describe('Shopping Basket Component', () => {
     expect(element).toBeTruthy();
     component.ngOnChanges();
     expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+  it('should create a basket quantities form on basket change', () => {
+    expect(component.form).toBeUndefined();
+    component.ngOnChanges();
+    fixture.detectChanges();
+    expect(component.form.get('items')).toBeTruthy();
+    expect(component.form.get('items').value.length).toEqual(1);
+  });
+
+  it('should render sub components after basket change', () => {
+    component.ngOnChanges();
+    fixture.detectChanges();
+    expect(element.getElementsByTagName('ish-product-image')[0].textContent).toContain('Product Image Component');
+    expect(element.getElementsByTagName('ish-basket-item-description')[0].textContent).toContain(
+      'Basket Item Description Component'
+    );
+  });
+
+  it('should throw deleteItem event when delete item is clicked', () => {
+    let firedItem = '';
+    component.deleteItem.subscribe(itemId => {
+      firedItem = itemId;
+    });
+
+    component.onDeleteItem('4712');
+    expect(firedItem).toBe('4712');
+  });
+
+  it('should throw update event when form is submitted', () => {
+    component.ngOnChanges();
+    let firedFormValue = '';
+    component.update.subscribe(x => {
+      firedFormValue = x;
+    });
+
+    component.submitForm();
+    expect(firedFormValue).toBe(component.form.value.items);
   });
 });
