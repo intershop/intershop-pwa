56ac733676d0898aca4da74a44bc0ec89fc855e6
Susanne Schneider <s.schneider@intershop.de>
2018-05-03 15:08:23 +0200

    ISREST-270: localization and documentation for error pages

--- src/app/shopping/components/error/error-page/error-page.component.ts
+++ src/app/shopping/components/error/error-page/error-page.component.ts
@@ -1,6 +1,12 @@
-// NEEDS_WORK: DUMMY COMPONENT
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+/**
+ * The Error Page Component informs the user that the server returned an error.
+ * It uses the {@link SearchBoxContainerComponent}.
+ *
+ * @example
+ * <ish-error-page></ish-error-page>
+ */
 @Component({
   selector: 'ish-error-page',
   templateUrl: './error-page.component.html',
