b153950f9541fc1004bc0097322a3123918a5301
Silke Grüber <s.grueber@intershop.de>
2018-05-03 13:49:14 +0200

    ISREST-259: basket item description component introduced

--- src/app/checkout/checkout-shared.module.ts
+++ src/app/checkout/checkout-shared.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { BasketItemDescriptionComponent } from './components/common/basket-item-description/basket-item-description.component';
 
 @NgModule({
   imports: [SharedModule],
+  declarations: [BasketItemDescriptionComponent],
+  exports: [BasketItemDescriptionComponent],
 })
 export class CheckoutSharedModule {}
