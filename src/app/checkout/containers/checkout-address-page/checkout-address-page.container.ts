5e85020d12809dfff8398464a97bed6912532fed
Silke Grüber <s.grueber@intershop.de>
2018-05-15 10:49:56 +0200

    #ISREST-285: display address component on address page container

--- src/app/checkout/containers/checkout-address-page/checkout-address-page.container.ts
+++ src/app/checkout/containers/checkout-address-page/checkout-address-page.container.ts
@@ -1,7 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
+import { CoreState } from '../../../core/store/core.state';
+import { getLoggedInUser } from '../../../core/store/user';
 import { Basket } from '../../../models/basket/basket.model';
+import { Customer } from '../../../models/customer/customer.model';
 import { getBasketLoading, getCurrentBasket } from '../../store/basket';
 import { CheckoutState } from '../../store/checkout.state';
 
@@ -11,13 +14,16 @@ import { CheckoutState } from '../../store/checkout.state';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CheckoutAddressPageContainerComponent implements OnInit {
+  customer$: Observable<Customer>;
   basket$: Observable<Basket>;
   basketLoading$: Observable<boolean>;
 
-  constructor(private store: Store<CheckoutState>) {}
+  constructor(private store: Store<CheckoutState>, private coreStore: Store<CoreState>) {}
 
   ngOnInit() {
     this.basket$ = this.store.pipe(select(getCurrentBasket));
     this.basketLoading$ = this.store.pipe(select(getBasketLoading));
+
+    this.customer$ = this.coreStore.pipe(select(getLoggedInUser));
   }
 }
