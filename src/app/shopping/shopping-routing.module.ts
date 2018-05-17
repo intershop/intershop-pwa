b7243db0d63233fe417e0238bf86cba38a0a3774
Stefan Hauke <s.hauke@intershop.de>
2018-05-17 17:45:22 +0200

    moved "baseUrl": "./" setting form src/tsconfig.*.json to tsconfig.json as is default in new Angular CLI projects and fitted paths to lazy loaded modules to relative paths that work with the change

--- src/app/shopping/shopping-routing.module.ts
+++ src/app/shopping/shopping-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', loadChildren: 'app/shopping/containers/home-page/home-page.module#HomePageModule' },
-  { path: 'error', loadChildren: 'app/shopping/containers/error-page/error-page.module#ErrorPageModule' },
-  { path: 'product', loadChildren: 'app/shopping/containers/product-page/product-page.module#ProductPageModule' },
-  { path: 'category', loadChildren: 'app/shopping/containers/category-page/category-page.module#CategoryPageModule' },
-  { path: 'compare', loadChildren: 'app/shopping/containers/compare-page/compare-page.module#ComparePageModule' },
-  { path: 'recently', loadChildren: 'app/shopping/containers/recently-page/recently-page.module#RecentlyPageModule' },
-  { path: 'search', loadChildren: 'app/shopping/containers/search-page/search-page.module#SearchPageModule' },
+  { path: 'home', loadChildren: './containers/home-page/home-page.module#HomePageModule' },
+  { path: 'error', loadChildren: './containers/error-page/error-page.module#ErrorPageModule' },
+  { path: 'product', loadChildren: './containers/product-page/product-page.module#ProductPageModule' },
+  { path: 'category', loadChildren: './containers/category-page/category-page.module#CategoryPageModule' },
+  { path: 'compare', loadChildren: './containers/compare-page/compare-page.module#ComparePageModule' },
+  { path: 'recently', loadChildren: './containers/recently-page/recently-page.module#RecentlyPageModule' },
+  { path: 'search', loadChildren: './containers/search-page/search-page.module#SearchPageModule' },
 ];
 
 @NgModule({
