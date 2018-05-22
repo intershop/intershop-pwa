2bf36533d514eb5281e1c3e219d2b317b2cca2fb
Sebastian Glaß <s.glass@intershop.de>
2018-05-22 11:16:48 +0200

    reduced logic in template

--- src/app/models/filter-navigation/filter-navigation.mapper.ts
+++ src/app/models/filter-navigation/filter-navigation.mapper.ts
@@ -2,6 +2,7 @@ import { Facet } from '../facet/facet.model';
 import { Filter } from '../filter/filter.model';
 import { FilterNavigationData } from './filter-navigation.interface';
 import { FilterNavigation } from './filter-navigation.model';
+
 export class FilterNavigationMapper {
   static fromData(data: FilterNavigationData): FilterNavigation {
     const filterNavigation: FilterNavigation = {
@@ -19,6 +20,8 @@ export class FilterNavigationMapper {
                   count: facetData.count,
                   selected: facetData.selected,
                   link: facetData.link,
+                  filterId: facetData.link.uri.split('/filters/')[1].split(';')[0],
+                  searchParameter: facetData.link.uri.split(';SearchParameter=')[1],
                 } as Facet)
             ),
           } as Filter)
--- src/app/shopping/components/filter/filter-dropdown/filter-dropdown.component.ts
+++ src/app/shopping/components/filter/filter-dropdown/filter-dropdown.component.ts
@@ -11,13 +11,12 @@ export class FilterDropdownComponent implements OnInit {
   @Input() filterElement: Filter;
   @Output() applyFilter: EventEmitter<{ filterId: string; searchParameter: string }> = new EventEmitter();
   hasSelected: boolean;
-  getFilterName = url => url.split('/filters/')[1].split(';')[0];
-  getQueryParam = url => url.split(';SearchParameter=')[1];
+
   ngOnInit() {
     const facet: Facet[] = this.filterElement.facets;
     this.hasSelected = !!facet.find(e => e.selected);
   }
-  filter(filterId, searchParameter) {
-    this.applyFilter.emit({ filterId, searchParameter });
+  filter(facet: Facet) {
+    this.applyFilter.emit({ filterId: facet.filterId, searchParameter: facet.searchParameter });
   }
 }
