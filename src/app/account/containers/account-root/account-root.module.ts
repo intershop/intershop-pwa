e10978ddaaa94f8bf0f591f9289424005073fb72
Stefan Hauke <s.hauke@intershop.de>
2018-05-30 14:49:06 +0200

    renamed account-page container/component to account-overview-page

--- src/app/account/containers/account-root/account-root.module.ts
+++ src/app/account/containers/account-root/account-root.module.ts
@@ -4,13 +4,21 @@ import { AuthGuard } from '../../../core/guards/auth.guard';
 import { SharedModule } from '../../../shared/shared.module';
 import { AccountSharedModule } from '../../account-shared.module';
 import { AccountNavigationComponent } from '../../components/account-navigation/account-navigation.component';
+import { AccountOverviewPageComponent } from '../../components/account-overview-page/account-overview-page.component';
 import { AccountRootComponent } from '../../components/account-root/account-root.component';
+import { AccountOverviewPageContainerComponent } from '../account-overview-page/account-overview-page.container';
 import { AccountRootContainerComponent } from './account-root.container';
 import { accountRootRoutes } from './account-root.routes';
 
 @NgModule({
   imports: [RouterModule.forChild(accountRootRoutes), SharedModule, AccountSharedModule],
-  declarations: [AccountRootContainerComponent, AccountRootComponent, AccountNavigationComponent],
+  declarations: [
+    AccountRootContainerComponent,
+    AccountRootComponent,
+    AccountNavigationComponent,
+    AccountOverviewPageContainerComponent,
+    AccountOverviewPageComponent,
+  ],
   providers: [AuthGuard],
 })
 export class AccountRootModule {}
