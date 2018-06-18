72e36edf533ea747df8be761af77add60b2fb14f
Derk Sacklowski <derk.sacklowski@flyacts.com>
2018-06-18 14:36:19 +0200

    added integration for quoting into shopping module

---
+++ src/app/b2b/components/product-add-to-quote/product-add-to-quote.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { Product } from '../../../models/product/product.model';
+
+/**
+ * The Product Add To Quote Component displays a button which emits productToQuote when triggered.
+ * It provides two display types, text and glyphicon.
+ *
+ * @example
+ * <ish-product-add-to-quote
+ *   [product]="product"
+ *   [disabled]="disabled"
+ *   (productToQuote)="addToQuote()"
+ *   [class]="'btn-block'"
+ * ></ish-product-add-to-quote>
+ */
+@Component({
+  selector: 'ish-product-add-to-quote',
+  templateUrl: './product-add-to-quote.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductAddToQuoteComponent implements OnChanges {
+  @Input() product: Product;
+  @Input() disabled = false;
+  @Input() displayType?: string;
+  @Input() class?: string;
+  @Output() productToQuote = new EventEmitter<void>();
+
+  isDisplayTypeGlyphicon = false;
+
+  ngOnChanges() {
+    this.isDisplayTypeGlyphicon = this.displayType === 'glyphicon';
+  }
+
+  addToQuote() {
+    this.productToQuote.emit();
+  }
+}
