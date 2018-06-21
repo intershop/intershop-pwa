50c587b857c83904095b60ddb668d5d6259c05ac
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-06-21 13:16:11 +0200

    ISREST-315: move auth token to store

--- src/app/core/store/user/user.selectors.ts
+++ src/app/core/store/user/user.selectors.ts
@@ -1,6 +1,6 @@
 import { createSelector } from '@ngrx/store';
 import { CoreState } from '../core.state';
-import { getAuthorized, getCustomer, getError, getUser } from './user.reducer';
+import { getAuthorized, getAuthToken, getCustomer, getError, getUser } from './user.reducer';
 
 const getUserState = (state: CoreState) => state.user;
 
@@ -8,3 +8,4 @@ export const getLoggedInCustomer = createSelector(getUserState, getCustomer);
 export const getLoggedInUser = createSelector(getUserState, getUser);
 export const getUserAuthorized = createSelector(getUserState, getAuthorized);
 export const getUserError = createSelector(getUserState, getError);
+export const getAPIToken = createSelector(getUserState, getAuthToken);
