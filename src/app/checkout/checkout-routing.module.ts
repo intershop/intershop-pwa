efb7aac63d02d3ba45adafe177dab6b830f1e11b
Silke Grüber <s.grueber@intershop.de>
2018-06-22 11:52:57 +0200

    ISREST-287: require login before starting checkout

--- src/app/checkout/checkout-routing.module.ts
+++ src/app/checkout/checkout-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'basket', loadChildren: './containers/basket-page/basket-page.module#BasketPageModule' },
   {
     path: 'checkout/address',
     loadChildren: './containers/checkout-address-page/checkout-address-page.module#CheckoutAddressPageModule',
+    canActivate: [AuthGuard],
   },
 ];
 
