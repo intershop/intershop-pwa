3f53ce635b71f52d42958743ffe0cfa5b5173eee
Derk Sacklowski <derk.sacklowski@flyacts.com>
2018-06-25 11:39:17 +0200

    removed unnecessary translateService provide

--- src/app/b2b/components/product-add-to-quote/product-add-to-quote.component.spec.ts
+++ src/app/b2b/components/product-add-to-quote/product-add-to-quote.component.spec.ts
@@ -13,7 +13,6 @@ describe('Product Add To Quote Component', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [TranslateModule.forRoot()],
-      providers: [TranslateService],
       declarations: [ProductAddToQuoteComponent],
     }).compileComponents();
   }));
