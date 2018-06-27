5558144b22a0c0b40467e37fcacced8054c14321
Stefan Hauke <s.hauke@intershop.de>
2018-06-27 11:53:03 +0200

    changed mock environment port from 4000 to 4200 to match the default ng serve port

--- .gitlab-ci-test-universal.sh
+++ .gitlab-ci-test-universal.sh
@@ -12,14 +12,14 @@ universalTest() {
   [ "$res" -ne "0" ] && exit 1
 }
 
-universalTest 1 "http://localhost:4000/" "router-outlet><ish-home-page-container>"
-universalTest 2 "http://localhost:4000/category/Cameras-Camcorders.584" "router-outlet><ish-category-page-container>"
-universalTest 3 "http://localhost:4000/login" "Forgot your password?"
-universalTest 4 "http://localhost:4000/register" "Create Account"
-universalTest 5 "http://localhost:4000/category/Cameras-Camcorders" "<h1>Cameras</h1>"
-universalTest 6 "http://localhost:4000/category/Cameras-Camcorders" "<h3>Webcams</h3>"
-universalTest 7 "http://localhost:4000/category/Cameras-Camcorders.584" "add-to-compare"
-universalTest 8 "http://localhost:4000/home" "intershop-pwa-state"
-universalTest 9 "http://localhost:4000/home" "&q;icmBaseURL&q;:&q;http://localhost:4000"
+universalTest 1 "http://localhost:4200/" "router-outlet><ish-home-page-container>"
+universalTest 2 "http://localhost:4200/category/Cameras-Camcorders.584" "router-outlet><ish-category-page-container>"
+universalTest 3 "http://localhost:4200/login" "Forgot your password?"
+universalTest 4 "http://localhost:4200/register" "Create Account"
+universalTest 5 "http://localhost:4200/category/Cameras-Camcorders" "<h1>Cameras</h1>"
+universalTest 6 "http://localhost:4200/category/Cameras-Camcorders" "<h3>Webcams</h3>"
+universalTest 7 "http://localhost:4200/category/Cameras-Camcorders.584" "add-to-compare"
+universalTest 8 "http://localhost:4200/home" "intershop-pwa-state"
+universalTest 9 "http://localhost:4200/home" "&q;icmBaseURL&q;:&q;http://localhost:4200"
 
 exit 0
