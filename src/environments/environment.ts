a0eef81aa727466a6122f23a1a2928003127c993
Stefan Hauke <s.hauke@intershop.de>
2018-06-27 11:50:25 +0200

    disabled the syncLocalStorage feature by default for now
    - still leads to unexpected issues, not finished yet and therefore marked as experimental

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+// `ng build --prod` then `environment.prod.ts` will be used instead.
+// The list of which configuration maps to which file can be found in `angular.json`.
 
 export const environment = {
   production: false,
@@ -10,7 +10,7 @@ export const environment = {
   icmServer: 'INTERSHOP/rest/WFS',
   icmApplication: 'inSPIRED-inTRONICS-Site',
   mainNavigationMaxSubCategoriesDepth: 2,
-  syncLocalStorage: true,
+  syncLocalStorage: false, // experimental feature to transfer the application state to another tab or keep it after a full page refresh/load - disabled for now
 
   locales: [
     { lang: 'en_US', currency: 'USD', value: 'en', displayName: 'English', displayLong: 'English (United States)' },
