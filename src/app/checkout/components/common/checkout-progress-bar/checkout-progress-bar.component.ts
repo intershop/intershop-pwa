fddcd5702f0e4a30cfe68071498cbdd354eaefaf
Silke Grüber <s.grueber@intershop.de>
2018-06-27 13:49:54 +0200

    ISREST-332: reworked checkout-progress-bar to be more generic (rendering controlled by component configuration) and used it on the checkout-page container
    - steps not yet changing with the navigation

--- src/app/checkout/components/common/checkout-progress-bar/checkout-progress-bar.component.ts
+++ src/app/checkout/components/common/checkout-progress-bar/checkout-progress-bar.component.ts
@@ -1,20 +1,55 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ish-checkout-progress-bar',
   templateUrl: './checkout-progress-bar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CheckoutProgressBarComponent implements OnChanges {
+export class CheckoutProgressBarComponent {
   @Input() step = 1;
 
-  // display step as link
-  displayStepLinks = [false, false, false, false, false, false];
+  /**
+   * Define the checkout steps.
+   */
+  checkoutSteps = [
+    {
+      step: 1,
+      link: '/checkout/address',
+      labelKey: 'checkout.progress.addresses.label',
+      stepKey: 'checkout.progress.step1.text',
+    },
+    {
+      step: 2,
+      link: '/checkout/shipping',
+      labelKey: 'checkout.progress.shipping.label',
+      stepKey: 'checkout.progress.step2.text',
+    },
+    {
+      step: 3,
+      link: '/checkout/payment',
+      labelKey: 'checkout.progress.payment.label',
+      stepKey: 'checkout.progress.step3.text',
+    },
+    {
+      step: 4,
+      link: '/checkout/review',
+      labelKey: 'checkout.progress.review.label',
+      stepKey: 'checkout.progress.step4.text',
+    },
+    {
+      step: 5,
+      link: '/checkout/receipt',
+      labelKey: 'checkout.progress.receipt.label',
+      stepKey: 'checkout.progress.step5.text',
+    },
+  ];
 
-  ngOnChanges() {
-    // ToDo: Extend conditions for basket excelleration
-    this.displayStepLinks[1] = this.step > 1 && this.step < 5;
-    this.displayStepLinks[2] = this.step > 2 && this.step < 5;
-    this.displayStepLinks[3] = this.step > 3 && this.step < 5;
+  /**
+   * Checks whether a checkout step should be displayed as link or not.
+   * @param step  The checkout step to evaluate.
+   * @returns     Returns 'true' if the step number is lover than the current step and if the current step is lower than 5.
+   */
+  checkoutStepLink(step): boolean {
+    return step < this.step && this.step < 5;
   }
 }
