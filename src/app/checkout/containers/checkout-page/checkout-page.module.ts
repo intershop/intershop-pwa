9ce54048195a3726f26129575331dc1762ae60cf
Stefan Hauke <s.hauke@intershop.de>
2018-06-29 11:43:52 +0200

    ISREST-332: added initial components and routing for the checkout review step

--- src/app/checkout/containers/checkout-page/checkout-page.module.ts
+++ src/app/checkout/containers/checkout-page/checkout-page.module.ts
@@ -8,9 +8,11 @@ import { ShoppingSharedModule } from '../../../shopping/shopping-shared.module';
 import { CheckoutSharedModule } from '../../checkout-shared.module';
 import { CheckoutAddressComponent } from '../../components/checkout/checkout-address/checkout-address.component';
 import { CheckoutPaymentComponent } from '../../components/checkout/checkout-payment/checkout-payment/checkout-payment.component';
+import { CheckoutReviewComponent } from '../../components/checkout/checkout-review/checkout-review.component';
 import { CheckoutShippingComponent } from '../../components/checkout/checkout-shipping/checkout-shipping.component';
 import { CheckoutAddressPageContainerComponent } from '../checkout-address-page/checkout-address-page.container';
 import { CheckoutPaymentPageContainerComponent } from '../checkout-payment-page/checkout-payment-page.container';
+import { CheckoutReviewPageContainerComponent } from '../checkout-review-page/checkout-review-page.container';
 import { CheckoutShippingPageContainerComponent } from '../checkout-shipping-page/checkout-shipping-page.container';
 import { CheckoutPageContainerComponent } from './checkout-page.container';
 import { checkoutPageRoutes } from './checkout-page.routes';
@@ -32,6 +34,8 @@ import { checkoutPageRoutes } from './checkout-page.routes';
     CheckoutShippingComponent,
     CheckoutPaymentPageContainerComponent,
     CheckoutPaymentComponent,
+    CheckoutReviewPageContainerComponent,
+    CheckoutReviewComponent,
   ],
   providers: [AuthGuard],
 })
