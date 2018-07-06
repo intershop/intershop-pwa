6f7cabd175918c4e81fbe0e3c7c0409fce64907c
Stefan Hauke <s.hauke@intershop.de>
2018-07-06 12:34:16 +0200

    added mainNavigationMaxSubCategoriesDepth checkfor the recursive rendering of the main navigation

--- src/app/core/components/header/header-navigation/sub-category-navigation/sub-category-navigation.component.spec.ts
+++ src/app/core/components/header/header-navigation/sub-category-navigation/sub-category-navigation.component.spec.ts
@@ -3,6 +3,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { createCategoryView } from '../../../../../models/category-view/category-view.model';
 import { Category } from '../../../../../models/category/category.model';
 import { categoryTree } from '../../../../../utils/dev/test-data-utils';
+import { MAIN_NAVIGATION_MAX_SUB_CATEGORIES_DEPTH } from '../../../../configurations/injection-keys';
 import { SubCategoryNavigationComponent } from './sub-category-navigation.component';
 
 describe('Sub Category Navigation Component', () => {
@@ -14,6 +15,7 @@ describe('Sub Category Navigation Component', () => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [SubCategoryNavigationComponent],
+      providers: [{ provide: MAIN_NAVIGATION_MAX_SUB_CATEGORIES_DEPTH, useValue: 2 }],
     }).compileComponents();
   }));
 
@@ -27,10 +29,11 @@ describe('Sub Category Navigation Component', () => {
       { uniqueId: 'A.1', name: 'CAT_A1', categoryPath: ['A', 'A.1'] },
       { uniqueId: 'A.2', name: 'CAT_A2', categoryPath: ['A', 'A.2'] },
       { uniqueId: 'A.1.a', name: 'CAT_A1a', categoryPath: ['A', 'A.1', 'A.1.a'] },
+      { uniqueId: 'A.1.a.alpha', name: 'CAT_A1aAlpha', categoryPath: ['A', 'A.1', 'A.1.a', 'A.1.a.alpha'] },
     ] as Category[]);
 
     component.category = createCategoryView(tree, 'A');
-    component.subCategoriesDepth = 0;
+    component.subCategoriesDepth = 1;
   });
 
   it('should be created', () => {
