2a5838b3990f750cfbf027b36377f1f5e3d8c3f1
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-07-06 12:08:21 +0200

    sort up failures in jest report

--- jest-reports.config.js
+++ jest-reports.config.js
@@ -17,7 +17,8 @@ module.exports = {
       ["jest-html-reporter", {
         pageTitle: "Intershop Progressive Webapp Unit Test Report",
         outputPath: "reports/unit-tests.html",
-        includeFailureMsg: true
+        includeFailureMsg: true,
+        sort: "status"
       }]
     ]
 }
