d36025570bbf4649be342c9d4c16c066bb1a3c32
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-07-26 11:49:28 +0200

    forbid rxjs deep imports

--- src/app/quoting/containers/quote-edit-page/quote-edit-page.container.ts
+++ src/app/quoting/containers/quote-edit-page/quote-edit-page.container.ts
@@ -3,7 +3,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { AddQuoteToBasket } from '../../../checkout/store/basket';
 import { Basket } from '../../../models/basket/basket.model';
 import { Quote } from '../../../models/quote/quote.model';
