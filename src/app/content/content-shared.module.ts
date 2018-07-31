6ef1ef5f740c5ac6ce74fe61a8ec101057a8a44d
Stefan Hauke <s.hauke@intershop.de>
2018-07-31 12:29:54 +0200

    ISREST-210: introduce 'CMSTextComponent' as rendering component for 'app_sf_responsive_cm:component.common.text.pagelet2-Component' pagelet type

--- src/app/content/content-shared.module.ts
+++ src/app/content/content-shared.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 
+import { CMSTextComponent } from './components/cms-text/cms-text.component';
 import { ContentIncludeContainerComponent } from './containers/content-include/content-include.container';
 import { ContentPageletContainerComponent } from './containers/content-pagelet/content-pagelet.container';
 import { ContentSlotContainerComponent } from './containers/content-slot/content-slot.container';
 
-const cmsComponents = [];
+const cmsComponents = [CMSTextComponent];
 
 @NgModule({
   imports: [CommonModule, SharedModule],
