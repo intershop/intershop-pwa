2e441e3871c4b489135603325365f9440e6a4a5d
Stefan Hauke <s.hauke@intershop.de>
2018-07-31 16:40:54 +0200

    ISREST-210: introduce 'CMSFreestyleComponent' as rendering component for 'app_sf_responsive_cm:component.common.freeStyle.pagelet2-Component' pagelet type

--- src/app/content/content-shared.module.ts
+++ src/app/content/content-shared.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 
+import { CMSFreestyleComponent } from './components/cms-freestyle/cms-freestyle.component';
 import { CMSTextComponent } from './components/cms-text/cms-text.component';
 import { ContentIncludeContainerComponent } from './containers/content-include/content-include.container';
 import { ContentPageletContainerComponent } from './containers/content-pagelet/content-pagelet.container';
 import { ContentSlotContainerComponent } from './containers/content-slot/content-slot.container';
 
-const cmsComponents = [CMSTextComponent];
+const cmsComponents = [CMSTextComponent, CMSFreestyleComponent];
 
 @NgModule({
   imports: [CommonModule, SharedModule],
