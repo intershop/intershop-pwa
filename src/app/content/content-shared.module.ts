4d73ec0f65c8b61ed6e687959ec66bb6246018eb
Stefan Hauke <s.hauke@intershop.de>
2018-08-03 12:17:44 +0200

    ISREST-210: introduce 'CMSImageEnhancedComponent' as rendering component for 'app_sf_responsive_cm:component.common.imageEnhanced.pagelet2-Component' pagelet type

--- src/app/content/content-shared.module.ts
+++ src/app/content/content-shared.module.ts
@@ -5,13 +5,20 @@ import { SharedModule } from '../shared/shared.module';
 
 import { CMSContainerComponent } from './components/cms-container/cms-container.component';
 import { CMSFreestyleComponent } from './components/cms-freestyle/cms-freestyle.component';
+import { CMSImageEnhancedComponent } from './components/cms-image-enhanced/cms-image-enhanced.component';
 import { CMSImageComponent } from './components/cms-image/cms-image.component';
 import { CMSTextComponent } from './components/cms-text/cms-text.component';
 import { ContentIncludeContainerComponent } from './containers/content-include/content-include.container';
 import { ContentPageletContainerComponent } from './containers/content-pagelet/content-pagelet.container';
 import { ContentSlotContainerComponent } from './containers/content-slot/content-slot.container';
 
-const cmsComponents = [CMSTextComponent, CMSFreestyleComponent, CMSContainerComponent, CMSImageComponent];
+const cmsComponents = [
+  CMSTextComponent,
+  CMSFreestyleComponent,
+  CMSContainerComponent,
+  CMSImageComponent,
+  CMSImageEnhancedComponent,
+];
 
 @NgModule({
   imports: [CommonModule, SharedModule],
