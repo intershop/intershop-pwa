70fe1e85db8a05bc27920ac9d921e7ba7099af38
Derk Sacklowski <derk.sacklowski@flyacts.com>
2018-08-20 12:57:55 +0200

    improve header doc and tests

--- src/app/core/components/header/header-navigation/header-navigation.component.ts
+++ src/app/core/components/header/header-navigation/header-navigation.component.ts
@@ -3,6 +3,17 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CategoryView } from '../../../../models/category-view/category-view.model';
 import { Category } from '../../../../models/category/category.model';
 
+/**
+ * The Header Navigation Component displays the first level navigation.
+ *
+ * It uses the {@link SubCategoryNavigationComponent} for rendering sub categories.
+ *
+ * @example
+ * <ish-header-navigation
+ *   [categories]="categories$ | async"
+ * >
+ * </ish-header-navigation>
+ */
 @Component({
   selector: 'ish-header-navigation',
   templateUrl: './header-navigation.component.html',
@@ -14,18 +25,36 @@ export class HeaderNavigationComponent {
 
   openedCategories = [];
 
+  /**
+   * Handle sub menu show.
+   * Adds hover class to rendered element.
+   * @param submenu The rendered sub menu element.
+   */
   subMenuShow(submenu) {
     submenu.classList.add('hover');
   }
 
+  /**
+   * Handle sub menu hide.
+   * Removes hover class from rendered element.
+   * @param submenu The rendered sub menu element.
+   */
   subMenuHide(submenu) {
     submenu.classList.remove('hover');
   }
 
+  /**
+   * Indicate if specific category is expanded.
+   * @param category The category item.
+   */
   isOpened(category: Category): boolean {
     return this.openedCategories.includes(category.uniqueId);
   }
 
+  /**
+   * Toggle category open state.
+   * @param category The category item.
+   */
   toggleOpen(category: Category) {
     const index = this.openedCategories.findIndex(id => id === category.uniqueId);
     index > -1 ? this.openedCategories.splice(index, 1) : this.openedCategories.push(category.uniqueId);
--- src/app/core/components/header/header/header.component.ts
+++ src/app/core/components/header/header/header.component.ts
@@ -3,6 +3,20 @@ import { ChangeDetectionStrategy, Component, HostListener, Inject, OnInit, PLATF
 
 import { MEDIUM_BREAKPOINT_WIDTH } from '../../../configurations/injection-keys';
 
+/**
+ * The Header Component displays the page header.
+ *
+ * It uses the {@link LoginStatusContainerComponent} for rendering the users login status.
+ * It uses the {@link ProductCompareStatusContainerComponent} for rendering the product compare button and count.
+ * It uses the {@link MobileBasketContainerComponent} for rendering the mobile basket button and basket item count.
+ * It uses the {@link LanguageSwitchContainerComponent} for rendering the language selection dropdown.
+ * It uses the {@link SearchBoxContainerComponent} for rendering the search box.
+ * It uses the {@link HeaderNavigationContainerComponent} for rendering the pages main navigation.
+ * It uses the {@link MiniBasketContainerComponent} for rendering mini basket on desktop sized viewports.
+ *
+ * @example
+ * <ish-header></ish-header>
+ */
 @Component({
   selector: 'ish-header',
   templateUrl: './header.component.html',
