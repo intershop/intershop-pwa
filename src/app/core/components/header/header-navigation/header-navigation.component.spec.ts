6157e6cb240cf48088c3afc3d008ad883b1dd123
Derk Sacklowski <derk.sacklowski@flyacts.com>
2018-08-20 12:57:55 +0200

    improve header doc and tests

--- src/app/core/components/header/header-navigation/header-navigation.component.spec.ts
+++ src/app/core/components/header/header-navigation/header-navigation.component.spec.ts
@@ -1,6 +1,10 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
+import { createCategoryView } from '../../../../models/category-view/category-view.model';
+import { Category } from '../../../../models/category/category.model';
+import { categoryTree } from '../../../../utils/dev/test-data-utils';
+
 import { HeaderNavigationComponent } from './header-navigation.component';
 
 describe('Header Navigation Component', () => {
@@ -19,11 +23,24 @@ describe('Header Navigation Component', () => {
     fixture = TestBed.createComponent(HeaderNavigationComponent);
     component = fixture.componentInstance;
     element = fixture.nativeElement;
+
+    const categories = categoryTree([
+      { uniqueId: 'A', name: 'CAT_A', categoryPath: ['A'] },
+      { uniqueId: 'B', name: 'CAT_B', categoryPath: ['B'] },
+      { uniqueId: 'C', name: 'CAT_C', categoryPath: ['C'] },
+    ] as Category[]);
+
+    component.categories = [
+      createCategoryView(categories, 'A'),
+      createCategoryView(categories, 'B'),
+      createCategoryView(categories, 'C'),
+    ];
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
     expect(() => fixture.detectChanges()).not.toThrow();
+    expect(element).toMatchSnapshot();
   });
 });
