f4811ec892daf988a5e8132f575f91ad6415efaf
Stefan Hauke <s.hauke@intershop.de>
2018-09-18 13:34:48 +0200

    add functionality to hide/show filter navigation for category and family pages on mobile devices

--- src/app/shopping/components/category/category-page/category-page.component.ts
+++ src/app/shopping/components/category/category-page/category-page.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { ChangeDetectionStrategy, Component, HostListener, Inject, Input, OnInit, PLATFORM_ID } from '@angular/core';
 
+import { MEDIUM_BREAKPOINT_WIDTH } from '../../../../core/configurations/injection-keys';
 import { CategoryView } from '../../../../models/category-view/category-view.model';
 
 @Component({
@@ -7,7 +9,25 @@ import { CategoryView } from '../../../../models/category-view/category-view.mod
   templateUrl: './category-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CategoryPageComponent {
+export class CategoryPageComponent implements OnInit {
   @Input()
   category: CategoryView;
+
+  isCollapsed = false;
+
+  constructor(
+    @Inject(MEDIUM_BREAKPOINT_WIDTH) private mediumBreakpointWidth: number,
+    @Inject(PLATFORM_ID) private platformId: string
+  ) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isCollapsed = window.innerWidth < this.mediumBreakpointWidth;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  mobileViewHandler(event) {
+    this.isCollapsed = event.target.innerWidth < this.mediumBreakpointWidth;
+  }
 }
