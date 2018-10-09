4e56f9dc12580b3088ed7d85517d85230d90ea88
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-10-09 11:39:23 +0200

    removed protractor

--- e2e/src/pages/shopping/search-result.page.ts
+++
@@ -1,7 +0,0 @@
-import { ProductListModule } from './product-list.module';
-
-export class SearchResultPage {
-  readonly tag = 'ish-search-result';
-
-  readonly productList = new ProductListModule();
-}
