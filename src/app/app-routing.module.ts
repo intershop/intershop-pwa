e9f8e7a9b358fe1e0af41ced21dbb342ec77eb55
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-10-11 17:29:18 +0200

    app.module cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', redirectTo: '/home', pathMatch: 'full' }, { path: '**', redirectTo: '/error' }];
+const routes: Routes = [{ path: '', redirectTo: '/home', pathMatch: 'full' }];
 
 @NgModule({
   imports: [
