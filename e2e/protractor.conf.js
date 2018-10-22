d094fe54686e6e48818383addccefff39d83cf1a
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-10-22 11:53:45 +0200

    increase e2e test timeout

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -4,7 +4,7 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
-  allScriptsTimeout: 30000,
+  allScriptsTimeout: 60000,
   specs: ['./src/**/*.e2e-spec.ts'],
   suites: {
     local: './src/**/*.local.e2e-spec.ts',
@@ -21,7 +21,7 @@ exports.config = {
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
+    defaultTimeoutInterval: 60000,
     print: function() {},
   },
   onPrepare() {
