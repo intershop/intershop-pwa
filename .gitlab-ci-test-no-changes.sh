8822be5ce39bb4cbc420b51c1c94c4a8e299de8b
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-11-02 12:32:13 +0100

    output diff on found changes

--- .gitlab-ci-test-no-changes.sh
+++ .gitlab-ci-test-no-changes.sh
@@ -3,4 +3,4 @@
 set -e
 set -o pipefail
 
-git status --porcelain | wc -l | xargs test '0' -eq || ( (>&2 echo "there are local changes -- $1:") ; git status --porcelain ; exit 1)
+git status --porcelain | wc -l | xargs test '0' -eq || ( (>&2 echo "there are local changes -- $1:") ; git diff ; exit 1)
