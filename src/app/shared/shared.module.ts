99c0c8883cee458df7fefc20d31fc48052763830
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-11-14 13:05:14 +0100

    moved common shared components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -13,12 +13,12 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { IconModule } from 'ish-core/icon.module';
 
-import { AccordionItemComponent } from './components/accordion-item/accordion-item.component';
-import { AccordionComponent } from './components/accordion/accordion.component';
-import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
-import { InfoBoxComponent } from './components/info-box/info-box.component';
-import { LoadingComponent } from './components/loading/loading.component';
-import { ModalDialogComponent } from './components/modal-dialog/modal-dialog.component';
+import { AccordionItemComponent } from './components/common/accordion-item/accordion-item.component';
+import { AccordionComponent } from './components/common/accordion/accordion.component';
+import { BreadcrumbComponent } from './components/common/breadcrumb/breadcrumb.component';
+import { InfoBoxComponent } from './components/common/info-box/info-box.component';
+import { LoadingComponent } from './components/common/loading/loading.component';
+import { ModalDialogComponent } from './components/common/modal-dialog/modal-dialog.component';
 import { FeatureToggleModule } from './feature-toggle.module';
 import { PipesModule } from './pipes.module';
 
