099e69ee93a5df45613c5ca44b3cf35c08879c3d
Danilo Hoffmann <d.hoffmann@intershop.de>
2018-11-25 15:29:14 +0100

    define import alias for core

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
     setupTestFrameworkScriptFile: "<rootDir>/src/test.ts",
     transformIgnorePatterns: [
       "node_modules/(?!(@ngrx|ngx-bootstrap|@angular/common/locales|lodash-es/.*))"
-    ]
+    ],
+    moduleNameMapper: {
+      "^ish-core(.*)$": "<rootDir>/src/app/core$1"
+    }
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -13,7 +13,7 @@
  *
  * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
  */
-// tslint:disable:ordered-imports no-commented-out-code no-any
+// tslint:disable:ish-ordered-imports no-commented-out-code no-any
 
 /***************************************************************************************************
  * BROWSER POLYFILLS
