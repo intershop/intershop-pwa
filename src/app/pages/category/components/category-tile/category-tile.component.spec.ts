90abe55540fa3a5737924daa38a1b00d5babab2b
Jens Stein <j.stein@intershop.de>
2018-12-03 15:40:01 +0100

    ISREST-161: added CategoryTileComponent and CategoryImageComponent and used it on the CategoryListComponent

---
+++ src/app/pages/category/components/category-tile/category-tile.component.spec.ts
@@ -0,0 +1,63 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { Category } from 'ish-core/models/category/category.model';
+import { MockComponent } from 'ish-core/utils/dev/mock.component';
+
+import { CategoryTileComponent } from './category-tile.component';
+
+describe('Category Tile Component', () => {
+  let component: CategoryTileComponent;
+  let fixture: ComponentFixture<CategoryTileComponent>;
+  let element: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [
+        CategoryTileComponent,
+        MockComponent({
+          selector: 'ish-category-image',
+          template: 'Category Image Component',
+          inputs: ['category'],
+        }),
+      ],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CategoryTileComponent);
+    component = fixture.componentInstance;
+    element = fixture.nativeElement;
+    const category = {
+      uniqueId: 'A',
+      categoryPath: ['A'],
+      images: [
+        {
+          name: 'front S',
+          type: 'Image',
+          imageActualHeight: 110,
+          imageActualWidth: 110,
+          viewID: 'front',
+          effectiveUrl: '/assets/product_img/a.jpg',
+          typeID: 'S',
+          primaryImage: false,
+        },
+      ],
+    } as Category;
+    component.category = category;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+    expect(element).toBeTruthy();
+    expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+  it('should always have created image', () => {
+    fixture.detectChanges();
+    const imgComponentElement = element.querySelector('ish-category-image');
+    expect(imgComponentElement).toBeTruthy();
+  });
+});
