d61960aa0aceca10ce08efcae29697a35cb2d67c
Jens Stein <j.stein@intershop.de>
2018-12-03 15:40:01 +0100

    ISREST-161: added CategoryTileComponent and CategoryImageComponent and used it on the CategoryListComponent

---
+++ src/app/pages/category/components/category-image/category-image.component.ts
@@ -0,0 +1,43 @@
+import { ChangeDetectionStrategy, Component, Inject, Input, OnChanges } from '@angular/core';
+
+import { Category } from 'ish-core/models/category/category.model';
+import { ICM_BASE_URL } from 'ish-core/utils/state-transfer/factories';
+
+/**
+ * The Category Image Component renders an HTML tag img of an image of a Category.
+ */
+@Component({
+  selector: 'ish-category-image',
+  templateUrl: './category-image.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CategoryImageComponent implements OnChanges {
+  /**
+   * The category for which the image should be displayed
+   */
+  @Input()
+  category: Category;
+
+  categoryImageUrl = '/assets/img/not_available.png';
+
+  constructor(@Inject(ICM_BASE_URL) public icmBaseURL) {}
+
+  ngOnChanges() {
+    this.setCategoryImageUrl();
+  }
+
+  /**
+   * Set the category image URL from the (non-)empty property effectiveUrl.
+   * The URL to a non-empty effectiveUrl has a prefix URL of property icmBaseURL.
+   */
+  setCategoryImageUrl() {
+    if (
+      this.category.images &&
+      this.category.images[0] &&
+      this.category.images[0].effectiveUrl &&
+      this.category.images[0].effectiveUrl.length > 0
+    ) {
+      this.categoryImageUrl = `${this.icmBaseURL}${this.category.images[0].effectiveUrl}`;
+    }
+  }
+}
