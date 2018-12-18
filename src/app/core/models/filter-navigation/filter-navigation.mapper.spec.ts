93b69d896432f9e7b1bc0a046f54ded4fd63ccf3
Sebastian Glaß <s.glass@intershop.de>
2018-12-18 13:35:03 +0100

    safety checks in filternavigation-mapper

---
+++ src/app/core/models/filter-navigation/filter-navigation.mapper.spec.ts
@@ -0,0 +1,39 @@
+import { FilterNavigationData } from './filter-navigation.interface';
+import { FilterNavigationMapper } from './filter-navigation.mapper';
+import { FilterNavigation } from './filter-navigation.model';
+
+describe('Filter Navigation Mapper', () => {
+  describe('fromData', () => {
+    it('should return empty FilterNavigation when elements not exists', () => {
+      const data = {} as FilterNavigationData;
+      const model = FilterNavigationMapper.fromData(data);
+      expect(model).toEqual({ filter: [] } as FilterNavigation);
+    });
+
+    it('should return empty FilterNavigation when elements.length is 0', () => {
+      const data = { elements: [] } as FilterNavigationData;
+      const model = FilterNavigationMapper.fromData(data);
+      expect(model).toEqual({ filter: [] } as FilterNavigation);
+    });
+
+    it('should parse objects when elements exists with facets', () => {
+      const data = {
+        elements: [{ facets: [{ name: 'testName', link: { uri: '/filters/uri;SearchParameter=param' } }] }],
+      } as FilterNavigationData;
+
+      const model = FilterNavigationMapper.fromData(data);
+      expect(model.filter).toBeTruthy();
+      expect(model.filter).toHaveLength(1);
+      expect(model.filter[0].facets).toHaveLength(1);
+      expect(model.filter[0].facets[0].searchParameter).toBe('param');
+    });
+
+    it('should parse objects when elements exists', () => {
+      const data = { elements: [{}] } as FilterNavigationData;
+      const model = FilterNavigationMapper.fromData(data);
+      expect(model.filter).toBeTruthy();
+      expect(model.filter).toHaveLength(1);
+      expect(model.filter[0].facets).toHaveLength(0);
+    });
+  });
+});
