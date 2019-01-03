d1aa133e1daa5ccd1bcbe8365148c1a24417a0d0
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-01-03 17:18:32 +0100

    composing cms image URLs in mapper, removing ContentImagePageletView

--- src/app/core/models/content-include/content-include.mapper.spec.ts
+++ src/app/core/models/content-include/content-include.mapper.spec.ts
@@ -1,9 +1,22 @@
+import { TestBed } from '@angular/core/testing';
+
+import { STATIC_URL } from 'ish-core/utils/state-transfer/factories';
+
 import { ContentIncludeData } from './content-include.interface';
 import { ContentIncludeMapper } from './content-include.mapper';
 
 describe('Content Include Mapper', () => {
+  let contentIncludeMapper: ContentIncludeMapper;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [{ provide: STATIC_URL, useValue: 'http://www.example.org/static' }],
+    });
+    contentIncludeMapper = TestBed.get(ContentIncludeMapper);
+  });
+
   it('should throw on falsy input', () => {
-    expect(() => ContentIncludeMapper.fromData(undefined)).toThrowError('falsy input');
+    expect(() => contentIncludeMapper.fromData(undefined)).toThrowError('falsy input');
   });
 
   it('should convert a complex example to complex type', () => {
@@ -53,7 +66,7 @@ describe('Content Include Mapper', () => {
       ],
     };
 
-    const result = ContentIncludeMapper.fromData(input);
+    const result = contentIncludeMapper.fromData(input);
 
     expect(result.pagelets).toHaveLength(3);
     expect(result.pagelets.map(p => p.id)).toIncludeAllMembers(['pagelet1', 'pagelet11', 'pagelet2']);
