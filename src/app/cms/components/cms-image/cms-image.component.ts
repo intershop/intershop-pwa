f2be27cab2fa0a1d400e3e16802b76c597edd4ba
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-01-03 15:58:25 +0100

    move getRouterLink from ContentImageView to ContentViewHelper

--- src/app/cms/components/cms-image/cms-image.component.ts
+++ src/app/cms/components/cms-image/cms-image.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, DoCheck, Inject, Input } from '@angular/core';
 
 import { ContentImagePageletView, createImagePageletView } from 'ish-core/models/content-view/content-image-view';
+import { ContentViewHelper } from 'ish-core/models/content-view/content-view.helper';
 import { STATIC_URL } from 'ish-core/utils/state-transfer/factories';
 
 @Component({
@@ -12,10 +13,12 @@ export class CMSImageComponent implements DoCheck {
   @Input()
   pagelet: ContentImagePageletView;
 
+  routerLink = ContentViewHelper.getRouterLink;
+
   constructor(@Inject(STATIC_URL) public staticURL: string) {}
 
   ngDoCheck() {
-    if (this.pagelet && !this.pagelet.routerLink) {
+    if (this.pagelet && !this.pagelet.imagePath) {
       // tslint:disable-next-line:no-assignement-to-inputs
       this.pagelet = createImagePageletView(this.pagelet);
     }
---
+++ src/app/core/models/content-view/content-view.helper.spec.ts
@@ -0,0 +1,30 @@
+import * as using from 'jasmine-data-provider';
+
+import { createSimplePageletView } from 'ish-core/utils/dev/test-data-utils';
+
+import { ContentViewHelper } from './content-view.helper';
+
+describe('Content View Helper', () => {
+  describe('getRouterLink', () => {
+    using(
+      [
+        { input: 'route://category/Computers', expected: '/category/Computers' },
+        { input: 'route://category/Home-Entertainment.SmartHome', expected: '/category/Home-Entertainment.SmartHome' },
+        { input: 'product://201807195@inSPIRED-inTRONICS', expected: '/product/201807195' },
+      ],
+      ({ input, expected }) => {
+        it(`should transform ${input} to ${expected}`, () => {
+          const pagelet = createSimplePageletView({
+            definitionQualifiedName: 'fq',
+            id: 'id',
+            configurationParameters: {
+              Link: input,
+            },
+          });
+
+          expect(ContentViewHelper.getRouterLink(pagelet, 'Link')).toEqual(expected);
+        });
+      }
+    );
+  });
+});
