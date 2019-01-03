5483a53cdbc1dd725cfc6014dd8eab1c2dbc4120
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-01-03 15:58:25 +0100

    move getRouterLink from ContentImageView to ContentViewHelper

--- src/app/core/models/content-view/content-image-view.spec.ts
+++ src/app/core/models/content-view/content-image-view.spec.ts
@@ -3,7 +3,7 @@ import * as using from 'jasmine-data-provider';
 import { createSimplePageletView } from 'ish-core/utils/dev/test-data-utils';
 import { ContentPagelet } from '../content-pagelet/content-pagelet.model';
 
-import { createImagePageletView, getImagePath, getRouterLink } from './content-image-view';
+import { createImagePageletView, getImagePath } from './content-image-view';
 
 describe('Content Image View', () => {
   describe('ContentImagePageletView', () => {
@@ -21,23 +21,6 @@ describe('Content Image View', () => {
     });
 
     describe('view created', () => {
-      it('should return a routerLink if Link is set', () => {
-        const view = createImagePageletView(
-          createSimplePageletView({
-            ...pagelet,
-            configurationParameters: {
-              Link: 'route://category/Home-Entertainment.SmartHome',
-            },
-          })
-        );
-        expect(view.routerLink('Link')).toEqual('/category/Home-Entertainment.SmartHome');
-      });
-
-      it('should return home route if Link is not set', () => {
-        const view = createImagePageletView(createSimplePageletView(pagelet));
-        expect(view.routerLink('Link')).toEqual('/home');
-      });
-
       it('should return an image path if Image is set', () => {
         const view = createImagePageletView(
           createSimplePageletView({
@@ -55,22 +38,6 @@ describe('Content Image View', () => {
         expect(view.imagePath('Image', 'http://example.org')).toBeUndefined();
       });
     });
-  });
-
-  describe('getRouterLink', () => {
-    using(
-      [
-        { input: 'route://category/Computers', expected: '/category/Computers' },
-        { input: 'route://category/Home-Entertainment.SmartHome', expected: '/category/Home-Entertainment.SmartHome' },
-        { input: 'product://201807195@inSPIRED-inTRONICS', expected: '/product/201807195' },
-      ],
-      ({ input, expected }) => {
-        it(`should transform ${input} to ${expected}`, () => {
-          // tslint:disable-next-line deprecation
-          expect(getRouterLink(input)).toEqual(expected);
-        });
-      }
-    );
   });
 
   describe('getImagePath', () => {
--- src/app/core/models/content-view/content-image-view.ts
+++ src/app/core/models/content-view/content-image-view.ts
@@ -4,7 +4,6 @@ import { memoize } from 'lodash-es';
 import { ContentPageletView } from './content-views';
 
 export interface ContentImagePageletView extends ContentPageletView {
-  routerLink(configParam: string): string;
   imagePath(configParam: string, staticUrl: string): string;
 }
 
@@ -19,42 +18,6 @@ export const getImagePath = (configParam: string, staticURL: string): string => 
   return configParam;
 };
 
-/**
- * @deprecated only visible for testing
- */
-export const getRouterLink = (configParam: string): string => {
-  let routerLink = '/home';
-  const linkPrefix = !!configParam && configParam.includes(':') ? configParam.split(':')[0] : undefined;
-  switch (linkPrefix) {
-    case 'product':
-      // for consistent product links it should have the default category in the route
-      routerLink = `/product/${configParam.substring(10, configParam.indexOf('@'))}`;
-      break;
-    case 'category':
-      // the configuration parameter currently only works for first level categories
-      routerLink = `/category/${configParam.substring(11, configParam.indexOf('@'))}`;
-      break;
-    case 'page':
-      // we do not yet have a '/page' route
-      routerLink = `/page/${configParam.substring(7)}`;
-      break;
-    case 'route':
-      // direct router links for the PWA
-      routerLink = `/${configParam.substring(8)}`;
-      break;
-    case 'http':
-    case 'https':
-      // use 'href' instead of 'routerLink' with external links
-      // tslint:disable-next-line:no-console
-      console.log('External links need handling', configParam);
-      break;
-    default:
-      // tslint:disable-next-line:no-console
-      console.log('Unknown link type:', configParam);
-  }
-  return routerLink;
-};
-
 // tslint:disable deprecation
 export const createImagePageletView = (pagelet: ContentPageletView): ContentImagePageletView =>
   !pagelet
@@ -62,5 +25,4 @@ export const createImagePageletView = (pagelet: ContentPageletView): ContentImag
     : {
         ...pagelet,
         imagePath: memoize((configParam, staticUrl) => getImagePath(pagelet.stringParam(configParam), staticUrl)),
-        routerLink: memoize(configParam => getRouterLink(pagelet.stringParam(configParam))),
       };
