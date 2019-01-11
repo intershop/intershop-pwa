d7fa19666df4e828cc55abcc802d4dc1ec40d5b1
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-01-11 13:50:57 +0100

    move feature toggle quards for quoting before auth guards

--- src/app/extensions/quoting/pages/quote-request-edit/quote-request-edit-page.module.ts
+++ src/app/extensions/quoting/pages/quote-request-edit/quote-request-edit-page.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { FeatureToggleGuard } from 'ish-core/feature-toggle.module';
 import { QuotingModule } from '../../quoting.module';
 
 import { QuoteRequestEditPageContainerComponent } from './quote-request-edit-page.container';
 
 const quoteRequestEditPageRoutes: Routes = [
-  {
-    path: ':quoteRequestId',
-    component: QuoteRequestEditPageContainerComponent,
-    canActivate: [FeatureToggleGuard],
-    data: { feature: 'quoting' },
-  },
+  { path: ':quoteRequestId', component: QuoteRequestEditPageContainerComponent },
 ];
 
 @NgModule({
