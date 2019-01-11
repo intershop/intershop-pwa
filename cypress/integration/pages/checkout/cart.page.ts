01f0f96cfa7dd8af1fae2f8020a93bc21c48c792
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-01-11 17:33:50 +0100

    ISREST-554: reconstruct error when merging baskets with e2e test

--- cypress/integration/pages/checkout/cart.page.ts
+++ cypress/integration/pages/checkout/cart.page.ts
@@ -1,5 +1,9 @@
+import { HeaderModule } from '../header.module';
+
 export class CartPage {
   readonly tag = 'ish-shopping-basket';
+
+  readonly header = new HeaderModule();
 
   beginCheckout() {
     cy.wait(1000);
@@ -7,5 +11,39 @@ export class CartPage {
       .find('button')
       .contains('Checkout')
       .click();
+  }
+
+  get lineItems() {
+    return cy.get(this.tag).find('div.pli-description');
+  }
+
+  lineItem(idx: number) {
+    return {
+      quantity: {
+        set: (num: number) =>
+          cy
+            .get(this.tag)
+            .find('input[data-testing-id="quantity"]')
+            .eq(idx)
+            .clear()
+            .type(num.toString())
+            .blur(),
+      },
+      remove: () =>
+        cy
+          .get(this.tag)
+          .find('svg[data-icon="trash-alt"]')
+          .eq(idx)
+          .click(),
+    };
+  }
+
+  get summary() {
+    return {
+      subtotal: cy
+        .get('ish-basket-cost-summary')
+        .find('dd')
+        .first(),
+    };
   }
 }
