60c350ea970c81838879d36715d430489a435a06
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-01-14 12:44:45 +0100

    comments on TestStore

--- src/app/core/utils/dev/ngrx-testing.ts
+++ src/app/core/utils/dev/ngrx-testing.ts
@@ -1,21 +1,26 @@
-// tslint:disable:no-console no-any
+// tslint:disable:no-any no-console
 import { Injectable, Type } from '@angular/core';
 import { Actions, Effect, EffectsModule } from '@ngrx/effects';
 import { Action, ActionReducerMap, Store, StoreModule } from '@ngrx/store';
 import { filter, tap } from 'rxjs/operators';
 
-export const containsActionWithType = (type: string) =>
-  ((actions: Action[]) => !!actions.filter(a => a.type === type).length) as ((arg?: any) => boolean);
+export const containsActionWithType = (type: string) => (actions: Action[]) =>
+  !!actions.filter(a => a.type === type).length;
 
-export const containsActionWithTypeAndPayload = (type: string, predicate: (payload: any) => boolean) =>
-  ((actions: { type: string; payload: any }[]) =>
-    !!actions.filter(a => a.type === type && predicate(a.payload)).length) as ((arg?: any) => boolean);
+export const containsActionWithTypeAndPayload = (type: string, predicate: (payload: any) => boolean) => (
+  actions: { type: string; payload: unknown }[]
+) => !!actions.filter(a => a.type === type && predicate(a.payload)).length;
 
 function includeAction(action: Action, include: (string | RegExp)[]) {
   const type = action.type;
   return include.some(inc => (typeof inc === 'string' ? type.indexOf(inc) >= 0 : type.search(inc) >= 0));
 }
 
+/**
+ * Service for monitoring actions and the current state.
+ * ! The listening is solved as Effects so it can be managed by the EffectsModule.
+ * ! Otherwise the order of the actions is interferred with.
+ */
 @Injectable()
 export class TestStore {
   private actions: Action[] = [];
