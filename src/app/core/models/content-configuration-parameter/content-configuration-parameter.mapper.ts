59b9273be5cf1c81df4b161ed1d45b96c33c0292
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-01-16 14:45:38 +0100

    include locale in CMS managed image URLs

--- src/app/core/models/content-configuration-parameter/content-configuration-parameter.mapper.ts
+++ src/app/core/models/content-configuration-parameter/content-configuration-parameter.mapper.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 
 import { getICMStaticURL } from 'ish-core/store/configuration';
+import { getCurrentLocale } from 'ish-core/store/locale';
+import { mapToProperty } from 'ish-core/utils/operators';
 
 import { ContentConfigurationParameterData } from './content-configuration-parameter.interface';
 
@@ -12,9 +14,16 @@ export declare interface ContentConfigurationParameters {
 @Injectable({ providedIn: 'root' })
 export class ContentConfigurationParameterMapper {
   private staticURL: string;
+  private lang = '-';
 
   constructor(store: Store<{}>) {
     store.pipe(select(getICMStaticURL)).subscribe(url => (this.staticURL = url));
+    store
+      .pipe(
+        select(getCurrentLocale),
+        mapToProperty('lang')
+      )
+      .subscribe(lang => (this.lang = lang || '-'));
   }
 
   fromData(data: { [name: string]: ContentConfigurationParameterData }): ContentConfigurationParameters {
@@ -30,7 +39,7 @@ export class ContentConfigurationParameterMapper {
       .filter(key => key.startsWith('Image') && data[key] && data[key].toString().includes(':'))
       .forEach(key => {
         const split = data[key].toString().split(':');
-        data[key] = `${this.staticURL}/${split[0]}/-${split[1]}`;
+        data[key] = `${this.staticURL}/${split[0]}/${this.lang}${split[1]}`;
       });
     return data;
   }
