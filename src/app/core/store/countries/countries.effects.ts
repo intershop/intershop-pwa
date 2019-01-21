71cc80883b891ed93936b456e82589bda69a089f
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-01-21 23:59:17 +0100

    prevent infinite loop in countries effects when http request fails

--- src/app/core/store/countries/countries.effects.ts
+++ src/app/core/store/countries/countries.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Store, select } from '@ngrx/store';
 import { ROUTER_NAVIGATION_TYPE } from 'ngrx-router';
-import { concatMap, concatMapTo, filter, map, take } from 'rxjs/operators';
+import { concatMap, filter, map, withLatestFrom } from 'rxjs/operators';
 
 import { mapErrorToAction } from 'ish-core/utils/operators';
 import { CountryService } from '../../services/country/country.service';
@@ -17,13 +17,8 @@ export class CountriesEffects {
   @Effect()
   loadCountries$ = this.actions$.pipe(
     ofType(ROUTER_NAVIGATION_TYPE),
-    take(1),
-    concatMapTo(
-      this.store.pipe(
-        select(getAllCountries),
-        filter(countries => !countries.length)
-      )
-    ),
+    withLatestFrom(this.store.pipe(select(getAllCountries))),
+    filter(([, countries]) => !countries.length),
     concatMap(() =>
       this.countryService.getCountries().pipe(
         map(countries => new countryActions.LoadCountriesSuccess({ countries })),
