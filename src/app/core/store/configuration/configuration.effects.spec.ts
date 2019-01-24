806d03c3b86c3baa5afa6bb668f90e020d3270f8
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-01-24 13:53:23 +0100

    remove InitialNavigationGuard and move solution to configuration effects

--- src/app/core/store/configuration/configuration.effects.spec.ts
+++ src/app/core/store/configuration/configuration.effects.spec.ts
@@ -1,30 +1,51 @@
-import { TestBed } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ROOT_EFFECTS_INIT } from '@ngrx/effects';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { Action, StoreModule } from '@ngrx/store';
+import { Action } from '@ngrx/store';
 import { Observable, Subject, of } from 'rxjs';
 import { take } from 'rxjs/operators';
 
-import { coreReducers } from '../core-store.module';
+import { Locale } from 'ish-core/models/locale/locale.model';
+import { TestStore, ngrxTesting } from 'ish-core/utils/dev/ngrx-testing';
+import { SetAvailableLocales, getCurrentLocale } from '../locale';
+import { localeReducer } from '../locale/locale.reducer';
 
-import { ConfigurationActionTypes } from './configuration.actions';
+import { ApplyConfiguration, ConfigurationActionTypes } from './configuration.actions';
 import { ConfigurationEffects } from './configuration.effects';
+import { configurationReducer } from './configuration.reducer';
+import { getFeatures, getRestEndpoint } from './configuration.selectors';
 
 describe('Configuration Effects', () => {
   let actions$: Observable<Action>;
   let effects: ConfigurationEffects;
+  let router: Router;
+  let location: Location;
+  let store$: TestStore;
 
   beforeEach(() => {
+    // tslint:disable-next-line:use-component-change-detection
+    @Component({ template: 'dummy' })
+    // tslint:disable-next-line:prefer-mocks-instead-of-stubs-in-tests
+    class DummyComponent {}
+
     TestBed.configureTestingModule({
+      declarations: [DummyComponent],
       imports: [
-        StoreModule.forRoot({
-          ...coreReducers,
-        }),
+        ...ngrxTesting({ configuration: configurationReducer, locale: localeReducer }, [ConfigurationEffects]),
+        RouterTestingModule.withRoutes([{ path: 'home', component: DummyComponent }]),
       ],
       providers: [ConfigurationEffects, provideMockActions(() => actions$)],
     });
 
     effects = TestBed.get(ConfigurationEffects);
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+    store$ = TestBed.get(TestStore);
+    store$.dispatch(new SetAvailableLocales({ locales: [{ lang: 'en_US' }, { lang: 'de_DE' }] as Locale[] }));
   });
 
   describe('setInitialRestEndpoint$', () => {
@@ -46,5 +67,67 @@ describe('Configuration Effects', () => {
       );
       // tslint:enable:use-async-synchronisation-in-tests
     });
+  });
+
+  describe('routerWatch$', () => {
+    it('should set imported channel to state', fakeAsync(() => {
+      router.navigateByUrl('/home;channel=site');
+      tick(500);
+      expect(location.path()).toMatchInlineSnapshot(`"/home;channel=site"`);
+      expect(getRestEndpoint(store$.state)).toMatchInlineSnapshot(`"http://localhost:4200/INTERSHOP/rest/WFS/site/-"`);
+    }));
+
+    it('should set imported channel and application to state', fakeAsync(() => {
+      router.navigateByUrl('/home;channel=site;application=app');
+      tick(500);
+      expect(location.path()).toMatchInlineSnapshot(`"/home;channel=site;application=app"`);
+      expect(getRestEndpoint(store$.state)).toMatchInlineSnapshot(
+        `"http://localhost:4200/INTERSHOP/rest/WFS/site/app"`
+      );
+    }));
+
+    it('should set imported channel to state and redirect if requested', fakeAsync(() => {
+      router.navigateByUrl('/home;channel=site;redirect=1');
+      tick(500);
+      expect(location.path()).toMatchInlineSnapshot(`"/home"`);
+      expect(getRestEndpoint(store$.state)).toMatchInlineSnapshot(`"http://localhost:4200/INTERSHOP/rest/WFS/site/-"`);
+    }));
+
+    it('should preserve query parameters when redirecting', fakeAsync(() => {
+      router.navigateByUrl('/home;channel=site;redirect=1?foo=bar&test=hello');
+      tick(500);
+      expect(location.path()).toMatchInlineSnapshot(`"/home?foo=bar&test=hello"`);
+      expect(getRestEndpoint(store$.state)).toMatchInlineSnapshot(`"http://localhost:4200/INTERSHOP/rest/WFS/site/-"`);
+    }));
+
+    it('should set imported features to state', fakeAsync(() => {
+      router.navigateByUrl('/home;features=a,b,c;redirect=1');
+      tick(500);
+      expect(location.path()).toMatchInlineSnapshot(`"/home"`);
+      expect(getFeatures(store$.state)).toIncludeAllMembers(['a', 'b', 'c']);
+    }));
+
+    it('should unset features if "none" was provided', fakeAsync(() => {
+      store$.dispatch(new ApplyConfiguration({ features: ['a', 'b', 'c'] }));
+      router.navigateByUrl('/home;features=none;redirect=1');
+      tick(500);
+      expect(location.path()).toMatchInlineSnapshot(`"/home"`);
+      expect(getFeatures(store$.state)).toBeEmpty();
+    }));
+
+    it('should not set features if "default" was provided', fakeAsync(() => {
+      store$.dispatch(new ApplyConfiguration({ features: ['a', 'b', 'c'] }));
+      router.navigateByUrl('/home;features=default;redirect=1');
+      tick(500);
+      expect(location.path()).toMatchInlineSnapshot(`"/home"`);
+      expect(getFeatures(store$.state)).toIncludeAllMembers(['a', 'b', 'c']);
+    }));
+
+    it('should set imported locale to state', fakeAsync(() => {
+      router.navigateByUrl('/home;redirect=1;lang=de_DE');
+      tick(500);
+      expect(location.path()).toMatchInlineSnapshot(`"/home"`);
+      expect(getCurrentLocale(store$.state).lang).toEqual('de_DE');
+    }));
   });
 });
--- src/app/core/store/configuration/configuration.effects.ts
+++ src/app/core/store/configuration/configuration.effects.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, ActivationStart, NavigationEnd, ParamMap, Router } from '@angular/router';
 import { Actions, Effect, ROOT_EFFECTS_INIT, ofType } from '@ngrx/effects';
-import { map, take, withLatestFrom } from 'rxjs/operators';
+import { filter, map, mergeMap, take, takeWhile, tap, withLatestFrom } from 'rxjs/operators';
 
 import { StatePropertiesService } from 'ish-core/utils/state-transfer/state-properties.service';
+import { SelectLocale } from '../locale';
 
 import { ApplyConfiguration } from './configuration.actions';
+import { ConfigurationState } from './configuration.reducer';
 
 @Injectable()
 export class ConfigurationEffects {
-  constructor(private actions$: Actions, private stateProperties: StatePropertiesService) {}
+  constructor(private actions$: Actions, private stateProperties: StatePropertiesService, private router: Router) {}
+
+  @Effect()
+  routerWatch$ = this.router.events.pipe(
+    takeWhile(event => !(event instanceof NavigationEnd)),
+    filter<ActivationStart>(event => event instanceof ActivationStart),
+    map(event => event.snapshot),
+    tap(snapshot => this.redirectIfNeeded(snapshot)),
+    mergeMap(({ paramMap }) => [...this.extractConfigurationParameters(paramMap), ...this.extractLanguage(paramMap)])
+  );
 
   @Effect()
   setInitialRestEndpoint$ = this.actions$.pipe(
@@ -29,4 +41,43 @@ export class ConfigurationEffects {
         new ApplyConfiguration({ baseURL, server, serverStatic, channel, application, features })
     )
   );
+
+  extractConfigurationParameters(paramMap: ParamMap) {
+    const keys: (keyof ConfigurationState)[] = ['channel', 'baseURL', 'application'];
+    const properties: { [id: string]: unknown } = keys
+      .filter(key => paramMap.has(key))
+      .map(key => ({ [key]: paramMap.get(key) }))
+      .reduce((acc, val) => ({ ...acc, ...val }), {});
+
+    if (paramMap.has('features') && paramMap.get('features') !== 'default') {
+      if (paramMap.get('features') === 'none') {
+        properties.features = [];
+      } else {
+        properties.features = paramMap.get('features').split(/,/g);
+      }
+    }
+
+    return Object.keys(properties).length ? [new ApplyConfiguration(properties)] : [];
+  }
+
+  extractLanguage(paramMap: ParamMap) {
+    return paramMap.has('lang') ? [new SelectLocale({ lang: paramMap.get('lang') })] : [];
+  }
+
+  getResolvedUrl(route: ActivatedRouteSnapshot): string {
+    const url = route.pathFromRoot.map(v => v.url.map(segment => segment.toString()).join('/')).join('/');
+    const params = Object.entries(route.queryParams)
+      .map(kvp => kvp.join('='))
+      .join('&');
+    return url + (params ? '?' + params : '');
+  }
+
+  redirectIfNeeded(snapshot: ActivatedRouteSnapshot) {
+    if (snapshot.paramMap.has('redirect')) {
+      const url = this.getResolvedUrl(snapshot);
+      const params = url.match(/\/.*?(;[^?]*).*?/);
+      const navigateTo = url.replace(params[1], '');
+      return this.router.navigateByUrl(navigateTo);
+    }
+  }
 }
