a733c8ec00940e861479853a530a1e631784519b
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-01-31 15:45:42 +0100

    build nginx from scratch

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -31,8 +31,6 @@ do
   i=$((i+1))
 done
 
-find /etc/nginx/conf.d -name '*.conf' -print -exec cat '{}' \;
-
 # Generate Pagespeed config based on environment variables
 env | grep NPSC_ | sed -e 's/^NPSC_//g' -e "s/\([A-Z_]*\)=/\L\1=/g" -e "s/_\([a-zA-Z]\)/\u\1/g" -e "s/^\([a-zA-Z]\)/\u\1/g" -e 's/=.*$//' -e 's/\=/ /' -e 's/^/\pagespeed /' > /tmp/pagespeed-prefix.txt
 
@@ -40,9 +38,11 @@ env | grep NPSC_ | sed -e 's/^[^=]*=//' -e 's/$/;/' > /tmp/pagespeed-suffix.txt
 
 paste -d" " /tmp/pagespeed-prefix.txt /tmp/pagespeed-suffix.txt >> /etc/nginx/pagespeed.conf
 
+find /etc/nginx -name '*.conf' -print -exec cat '{}' \;
+
 if [ -z "$*" ]
 then
-  nginx -g "daemon off;"
+  /usr/local/nginx/sbin/nginx -c /etc/nginx/nginx.conf -g "daemon off;"
 else
   exec "$@"
 fi
