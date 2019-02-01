276eb7d3ad2e445a0302866bf9575e3c02dde48c
Stefan Hauke <s.hauke@intershop.de>
2019-02-01 10:01:16 +0100

    move SafeHtmlPipe form CMS module to the global Pipes module so it can be used everywhere

--- src/app/core/pipes.module.ts
+++ src/app/core/pipes.module.ts
@@ -5,10 +5,19 @@ import { PricePipe } from './models/price/price.pipe';
 import { DatePipe } from './pipes/date.pipe';
 import { MakeHrefPipe } from './pipes/make-href.pipe';
 import { ProductRoutePipe } from './pipes/product-route.pipe';
+import { SafeHtmlPipe } from './pipes/safe-html.pipe';
 import { SanitizePipe } from './pipes/sanitize.pipe';
 
 @NgModule({
-  declarations: [AttributeToStringPipe, DatePipe, MakeHrefPipe, PricePipe, ProductRoutePipe, SanitizePipe],
-  exports: [AttributeToStringPipe, DatePipe, MakeHrefPipe, PricePipe, ProductRoutePipe, SanitizePipe],
+  declarations: [
+    AttributeToStringPipe,
+    DatePipe,
+    MakeHrefPipe,
+    PricePipe,
+    ProductRoutePipe,
+    SafeHtmlPipe,
+    SanitizePipe,
+  ],
+  exports: [AttributeToStringPipe, DatePipe, MakeHrefPipe, PricePipe, ProductRoutePipe, SafeHtmlPipe, SanitizePipe],
 })
 export class PipesModule {}
