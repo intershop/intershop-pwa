30fa60279c6919e710447ffab0425e428128d2fe
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-02-05 12:23:02 +0100

    use value of environment variable DISPLAY_VERSION on server side for version display in client footer

--- src/app/shell/footer/components/footer/footer.component.ts
+++ src/app/shell/footer/components/footer/footer.component.ts
@@ -1,20 +1,27 @@
 // NEEDS_WORK: DUMMY COMPONENT
 import { isPlatformBrowser } from '@angular/common';
 import { ChangeDetectionStrategy, Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { TransferState } from '@angular/platform-browser';
 
 import { MEDIUM_BREAKPOINT_WIDTH } from 'ish-core/configurations/injection-keys';
+import { DISPLAY_VERSION } from 'ish-core/configurations/state-keys';
 
+/**
+ * Footer Component
+ * TODO: the content of the footer needs to come from the Intershop CMS
+ */
 @Component({
   selector: 'ish-footer',
   templateUrl: './footer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent implements OnInit {
-  // TODO: the content of the footer needs to come from the Intershop CMS
+  appVersion: string;
 
   constructor(
     @Inject(MEDIUM_BREAKPOINT_WIDTH) private mediumBreakpointWidth: number,
-    @Inject(PLATFORM_ID) private platformId: string
+    @Inject(PLATFORM_ID) private platformId: string,
+    private transferState: TransferState
   ) {}
 
   collapsed: boolean[] = [false, false, false, false, false, false];
@@ -22,6 +29,7 @@ export class FooterComponent implements OnInit {
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
       this.collapsed = this.collapsed.map(() => window.innerWidth < this.mediumBreakpointWidth);
+      this.appVersion = this.transferState.get(DISPLAY_VERSION, '');
     }
   }
 
