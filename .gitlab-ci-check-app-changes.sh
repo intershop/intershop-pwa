ef7860acb61aa59cea1199e7c80a9c5cc2e66257
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-02-06 14:47:29 +0100

    skip nginx image build when no changes were comitted

--- .gitlab-ci-check-app-changes.sh
+++ .gitlab-ci-check-app-changes.sh
@@ -2,4 +2,7 @@
 
 set -e
 
-git diff --name-only "$(git merge-base HEAD origin/develop)" HEAD | grep -qE '(^src|^[^/]*$)'
+CHANGES_REGEX='(^src|^[^/]*$)'
+[ ! -z "$1" ] && CHANGES_REGEX="$1"
+
+git diff --name-only "$(git merge-base HEAD origin/develop)" HEAD | grep -qE "$CHANGES_REGEX"
