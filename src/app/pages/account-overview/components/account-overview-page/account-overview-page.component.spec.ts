a543efb30e12238fa905d56566d1c5a284c95506
Silke Grüber <s.grueber@intershop.de>
2019-02-19 10:29:41 +0100

    display addresses on account overview widget

--- src/app/pages/account-overview/components/account-overview-page/account-overview-page.component.spec.ts
+++ src/app/pages/account-overview/components/account-overview-page/account-overview-page.component.spec.ts
@@ -53,6 +53,12 @@ describe('Account Overview Page Component', () => {
     expect(element.querySelector('h1').textContent).toContain(user.firstName);
   });
 
+  it('should display dashboard on page', () => {
+    fixture.detectChanges();
+    expect(element.querySelector('.account-dashboard')).toBeTruthy();
+    expect(element.querySelectorAll('div.circle-icon fa-icon')).toHaveLength(3);
+  });
+
   it('should render order widget component on page', () => {
     fixture.detectChanges();
     expect(element.querySelector('ish-order-widget')).toBeTruthy();
