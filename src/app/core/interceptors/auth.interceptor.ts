dde8fb389d6850b96ee01f35f45296b1d50da206
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-02-27 14:48:46 +0100

    ISREST-657: set authentication token in ApiService

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,18 +1,16 @@
 // tslint:disable:no-any
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Store, select } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { concatMap, map, take, tap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
-import { SetAPIToken, getAPIToken } from '../store/user';
-
-const tokenHeaderKeyName = 'authentication-token';
-const authorizationHeaderKey = 'Authorization';
+import { ApiService } from 'ish-core/services/api/api.service';
+import { SetAPIToken } from '../store/user';
 
 function setTokenFromResponse(store: Store<{}>, event: HttpEvent<any>) {
   if (event instanceof HttpResponse) {
-    const apiToken = event.headers.get(tokenHeaderKeyName);
+    const apiToken = event.headers.get(ApiService.TOKEN_HEADER_KEY);
     if (apiToken) {
       store.dispatch(new SetAPIToken({ apiToken }));
     }
@@ -20,7 +18,6 @@ function setTokenFromResponse(store: Store<{}>, event: HttpEvent<any>) {
 }
 
 /**
- * Intercepts outgoing HTTP request and sets authentication-token if available.
  * Intercepts incoming HTTP response and updates authentication-token.
  */
 @Injectable()
@@ -28,15 +25,6 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private store: Store<{}>) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return this.store.pipe(
-      select(getAPIToken),
-      take(1),
-      map(token =>
-        token && !req.headers.has(authorizationHeaderKey)
-          ? req.clone({ headers: req.headers.set(tokenHeaderKeyName, token) })
-          : req
-      ),
-      concatMap(r => next.handle(r).pipe(tap(event => setTokenFromResponse(this.store, event))))
-    );
+    return next.handle(req).pipe(tap(event => setTokenFromResponse(this.store, event)));
   }
 }
