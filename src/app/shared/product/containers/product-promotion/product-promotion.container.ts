3b03870c9ab9c266841e04463fbc5a87fcafd7bb
Matthias Legeler <m.legeler@intershop.de>
2019-04-12 13:36:45 +0200

    ISREST-695: rework to promotions

--- src/app/shared/product/containers/product-promotion/product-promotion.container.ts
+++ src/app/shared/product/containers/product-promotion/product-promotion.container.ts
@@ -3,6 +3,7 @@ import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { filter, take } from 'rxjs/operators';
 
+import { Product } from 'ish-core/models/product/product.model';
 import { Promotion } from 'ish-core/models/promotion/promotion.model';
 import { LoadPromotion, getPromotion } from 'ish-core/store/shopping/promotions';
 
@@ -12,20 +13,21 @@ import { LoadPromotion, getPromotion } from 'ish-core/store/shopping/promotions'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductPromotionContainerComponent implements OnInit {
-  @Input() promotionId: string;
+  @Input() product: Product;
 
   promotion$: Observable<Promotion>;
 
   constructor(private store: Store<{}>) {}
 
   ngOnInit() {
-    this.promotion$ = this.store.pipe(select(getPromotion, { id: this.promotionId }));
+    this.promotion$ = this.store.pipe(select(getPromotion, { promoId: this.product.promotions[0].itemid }));
+
     // Checks if the promotion is already in the store and only dispatches a LoadPromotion action if it is not
     this.promotion$
       .pipe(
         take(1),
         filter(x => !x)
       )
-      .subscribe(() => this.store.dispatch(new LoadPromotion({ id: this.promotionId })));
+      .subscribe(() => this.store.dispatch(new LoadPromotion({ promoId: this.product.promotions[0].itemid })));
   }
 }
