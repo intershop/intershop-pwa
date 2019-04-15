b05fcd6900d6c1ace129fc71784c7a1f9c587cf0
Matthias Legeler <m.legeler@intershop.de>
2019-04-15 10:09:05 +0200

    ISREST-695: added product promotion container to tile, row and details

--- src/app/shared/product/components/product-tile/product-tile.component.spec.ts
+++ src/app/shared/product/components/product-tile/product-tile.component.spec.ts
@@ -10,6 +10,7 @@ import { PipesModule } from 'ish-core/pipes.module';
 import { configurationReducer } from 'ish-core/store/configuration/configuration.reducer';
 import { LazyProductAddToQuoteComponent } from '../../../../extensions/quoting/exports/product/components/lazy-product-add-to-quote/lazy-product-add-to-quote.component';
 import { ProductImageComponent } from '../../../../shell/header/components/product-image/product-image.component';
+import { ProductPromotionContainerComponent } from '../../containers/product-promotion/product-promotion.container';
 import { ProductAddToBasketComponent } from '../product-add-to-basket/product-add-to-basket.component';
 import { ProductAddToCompareComponent } from '../product-add-to-compare/product-add-to-compare.component';
 import { ProductLabelComponent } from '../product-label/product-label.component';
@@ -39,6 +40,7 @@ describe('Product Tile Component', () => {
         MockComponent(ProductImageComponent),
         MockComponent(ProductLabelComponent),
         MockComponent(ProductPriceComponent),
+        MockComponent(ProductPromotionContainerComponent),
         MockComponent(ProductVariationSelectComponent),
         ProductTileComponent,
       ],
