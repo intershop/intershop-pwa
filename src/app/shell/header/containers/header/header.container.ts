024dc84cb6726e17795125cd1c5e259f839a156b
Susanne Schneider <s.schneider@intershop.de>
2019-04-26 11:09:57 +0200

    ISREST-509: header container gets sticky class after scrolling (for all pages except error and checkout pages)

--- src/app/shell/header/containers/header/header.container.ts
+++ src/app/shell/header/containers/header/header.container.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 
 import { getHeaderType } from 'ish-core/store/viewconf';
@@ -10,6 +10,20 @@ import { getHeaderType } from 'ish-core/store/viewconf';
 })
 export class HeaderContainerComponent {
   headerType$ = this.store.pipe(select(getHeaderType));
+  headerType = '';
+  isSticky = false;
 
   constructor(private store: Store<{}>) {}
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    this.headerType$.subscribe(value => (this.headerType = value));
+    this.isSticky = window.pageYOffset >= 170;
+    // TODO: use a store value here as it is done with wrapperClass (app component)
+    if (this.isSticky && this.headerType !== 'simple' && this.headerType !== 'checkout') {
+      document.getElementsByTagName('header')[0].parentElement.classList.add('sticky-header');
+    } else {
+      document.getElementsByTagName('header')[0].parentElement.classList.remove('sticky-header');
+    }
+  }
 }
