31482d69601dad98256f6b4e09545071d0ee9351
Sylvio Koch <s.koch@intershop.de>
2019-05-13 10:47:43 +0200

    ISREST-596: fix region hanlding in address processing

--- src/app/core/store/regions/regions.selectors.ts
+++ src/app/core/store/regions/regions.selectors.ts
@@ -12,12 +12,21 @@ const getRegionState = createSelector(
 
 export const { selectAll: getAllRegions } = regionAdapter.getSelectors(getRegionState);
 
-export const getRegionsByCountryCode = createSelector(
-  getAllRegions,
-  (entities, props: { countryCode: string }): Region[] => entities.filter(e => e.countryCode === props.countryCode)
-);
-
 export const getRegionsLoading = createSelector(
   getRegionState,
   regions => regions.loading
+);
+
+export const getRegionsByCountryCode = createSelector(
+  getAllRegions,
+  getRegionsLoading,
+  (entities, loading, props: { countryCode: string }): Region[] => {
+    const regionsForCountry = entities.filter(e => e.countryCode === props.countryCode);
+    // when still loading, do not return empty array because we can't be sure there are no regions
+    if (loading && !regionsForCountry.length) {
+      return;
+    }
+
+    return regionsForCountry;
+  }
 );
