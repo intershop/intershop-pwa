399a358a3957941f24ac5bdcb3165d8e625eabb5
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-05-15 15:11:29 +0200

    add basket via rest

--- cypress/integration/framework/users.ts
+++ cypress/integration/framework/users.ts
@@ -1,3 +1,4 @@
+// tslint:disable:no-unused-expression
 import { Registration } from '../pages/account/registration.page';
 
 export function createUserViaREST(user: Partial<Registration>) {
@@ -32,4 +33,39 @@ export function createUserViaREST(user: Partial<Registration>) {
   cy.request('POST', Cypress.env('ICM_BASE_URL') + '/INTERSHOP/rest/WFS/inSPIRED-inTRONICS-Site/-/customers', customer)
     .its('status')
     .should('equal', 201);
+}
+
+export function createBasketViaREST(user: Partial<Registration>, lineItems: { [sku: string]: number }) {
+  cy.request({
+    method: 'POST',
+    url: Cypress.env('ICM_BASE_URL') + '/INTERSHOP/rest/WFS/inSPIRED-inTRONICS-Site/-/baskets',
+    body: {},
+    auth: {
+      user: user.login,
+      pass: user.password,
+    },
+    headers: {
+      Accept: 'application/vnd.intershop.basket.v1+json',
+    },
+  }).then(basketCreationResponse => {
+    expect(basketCreationResponse.status).to.equal(201);
+    const basketUrl = basketCreationResponse.body.links.self;
+    expect(basketUrl).not.to.be.empty;
+    const authToken = basketCreationResponse.headers['authentication-token'];
+    expect(authToken).not.to.be.empty;
+
+    const body = Object.keys(lineItems).map(product => ({ product, quantity: { value: lineItems[product] } }));
+
+    cy.request({
+      method: 'POST',
+      url: basketUrl + '/items',
+      body,
+      headers: {
+        'authentication-token': authToken,
+        Accept: 'application/vnd.intershop.basket.v1+json',
+      },
+    }).then(data => {
+      expect(data.status).to.equal(201);
+    });
+  });
 }
