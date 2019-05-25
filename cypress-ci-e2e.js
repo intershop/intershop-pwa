f1a8e0082689d39b9bdb884caa46f606e963e473
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-05-25 18:32:50 +0200

    tune settings for slower build slave

--- cypress-ci-e2e.js
+++ cypress-ci-e2e.js
@@ -23,6 +23,7 @@ if (!process.env.ICM_BASE_URL) {
 
 const DEFAULT_CONFIG = {
   browser: BROWSER,
+  defaultCommandTimeout: 15000,
   reporter: 'junit',
   reporterOptions: 'mochaFile=reports/e2e-remote-[hash]-report.xml,includePending=true',
   spec: TEST_FILES,
