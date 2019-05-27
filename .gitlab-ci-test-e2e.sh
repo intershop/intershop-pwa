c1e1bd000f1870f2370d8e565282b4b210fdeed9
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-05-27 11:07:51 +0200

    resolve PWA_BASE_URL inside script

--- .gitlab-ci-test-e2e.sh
+++ .gitlab-ci-test-e2e.sh
@@ -5,5 +5,13 @@ set -e
 [ -z "$1" ] && echo "test file required" && exit 1
 files="$(echo "$*" | tr ' ' ',')"
 
+export CYPRESS_NO_CHANNEL_REDIRECT=1
+
+echo "PWA_BASE_URL is $PWA_BASE_URL"
+export PWA_BASE_URL="$(curl -Ls -o /dev/null -w %{url_effective} $PWA_BASE_URL)"
+echo "effective PWA_BASE_URL is $PWA_BASE_URL"
+
+wget --wait 10 --tries 10 --retry-connrefused $PWA_BASE_URL
+
 TIMEOUT="20m"
 timeout $TIMEOUT node cypress-ci-e2e "$files" || ([ "$?" -eq "124" ] && timeout $TIMEOUT node cypress-ci-e2e "$files") || ([ "$?" -eq "124" ] && timeout $TIMEOUT node cypress-ci-e2e "$files")
