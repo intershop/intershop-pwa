583ebd13572769de4376109caddeaef3d2fcf6d3
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-06-07 00:47:17 +0200

    adopt new tslint rules

--- src/app/core/utils/operators.spec.ts
+++ src/app/core/utils/operators.spec.ts
@@ -1,12 +1,11 @@
 import { HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Action } from '@ngrx/store';
 import { cold, hot } from 'jest-marbles';
-import { Observable, merge, of } from 'rxjs';
-import { concatMap, filter, last, mapTo } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 import { HttpError } from 'ish-core/models/http-error/http-error.model';
 
-import { Partition, distinctCompareWith, mapErrorToAction, mapToProperty, partitionBy } from './operators';
+import { distinctCompareWith, mapErrorToAction, mapToProperty } from './operators';
 
 describe('Operators', () => {
   describe('distinctCompareWith', () => {
@@ -24,53 +23,6 @@ describe('Operators', () => {
       const expt$ = cold('--b---a-----b-c-a--');
 
       expect(input$.pipe(distinctCompareWith(compa$))).toBeObservable(expt$);
-    });
-  });
-
-  describe('partitionBy', () => {
-    describe('merge usage', () => {
-      it('should always split input into two streams', () => {
-        const input$ = hot('1-2-3-4-5-6-7-8-9--|') as Observable<number>;
-        const resu$ = cold('--X---X-5-X-7-X-9--(C|)');
-
-        const output$ = input$.pipe(
-          partitionBy(num => num % 2 === 0),
-          concatMap(part =>
-            merge(
-              part.isTrue.pipe(mapTo('X')),
-              part.isFalse.pipe(filter(x => x > 4)),
-              part.isTrue.pipe(
-                last(),
-                mapTo('C')
-              )
-            )
-          )
-        );
-
-        expect(output$).toBeObservable(resu$);
-      });
-    });
-
-    describe('local variable usage', () => {
-      let partition$: Observable<Partition<number>>;
-
-      beforeEach(() => {
-        const input$ = hot('1-2-3-4-5-6-7-8-9--|') as Observable<number>;
-        partition$ = input$.pipe(partitionBy(x => x % 2 === 0));
-      });
-
-      it('should fire when true path emits', () => {
-        const output$ = partition$.pipe(concatMap(part => part.isTrue));
-        expect(output$).toBeObservable(cold('--2---4---6---8----|'));
-      });
-
-      it('should fire when false path emits', () => {
-        const output$ = partition$.pipe(
-          concatMap(part => part.isFalse),
-          mapTo('X')
-        );
-        expect(output$).toBeObservable(cold('X---X---X---X---X--|'));
-      });
     });
   });
 
