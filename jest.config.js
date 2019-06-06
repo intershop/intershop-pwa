5e171eaf74da3060bd45a783ed0984e409b72226
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-06-07 00:01:02 +0200

    upgrade jest and dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'jest-preset-angular',
   roots: ['src'],
-  setupTestFrameworkScriptFile: '<rootDir>/src/test.ts',
+  setupFilesAfterEnv: ['<rootDir>/src/setupJest.ts'],
   transformIgnorePatterns: [
     'node_modules/(?!(@ngrx|ngx-bootstrap|@angular/common/locales|lodash-es/.*|@ngx-utils/cookies))',
   ],
