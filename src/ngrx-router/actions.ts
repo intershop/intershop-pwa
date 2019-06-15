dcc7e923edea3cc1720199cf5ea10b6335e12667
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-06-15 14:18:39 +0200

    remove code not used in the intershop-pwa

--- src/ngrx-router/actions.ts
+++ src/ngrx-router/actions.ts
@@ -1,33 +1,9 @@
-// tslint:disable:no-any
-import { NavigationExtras } from '@angular/router';
 import { Action } from '@ngrx/store';
 
-export const ROUTER_GO_TYPE = '[Router] Go';
-export const ROUTER_BACK_TYPE = '[Router] Back';
-export const ROUTER_FORWARD_TYPE = '[Router] Forward';
 export const ROUTER_NAVIGATION_TYPE = '[Router] Navigation';
-
-export class RouterGo implements Action {
-  readonly type = ROUTER_GO_TYPE;
-
-  constructor(
-    public payload: {
-      path: any[];
-      queryParams?: object;
-      extras?: NavigationExtras;
-    }
-  ) {}
-}
-
-export class RouterBack implements Action {
-  readonly type = ROUTER_BACK_TYPE;
-}
-
-export class RouterForward implements Action {
-  readonly type = ROUTER_FORWARD_TYPE;
-}
 
 export class RouteNavigation implements Action {
   readonly type = ROUTER_NAVIGATION_TYPE;
+  // tslint:disable-next-line:no-any
   constructor(public payload: { path: string; params?: any; queryParams?: any; data?: any }) {}
 }
--- src/ngrx-router/effects.ts
+++ src/ngrx-router/effects.ts
@@ -1,59 +1,34 @@
 // tslint:disable:no-any
-import { Location } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { ActivationStart, NavigationEnd, Router } from '@angular/router';
-import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Store } from '@ngrx/store';
-import { debounce, filter, map, tap } from 'rxjs/operators';
+import { Effect } from '@ngrx/effects';
+import { debounce, filter, map } from 'rxjs/operators';
 
-import { ROUTER_BACK_TYPE, ROUTER_FORWARD_TYPE, ROUTER_GO_TYPE, RouteNavigation, RouterGo } from './actions';
+import { RouteNavigation } from './actions';
 
 @Injectable()
 export class RouterEffects {
-  @Effect({ dispatch: false })
-  navigate$ = this.actions$.pipe(
-    ofType<RouterGo>(ROUTER_GO_TYPE),
-    map(action => action.payload),
-    tap(({ path, queryParams, extras }) => setTimeout(() => this.router.navigate(path, { queryParams, ...extras })))
-  );
-
-  @Effect({ dispatch: false })
-  navigateBack$ = this.actions$.pipe(
-    ofType(ROUTER_BACK_TYPE),
-    tap(() => setTimeout(() => this.location.back()))
-  );
-
-  @Effect({ dispatch: false })
-  navigateForward$ = this.actions$.pipe(
-    ofType(ROUTER_FORWARD_TYPE),
-    tap(() => setTimeout(() => this.location.forward()))
-  );
-
-  constructor(private actions$: Actions, private router: Router, private location: Location, private store: Store<{}>) {
-    this.listenToRouter();
-  }
+  constructor(private router: Router) {}
 
   private navEnd$ = this.router.events.pipe(filter(event => event instanceof NavigationEnd));
 
-  private listenToRouter() {
-    this.router.events
-      .pipe(
-        filter(event => event instanceof ActivationStart),
-        debounce(() => this.navEnd$)
-      )
-      .subscribe((event: any) => {
-        let route = event.snapshot;
-        const path: any[] = [];
-        const { params, queryParams, data } = route;
+  @Effect()
+  listenToRouter$ = this.router.events.pipe(
+    filter(event => event instanceof ActivationStart),
+    debounce(() => this.navEnd$),
+    map((event: any) => {
+      let route = event.snapshot;
+      const path: any[] = [];
+      const { params, queryParams, data } = route;
 
-        while (route.parent) {
-          if (route.routeConfig && route.routeConfig.path) {
-            path.push(route.routeConfig.path);
-          }
-          route = route.parent;
+      while (route.parent) {
+        if (route.routeConfig && route.routeConfig.path) {
+          path.push(route.routeConfig.path);
         }
-        const routerState = { params, queryParams, data, path: path.reverse().join('/') };
-        this.store.dispatch(new RouteNavigation(routerState));
-      });
-  }
+        route = route.parent;
+      }
+      const routerState = { params, queryParams, data, path: path.reverse().join('/') };
+      return new RouteNavigation(routerState);
+    })
+  );
 }
