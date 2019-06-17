5836e8255d34ab926e9948438d5cdc8b9e139077
Stefan Hauke <s.hauke@intershop.de>
2019-06-17 16:55:02 +0200

    improve the content view helper and fit the CMS components with Link parameter to work with routerLinks and external links

--- src/app/core/models/content-view/content-view.helper.ts
+++ src/app/core/models/content-view/content-view.helper.ts
@@ -4,13 +4,25 @@ import { ContentConfigurationParameterView } from './content-views';
 
 export class ContentViewHelper {
   /**
-   * get RouterLink for ish-cms-pagelet by configParam
-   * defaultRouterLink is '/home'
-   * @param pagelet
+   * get a routerLink for a given configuration parameter of the given configuration parameter container
+   * the configuration parameter value may contain an ICM short link notation or an external URL
+   * the default route is '/home'
+   * @param configParamCtnr
    * @param configParam
    */
-  static getRouterLink(pagelet: ContentConfigurationParameterView, configParam: string): string {
-    const configParamValue = pagelet.stringParam(configParam);
+  static getRouterLink(configParamCtnr: ContentConfigurationParameterView, configParam: string): string {
+    const configParamValue = configParamCtnr.stringParam(configParam);
     return LinkParser.parseLink(configParamValue) || '/home';
+  }
+
+  /**
+   * determine whether the a given configuration parameter of the given configuration parameter container
+   * contains a link value that is supposed to be handled as a routerLink or not (external link)
+   * @param configParamCtnr
+   * @param configParam
+   */
+  static isRouterLink(configParamCtnr: ContentConfigurationParameterView, configParam: string): boolean {
+    const configParamValue = configParamCtnr.stringParam(configParam);
+    return configParamValue && !configParamValue.startsWith('http');
   }
 }
