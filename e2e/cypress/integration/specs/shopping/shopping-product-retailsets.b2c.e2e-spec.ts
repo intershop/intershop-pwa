80a7eeef285d6a80e14745777ccd609f93fbf49f
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-07-18 20:28:02 +0200

    cypress test for retail set

---
+++ e2e/cypress/integration/specs/shopping/shopping-product-retailsets.b2c.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { at } from '../../framework';
+import { CartPage } from '../../pages/checkout/cart.page';
+import { ProductDetailPage } from '../../pages/shopping/product-detail.page';
+
+const _ = {
+  retailSetSKU: '201807198',
+  retailSetParts: ['201807195', '201807197', '201807196', '201807199'],
+};
+
+describe('Shopping User', () => {
+  before(() => ProductDetailPage.navigateTo(_.retailSetSKU));
+
+  it('starting at product detail page of a retail set', () => {
+    at(ProductDetailPage, page => {
+      page.sku.should('have.text', _.retailSetSKU);
+      page.retailSetParts.visibleProductSKUs.should('deep.equal', _.retailSetParts);
+    });
+  });
+
+  it('adding retail set to cart', () => {
+    at(ProductDetailPage, page => {
+      page.addProductToCart();
+      page.header.miniCart.goToCart();
+    });
+  });
+
+  it('should see the product retail set as multiple items in cart', () => {
+    at(CartPage, page => {
+      page.lineItems.should('have.length', 4);
+      for (let idx = 0; idx < _.retailSetParts.length; idx++) {
+        page.lineItem(idx).sku.should('contain', _.retailSetParts[idx]);
+      }
+    });
+  });
+});
