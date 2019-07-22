a2db0cf546ab25612a36eb74bd01235d4fa96982
Susanne Schneider <s.schneider@intershop.de>
2019-07-22 16:39:46 +0200

    ISREST-797: implement list component for product links

---
+++ src/app/pages/product/components/product-links-list/product-links-list.component.spec.ts
@@ -0,0 +1,37 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { MockComponent } from 'ng-mocks';
+
+import { ProductLinkView } from 'ish-core/models/product-links/product-links.model';
+import { ProductItemContainerComponent } from '../../../../shared/product/containers/product-item/product-item.container';
+
+import { ProductLinksListComponent } from './product-links-list.component';
+
+describe('Product Links List Component', () => {
+  let component: ProductLinksListComponent;
+  let fixture: ComponentFixture<ProductLinksListComponent>;
+  let element: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
+      declarations: [MockComponent(ProductItemContainerComponent), ProductLinksListComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    const productLink = { productSKUs: ['sku'], categoryIds: ['catID'] } as ProductLinkView;
+
+    fixture = TestBed.createComponent(ProductLinksListComponent);
+    component = fixture.componentInstance;
+    element = fixture.nativeElement;
+    component.links = productLink;
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+    expect(element).toBeTruthy();
+    expect(() => fixture.detectChanges()).not.toThrow();
+    expect(element).toMatchSnapshot();
+  });
+});
--- src/app/pages/product/containers/product-links/product-links.container.spec.ts
+++ src/app/pages/product/containers/product-links/product-links.container.spec.ts
@@ -1,7 +1,11 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { StoreModule } from '@ngrx/store';
+import { TranslateModule } from '@ngx-translate/core';
+import { MockComponent } from 'ng-mocks';
 
 import { coreReducers } from 'ish-core/store/core-store.module';
+import { ProductItemContainerComponent } from '../../../../shared/product/containers/product-item/product-item.container';
+import { ProductLinksListComponent } from '../../components/product-links-list/product-links-list.component';
 
 import { ProductLinksContainerComponent } from './product-links.container';
 
@@ -12,8 +16,12 @@ describe('Product Links Container', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [StoreModule.forRoot(coreReducers)],
-      declarations: [ProductLinksContainerComponent],
+      imports: [StoreModule.forRoot(coreReducers), TranslateModule.forRoot()],
+      declarations: [
+        MockComponent(ProductItemContainerComponent),
+        MockComponent(ProductLinksListComponent),
+        ProductLinksContainerComponent,
+      ],
     }).compileComponents();
   }));
 
@@ -26,6 +34,7 @@ describe('Product Links Container', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
+    expect(() => component.ngOnChanges()).not.toThrow();
     expect(() => fixture.detectChanges()).not.toThrow();
   });
 });
