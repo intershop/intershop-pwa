c87867204fd08f8705386ba86f09a3c8594d5f05
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-07-22 09:54:28 +0200

    remove dead code

--- src/app/shared/forms/utils/form-utils.spec.ts
+++ src/app/shared/forms/utils/form-utils.spec.ts
@@ -1,34 +1,8 @@
 import { FormControl, FormGroup } from '@angular/forms';
 
-import {
-  arrayDiff,
-  arrayIntersect,
-  determineSalutations,
-  markAsDirtyRecursive,
-  markFormControlsAsInvalid,
-} from './form-utils';
+import { determineSalutations, markAsDirtyRecursive, markFormControlsAsInvalid } from './form-utils';
 
 describe('Form Utils', () => {
-  describe('array utils', () => {
-    it('should return diff for two arrays', () => {
-      const a = [1, 2, 3, 4, 5, 6];
-      const b = [4, 5, 6, 7, 8, 9];
-      const expected = [1, 2, 3];
-
-      const result = arrayDiff(a, b);
-      expect(result).toEqual(expected);
-    });
-
-    it('should return intersection for two arrays', () => {
-      const a = [1, 2, 3, 4, 5, 6];
-      const b = [4, 5, 6, 7, 8, 9];
-      const expected = [4, 5, 6];
-
-      const result = arrayIntersect(a, b);
-      expect(result).toEqual(expected);
-    });
-  });
-
   describe('markAsDirtyRecursive', () => {
     it('should mark all controls dirty for flat form group', () => {
       const group = new FormGroup({
