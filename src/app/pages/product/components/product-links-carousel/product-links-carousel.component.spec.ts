0e2f8a38db56ed98c214a4c69677dea50c25aab8
Susanne Schneider <s.schneider@intershop.de>
2019-07-30 17:50:05 +0200

    ISREST-797: implement carousel component for product links

---
+++ src/app/pages/product/components/product-links-carousel/product-links-carousel.component.spec.ts
@@ -0,0 +1,35 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { MockComponent } from 'ng-mocks';
+
+import { ProductLinkView } from 'ish-core/models/product-links/product-links.model';
+import { ProductItemContainerComponent } from '../../../../shared/product/containers/product-item/product-item.container';
+
+import { ProductLinksCarouselComponent } from './product-links-carousel.component';
+
+describe('Product Links Carousel Component', () => {
+  let component: ProductLinksCarouselComponent;
+  let fixture: ComponentFixture<ProductLinksCarouselComponent>;
+  let element: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MockComponent(ProductItemContainerComponent), ProductLinksCarouselComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    const productLink = { productSKUs: ['sku'], categoryIds: ['catID'] } as ProductLinkView;
+
+    fixture = TestBed.createComponent(ProductLinksCarouselComponent);
+    component = fixture.componentInstance;
+    element = fixture.nativeElement;
+    component.links = productLink;
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+    expect(element).toBeTruthy();
+    expect(() => fixture.detectChanges()).not.toThrow();
+    expect(element).toMatchSnapshot();
+  });
+});
