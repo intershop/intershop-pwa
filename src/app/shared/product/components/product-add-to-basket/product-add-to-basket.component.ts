1ae9f100cae2e2df0a02f46c7f0de92771295d2c
Silke Grüber <s.grueber@intershop.de>
2019-08-09 09:23:06 +0200

    ISREST-618: display a spinner after addToCart button has been pressed

--- src/app/shared/product/components/product-add-to-basket/product-add-to-basket.component.ts
+++ src/app/shared/product/components/product-add-to-basket/product-add-to-basket.component.ts
@@ -1,21 +1,85 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+// tslint:disable:ccp-no-intelligence-in-components
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { Product } from 'ish-core/models/product/product.model';
+import { getBasketLoading } from 'ish-core/store/checkout/basket';
+import { whenFalsy } from 'ish-core/utils/operators';
 
+/**
+ * Displays an add to cart button with an icon or a text label. After clicking the button a loading animation is displayed
+ *
+ * @example
+ * <ish-product-add-to-basket
+    [product]="product"
+    [class]="'btn-lg btn-block'"
+    [disabled]="productDetailForm.invalid"
+    [translationKey]="isRetailSet(product) ? 'product.add_to_cart.retailset.link' : 'product.add_to_cart.link'"
+    (productToBasket)="addToBasket()"
+  ></ish-product-add-to-basket>
+ */
 @Component({
   selector: 'ish-product-add-to-basket',
   templateUrl: './product-add-to-basket.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductAddToBasketComponent {
+export class ProductAddToBasketComponent implements OnInit, OnDestroy {
+  basketLoading$ = this.store.pipe(select(getBasketLoading));
+
+  /**
+   * The product that can be added to basket
+   */
   @Input() product: Product;
+  /**
+   * When true, it specifies that the button should be disabled
+   */
   @Input() disabled = false;
+  /**
+   * when 'icon', the button label is an icon, otherwise it is text
+   */
   @Input() displayType?: string;
+  /**
+   * additional css styling
+   */
   @Input() class?: string;
+  /**
+   * translationKey for the button label
+   */
   @Input() translationKey = 'product.add_to_cart.link';
+  /**
+   * button was clicked event
+   */
   @Output() productToBasket = new EventEmitter<void>();
+
+  constructor(private store: Store<{}>) {}
+
+  // fires 'true' after add To Cart is clicked and basket is loading
+  displaySpinner$ = new BehaviorSubject(false);
+
+  private destroy$ = new Subject();
+
+  ngOnInit() {
+    // update emitted to display spinning animation
+    this.basketLoading$
+      .pipe(
+        whenFalsy(),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(this.displaySpinner$); // false
+  }
 
   addToBasket() {
     this.productToBasket.emit();
+    this.displaySpinner$.next(true);
+  }
+
+  get displayIcon(): boolean {
+    return this.displayType === 'icon';
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
   }
 }
