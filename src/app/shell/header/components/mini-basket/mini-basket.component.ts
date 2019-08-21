4912dcd5d54b0f63cd36b104d759d0aa672f529b
Silke Grüber <s.grueber@intershop.de>
2019-08-21 14:08:13 +0200

    ISREST-275: navigation arrows replaced by a scroll bar

--- src/app/shell/header/components/mini-basket/mini-basket.component.ts
+++ src/app/shell/header/components/mini-basket/mini-basket.component.ts
@@ -1,13 +1,4 @@
-import { AnimationBuilder, animate, style } from '@angular/animations';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  Input,
-  OnChanges,
-  SimpleChanges,
-  ViewChild,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 import { BasketView } from 'ish-core/models/basket/basket.model';
 import { HttpError } from 'ish-core/models/http-error/http-error.model';
@@ -37,24 +28,15 @@ export class MiniBasketComponent implements OnChanges {
   @Input() view: 'auto' | 'small' | 'full' = 'auto';
   @Input() basketAnimation = '';
   @Input() error: HttpError;
-  /**
-   * The vertical product slider element reference.
-   */
-  @ViewChild('slider')
-  slider: ElementRef;
 
   isCollapsed = true;
   itemCount = 0;
-  currentProduct = 0;
-
-  constructor(private animationBuilder: AnimationBuilder) {}
 
   ngOnChanges(c: SimpleChanges) {
     this.animateBasket(c);
     if (this.basket) {
       this.itemCount = this.basket.totalProductQuantity;
     } else {
-      this.resetScroller();
       this.itemCount = 0;
     }
   }
@@ -95,64 +77,5 @@ export class MiniBasketComponent implements OnChanges {
    */
   open() {
     this.isCollapsed = false;
-  }
-
-  /**
-   * Slider control scroll up.
-   */
-  scrollUp() {
-    if (!this.slider || this.currentProduct === 0) {
-      return;
-    }
-
-    const slider: HTMLDivElement = this.slider.nativeElement;
-    const tileHeight = slider.children.length > 0 ? slider.lastElementChild.getBoundingClientRect().height : 0;
-
-    this.currentProduct -= 1;
-    const offset = tileHeight * this.currentProduct;
-    this.animate(offset);
-  }
-
-  /**
-   * Slider control scroll down.
-   */
-  scrollDown() {
-    if (!this.slider || !this.basket || !this.basket.lineItems) {
-      return;
-    }
-
-    const slider: HTMLDivElement = this.slider.nativeElement;
-    const tileHeight = slider.children.length > 0 ? slider.lastElementChild.getBoundingClientRect().height : 0;
-    if (this.currentProduct < this.basket.lineItems.length - 2) {
-      this.currentProduct += 1;
-      const offset = tileHeight * this.currentProduct;
-      this.animate(offset);
-    }
-  }
-
-  /**
-   * Animate the slider container.
-   * @param offset vertical translate offset
-   */
-  animate(offset: number) {
-    if (!this.slider) {
-      return;
-    }
-
-    const slider = this.slider.nativeElement as HTMLDivElement;
-    const scrollAnimation = this.animationBuilder.build([
-      animate('200ms ease-in', style({ transform: `translateY(-${offset}px)` })),
-    ]);
-
-    const player = scrollAnimation.create(slider);
-    player.play();
-  }
-
-  /**
-   * Reset vertical scroll component if basket changes
-   */
-  resetScroller() {
-    this.currentProduct = 0;
-    this.animate(0);
   }
 }
