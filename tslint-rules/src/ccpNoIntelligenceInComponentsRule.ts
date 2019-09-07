bbbee5db40bed75e147a097047c8e54f3f7d0f15
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-09-07 22:47:47 +0200

    rework ccp-no-intelligence-in-components rule

--- tslint-rules/src/ccpNoIntelligenceInComponentsRule.ts
+++ tslint-rules/src/ccpNoIntelligenceInComponentsRule.ts
@@ -1,62 +1,61 @@
 import * as Lint from 'tslint';
-import { ImportDeclaration, SourceFile, SyntaxKind } from 'typescript';
+import * as ts from 'typescript';
 
 import { RuleHelpers } from './ruleHelpers';
 
 interface RuleSetting {
   ngrx: boolean;
   service: boolean;
+  router: boolean;
 }
 
-class CCPNoIntelligenceInComponentsWalker extends Lint.RuleWalker {
+export class Rule extends Lint.Rules.AbstractRule {
   ruleSettings: { [key: string]: RuleSetting } = {};
   isContainer: boolean;
 
-  constructor(sourceFile: SourceFile, options: Lint.IOptions) {
-    super(sourceFile, options);
+  constructor(options: Lint.IOptions) {
+    super(options);
 
     this.ruleSettings.component = options.ruleArguments[0].component;
     this.ruleSettings.container = options.ruleArguments[0].container;
   }
 
-  visitSourceFile(sourceFile: SourceFile) {
-    if (sourceFile.fileName.match(/.*(component|container)\.ts/)) {
+  apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+    if (!sourceFile.fileName.match(/.*(component|container)\.ts/)) {
+      return [];
+    }
+    return this.applyWithFunction(sourceFile, ctx => {
       this.isContainer = sourceFile.fileName.indexOf('container') >= 0;
-      super.visitSourceFile(sourceFile);
-    }
-  }
+      ctx.sourceFile.statements.filter(ts.isImportDeclaration).forEach((importStatement: ts.ImportDeclaration) => {
+        const fromStringToken = RuleHelpers.getNextChildTokenOfKind(importStatement, ts.SyntaxKind.StringLiteral);
+        const fromStringText = fromStringToken.getText().substring(1, fromStringToken.getText().length - 1);
 
-  visitImportDeclaration(importStatement: ImportDeclaration) {
-    const fromStringToken = RuleHelpers.getNextChildTokenOfKind(importStatement, SyntaxKind.StringLiteral);
-    const fromStringText = fromStringToken.getText().substring(1, fromStringToken.getText().length - 1);
+        let c: string;
+        if (this.isContainer) {
+          c = 'container';
+        } else {
+          c = 'component';
+        }
 
-    let c: string;
-    if (this.isContainer) {
-      c = 'container';
-    } else {
-      c = 'component';
-    }
-
-    if (fromStringText.search(/\/store(\/|$)/) > 0 && !this.ruleSettings[c].ngrx) {
-      this.addFailureAtNode(
-        importStatement,
-        `ngrx handling is not allowed in ${c}s. (found ${importStatement.getText()})`
-      );
-    }
-    if (fromStringText.search(/\.service$/) > 0 && !this.ruleSettings[c].service) {
-      this.addFailureAtNode(
-        importStatement,
-        `service usage is not allowed in ${c}s. (found ${importStatement.getText()})`
-      );
-    }
-  }
-}
-
-/**
- * Implementation of the ccp-no-intelligence-in-components rule.
- */
-export class Rule extends Lint.Rules.AbstractRule {
-  apply(sourceFile: SourceFile): Lint.RuleFailure[] {
-    return this.applyWithWalker(new CCPNoIntelligenceInComponentsWalker(sourceFile, this.getOptions()));
+        if (fromStringText.search(/\/store(\/|$)/) >= 0 && !this.ruleSettings[c].ngrx) {
+          ctx.addFailureAtNode(
+            importStatement,
+            `ngrx handling is not allowed in ${c}s. (found ${importStatement.getText()})`
+          );
+        }
+        if (fromStringText.search(/\.service$/) >= 0 && !this.ruleSettings[c].service) {
+          ctx.addFailureAtNode(
+            importStatement,
+            `service usage is not allowed in ${c}s. (found ${importStatement.getText()})`
+          );
+        }
+        if (fromStringText.search(/angular\/router/) >= 0 && !this.ruleSettings[c].router) {
+          ctx.addFailureAtNode(
+            importStatement,
+            `router usage is not allowed in ${c}s. (found ${importStatement.getText()})`
+          );
+        }
+      });
+    });
   }
 }
