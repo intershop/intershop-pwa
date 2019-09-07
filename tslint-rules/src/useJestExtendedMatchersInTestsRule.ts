41799395c78a974a9944e5a7e26016f6317f53ca
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-09-07 20:44:04 +0200

    rework use-jest-extended-matchers-in-tests rule

--- tslint-rules/src/useJestExtendedMatchersInTestsRule.ts
+++ tslint-rules/src/useJestExtendedMatchersInTestsRule.ts
@@ -1,8 +1,9 @@
+import { tsquery } from '@phenomnomnominal/tsquery';
 import * as Lint from 'tslint';
 import * as ts from 'typescript';
 
-class UseJestExtendedMatchersInTestsWalker extends Lint.RuleWalker {
-  private replacements = [
+export class Rule extends Lint.Rules.AbstractRule {
+  static REPLACEMENTS = [
     { pattern: /(toBe|toEqual)\(false\)$/, replacement: 'toBeFalse()', text: 'toBeFalse' },
     { pattern: /(toBe|toEqual)\(true\)$/, replacement: 'toBeTrue()', text: 'toBeTrue' },
     { pattern: /(toBe|toEqual)\(undefined\)$/, replacement: 'toBeUndefined()', text: 'toBeUndefined' },
@@ -18,37 +19,28 @@ class UseJestExtendedMatchersInTestsWalker extends Lint.RuleWalker {
     },*/
   ];
 
-  visitSourceFile(sourceFile: ts.SourceFile) {
-    if (sourceFile.fileName.endsWith('.spec.ts')) {
-      super.visitSourceFile(sourceFile);
-    }
-  }
-
-  visitIdentifier(node: ts.Identifier) {
-    if (node.escapedText === 'toBe' || node.escapedText === 'toEqual') {
-      const expectStatement = node.parent.parent as ts.CallExpression;
-      const text = expectStatement.getText();
-      for (let index = 0; index < this.replacements.length; index++) {
-        const rep = this.replacements[index];
-        if (text.search(rep.pattern) > 0) {
-          const fix = new Lint.Replacement(
-            expectStatement.getStart(),
-            expectStatement.getWidth(),
-            text.replace(rep.pattern, rep.replacement)
-          );
-          this.addFailureAtNode(expectStatement, `use ${rep.text}`, fix);
-          break;
-        }
-      }
-    }
-  }
-}
-
-/**
- * Implementation of the use-jest-extended-matchers-in-tests rule.
- */
-export class Rule extends Lint.Rules.AbstractRule {
   apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    return this.applyWithWalker(new UseJestExtendedMatchersInTestsWalker(sourceFile, this.getOptions()));
+    if (!sourceFile.fileName.endsWith('.spec.ts')) {
+      return [];
+    }
+
+    return this.applyWithFunction(sourceFile, ctx => {
+      tsquery(ctx.sourceFile, 'Identifier[name=/(toBe|toEqual)/]').forEach(node => {
+        const expectStatement = node.parent.parent as ts.CallExpression;
+        const text = expectStatement.getText();
+        for (let index = 0; index < Rule.REPLACEMENTS.length; index++) {
+          const rep = Rule.REPLACEMENTS[index];
+          if (text.search(rep.pattern) > 0) {
+            const fix = new Lint.Replacement(
+              expectStatement.getStart(),
+              expectStatement.getWidth(),
+              text.replace(rep.pattern, rep.replacement)
+            );
+            ctx.addFailureAtNode(expectStatement, `use ${rep.text}`, fix);
+            break;
+          }
+        }
+      });
+    });
   }
 }
