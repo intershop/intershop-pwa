ee3897d07a53c6896849461ff61859dee4ff2a9c
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-09-07 19:24:14 +0200

    rework ngrx-use-empty-store-type rule

--- tslint-rules/src/ngrxUseEmptyStoreTypeRule.ts
+++ tslint-rules/src/ngrxUseEmptyStoreTypeRule.ts
@@ -1,27 +1,21 @@
+import { tsquery } from '@phenomnomnominal/tsquery';
 import * as Lint from 'tslint';
 import * as ts from 'typescript';
 
-class NgrxUseEmptyStoreTypeWalker extends Lint.RuleWalker {
-  visitIdentifier(node: ts.Identifier) {
-    if (node.getText() === 'Store') {
-      const storeType = node.parent;
-      if (storeType.kind === ts.SyntaxKind.TypeReference && storeType.getText() !== 'Store<{}>') {
-        const fix = new Lint.Replacement(storeType.getStart(), storeType.getWidth(), 'Store<{}>');
-        this.addFailureAtNode(
-          storeType,
-          'use empty store type (Store<{}>) and use selectors to access the store.',
-          fix
-        );
-      }
-    }
-  }
-}
-
-/**
- * Implementation of the ngrx-use-empty-store-type rule.
- */
 export class Rule extends Lint.Rules.AbstractRule {
   apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    return this.applyWithWalker(new NgrxUseEmptyStoreTypeWalker(sourceFile, this.getOptions()));
+    return this.applyWithFunction(sourceFile, ctx => {
+      tsquery(sourceFile, 'Identifier[name="Store"]').forEach(node => {
+        const storeType = node.parent;
+        if (storeType.kind === ts.SyntaxKind.TypeReference && storeType.getText() !== 'Store<{}>') {
+          const fix = new Lint.Replacement(storeType.getStart(), storeType.getWidth(), 'Store<{}>');
+          ctx.addFailureAtNode(
+            storeType,
+            'use empty store type (Store<{}>) and use selectors to access the store.',
+            fix
+          );
+        }
+      });
+    });
   }
 }
