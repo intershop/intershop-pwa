bed99912964f29d915f920407ae50df245017100
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-09-07 23:04:21 +0200

    rework ngrx-use-complex-type-with-action-payload rule

--- tslint-rules/dist/ngrxUseComplexTypeWithActionPayloadRule.js
+++ tslint-rules/dist/ngrxUseComplexTypeWithActionPayloadRule.js
@@ -14,66 +14,65 @@ var __extends = (this && this.__extends) || (function () {
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
 var Lint = require("tslint");
-var NgrxUseComplexTypeWithActionPayloadWalker = (function (_super) {
-    __extends(NgrxUseComplexTypeWithActionPayloadWalker, _super);
-    function NgrxUseComplexTypeWithActionPayloadWalker() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    NgrxUseComplexTypeWithActionPayloadWalker.prototype.visitCallExpression = function (node) {
-        _super.prototype.visitCallExpression.call(this, node);
-        if (node.getChildAt(0).getText() === 'action') {
-            if (node.getChildAt(2).getChildCount() > 3) {
-                this.addFailureAtNode(node, 'Actions should have only one payload parameter.');
-            }
-            var payload = node.getChildAt(2).getChildAt(2);
-            if (payload) {
-                if (payload.getChildAt(0).getText() !== 'payload') {
-                    this.addFailureAtNode(node, 'Actions should have only one payload parameter.');
-                }
-                else if (!payload
-                    .getChildAt(2)
-                    .getText()
-                    .startsWith('{')) {
-                    this.addFailureAtNode(payload, 'The payload of actions should be a complex type with named content.');
-                }
-            }
-        }
-    };
-    NgrxUseComplexTypeWithActionPayloadWalker.prototype.visitConstructorDeclaration = function (node) {
-        _super.prototype.visitConstructorDeclaration.call(this, node);
-        var isActionClass = node.parent
-            .getChildren()
-            .map(function (c) { return c.getText() === 'implements Action'; })
-            .reduce(function (a, b) { return a || b; });
-        if (isActionClass) {
-            var constructorParameterList = node.getChildAt(2);
-            if (constructorParameterList.getChildCount() > 1) {
-                this.addFailureAtNode(constructorParameterList, 'Actions should have only one payload parameter called "payload".');
-            }
-            else {
-                var firstConstructorParameter = constructorParameterList.getChildAt(0);
-                if (firstConstructorParameter.getChildAt(1).getText() !== 'payload') {
-                    this.addFailureAtNode(firstConstructorParameter.getChildAt(1), 'Actions should have only one payload parameter called "payload".');
-                }
-                else {
-                    var typeOfFirstConstructorParameter = firstConstructorParameter.getChildAt(firstConstructorParameter.getChildCount() - 1);
-                    if (!typeOfFirstConstructorParameter.getText().startsWith('{') &&
-                        !typeOfFirstConstructorParameter.getText().endsWith('Type')) {
-                        this.addFailureAtNode(typeOfFirstConstructorParameter, 'The payload of actions should be a complex type with named content.');
-                    }
-                }
-            }
-        }
-    };
-    return NgrxUseComplexTypeWithActionPayloadWalker;
-}(Lint.RuleWalker));
+var ts = require("typescript");
+var tsquery_1 = require("@phenomnomnominal/tsquery");
 var Rule = (function (_super) {
     __extends(Rule, _super);
     function Rule() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     Rule.prototype.apply = function (sourceFile) {
-        return this.applyWithWalker(new NgrxUseComplexTypeWithActionPayloadWalker(sourceFile, this.getOptions()));
+        if (!sourceFile.fileName.endsWith('actions.ts')) {
+            return [];
+        }
+        return this.applyWithFunction(sourceFile, function (ctx) {
+            tsquery_1.tsquery(ctx.sourceFile, 'CallExpression').forEach(function (node) {
+                if (node.getChildAt(0).getText() === 'action') {
+                    if (node.getChildAt(2).getChildCount() > 3) {
+                        ctx.addFailureAtNode(node, 'Actions should have only one payload parameter.');
+                    }
+                    var payload = node.getChildAt(2).getChildAt(2);
+                    if (payload) {
+                        if (payload.getChildAt(0).getText() !== 'payload') {
+                            ctx.addFailureAtNode(node, 'Actions should have only one payload parameter.');
+                        }
+                        else if (!payload
+                            .getChildAt(2)
+                            .getText()
+                            .startsWith('{')) {
+                            ctx.addFailureAtNode(payload, 'The payload of actions should be a complex type with named content.');
+                        }
+                    }
+                }
+            });
+            tsquery_1.tsquery(ctx.sourceFile, '*')
+                .filter(ts.isConstructorDeclaration)
+                .forEach(function (node) {
+                var isActionClass = node.parent
+                    .getChildren()
+                    .map(function (c) { return c.getText() === 'implements Action'; })
+                    .reduce(function (a, b) { return a || b; });
+                if (isActionClass) {
+                    var constructorParameterList = node.getChildAt(2);
+                    if (constructorParameterList.getChildCount() > 1) {
+                        ctx.addFailureAtNode(constructorParameterList, 'Actions should have only one payload parameter called "payload".');
+                    }
+                    else {
+                        var firstConstructorParameter = constructorParameterList.getChildAt(0);
+                        if (firstConstructorParameter.getChildAt(1).getText() !== 'payload') {
+                            ctx.addFailureAtNode(firstConstructorParameter.getChildAt(1), 'Actions should have only one payload parameter called "payload".');
+                        }
+                        else {
+                            var typeOfFirstConstructorParameter = firstConstructorParameter.getChildAt(firstConstructorParameter.getChildCount() - 1);
+                            if (!typeOfFirstConstructorParameter.getText().startsWith('{') &&
+                                !typeOfFirstConstructorParameter.getText().endsWith('Type')) {
+                                ctx.addFailureAtNode(typeOfFirstConstructorParameter, 'The payload of actions should be a complex type with named content.');
+                            }
+                        }
+                    }
+                }
+            });
+        });
     };
     return Rule;
 }(Lint.Rules.AbstractRule));
