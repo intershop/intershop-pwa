c9b4bd6c7b6fa5d9c5e37311cfaa330a496a7396
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-09-07 17:20:04 +0200

    rework no-barrel-files rule

--- tslint-rules/dist/noBarrelFilesRule.js
+++ tslint-rules/dist/noBarrelFilesRule.js
@@ -14,25 +14,17 @@ var __extends = (this && this.__extends) || (function () {
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
 var Lint = require("tslint");
-var NoBarrelFilesWalker = (function (_super) {
-    __extends(NoBarrelFilesWalker, _super);
-    function NoBarrelFilesWalker() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    NoBarrelFilesWalker.prototype.visitSourceFile = function (sourceFile) {
-        if (sourceFile.fileName.search('index.ts') > 0) {
-            this.addFailureAt(0, 1, 'The use of barrel files is deprecated!');
-        }
-    };
-    return NoBarrelFilesWalker;
-}(Lint.RuleWalker));
 var Rule = (function (_super) {
     __extends(Rule, _super);
     function Rule() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     Rule.prototype.apply = function (sourceFile) {
-        return this.applyWithWalker(new NoBarrelFilesWalker(sourceFile, this.getOptions()));
+        return this.applyWithFunction(sourceFile, function (ctx) {
+            if (sourceFile.fileName.search('index.ts') > 0) {
+                ctx.addFailureAt(0, 1, 'The use of barrel files is deprecated!');
+            }
+        });
     };
     return Rule;
 }(Lint.Rules.AbstractRule));
