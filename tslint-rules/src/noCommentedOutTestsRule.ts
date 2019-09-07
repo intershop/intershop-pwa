56f66b18748071c426885a4aa2b8320a0a8311b6
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-09-07 20:49:40 +0200

    rework no-commented-out-tests rule

--- tslint-rules/src/noCommentedOutTestsRule.ts
+++ tslint-rules/src/noCommentedOutTestsRule.ts
@@ -5,28 +5,22 @@ import * as ts from 'typescript';
 const SINGLE_LINE_COMMENT_REGEX = /\/\/.*(?=(it|describe|xit|xdescribe|fit|fdescribe)\()/;
 const MULTI_LINE_COMMENT_REGEX = /\/\*[^(\*\/)]*(?=(it|describe|xit|xdescribe|fit|fdescribe)\()/;
 
-class NoCommentedOutTestsWalker extends Lint.RuleWalker {
-  visitSourceFile(sourceFile: ts.SourceFile) {
-    if (sourceFile.fileName.endsWith('.spec.ts')) {
+export class Rule extends Lint.Rules.AbstractRule {
+  apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+    if (!sourceFile.fileName.endsWith('.spec.ts')) {
+      return [];
+    }
+    return this.applyWithFunction(sourceFile, ctx => {
       forEachComment(sourceFile, (fullFileText, commentRange) => {
         const comment = fullFileText.substring(commentRange.pos, commentRange.end);
         if (SINGLE_LINE_COMMENT_REGEX.test(comment) || MULTI_LINE_COMMENT_REGEX.test(comment)) {
-          this.addFailureFromStartToEnd(
+          ctx.addFailureAt(
             commentRange.pos,
-            commentRange.end,
+            commentRange.end - commentRange.pos,
             'Comment contains commented out test cases. Use xdescribe or xit to exclude tests.'
           );
         }
       });
-    }
-  }
-}
-
-/**
- * Implementation of the no-commented-out-tests rule.
- */
-export class Rule extends Lint.Rules.AbstractRule {
-  apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    return this.applyWithWalker(new NoCommentedOutTestsWalker(sourceFile, this.getOptions()));
+    });
   }
 }
