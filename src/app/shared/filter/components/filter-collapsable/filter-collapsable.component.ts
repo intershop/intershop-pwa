6b0129d0d4876c560cd817ac05d0970876a982ee
Sebastian Hähnlein <s.haehnlein@intershop.de>
2019-09-23 11:14:43 +0200

    extend filter-collapsaple.component with in- & outputs

--- src/app/shared/filter/components/filter-collapsable/filter-collapsable.component.ts
+++ src/app/shared/filter/components/filter-collapsable/filter-collapsable.component.ts
@@ -1,12 +1,23 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'ish-filter-collapsable',
   templateUrl: './filter-collapsable.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FilterCollapsableComponent {
+export class FilterCollapsableComponent implements OnInit {
   @Input() title: string;
+  @Input() collapsed: boolean;
+  @Output() collabsedChanged = new EventEmitter<boolean>();
 
   isCollapsed = false;
+
+  ngOnInit() {
+    this.isCollapsed = this.collapsed;
+  }
+
+  toggle() {
+    this.isCollapsed = !this.isCollapsed;
+    this.collabsedChanged.emit(this.isCollapsed);
+  }
 }
