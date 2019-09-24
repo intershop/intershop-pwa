127c706ff3dfc4f946df91689096a8cd1a362433
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-09-24 17:35:18 +0200

    provide jest snapshot serializers through separate files and config

---
+++ src/jest-serializer/CategoryTreeSerializer.js
@@ -0,0 +1,28 @@
+import { asTree } from 'treeify';
+
+const arrayToObject = (val, edges) =>
+  val.reduce((tree, node) => {
+    tree[node] = edges[node] ? arrayToObject(edges[node], edges) : undefined;
+    return tree;
+  }, {});
+
+const serializeCategoryTree = ct => {
+  const tree = arrayToObject(ct.rootIds, ct.edges);
+  const assignedKeys = Object.keys(ct.edges)
+    .filter(key => key in ct.nodes)
+    .reduce((acc, key) => [...acc, ...ct.edges[key]], [])
+    .concat(ct.rootIds);
+  const danglingKeys = Object.keys(ct.nodes).filter(key => !assignedKeys.includes(key));
+  if (danglingKeys.length) {
+    // tslint:disable-next-line:no-string-literal
+    tree['dangling'] = arrayToObject(danglingKeys, ct.edges);
+  }
+  return asTree(tree);
+};
+
+const test = val => val && val.rootIds && val.edges && val.nodes;
+
+module.exports = {
+  print: serializeCategoryTree,
+  test: test,
+};
