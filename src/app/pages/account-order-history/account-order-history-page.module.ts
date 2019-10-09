99e2ee387bc39dd9340b9539cc503ce4872d1974
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-10-09 16:19:31 +0200

    merge account-order-history-page container and component

--- src/app/pages/account-order-history/account-order-history-page.module.ts
+++ src/app/pages/account-order-history/account-order-history-page.module.ts
@@ -4,11 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from 'ish-shared/shared.module';
 
 import { AccountOrderHistoryPageContainerComponent } from './account-order-history-page.container';
-import { AccountOrderHistoryPageComponent } from './components/account-order-history-page/account-order-history-page.component';
 
 const routes: Routes = [{ path: '', component: AccountOrderHistoryPageContainerComponent }];
 @NgModule({
   imports: [RouterModule.forChild(routes), SharedModule],
-  declarations: [AccountOrderHistoryPageComponent, AccountOrderHistoryPageContainerComponent],
+  declarations: [AccountOrderHistoryPageContainerComponent],
 })
 export class AccountOrderHistoryPageModule {}
