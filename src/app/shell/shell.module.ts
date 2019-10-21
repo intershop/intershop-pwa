873adac52b79702bfcfb99513f6d3cebeb4130ff
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-10-21 11:02:10 +0200

    merge login-status container and component

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -16,8 +16,6 @@ import { FooterComponent } from './footer/components/footer/footer.component';
 import { HeaderCheckoutComponent } from './header/components/header-checkout/header-checkout.component';
 import { HeaderDefaultComponent } from './header/components/header-default/header-default.component';
 import { HeaderSimpleComponent } from './header/components/header-simple/header-simple.component';
-import { LoginStatusComponent } from './header/components/login-status/login-status.component';
-import { LogoutComponent } from './header/components/logout/logout.component';
 import { ProductImageComponent } from './header/components/product-image/product-image.component';
 import { SearchBoxComponent } from './header/components/search-box/search-box.component';
 import { SubCategoryNavigationComponent } from './header/components/sub-category-navigation/sub-category-navigation.component';
@@ -58,9 +56,7 @@ const exportedComponents = [
     HeaderNavigationContainerComponent,
     HeaderSimpleComponent,
     LanguageSwitchContainerComponent,
-    LoginStatusComponent,
     LoginStatusContainerComponent,
-    LogoutComponent,
     MiniBasketContainerComponent,
     ProductCompareStatusContainerComponent,
     SearchBoxComponent,
