26659cff3cdceb077a93dce504548aa757681d12
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-10-21 10:26:51 +0200

    merge mini-basket container and component

--- src/app/shell/header/containers/mini-basket/mini-basket.container.spec.ts
+++ src/app/shell/header/containers/mini-basket/mini-basket.container.spec.ts
@@ -1,12 +1,16 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { combineReducers } from '@ngrx/store';
-import { MockComponent } from 'ng-mocks';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MockComponent, MockPipe } from 'ng-mocks';
+import { EMPTY, of } from 'rxjs';
+import { instance, mock, when } from 'ts-mockito';
 
-import { checkoutReducers } from 'ish-core/store/checkout/checkout-store.module';
-import { shoppingReducers } from 'ish-core/store/shopping/shopping-store.module';
-import { ngrxTesting } from 'ish-core/utils/dev/ngrx-testing';
-import { MiniBasketComponent } from 'ish-shell/header/components/mini-basket/mini-basket.component';
+import { CheckoutFacade } from 'ish-core/facades/checkout.facade';
+import { PricePipe } from 'ish-core/models/price/price.pipe';
+import { ProductRoutePipe } from 'ish-core/pipes/product-route.pipe';
+import { BasketMockData } from 'ish-core/utils/dev/basket-mock-data';
+import { ProductImageComponent } from 'ish-shell/header/components/product-image/product-image.component';
 
 import { MiniBasketContainerComponent } from './mini-basket.container';
 
@@ -14,28 +18,100 @@ describe('Mini Basket Container', () => {
   let component: MiniBasketContainerComponent;
   let fixture: ComponentFixture<MiniBasketContainerComponent>;
   let element: HTMLElement;
+  let checkoutFacade: CheckoutFacade;
 
   beforeEach(async(() => {
+    checkoutFacade = mock(CheckoutFacade);
     TestBed.configureTestingModule({
-      declarations: [MiniBasketContainerComponent, MockComponent(MiniBasketComponent)],
-      imports: [
-        RouterTestingModule,
-        ngrxTesting({
-          reducers: { shopping: combineReducers(shoppingReducers), checkout: combineReducers(checkoutReducers) },
-        }),
+      declarations: [
+        MiniBasketContainerComponent,
+        MockComponent(FaIconComponent),
+        MockComponent(ProductImageComponent),
+        MockPipe(ProductRoutePipe),
+        PricePipe,
       ],
-    })
-      .compileComponents()
-      .then(() => {
-        fixture = TestBed.createComponent(MiniBasketContainerComponent);
-        component = fixture.componentInstance;
-        element = fixture.nativeElement;
-      });
+      imports: [RouterTestingModule, TranslateModule.forRoot()],
+      providers: [
+        {
+          provide: CheckoutFacade,
+          useFactory: () => instance(checkoutFacade),
+        },
+      ],
+    }).compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MiniBasketContainerComponent);
+    component = fixture.componentInstance;
+    element = fixture.nativeElement;
+    const translate = TestBed.get(TranslateService);
+    translate.setDefaultLang('en');
+    translate.use('en');
+    translate.setTranslation('en', {
+      'shopping_cart.ministatus.items.text': { other: '# items' },
+      'shopping_cart.pli.qty.label': 'x',
+      'shopping_cart.ministatus.view_cart.link': 'VIEW CART',
+    });
+    const lineItem = BasketMockData.getBasketItem();
+
+    when(checkoutFacade.basketItemCount$).thenReturn(of(lineItem.quantity.value * 3));
+    when(checkoutFacade.basketItemTotal$).thenReturn(of(BasketMockData.getTotals().itemTotal));
+    when(checkoutFacade.basketLineItems$).thenReturn(of([lineItem, lineItem, lineItem]));
+    when(checkoutFacade.basketError$).thenReturn(EMPTY);
+    when(checkoutFacade.basketChange$).thenReturn(EMPTY);
+  });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
     expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+  it('should not render product rows if collapsed', () => {
+    fixture.detectChanges();
+    expect(element.getElementsByClassName('product-row')).toHaveLength(0);
+  });
+
+  it('should render product rows if expanded', () => {
+    component.open();
+    fixture.detectChanges();
+    expect(element.getElementsByClassName('product-row')).toHaveLength(3);
+  });
+
+  it('should display summary when collapsed', () => {
+    fixture.detectChanges();
+    expect(element.textContent.replace(/ /g, '')).toMatchInlineSnapshot(`"30items/$141,796.98"`);
+  });
+
+  it('should display right amounts, lineItems and totals when expanded', () => {
+    component.open();
+    fixture.detectChanges();
+    expect(element.textContent.replace(/ /g, '')).toMatchInlineSnapshot(
+      `"30items/$141,796.98pliname$3.00x10pliname$3.00x10pliname$3.00x10VIEWCART"`
+    );
+  });
+
+  it('should render product image component on expanded component', () => {
+    component.open();
+    fixture.detectChanges();
+    expect(element.getElementsByTagName('ish-product-image')).toHaveLength(3);
+  });
+
+  it('should set isCollapsed to proper value if toggleCollapsed is called', () => {
+    component.isCollapsed = true;
+    component.toggleCollapse();
+    expect(component.isCollapsed).toBeFalsy();
+    component.toggleCollapse();
+    expect(component.isCollapsed).toBeTruthy();
+  });
+
+  it('should set isCollapsed to true if collapse() is called', () => {
+    component.collapse();
+    expect(component.isCollapsed).toBeTruthy();
+  });
+
+  it('should set isCollapsed to false if open() is called', () => {
+    component.open();
+    expect(component.isCollapsed).toBeFalsy();
   });
 });
--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -18,7 +18,6 @@ import { HeaderDefaultComponent } from './header/components/header-default/heade
 import { HeaderSimpleComponent } from './header/components/header-simple/header-simple.component';
 import { LoginStatusComponent } from './header/components/login-status/login-status.component';
 import { LogoutComponent } from './header/components/logout/logout.component';
-import { MiniBasketComponent } from './header/components/mini-basket/mini-basket.component';
 import { ProductImageComponent } from './header/components/product-image/product-image.component';
 import { SearchBoxComponent } from './header/components/search-box/search-box.component';
 import { SubCategoryNavigationComponent } from './header/components/sub-category-navigation/sub-category-navigation.component';
@@ -62,7 +61,6 @@ const exportedComponents = [
     LoginStatusComponent,
     LoginStatusContainerComponent,
     LogoutComponent,
-    MiniBasketComponent,
     MiniBasketContainerComponent,
     ProductCompareStatusContainerComponent,
     SearchBoxComponent,
