80b1fede46f6d4db5d2af701d1df0f9e651fa263
Sebastian Hähnlein <s.haehnlein@intershop.de>
2019-10-29 14:57:38 +0100

    retailSet pricerange on product lists

--- src/app/core/services/products/products.service.ts
+++ src/app/core/services/products/products.service.ts
@@ -22,7 +22,7 @@ import { FeatureToggleService } from 'ish-core/utils/feature-toggle/feature-togg
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
   private static STUB_ATTRS =
-    'sku,salePrice,listPrice,availability,manufacturer,image,minOrderQuantity,inStock,promotions,packingUnit,mastered,productMaster,productMasterSKU,roundedAverageRating';
+    'sku,salePrice,listPrice,availability,manufacturer,image,minOrderQuantity,inStock,promotions,packingUnit,mastered,productMaster,productMasterSKU,roundedAverageRating,retailSet';
 
   private itemsPerPage: number;
 
