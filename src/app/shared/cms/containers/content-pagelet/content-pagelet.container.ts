7a5cf1beae6b78f2a4c739ece86f9b04fbbe080e
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-10-30 14:05:58 +0100

    handle pagelet id changes reactively

--- src/app/shared/cms/containers/content-pagelet/content-pagelet.container.ts
+++ src/app/shared/cms/containers/content-pagelet/content-pagelet.container.ts
@@ -1,19 +1,23 @@
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   ComponentFactoryResolver,
   Injector,
   Input,
   OnChanges,
+  OnDestroy,
+  OnInit,
   SimpleChange,
   ViewChild,
   ViewContainerRef,
 } from '@angular/core';
-import { Observable } from 'rxjs';
-import { filter, take } from 'rxjs/operators';
+import { ReplaySubject, Subject } from 'rxjs';
+import { switchMap, takeUntil } from 'rxjs/operators';
 
 import { CMSFacade } from 'ish-core/facades/cms.facade';
 import { ContentPageletView } from 'ish-core/models/content-view/content-view.model';
+import { whenTruthy } from 'ish-core/utils/operators';
 import { CMSComponentProvider, CMS_COMPONENT } from 'ish-shared/cms/configurations/injection-keys';
 import { CMSComponent } from 'ish-shared/cms/models/cms-component/cms-component.model';
 import { SfeAdapterService } from 'ish-shared/cms/sfe-adapter/sfe-adapter.service';
@@ -21,12 +25,12 @@ import { SfeMetadataWrapper } from 'ish-shared/cms/sfe-adapter/sfe-metadata-wrap
 import { SfeMapper } from 'ish-shared/cms/sfe-adapter/sfe.mapper';
 
 /**
- * The Content Pagelet Container Component renders the given 'pagelet'.
+ * The Content Pagelet Container Component renders the pagelet for the given 'pageletId'.
  * For the rendering an Angular component is used that has to be provided
  * for the DefinitionQualifiedName of the pagelet.
  *
  * @example
- * <ish-content-pagelet [pageletId]="pagelet.id"></ish-content-pagelet>
+ * <ish-content-pagelet [pageletId]="pagelet"></ish-content-pagelet>
  */
 @Component({
   selector: 'ish-content-pagelet',
@@ -34,58 +38,61 @@ import { SfeMapper } from 'ish-shared/cms/sfe-adapter/sfe.mapper';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 // tslint:disable-next-line:ccp-no-markup-in-containers ccp-no-intelligence-in-components
-export class ContentPageletContainerComponent extends SfeMetadataWrapper implements OnChanges {
+export class ContentPageletContainerComponent extends SfeMetadataWrapper implements OnChanges, OnInit, OnDestroy {
   /**
    * The Id of the Pagelet that is to be rendered.
    */
   @Input() pageletId: string;
 
-  pagelet$: Observable<ContentPageletView>;
-  pagelet: ContentPageletView;
-
-  noMappingFound: boolean;
-
-  private components: CMSComponentProvider[] = [];
-
   @ViewChild('cmsOutlet', { read: ViewContainerRef, static: true }) cmsOutlet: ViewContainerRef;
 
+  private pageletId$ = new ReplaySubject<string>(1);
+  private destroy$ = new Subject();
+
   constructor(
-    injector: Injector,
+    private injector: Injector,
     private componentFactoryResolver: ComponentFactoryResolver,
     private sfeAdapter: SfeAdapterService,
-    private cmsFacade: CMSFacade
+    private cmsFacade: CMSFacade,
+    private cdRef: ChangeDetectorRef
   ) {
     super();
-    this.components = injector.get<CMSComponentProvider[]>(CMS_COMPONENT, []);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+  }
+
+  ngOnInit() {
+    this.pageletId$
+      .pipe(
+        switchMap(pageletId => this.cmsFacade.pagelet$(pageletId)),
+        whenTruthy(),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(pagelet => {
+        this.mapComponent(pagelet);
+        if (this.sfeAdapter.isInitialized()) {
+          this.setSfeMetadata(SfeMapper.mapPageletViewToSfeMetadata(pagelet));
+        }
+        this.cdRef.markForCheck();
+      });
   }
 
   ngOnChanges() {
-    this.pagelet$ = this.cmsFacade.pagelet$(this.pageletId).pipe(
-      filter(x => !!x),
-      take(1)
-    );
-
-    this.pagelet$.subscribe(x => {
-      this.pagelet = x;
-
-      this.mapComponent();
-      if (this.pagelet && this.sfeAdapter.isInitialized()) {
-        this.setSfeMetadata(SfeMapper.mapPageletViewToSfeMetadata(this.pagelet));
-      }
-    });
+    this.pageletId$.next(this.pageletId);
   }
 
-  private mapComponent() {
-    const mappedComponent = this.components.find(
-      c => c.definitionQualifiedName === this.pagelet.definitionQualifiedName
-    );
+  private mapComponent(pagelet: ContentPageletView) {
+    const components = this.injector.get<CMSComponentProvider[]>(CMS_COMPONENT, []);
+    const mappedComponent = components.find(c => c.definitionQualifiedName === pagelet.definitionQualifiedName);
 
     if (mappedComponent) {
       const componentRef = this.createComponent(mappedComponent);
-      this.initializeComponent(componentRef.instance);
+      this.initializeComponent(componentRef.instance, pagelet);
+    } else {
+      console.warn(`did not find mapping for ${pagelet.id} (${pagelet.definitionQualifiedName})`);
     }
-
-    this.noMappingFound = !mappedComponent;
   }
 
   private createComponent(mappedComponent: CMSComponentProvider) {
@@ -94,12 +101,12 @@ export class ContentPageletContainerComponent extends SfeMetadataWrapper impleme
     return this.cmsOutlet.createComponent(factory);
   }
 
-  private initializeComponent(instance: CMSComponent) {
-    instance.pagelet = this.pagelet;
+  private initializeComponent(instance: CMSComponent, pagelet: ContentPageletView) {
+    instance.pagelet = pagelet;
 
     // OnChanges has to be manually invoked on dynamically created components
     if (instance.ngOnChanges) {
-      instance.ngOnChanges({ pagelet: new SimpleChange(undefined, this.pagelet, true) });
+      instance.ngOnChanges({ pagelet: new SimpleChange(undefined, pagelet, true) });
     }
   }
 }
