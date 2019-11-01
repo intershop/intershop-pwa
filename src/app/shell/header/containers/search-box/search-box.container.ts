84d73a9596bb268b792d407fb9fbc2ef2c98b915
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-11-01 12:15:14 +0100

    refactor: move search-box configuration into search-box container

--- src/app/shell/header/containers/search-box/search-box.container.ts
+++ src/app/shell/header/containers/search-box/search-box.container.ts
@@ -6,7 +6,37 @@ import { filter, first, map, shareReplay, withLatestFrom } from 'rxjs/operators'
 import { ShoppingFacade } from 'ish-core/facades/shopping.facade';
 import { SuggestTerm } from 'ish-core/models/suggest-term/suggest-term.model';
 import { whenTruthy } from 'ish-core/utils/operators';
-import { SearchBoxConfiguration } from 'ish-shell/header/configurations/search-box.configuration';
+
+export interface SearchBoxConfiguration {
+  /**
+   * id to support the multiple use on a page
+   */
+  id?: string;
+  /**
+   * text for search button on search box, icon is used if no text is provided
+   */
+  buttonText?: string;
+  /**
+   * placeholder text for search input field
+   */
+  placeholder?: string;
+  /**
+   * if autoSuggest is set to true auto suggestion is provided for search box, else no auto suggestion is provided
+   */
+  autoSuggest?: boolean;
+  /**
+   * configures the number of suggestions if auto suggestion is provided
+   */
+  maxAutoSuggests?: number;
+  /**
+   * configure search box icon
+   */
+  icon?: string;
+  /**
+   * show last search term as search box value
+   */
+  showLastSearchTerm?: boolean;
+}
 
 /**
  * The search box container component
