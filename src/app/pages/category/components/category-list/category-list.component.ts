16a0d146f26d3a47f9f51cdd62d031f7e34ac857
Stefan Hauke <s.hauke@intershop.de>
2019-11-22 10:46:33 +0100

    fix: improve categories list rendering with a trackBy function (prevents unnecessary re-rendering)

--- src/app/pages/category/components/category-list/category-list.component.ts
+++ src/app/pages/category/components/category-list/category-list.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
+import { CategoryView } from 'ish-core/models/category-view/category-view.model';
 import { Category } from 'ish-core/models/category/category.model';
 
 @Component({
@@ -9,4 +10,8 @@ import { Category } from 'ish-core/models/category/category.model';
 })
 export class CategoryListComponent {
   @Input() categories: Category[];
+
+  trackByFn(_, item: CategoryView) {
+    return item.uniqueId;
+  }
 }
