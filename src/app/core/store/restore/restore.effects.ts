d6daec436897519747021fdc05a671ceed1199b5
Danilo Hoffmann <d.hoffmann@intershop.de>
2019-11-25 18:23:48 +0100

    feat: keep session alive by regularly fetching basket

--- src/app/core/store/restore/restore.effects.ts
+++ src/app/core/store/restore/restore.effects.ts
@@ -3,14 +3,26 @@ import { ApplicationRef, Inject, PLATFORM_ID } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Store, select } from '@ngrx/store';
-import { combineLatest, interval } from 'rxjs';
-import { concatMapTo, filter, first, map, mapTo, takeWhile, tap, withLatestFrom } from 'rxjs/operators';
+import { EMPTY, combineLatest, interval } from 'rxjs';
+import {
+  concatMapTo,
+  filter,
+  first,
+  map,
+  mapTo,
+  mergeMapTo,
+  switchMap,
+  takeWhile,
+  tap,
+  withLatestFrom,
+} from 'rxjs/operators';
 
 import { CookiesService } from 'ish-core/services/cookies/cookies.service';
-import { LoadBasketByAPIToken, ResetBasket, getCurrentBasket } from 'ish-core/store/checkout/basket';
+import { LoadBasket, LoadBasketByAPIToken, ResetBasket, getCurrentBasket } from 'ish-core/store/checkout/basket';
 import { LoadOrderByAPIToken, getSelectedOrderId } from 'ish-core/store/orders';
 import { LoadUserByAPIToken, LogoutUser, UserActionTypes, getAPIToken, getLoggedInUser } from 'ish-core/store/user';
 import { whenTruthy } from 'ish-core/utils/operators';
+import { SfeAdapterService } from 'ish-shared/cms/sfe-adapter/sfe-adapter.service';
 
 interface CookieType {
   apiToken: string;
@@ -19,13 +31,15 @@ interface CookieType {
 }
 
 export class RestoreEffects {
+  static readonly SESSION_KEEP_ALIVE = 600000;
   constructor(
     private actions$: Actions,
     private store$: Store<{}>,
     private router: Router,
     private cookieService: CookiesService,
     @Inject(PLATFORM_ID) private platformId: string,
-    private appRef: ApplicationRef
+    private appRef: ApplicationRef,
+    private sfeAdapterService: SfeAdapterService
   ) {}
 
   /**
@@ -120,6 +134,23 @@ export class RestoreEffects {
         map(([, user, basket]) => ({ user, basket, apiToken: this.cookieService.get('apiToken') })),
         filter(({ user, basket, apiToken }) => !user && basket && !apiToken),
         mapTo(new ResetBasket())
+      )
+    )
+  );
+
+  @Effect()
+  sessionKeepAlive$ = this.appRef.isStable.pipe(
+    takeWhile(() => isPlatformBrowser(this.platformId)),
+    whenTruthy(),
+    first(),
+    concatMapTo(
+      this.store$.pipe(
+        select(getCurrentBasket),
+        switchMap(basket =>
+          this.sfeAdapterService.isInitialized()
+            ? EMPTY
+            : interval(RestoreEffects.SESSION_KEEP_ALIVE).pipe(mergeMapTo(basket ? [new LoadBasket()] : []))
+        )
       )
     )
   );
