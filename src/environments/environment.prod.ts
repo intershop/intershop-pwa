005f3e80be5decf9ba3f07d0184248b58faec9bc
Stefan Hauke <s.hauke@intershop.de>
2019-11-25 15:43:56 +0100

    configure 'rest' application type

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,4 +7,5 @@ export const environment: Environment = {
 
   /* INTERSHOP COMMERCE MANAGEMENT REST API CONFIGURATION */
   icmBaseURL: 'https://intershoppwa.azurewebsites.net',
+  icmApplication: 'rest',
 };
