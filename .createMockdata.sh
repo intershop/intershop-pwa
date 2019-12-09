368c72a05798705e2d88d5557dc1884a1746c29f
Stefan Hauke <s.hauke@intershop.de>
2019-12-09 13:58:17 +0100

    fix: remove trademark content from open source project (#39)
    - remove not used, obsolete images and styles
    - remove unnecessary demo content

--- .createMockdata.sh
+++ .createMockdata.sh
@@ -28,5 +28,5 @@ do
     mkdir -p src/assets/mock-data/$path
 
     echo "  to src/assets/mock-data/$path/get.json"
-    curl -sf -H "$header" "http://10.0.206.226:4322/INTERSHOP/rest/WFS/inSPIRED-inTRONICS-Site/-/${path}${params}" | jq -S -M "${jqquery}" | sed -e 's%/INTERSHOP/static/.*.jpg%assets/mock-img/product/a.jpg%' | sed -e 's%inSPIRED-inTRONICS-b2c-responsive:%assets/mock-img%' > src/assets/mock-data/$path/get.json
+    curl -sf -H "$header" "http://10.0.206.226:4322/INTERSHOP/rest/WFS/inSPIRED-inTRONICS-Site/-/${path}${params}" | jq -S -M "${jqquery}" | sed -e 's%/INTERSHOP/static/.*.jpg%assets/img/not_available.png%' > src/assets/mock-data/$path/get.json
 done
