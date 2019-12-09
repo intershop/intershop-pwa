1a197a6fcbdcca5e7e78d3edc7eec8573cde0341
Sebastian Hähnlein <50739746+Sebastian-Haehnlein@users.noreply.github.com>
2019-12-09 14:56:45 +0100

    fix: display error messages on empty cart

--- src/app/pages/basket/components/shopping-basket-empty/shopping-basket-empty.component.ts
+++ src/app/pages/basket/components/shopping-basket-empty/shopping-basket-empty.component.ts
@@ -1,8 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+import { HttpError } from 'ish-core/models/http-error/http-error.model';
 
 @Component({
   selector: 'ish-shopping-basket-empty',
   templateUrl: './shopping-basket-empty.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShoppingBasketEmptyComponent {}
+export class ShoppingBasketEmptyComponent {
+  @Input() error: HttpError;
+}
