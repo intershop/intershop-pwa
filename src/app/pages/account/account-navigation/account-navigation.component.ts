25ce59a336edc5739118ef0983895bd3518349a3
Sebastian Hähnlein <S.Haehnlein@intershop.de>
2019-12-09 15:28:05 +0100

    fix: my account overview breadcrumb should not have parent / child structure (#43)

--- src/app/pages/account/account-navigation/account-navigation.component.ts
+++ src/app/pages/account/account-navigation/account-navigation.component.ts
@@ -17,7 +17,7 @@ export class AccountNavigationComponent implements OnInit, OnChanges {
    * Manages the Account Navigation items.
    */
   navigationItems: { link: string; localizationKey: string; dataTestingId?: string; feature?: string }[] = [
-    { link: '/account/overview', localizationKey: 'account.my_account.link' },
+    { link: '/account', localizationKey: 'account.my_account.link' },
     { link: '/account/orders', localizationKey: 'account.order_history.link' },
     { link: '/account/addresses', localizationKey: 'account.saved_addresses.link', dataTestingId: 'addresses-link' },
     { link: '/account/profile', localizationKey: 'account.profile.link' },
