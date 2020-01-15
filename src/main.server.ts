f39bd8315ae5268ec51a4c27c7a89eacc8f20105
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-01-15 14:03:34 +0100

    feat: proxy ICM through express server (#99)

--- src/main.server.ts
+++ src/main.server.ts
@@ -9,3 +9,4 @@ if (environment.production) {
 export { AppServerModule } from './app/app.server.module';
 export { ngExpressEngine } from '@nguniversal/express-engine';
 export { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+export { environment } from './environments/environment';
