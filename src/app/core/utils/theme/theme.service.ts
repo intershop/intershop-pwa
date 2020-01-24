46e84ba155c68cc8ab56328ea0751daf1eeb069f
Sebastian Hähnlein <S.Haehnlein@intershop.de>
2020-01-24 10:05:26 +0100

    feat: extended multiple theme support to control system icons (e.g. favicon), manifest.webmanifest and theme-color as well (#88, #100)
    closes: #88

--- src/app/core/utils/theme/theme.service.ts
+++ src/app/core/utils/theme/theme.service.ts
@@ -17,15 +17,32 @@ export class ThemeService {
   private head: HTMLElement;
   private themeLinks: HTMLElement[] = [];
 
+  private iconLink: HTMLLinkElement;
+  private webmanifestLink: HTMLLinkElement;
+  private appleTouchIconLink: HTMLLinkElement;
+  private appleTouchIcon152x152Link: HTMLLinkElement;
+  private appleTouchIcon167x167Link: HTMLLinkElement;
+  private appleTouchIcon180x180Link: HTMLLinkElement;
+  private themeColor: HTMLMetaElement;
+
   constructor(
     private rendererFactory: RendererFactory2,
     @Inject(DOCUMENT) private document: Document,
     private store: Store<{}>
-  ) {}
-
-  init() {
+  ) {
     this.head = this.document.head;
     this.renderer = this.rendererFactory.createRenderer(undefined, undefined);
+
+    this.iconLink = this.document.querySelector('link[rel="icon"]');
+    this.webmanifestLink = this.document.querySelector('link[rel="manifest"]');
+    this.appleTouchIconLink = this.document.querySelector('link[rel="apple-touch-icon"]:not([sizes])');
+    this.appleTouchIcon152x152Link = this.document.querySelector('link[rel="apple-touch-icon"][sizes="152x152"]');
+    this.appleTouchIcon167x167Link = this.document.querySelector('link[rel="apple-touch-icon"][sizes="167x167"]');
+    this.appleTouchIcon180x180Link = this.document.querySelector('link[rel="apple-touch-icon"][sizes="180x180"]');
+    this.themeColor = this.document.querySelector('meta[name="theme-color"]');
+  }
+
+  init() {
     this.store
       .pipe(select(getTheme))
       .pipe(
@@ -33,12 +50,22 @@ export class ThemeService {
         filter(x => !!x)
       )
       .subscribe(async theme => {
-        await this.loadCss(`${theme}.css`);
+        const themeData = theme.split('|');
+        const themeName = themeData[0];
+        const themeColor = themeData[1];
 
-        // remove style of previous theme
-        if (this.themeLinks.length === 2) {
-          this.renderer.removeChild(this.head, this.themeLinks.shift());
+        this.iconLink.setAttribute('href', `assets/themes/${themeName}/img/favicon.ico`);
+        this.webmanifestLink.setAttribute('href', `assets/themes/${themeName}/manifest.webmanifest`);
+        this.appleTouchIconLink.setAttribute('href', `assets/themes/${themeName}/img/logo_apple_120x120.png`);
+        this.appleTouchIcon152x152Link.setAttribute('href', `assets/themes/${themeName}/img/logo_apple_152x152.png`);
+        this.appleTouchIcon167x167Link.setAttribute('href', `assets/themes/${themeName}/img/logo_apple_167x167.png`);
+        this.appleTouchIcon180x180Link.setAttribute('href', `assets/themes/${themeName}/img/logo_apple_180x180.png`);
+
+        if (themeColor) {
+          this.themeColor.setAttribute('content', `#${themeColor}`);
         }
+
+        await this.loadCss(`${themeName}.css`);
       });
   }
 
@@ -51,6 +78,11 @@ export class ThemeService {
       this.renderer.setProperty(linkEl, 'onload', resolve);
       this.renderer.appendChild(this.head, linkEl);
       this.themeLinks = [...this.themeLinks, linkEl];
+
+      // remove style of previous theme
+      if (this.themeLinks.length === 2) {
+        this.renderer.removeChild(this.head, this.themeLinks.shift());
+      }
     });
   }
 }
