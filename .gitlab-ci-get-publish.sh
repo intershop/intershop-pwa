ec9cd9d59bde350c6f4bb3bd6827a5e6ea000740
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-01-30 10:47:27 +0100

    chore: gitlab pipeline adaptions

--- .gitlab-ci-get-publish.sh
+++ .gitlab-ci-get-publish.sh
@@ -3,7 +3,7 @@
 [ -z "$1" ] && echo "instance name required" && exit 1
 internalPort="${2:-4200}"
 
-currentPort="$(docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}}{{(index $conf 0).HostPort}}{{end}}' "$1")"
+currentPort="$(docker inspect --format='{{(index (index .NetworkSettings.Ports "$internalPort/tcp") 0).HostPort}}' "$1")"
 
 if [ -z "$currentPort" ]
 then
