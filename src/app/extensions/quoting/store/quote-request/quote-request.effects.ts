3782d29f25432ecac737f593fc160ef96cfebaef
Sebastian Hähnlein <S.Haehnlein@intershop.de>
2020-02-12 13:17:35 +0100

    fix: 'submit quote request' and 'copy submitted quote request' from modal dialog should not navigate to my account (#112)

--- src/app/extensions/quoting/store/quote-request/quote-request.effects.ts
+++ src/app/extensions/quoting/store/quote-request/quote-request.effects.ts
@@ -152,14 +152,19 @@ export class QuoteRequestEffects {
    */
   @Effect()
   createQuoteRequestFromQuoteRequest$ = this.actions$.pipe(
-    ofType(actions.QuoteRequestActionTypes.CreateQuoteRequestFromQuoteRequest),
+    ofType<actions.CreateQuoteRequestFromQuoteRequest>(
+      actions.QuoteRequestActionTypes.CreateQuoteRequestFromQuoteRequest
+    ),
+    mapToPayloadProperty('redirect'),
     withLatestFrom(this.store.pipe(select(getSelectedQuoteRequestWithProducts))),
-    concatMap(([, currentQuoteRequest]) =>
+    concatMap(([redirect, currentQuoteRequest]) =>
       this.quoteRequestService.createQuoteRequestFromQuoteRequest(currentQuoteRequest).pipe(
         map(quoteLineItemResult => new actions.CreateQuoteRequestFromQuoteRequestSuccess({ quoteLineItemResult })),
-        tap(quoteLineItemResult =>
-          this.router.navigate([`/account/quote-request/${quoteLineItemResult.payload.quoteLineItemResult.title}`])
-        ),
+        tap(quoteLineItemResult => {
+          if (redirect) {
+            this.router.navigate([`/account/quote-request/${quoteLineItemResult.payload.quoteLineItemResult.title}`]);
+          }
+        }),
         mapErrorToAction(actions.CreateQuoteRequestFromQuoteRequestFail)
       )
     )
@@ -319,6 +324,18 @@ export class QuoteRequestEffects {
     tap(quoteRequestId => {
       this.router.navigate([`/account/quote-request/${quoteRequestId}`]);
     })
+  );
+
+  /**
+   * Triggers a SelectQuoteRequest action for the just created copy after successfully creating a quote request copy.
+   */
+  @Effect()
+  selectQuoteRequestAfterCopy$ = this.actions$.pipe(
+    ofType<actions.CreateQuoteRequestFromQuoteRequestSuccess>(
+      actions.QuoteRequestActionTypes.CreateQuoteRequestFromQuoteRequestSuccess
+    ),
+    mapToPayload(),
+    map(payload => new actions.SelectQuoteRequest({ id: payload.quoteLineItemResult.title }))
   );
 
   /**
