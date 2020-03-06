8b798af2b0e2eb7cf5b73c470363cca593901fd5
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-03-06 17:21:11 +0100

    test: increase number of parallel cypress jobs for gitlab ci

--- e2e/e2e-partition.sh
+++ e2e/e2e-partition.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# for (( i=1 ; i<=10 ; i++ )) ; do echo $i ; sh e2e-partition.sh 10 $i '*b2c*.e2e-spec.ts' ; done
+
 set -e
 
 [ -z "$1" ] && echo "number of partitions required" && exit 1
@@ -8,13 +10,16 @@ set -e
 
 tests="$(find "$(dirname "$(readlink -f "$0")")" -name $3 | sort)"
 no="$(echo "$tests" | wc -l)"
-psize="$(($no / $1 + 1))"
+psize="$(($no / $1 + 1 ))"
 
 # echo "found $no tests, partitions with size $psize"
 
 if [ "$2" -eq "1" ]
 then
   echo "$tests" | head -n $psize
+elif [ "$2" -eq "$1" ]
+then
+  echo "$tests" | tail -n +$(( 1 + ($psize * ($2 - 1))))
 else
   echo "$tests" | tail -n +$(( 1 + ($psize * ($2 - 1)))) | head -n $psize
 fi
