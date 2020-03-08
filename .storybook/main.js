9f2e2e3d8047c3a231624d3c6f9aa42b0a83f1b6
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-03-08 19:52:59 +0100

    chore: add storybook viewport addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
   stories: ['../src/**/*.stories.ts'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-notes',
+    '@storybook/addon-viewport/register',
+  ],
 };
