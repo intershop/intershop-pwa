7a949896eba09f91a132b6d609176849973bfc72
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-03-30 16:02:35 +0200

    refactor: ngrx state transfer as app initializer

--- src/app/core/state-management.module.ts
+++ src/app/core/state-management.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserTransferStateModule } from '@angular/platform-browser';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserTransferStateModule, TransferState } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { Actions } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { environment } from '../../environments/environment';
 
-import { NgrxStateTransfer } from './configurations/ngrx-state-transfer';
+import { ngrxStateTransfer } from './configurations/ngrx-state-transfer';
 import { CoreStoreModule } from './store/core-store.module';
 
 @NgModule({
@@ -18,10 +20,8 @@ import { CoreStoreModule } from './store/core-store.module';
       logOnly: environment.production, // Restrict extension to log-only mode
     }),
   ],
-  providers: [NgrxStateTransfer],
+  providers: [
+    { provide: APP_INITIALIZER, useFactory: ngrxStateTransfer, deps: [TransferState, Store, Actions], multi: true },
+  ],
 })
-export class StateManagementModule {
-  constructor(ngrxStateTransfer: NgrxStateTransfer) {
-    ngrxStateTransfer.do();
-  }
-}
+export class StateManagementModule {}
