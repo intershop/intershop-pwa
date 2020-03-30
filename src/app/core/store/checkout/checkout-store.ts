f4ace83ddf8bcc30a6b775558694fb90a996123e
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-03-30 16:19:29 +0200

    refactor: remove state handling checkoutStep and access it via router-store (#167)

--- src/app/core/store/checkout/checkout-store.ts
+++ src/app/core/store/checkout/checkout-store.ts
@@ -1,11 +1,9 @@
 import { createFeatureSelector } from '@ngrx/store';
 
 import { BasketState } from './basket/basket.reducer';
-import { ViewconfState } from './viewconf/viewconf.reducer';
 
 export interface CheckoutState {
   basket: BasketState;
-  viewconf: ViewconfState;
 }
 
 export const getCheckoutState = createFeatureSelector<CheckoutState>('checkout');
