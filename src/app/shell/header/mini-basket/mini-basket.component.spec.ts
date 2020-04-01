44ad2a5abe7b1e980798fdbd62cd340d2086be7a
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-04-01 20:25:13 +0200

    feat: display checkout and order prices respecting ICM pricing settings (#179)

--- src/app/shell/header/mini-basket/mini-basket.component.spec.ts
+++ src/app/shell/header/mini-basket/mini-basket.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { provideMockStore } from '@ngrx/store/testing';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MockComponent, MockPipe } from 'ng-mocks';
 import { EMPTY, of } from 'rxjs';
@@ -9,6 +10,7 @@ import { instance, mock, when } from 'ts-mockito';
 import { CheckoutFacade } from 'ish-core/facades/checkout.facade';
 import { PricePipe } from 'ish-core/models/price/price.pipe';
 import { ProductRoutePipe } from 'ish-core/routing/product/product-route.pipe';
+import { getPriceDisplayType } from 'ish-core/store/user';
 import { BasketMockData } from 'ish-core/utils/dev/basket-mock-data';
 import { ProductImageComponent } from 'ish-shell/header/product-image/product-image.component';
 
@@ -36,6 +38,7 @@ describe('Mini Basket Component', () => {
           provide: CheckoutFacade,
           useFactory: () => instance(checkoutFacade),
         },
+        provideMockStore({ selectors: [{ selector: getPriceDisplayType, value: 'gross' }] }),
       ],
     }).compileComponents();
   }));
