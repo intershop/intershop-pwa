d94bc400e80e1327271a629b97ece2276ab9118b
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-04-14 15:52:29 +0200

    build: check for dead links in docs

---
+++ docs/check-dead-links.js
@@ -0,0 +1,108 @@
+const fs = require('fs');
+const path = require('path');
+const async = require('async');
+const { promisify } = require('util');
+const glob = promisify(require('glob'));
+
+async function checkExternalLinkError(link) {
+  console.log('check', link);
+  const client = link.startsWith('https') ? require('https') : require('http');
+  return new Promise(resolve => {
+    const req = client.request(link, res => {
+      isError = res.statusCode >= 400;
+      if (isError) {
+        console.warn('found dead link to', link);
+      }
+      resolve(isError);
+    });
+
+    req.on('error', () => {
+      console.warn('found dead link to', link);
+      resolve(true);
+    });
+    req.end();
+  });
+}
+
+function sleep(milliseconds) {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+function getLineInfoOfString(data, str) {
+  var perLine = data.split('\n');
+  for (let line = 0; line < perLine.length; line++) {
+    const index = perLine[line].indexOf(str);
+    if (index > 0) {
+      return ':' + (line + 1) + ':' + (index + 1);
+    }
+  }
+  return '';
+}
+
+glob('**/*.md')
+  .then(files => {
+    const externalLinks = [];
+    let isError = false;
+
+    files
+      .filter(file => !file.includes('node_modules'))
+      .forEach(file => {
+        const content = fs.readFileSync(file, { encoding: 'utf-8' });
+        const match = content.match(/\[.*?\](\(|:\s+)[^\s]*\)?/g);
+        if (match) {
+          match.forEach(link => {
+            const linkTo = /\](\(<?|:\s+)(.*?)(>?\)|$|#)/.exec(link)[2];
+
+            if (linkTo) {
+              // link is not document-internal
+              if (linkTo.startsWith('http')) {
+                externalLinks.push(linkTo);
+              } else {
+                const normalized = path.normalize(path.join(path.dirname(file), linkTo));
+                if (!fs.existsSync(normalized)) {
+                  console.warn(file + getLineInfoOfString(content, linkTo) + ': found dead link to "' + linkTo + '"');
+                  isError = true;
+                }
+              }
+            }
+          });
+        }
+      });
+    if (isError) {
+      throw new Error('found dead internal links');
+    }
+    return externalLinks;
+  })
+  .then(externalLinks => {
+    if (process.argv.length > 2 && process.argv[2] === 'fast') {
+      console.warn('skipping external link check');
+      return;
+    }
+
+    const filtered = externalLinks
+      .map(link => link.replace(/\/$/, ''))
+      .filter((val, idx, arr) => arr.indexOf(val) === idx)
+      .filter(
+        link =>
+          !link.includes('repository.intershop.de') &&
+          !link.includes('support.intershop.com') &&
+          !link.includes('github.com/intershop/intershop-pwa/commit')
+      )
+      .sort();
+
+    async.eachSeries(
+      filtered,
+      async link => {
+        const isError = await checkExternalLinkError(link);
+        if (isError) throw new Error('found dead external link to', link);
+        await sleep(1000);
+      },
+      err => {
+        if (err) throw err;
+      }
+    );
+  })
+  .catch(err => {
+    console.error(err.message);
+    process.exit(1);
+  });
