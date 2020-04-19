f5a47025d1289ce4fc5704da10a46807b45bb428
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-04-19 18:14:51 +0200

    refactor: introduce new format for ngrxTesting

--- src/app/core/feature-toggle.module.ts
+++ src/app/core/feature-toggle.module.ts
@@ -1,13 +1,33 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 
 import { FeatureToggleDirective } from './directives/feature-toggle.directive';
 import { NotFeatureToggleDirective } from './directives/not-feature-toggle.directive';
+import { FeatureToggleService, checkFeature } from './utils/feature-toggle/feature-toggle.service';
 
 @NgModule({
   declarations: [FeatureToggleDirective, NotFeatureToggleDirective],
   exports: [FeatureToggleDirective, NotFeatureToggleDirective],
 })
-export class FeatureToggleModule {}
+export class FeatureToggleModule {
+  private static features: string[];
+
+  static forTesting(...features: string[]): ModuleWithProviders {
+    FeatureToggleModule.switchTestingFeatures(...features);
+    return {
+      ngModule: FeatureToggleModule,
+      providers: [
+        {
+          provide: FeatureToggleService,
+          useValue: { enabled: (feature: string) => checkFeature(FeatureToggleModule.features, feature) },
+        },
+      ],
+    };
+  }
+
+  static switchTestingFeatures(...features: string[]) {
+    FeatureToggleModule.features = features;
+  }
+}
 
 export { FeatureToggleService } from './utils/feature-toggle/feature-toggle.service';
 export { FeatureToggleGuard } from './guards/feature-toggle.guard';
--- src/app/extensions/quoting/store/quoting-store.module.ts
+++ src/app/extensions/quoting/store/quoting-store.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { ActionReducerMap, StoreModule } from '@ngrx/store';
+import { pick } from 'lodash-es';
 
 import { QuoteRequestEffects } from './quote-request/quote-request.effects';
 import { quoteRequestReducer } from './quote-request/quote-request.reducer';
@@ -8,15 +9,22 @@ import { QuoteEffects } from './quote/quote.effects';
 import { quoteReducer } from './quote/quote.reducer';
 import { QuotingState } from './quoting-store';
 
+/** @deprecated will be made private in version 0.23 */
 export const quotingReducers: ActionReducerMap<QuotingState> = {
   quote: quoteReducer,
   quoteRequest: quoteRequestReducer,
 };
 
+/** @deprecated will be made private in version 0.23 */
 export const quotingEffects = [QuoteEffects, QuoteRequestEffects];
+// tslint:disable: deprecation
 
 // not-dead-code
 @NgModule({
   imports: [EffectsModule.forFeature(quotingEffects), StoreModule.forFeature('quoting', quotingReducers)],
 })
-export class QuotingStoreModule {}
+export class QuotingStoreModule {
+  static forTesting(...reducers: (keyof ActionReducerMap<QuotingState>)[]) {
+    return StoreModule.forFeature('quoting', pick(quotingReducers, reducers));
+  }
+}
