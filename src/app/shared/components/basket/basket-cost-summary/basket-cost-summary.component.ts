67fa0610b8a6c80e0685e14994a2ee3d428eb9ca
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-04-27 15:10:40 +0200

    fix: suppress display of total payment costs with zero value on cost summary

--- src/app/shared/components/basket/basket-cost-summary/basket-cost-summary.component.ts
+++ src/app/shared/components/basket/basket-cost-summary/basket-cost-summary.component.ts
@@ -4,6 +4,7 @@ import { map } from 'rxjs/operators';
 
 import { AccountFacade } from 'ish-core/facades/account.facade';
 import { BasketTotal } from 'ish-core/models/basket-total/basket-total.model';
+import { PriceItemHelper } from 'ish-core/models/price-item/price-item.helper';
 import { PriceHelper } from 'ish-core/models/price/price.model';
 
 /**
@@ -31,5 +32,10 @@ export class BasketCostSummaryComponent implements OnInit {
     this.taxTranslation$ = this.accountFacade.userPriceDisplayType$.pipe(
       map(type => (type === 'net' ? 'checkout.tax.text' : 'checkout.tax.TaxesLabel.TotalOrderVat'))
     );
+  }
+
+  get hasPaymentCostsTotal(): boolean {
+    const paymentCosts = PriceItemHelper.selectType(this.totals && this.totals.paymentCostsTotal, 'gross');
+    return !!paymentCosts && !!paymentCosts.value;
   }
 }
