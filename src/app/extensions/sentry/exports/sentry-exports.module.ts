9d9de6915e675a57155c2d96e0005d91f4f99a6c
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-05-05 12:16:48 +0200

    refactor: lazy-load sentry module

---
+++ src/app/extensions/sentry/exports/sentry-exports.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { LAZY_FEATURE_MODULE } from 'ish-core/utils/module-loader/module-loader.service';
+
+@NgModule({
+  providers: [
+    {
+      provide: LAZY_FEATURE_MODULE,
+      useValue: { feature: 'sentry', location: import('../sentry.module') },
+      multi: true,
+    },
+  ],
+})
+export class SentryExportsModule {}
