c7d49c235cd090724517558a0b24b7540a23277d
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-05-12 14:13:52 +0200

    feat: respect returnUrl query parameter when logout guard is triggered

--- src/app/core/guards/logout.guard.ts
+++ src/app/core/guards/logout.guard.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
+import { map } from 'rxjs/operators';
 
+import { selectQueryParam } from 'ish-core/store/router';
 import { LogoutUser } from 'ish-core/store/user';
 
 /**
  * triggers logging out the user if the guarded route is visited
+ *
+ * if returnUrl is supplied as a queryParam, it redirects there (default is /home)
  */
 @Injectable({ providedIn: 'root' })
 export class LogoutGuard implements CanActivate {
@@ -13,6 +17,10 @@ export class LogoutGuard implements CanActivate {
 
   canActivate() {
     this.store.dispatch(new LogoutUser());
-    return this.router.createUrlTree(['/home']);
+    return this.store.pipe(
+      select(selectQueryParam('returnUrl')),
+      map(returnUrl => returnUrl || '/home'),
+      map(returnUrl => this.router.parseUrl(returnUrl))
+    );
   }
 }
