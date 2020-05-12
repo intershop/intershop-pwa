788cabca29c97ff414ab7055baeb213538c20efb
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-05-12 14:13:52 +0200

    feat: respect returnUrl query parameter when logout guard is triggered

--- src/app/core/guards/logout.guard.spec.ts
+++ src/app/core/guards/logout.guard.spec.ts
@@ -1,42 +1,58 @@
-import { TestBed, async } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Action, Store } from '@ngrx/store';
-import { anything, capture, spy, verify } from 'ts-mockito';
 
 import { UserActionTypes } from 'ish-core/store/user';
-import { ngrxTesting } from 'ish-core/utils/dev/ngrx-testing';
+import { TestStore, containsActionWithType, ngrxTesting } from 'ish-core/utils/dev/ngrx-testing';
 
 import { LogoutGuard } from './logout.guard';
 
 describe('Logout Guard', () => {
-  describe('canActivate()', () => {
-    let logoutGuard: LogoutGuard;
-    let store$: Store;
+  let store$: TestStore;
+  let router: Router;
+  let location: Location;
 
-    beforeEach(async(() => {
-      TestBed.configureTestingModule({
-        imports: [RouterTestingModule, ngrxTesting()],
-      }).compileComponents();
+  beforeEach(() => {
+    @Component({ template: 'dummy' })
+    class DummyComponent {}
 
-      store$ = spy(TestBed.inject(Store));
-    }));
+    TestBed.configureTestingModule({
+      declarations: [DummyComponent],
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'home', component: DummyComponent },
+          { path: 'foo', component: DummyComponent },
+          { path: '**', component: DummyComponent, canActivate: [LogoutGuard] },
+        ]),
+        ngrxTesting({ routerStore: true }),
+      ],
+    }).compileComponents();
 
-    beforeEach(() => {
-      logoutGuard = TestBed.inject(LogoutGuard);
-    });
-
-    it('should log out when called', () => {
-      logoutGuard.canActivate();
-      verify(store$.dispatch(anything())).called();
-
-      const [arg] = capture(store$.dispatch).last();
-      expect((arg as Action).type).toBe(UserActionTypes.LogoutUser);
-    });
-
-    it('should redirect to /home when called', () => {
-      const tree = logoutGuard.canActivate();
-
-      expect(tree.toString()).toMatchInlineSnapshot(`"/home"`);
-    });
+    store$ = TestBed.inject(TestStore);
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
   });
+
+  it('should log out when called', fakeAsync(() => {
+    router.navigateByUrl('/any');
+    tick(500);
+
+    expect(store$.actionsArray()).toSatisfy(containsActionWithType(UserActionTypes.LogoutUser));
+  }));
+
+  it('should redirect to /home when called', fakeAsync(() => {
+    router.navigateByUrl('/any');
+    tick(500);
+
+    expect(location.path()).toMatchInlineSnapshot(`"/home"`);
+  }));
+
+  it('should redirect to returnUrl when supplied', fakeAsync(() => {
+    router.navigateByUrl('/any?returnUrl=/foo');
+    tick(500);
+
+    expect(location.path()).toMatchInlineSnapshot(`"/foo"`);
+  }));
 });
