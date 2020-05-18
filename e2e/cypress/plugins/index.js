65a1d886b784d31fc9939208610d7350abcdf38a
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-05-18 16:37:31 +0200

    deps(e2e): cleanup e2e dependencies

--- e2e/cypress/plugins/index.js
+++ e2e/cypress/plugins/index.js
@@ -1,7 +1,4 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
-
 module.exports = on => {
-  on('file:preprocessor', cypressTypeScriptPreprocessor);
   on('task', {
     failed: require('cypress-failed-log/src/failed')(),
   });
