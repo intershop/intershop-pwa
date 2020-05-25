d14624434d2abc717661fdf907622eb86d273b46
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-05-25 17:19:25 +0200

    feat(schematics): add helper for adding destroy subject to Angular artifact

---
+++ schematics/src/utils/ts-morph.ts
@@ -0,0 +1,15 @@
+import { Tree } from '@angular-devkit/schematics';
+import { existsSync, readFileSync } from 'fs';
+import { FileSystemHost, Project } from 'ts-morph';
+
+export function createTsMorphProject(host: Tree) {
+  return new Project({
+    // tslint:disable-next-line: ish-no-object-literal-type-assertion
+    fileSystem: {
+      getCurrentDirectory: () => '',
+      directoryExistsSync: p => host.exists(p) || existsSync(p),
+      fileExistsSync: p => host.exists(p) || existsSync(p),
+      readFileSync: (p, encoding) => (host.read(p) || readFileSync(p)).toString(encoding),
+    } as FileSystemHost,
+  });
+}
