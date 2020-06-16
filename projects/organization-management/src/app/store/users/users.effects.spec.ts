924edd05e43427f1938bbea49bfec4d41d3e184b
Susanne Schneider <s.schneider@intershop.de>
2020-06-16 09:40:44 +0200

    wip: feat: user management - breadcrumb user detail page

--- projects/organization-management/src/app/store/users/users.effects.spec.ts
+++ projects/organization-management/src/app/store/users/users.effects.spec.ts
@@ -1,28 +1,48 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Action } from '@ngrx/store';
+import { TranslateModule } from '@ngx-translate/core';
 import { Observable, of } from 'rxjs';
 import { instance, mock, verify, when } from 'ts-mockito';
 
 import { User } from 'ish-core/models/user/user.model';
-import { ngrxTesting } from 'ish-core/utils/dev/ngrx-testing';
+import { TestStore, ngrxTesting } from 'ish-core/utils/dev/ngrx-testing';
 
 import { UsersService } from '../../services/users/users.service';
+import { OrganizationManagementStoreModule } from '../organization-management-store.module';
 
 import * as actions from './users.actions';
 import { UsersEffects } from './users.effects';
+
+@Component({ template: 'dummy' })
+class DummyComponent {}
+
+const users = [{ login: '1', firstName: 'Patricia', lastName: 'Miller' }, { login: '2' }] as User[];
 
 describe('Users Effects', () => {
   let actions$: Observable<Action>;
   let effects: UsersEffects;
   let usersService: UsersService;
+  let store$: TestStore;
+  let router: Router;
 
   beforeEach(() => {
     usersService = mock(UsersService);
-    when(usersService.getUsers()).thenReturn(of([{ businessPartnerNo: '1' }, { businessPartnerNo: '2' }] as User[]));
+    when(usersService.getUsers()).thenReturn(of(users));
 
     TestBed.configureTestingModule({
-      imports: [ngrxTesting()],
+      declarations: [DummyComponent],
+      imports: [
+        OrganizationManagementStoreModule.forTesting('users'),
+        RouterTestingModule.withRoutes([{ path: 'users/:B2BCustomerLogin', component: DummyComponent }]),
+        TranslateModule.forRoot(),
+        ngrxTesting({
+          routerStore: true,
+        }),
+      ],
       providers: [
         UsersEffects,
         provideMockActions(() => actions$),
@@ -31,6 +51,8 @@ describe('Users Effects', () => {
     });
 
     effects = TestBed.inject(UsersEffects);
+    store$ = TestBed.inject(TestStore);
+    router = TestBed.inject(Router);
   });
 
   describe('loadUsers$', () => {
@@ -49,7 +71,30 @@ describe('Users Effects', () => {
       effects.loadUsers$.subscribe(action => {
         expect(action).toMatchInlineSnapshot(`
           [Users API] Load Users Success:
-            users: [{"businessPartnerNo":"1"},{"businessPartnerNo":"2"}]
+            users: [{"login":"1","firstName":"Patricia","lastName":"Miller"},{"...
+        `);
+        done();
+      });
+    });
+  });
+
+  describe('setUserDetailBreadcrumb$', () => {
+    it('should set the breadcrumb of user detail', done => {
+      store$.dispatch(new actions.LoadUsersSuccess({ users }));
+      router.navigate(['users', '1']);
+      effects.setUserDetailBreadcrumb$.subscribe(action => {
+        expect(action.payload).toMatchInlineSnapshot(`
+          Object {
+            "breadcrumbData": Array [
+              Object {
+                "key": "account.organization.user_management",
+                "link": "/account/organization/users",
+              },
+              Object {
+                "text": "account.organization.user_management.user_detail.breadcrumb - Patricia Miller",
+              },
+            ],
+          }
         `);
         done();
       });
