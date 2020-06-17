a7f7acfebc41c101e8b473186af1e36e88699f47
Susanne Schneider <s.schneider@intershop.de>
2020-06-17 16:38:43 +0200

    feat: user management - add user detail test

--- projects/organization-management/src/app/pages/users-detail/users-detail-page.component.spec.ts
+++ projects/organization-management/src/app/pages/users-detail/users-detail-page.component.spec.ts
@@ -1,8 +1,11 @@
-import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { TranslateModule } from '@ngx-translate/core';
 import { MockComponent } from 'ng-mocks';
-import { instance, mock } from 'ts-mockito';
+import { of } from 'rxjs';
+import { instance, mock, when } from 'ts-mockito';
+
+import { User } from 'ish-core/models/user/user.model';
 
 import { OrganizationManagementFacade } from '../../facades/organization-management.facade';
 
@@ -12,26 +15,35 @@ describe('Users Detail Page Component', () => {
   let component: UsersDetailPageComponent;
   let fixture: ComponentFixture<UsersDetailPageComponent>;
   let element: HTMLElement;
+  let organizationManagementFacade: OrganizationManagementFacade;
 
-  beforeEach(async(() => {
+  const user = { login: '1', firstName: 'Patricia', lastName: 'Miller', email: 'pmiller@test.intershop.de' } as User;
+
+  beforeEach(() => {
+    organizationManagementFacade = mock(OrganizationManagementFacade);
     TestBed.configureTestingModule({
       imports: [TranslateModule.forRoot()],
       declarations: [MockComponent(FaIconComponent), UsersDetailPageComponent],
-      providers: [
-        { provide: OrganizationManagementFacade, useFactory: () => instance(mock(OrganizationManagementFacade)) },
-      ],
-    }).compileComponents();
-  }));
+      providers: [{ provide: OrganizationManagementFacade, useFactory: () => instance(organizationManagementFacade) }],
+    });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(UsersDetailPageComponent);
     component = fixture.componentInstance;
     element = fixture.nativeElement;
+    when(organizationManagementFacade.currentUser$).thenReturn(of(user));
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
     expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+  it('should display user data after creation ', () => {
+    fixture.detectChanges();
+
+    expect(element.querySelector('[data-testing-id="name-field"]').innerHTML).toBe('Patricia Miller');
+    expect(element.querySelector('[data-testing-id="email-field"]').innerHTML).toBe('pmiller@test.intershop.de');
+    expect(element.querySelector('[data-testing-id="phone-label"]')).toBeTruthy();
   });
 });
