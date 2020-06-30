5136221c938802d93b5ba289f67b93e497de4f94
suschneider <57661483+suschneider@users.noreply.github.com>
2020-06-30 12:00:52 +0200

    fix: add styling for user management as standalone app (#305) (#306)

--- projects/organization-management/src/app.component.ts
+++ projects/organization-management/src/app.component.ts
@@ -1,15 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { AccountFacade } from 'ish-core/facades/account.facade';
+import { User } from 'ish-core/models/user/user.model';
 
 @Component({
   selector: 'ish-organization-management-root',
-  template: `<div>
-      <a routerLink="/logout" [queryParams]="{ returnUrl: '/login?returnUrl=/organization-management' }">Logout</a
-      >&nbsp;
-      <a routerLink="/organization-management/users">Users</a>
-    </div>
-    <hr />
-    <router-outlet></router-outlet>`,
+  templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 // tslint:disable-next-line: component-creation-test
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  user$: Observable<User>;
+
+  constructor(private accountFacade: AccountFacade) {}
+
+  ngOnInit() {
+    this.user$ = this.accountFacade.user$;
+  }
+}
