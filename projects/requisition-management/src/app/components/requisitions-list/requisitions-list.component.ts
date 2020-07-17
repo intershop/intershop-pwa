af08eb6d00c808bbeac35c1a7e0dc28ff02c1e98
Stefan Hauke <s.hauke@intershop.de>
2020-07-17 17:41:07 +0200

    introduce specific requisition list component - that needs to be made configurable

---
+++ projects/requisition-management/src/app/components/requisitions-list/requisitions-list.component.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { HttpError } from 'ish-core/models/http-error/http-error.model';
+
+import { RequisitionManagementFacade } from '../../facades/requisition-management.facade';
+import { Requisition } from '../../models/requisition/requisition.model';
+
+@Component({
+  selector: 'ish-requisitions-list',
+  templateUrl: './requisitions-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RequisitionsListComponent implements OnInit {
+  requisitions$: Observable<Requisition[]>;
+  error$: Observable<HttpError>;
+  loading$: Observable<boolean>;
+
+  constructor(private requisitionManagementFacade: RequisitionManagementFacade) {}
+
+  ngOnInit() {
+    this.requisitions$ = this.requisitionManagementFacade.requisitions$();
+    this.error$ = this.requisitionManagementFacade.requisitionsError$;
+    this.loading$ = this.requisitionManagementFacade.requisitionsLoading$;
+  }
+}
