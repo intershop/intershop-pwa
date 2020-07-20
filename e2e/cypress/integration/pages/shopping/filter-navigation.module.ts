027099f356f7c39630771cde64593374a7fba61e
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-07-20 08:50:08 +0200

    test: stabilize selecting filters

--- e2e/cypress/integration/pages/shopping/filter-navigation.module.ts
+++ e2e/cypress/integration/pages/shopping/filter-navigation.module.ts
@@ -1,9 +1,15 @@
+import { waitLoadingEnd } from '../../framework';
+
 export class FilterNavigationModule {
   filter(name: string) {
-    const filterGroup = cy.get('div.filter-group').contains(name).parent();
+    const filterGroup = () => cy.get('div.filter-group').contains(name).parent();
     return {
-      filterClick: (value: string) => filterGroup.find(`[data-testing-id=filter-link-${value}]`).click(),
-      getFilter: (value: string) => filterGroup.find(`[data-testing-id=filter-link-${value}]`),
+      filterClick: (value: string) => {
+        waitLoadingEnd(1000);
+        filterGroup().find(`[data-testing-id=filter-link-${value}]`).click();
+        waitLoadingEnd(2000);
+      },
+      getFilter: (value: string) => filterGroup().find(`[data-testing-id=filter-link-${value}]`),
     };
   }
 }
