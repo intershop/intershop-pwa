721ed85f5889347aa7fe9e6daf6ead00369971cc
Stefan Hauke <s.hauke@intershop.de>
2020-07-21 17:00:06 +0200

    made the requisitions list component a view component and let the pages handle the requisitions fetching

--- projects/requisition-management/src/app/pages/approver/approver-page.component.ts
+++ projects/requisition-management/src/app/pages/approver/approver-page.component.ts
@@ -1,8 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { HttpError } from 'ish-core/models/http-error/http-error.model';
+
+import { RequisitionManagementFacade } from '../../facades/requisition-management.facade';
+import { Requisition } from '../../models/requisition/requisition.model';
 
 @Component({
   selector: 'ish-approver-page',
   templateUrl: './approver-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ApproverPageComponent {}
+export class ApproverPageComponent implements OnInit {
+  requisitions$: Observable<Requisition[]>;
+  error$: Observable<HttpError>;
+  loading$: Observable<boolean>;
+
+  constructor(private requisitionManagementFacade: RequisitionManagementFacade) {}
+
+  ngOnInit() {
+    this.requisitions$ = this.requisitionManagementFacade.requisitions$;
+    this.error$ = this.requisitionManagementFacade.requisitionsError$;
+    this.loading$ = this.requisitionManagementFacade.requisitionsLoading$;
+  }
+}
