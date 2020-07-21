2dcc6935d52f1bfa0f6931faf2c8cd00cd17ccb2
Stefan Hauke <s.hauke@intershop.de>
2020-07-21 17:00:06 +0200

    made the requisitions list component a view component and let the pages handle the requisitions fetching

--- projects/requisition-management/src/app/components/requisitions-list/requisitions-list.component.ts
+++ projects/requisition-management/src/app/components/requisitions-list/requisitions-list.component.ts
@@ -1,9 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
-import { HttpError } from 'ish-core/models/http-error/http-error.model';
-
-import { RequisitionManagementFacade } from '../../facades/requisition-management.facade';
 import { Requisition } from '../../models/requisition/requisition.model';
 
 @Component({
@@ -11,16 +7,9 @@ import { Requisition } from '../../models/requisition/requisition.model';
   templateUrl: './requisitions-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RequisitionsListComponent implements OnInit {
-  requisitions$: Observable<Requisition[]>;
-  error$: Observable<HttpError>;
-  loading$: Observable<boolean>;
-
-  constructor(private requisitionManagementFacade: RequisitionManagementFacade) {}
-
-  ngOnInit() {
-    this.requisitions$ = this.requisitionManagementFacade.requisitions$;
-    this.error$ = this.requisitionManagementFacade.requisitionsError$;
-    this.loading$ = this.requisitionManagementFacade.requisitionsLoading$;
-  }
+export class RequisitionsListComponent {
+  /**
+   * The requisitions to be listed
+   */
+  @Input() requisitions: Requisition[];
 }
