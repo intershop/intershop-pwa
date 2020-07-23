cdec5b8d5488c64d553642cf0a68c82be116291f
Stefan Hauke <s.hauke@intershop.de>
2020-07-23 18:53:54 +0200

    requisition detail handling - not good yet, side effects with requisitions lists facade/effects calls

--- projects/requisition-management/src/app/store/requisitions/requisitions.effects.ts
+++ projects/requisition-management/src/app/store/requisitions/requisitions.effects.ts
@@ -2,11 +2,19 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { map, switchMap } from 'rxjs/operators';
 
+import { log } from 'ish-core/utils/dev/operators';
 import { mapErrorToAction, mapToPayload } from 'ish-core/utils/operators';
 
 import { RequisitionsService } from '../../services/requisitions/requisitions.service';
 
-import { loadRequisitions, loadRequisitionsFail, loadRequisitionsSuccess } from './requisitions.actions';
+import {
+  loadRequisition,
+  loadRequisitionFail,
+  loadRequisitionSuccess,
+  loadRequisitions,
+  loadRequisitionsFail,
+  loadRequisitionsSuccess,
+} from './requisitions.actions';
 
 @Injectable()
 export class RequisitionsEffects {
@@ -16,10 +24,25 @@ export class RequisitionsEffects {
     this.actions$.pipe(
       ofType(loadRequisitions),
       mapToPayload(),
+      log('loadRequisitionS Effekt'),
       switchMap(({ view, status }) =>
         this.requisitionsService.getRequisitions(view, status).pipe(
           map(requisitions => loadRequisitionsSuccess({ requisitions })),
           mapErrorToAction(loadRequisitionsFail)
+        )
+      )
+    )
+  );
+
+  loadRequisition$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadRequisition),
+      mapToPayload(),
+      log('loadRequisition Effekt'),
+      switchMap(({ requisitionId }) =>
+        this.requisitionsService.getRequisition(requisitionId).pipe(
+          map(requisition => loadRequisitionSuccess({ requisition })),
+          mapErrorToAction(loadRequisitionFail)
         )
       )
     )
