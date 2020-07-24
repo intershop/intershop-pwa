b292cbd9b13e507ce6f8da6c73d8e3f8e1283078
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-07-24 11:47:45 +0200

    feat: force user to login for configuration

--- src/app/extensions/tacton/pages/tacton-routing.module.ts
+++ src/app/extensions/tacton/pages/tacton-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from 'ish-core/guards/auth.guard';
+
 const routes: Routes = [
   {
     path: 'configure',
     loadChildren: () => import('./configure/configure-page.module').then(m => m.ConfigurePageModule),
+    canActivate: [AuthGuard],
   },
 ];
 
