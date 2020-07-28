9365c27ff87b0c70b31edef8d99f1243b048e098
Johannes Metzner <jometzner@gmx.net>
2020-07-28 10:14:30 +0200

    Feature/company structure node model (#343)
    * Add model for node REST api
    
    * Add node relationship capabilities
    
    * Add node tree data structure
    
    * Add mapper tests for NodeResourceIdentifier
    
    * Refactoring
    
    * Fix linter errors

---
+++ projects/organization-management/src/app/models/node/node.model.ts
@@ -0,0 +1,12 @@
+export interface Node {
+  id: string;
+  name: string;
+  description?: string;
+  organization: string;
+}
+
+export interface NodeTree {
+  nodes: { [id: string]: Node };
+  edges: { [id: string]: string[] };
+  rootIds: string[];
+}
