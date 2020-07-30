58d458e133cc5ca4e477df8fd69ea30f9f48e029
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-07-30 10:53:32 +0200

    refactor: rework self-service API service to always work with externalId

--- src/app/extensions/tacton/services/tacton-self-service-api/tacton-self-service-api.service.ts
+++ src/app/extensions/tacton/services/tacton-self-service-api/tacton-self-service-api.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Store, select } from '@ngrx/store';
-import { EMPTY, Observable, of } from 'rxjs';
-import { first, map, switchMap, withLatestFrom } from 'rxjs/operators';
+import { pick } from 'lodash-es';
+import { EMPTY, Observable } from 'rxjs';
+import { concatMap, first, map, withLatestFrom } from 'rxjs/operators';
 
 import { getLoggedInCustomer, getLoggedInUser } from 'ish-core/store/customer/user';
 import { whenTruthy } from 'ish-core/utils/operators';
@@ -15,37 +16,83 @@ import { getNewExternalId, getSelfServiceApiConfiguration } from '../../store/ta
 
 @Injectable({ providedIn: 'root' })
 export class TactonSelfServiceApiService {
+  private static FORM_URLENCODED_HEADERS = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
   constructor(private http: HttpClient, private store: Store) {}
 
-  private constructAPIAuth(config: TactonSelfServiceApiConfiguration, externalId: string): string {
-    return `_key=${config.apiKey}` + (externalId ? `&_externalId=` + externalId : '');
+  private apiConfig$(parameters?: { externalId?: string }) {
+    return this.store.pipe(
+      select(getSelfServiceApiConfiguration),
+      whenTruthy(),
+      first(),
+      withLatestFrom(
+        this.store.pipe(
+          select(getNewExternalId),
+          map(externalId => parameters?.externalId || externalId)
+        )
+      )
+    );
   }
 
-  private addConfigReference(config: Partial<TactonProductConfiguration>): string {
-    return config.configId ? `&configId=${config.configId}` : `&configState=${encodeURIComponent(config.configState)}`;
+  private constructParameters(
+    config: TactonSelfServiceApiConfiguration,
+    externalId: string,
+    parameters?: { [key: string]: string }
+  ): string {
+    return Object.keys(parameters || {})
+      .filter(k => k !== 'externalId')
+      .reduce(
+        (acc, val) => acc.set(val, parameters[val]),
+        new HttpParams().set('_key', config.apiKey).set('_externalId', externalId)
+      )
+      .toString();
+  }
+
+  private post<T>(resource: string, parameters?: { [key: string]: string }): Observable<T> {
+    return this.apiConfig$(parameters).pipe(
+      concatMap(([config, externalId]) =>
+        this.http
+          .post<T>(
+            `${config.endPoint}/self-service-api/${resource}`,
+            this.constructParameters(config, externalId, parameters),
+            { headers: TactonSelfServiceApiService.FORM_URLENCODED_HEADERS }
+          )
+          .pipe(map(result => ({ ...result, externalId })))
+      )
+    );
+  }
+
+  private put<T>(resource: string, parameters?: { [key: string]: string }): Observable<T> {
+    return this.apiConfig$(parameters).pipe(
+      concatMap(([config, externalId]) =>
+        this.http
+          .put<T>(
+            `${config.endPoint}/self-service-api/${resource}`,
+            this.constructParameters(config, externalId, parameters),
+            { headers: TactonSelfServiceApiService.FORM_URLENCODED_HEADERS }
+          )
+          .pipe(map(result => ({ ...result, externalId })))
+      )
+    );
+  }
+
+  private postConfig<T = TactonProductConfiguration>(
+    resource: string,
+    parameters?: { [key: string]: string }
+  ): Observable<T> {
+    return this.store.pipe(
+      select(getCurrentProductConfiguration),
+      whenTruthy(),
+      first(),
+      concatMap(config => this.post<T>(resource, { ...pick(config, 'configId', 'externalId'), ...parameters }))
+    );
   }
 
   startConfiguration(productPath: string): Observable<TactonProductConfiguration> {
     if (!productPath) {
       return EMPTY;
     }
-    return this.store.pipe(
-      select(getSelfServiceApiConfiguration),
-      whenTruthy(),
-      first(),
-      withLatestFrom(this.store.pipe(select(getNewExternalId))),
-      switchMap(([config, externalId]) =>
-        this.http
-          .post<TactonProductConfiguration>(
-            `${config.endPoint}/self-service-api/config/start/${productPath}`,
-            this.constructAPIAuth(config, externalId),
-            {
-              headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
-            }
-          )
-          .pipe(switchMap(result => this.getBOM(result).pipe(map(bom => ({ ...result, bom: bom.bom, externalId })))))
-      )
-    );
+    return this.post<TactonProductConfiguration>(`config/start/${productPath}`);
   }
 
   continueConfiguration(savedConfiguration: TactonSavedConfiguration): Observable<TactonProductConfiguration> {
@@ -55,133 +102,27 @@ export class TactonSelfServiceApiService {
     if (!savedConfiguration.step) {
       return this.startConfiguration(savedConfiguration.productId);
     }
-    return this.store.pipe(
-      select(getSelfServiceApiConfiguration),
-      whenTruthy(),
-      first(),
-      switchMap(config =>
-        this.http
-          .post<TactonProductConfiguration>(
-            `${config.endPoint}/self-service-api/config/step`,
-            // tslint:disable-next-line: prefer-template
-            this.constructAPIAuth(config, savedConfiguration.externalId) +
-              this.addConfigReference(savedConfiguration) +
-              `&step=${savedConfiguration.step}`,
-            {
-              headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
-            }
-          )
-          .pipe(
-            switchMap(result =>
-              this.getBOM(result).pipe(
-                map(bom => ({ ...result, bom: bom.bom, externalId: savedConfiguration.externalId }))
-              )
-            )
-          )
-      )
-    );
+    return this.post('config/step', pick(savedConfiguration, 'externalId', 'step', 'configId'));
   }
 
   commitValue(valueId: string, value: string): Observable<TactonProductConfiguration> {
-    return this.store.pipe(
-      select(getCurrentProductConfiguration),
-      whenTruthy(),
-      first(),
-      withLatestFrom(this.store.pipe(select(getSelfServiceApiConfiguration))),
-      switchMap(([config, selfService]) =>
-        this.http
-          .post<TactonProductConfiguration>(
-            `${selfService.endPoint}/self-service-api/config/commit/${valueId}/${value}`,
-            this.constructAPIAuth(selfService, config.externalId) + this.addConfigReference(config),
-            {
-              headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
-            }
-          )
-          .pipe(
-            switchMap(result =>
-              this.getBOM(result).pipe(map(bom => ({ ...result, bom: bom.bom, externalId: config.externalId })))
-            )
-          )
-      )
-    );
+    return this.postConfig(`config/commit/${valueId}/${value}`);
   }
 
   uncommitValue(valueId: string): Observable<TactonProductConfiguration> {
-    return this.store.pipe(
-      select(getCurrentProductConfiguration),
-      whenTruthy(),
-      first(),
-      withLatestFrom(this.store.pipe(select(getSelfServiceApiConfiguration))),
-      switchMap(([config, selfService]) =>
-        this.http
-          .post<TactonProductConfiguration>(
-            `${selfService.endPoint}/self-service-api/config/uncommit/${valueId}`,
-            this.constructAPIAuth(selfService, config.externalId) + this.addConfigReference(config),
-            {
-              headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
-            }
-          )
-          .pipe(
-            switchMap(result =>
-              this.getBOM(result).pipe(map(bom => ({ ...result, bom: bom.bom, externalId: config.externalId })))
-            )
-          )
-      )
-    );
+    return this.postConfig(`config/uncommit/${valueId}`);
   }
 
   acceptConflictResolution(valueId: string, value: string): Observable<TactonProductConfiguration> {
     if (!valueId) {
       return EMPTY;
     }
-    return this.store.pipe(
-      select(getCurrentProductConfiguration),
-      whenTruthy(),
-      first(),
-      withLatestFrom(this.store.pipe(select(getSelfServiceApiConfiguration))),
-      switchMap(([config, selfService]) =>
-        this.http
-          .post<TactonProductConfiguration>(
-            `${selfService.endPoint}/self-service-api/config/accept`,
-            // tslint:disable-next-line: prefer-template
-            this.constructAPIAuth(selfService, config.externalId) +
-              this.addConfigReference(config) +
-              `&parameter=${valueId}&value=${value}`,
-            {
-              headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
-            }
-          )
-          .pipe(
-            switchMap(result =>
-              this.getBOM(result).pipe(
-                map(bom => ({
-                  ...result,
-                  bom: bom.bom,
-                  externalId: config.externalId,
-                  // override faulty API response
-                  response: { status: 'OK' as 'OK' },
-                }))
-              )
-            )
-          )
-      )
-    );
-  }
-
-  private getBOM(config: TactonProductConfiguration): Observable<TactonProductConfiguration> {
-    if (config.bom) {
-      return of(config);
-    }
-    return this.store.pipe(
-      select(getSelfServiceApiConfiguration),
-      whenTruthy(),
-      first(),
-      switchMap(selfService =>
-        this.http.get<TactonProductConfiguration>(
-          `${selfService.endPoint}/self-service-api/config/bom?_key=${selfService.apiKey}` +
-            this.addConfigReference(config)
-        )
-      )
+    return this.postConfig('config/accept', { parameter: valueId, value }).pipe(
+      map(result => ({
+        ...result,
+        // override faulty API response
+        response: { status: 'OK' as 'OK' },
+      }))
     );
   }
 
@@ -189,79 +130,28 @@ export class TactonSelfServiceApiService {
     if (!step) {
       return EMPTY;
     }
-    return this.store.pipe(
-      select(getCurrentProductConfiguration),
-      whenTruthy(),
-      first(),
-      withLatestFrom(this.store.pipe(select(getSelfServiceApiConfiguration))),
-      switchMap(([config, selfService]) =>
-        this.http
-          .post<TactonProductConfiguration>(
-            `${selfService.endPoint}/self-service-api/config/step`,
-            // TODO: refactor
-            // tslint:disable-next-line: prefer-template
-            this.constructAPIAuth(selfService, config.externalId) + this.addConfigReference(config) + `&step=${step}`,
-            {
-              headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
-            }
-          )
-          .pipe(
-            switchMap(result =>
-              this.getBOM(result).pipe(map(bom => ({ ...result, bom: bom.bom, externalId: config.externalId })))
-            )
-          )
-      )
-    );
+    return this.postConfig('config/step', { step });
   }
 
   submitConfiguration(savedConfiguration: TactonSavedConfiguration) {
     if (!savedConfiguration) {
       return EMPTY;
     } else {
-      return this.store.pipe(
-        select(getSelfServiceApiConfiguration),
-        whenTruthy(),
-        first(),
-        switchMap(config =>
-          this.http
-            .post(
-              `${config.endPoint}/self-service-api/cart/items`,
-              // tslint:disable-next-line: prefer-template
-              this.constructAPIAuth(config, savedConfiguration.externalId) +
-                this.addConfigReference(savedConfiguration) +
-                `&qty=1`,
-              {
-                headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
-              }
+      return this.post('cart/items', { ...pick(savedConfiguration, 'externalId', 'configId'), qty: '1' }).pipe(
+        withLatestFrom(this.store.pipe(select(getLoggedInUser)), this.store.pipe(select(getLoggedInCustomer))),
+        concatMap(([, user, customer]) =>
+          this.put('cart', {
+            externalId: savedConfiguration.externalId,
+            customerEmail: user.login,
+            customerPhoneNumber: user.phoneHome || 'N/A',
+            customerName: `${customer.companyName} - ${user.firstName} ${user.lastName}`,
+          }).pipe(
+            concatMap(() =>
+              this.post('proposal/firm-requests', {
+                externalId: savedConfiguration.externalId,
+              })
             )
-            .pipe(
-              withLatestFrom(this.store.pipe(select(getLoggedInUser)), this.store.pipe(select(getLoggedInCustomer))),
-              switchMap(([, user, customer]) =>
-                this.http
-                  .put(
-                    `${config.endPoint}/self-service-api/cart`,
-                    // tslint:disable-next-line: prefer-template
-                    this.constructAPIAuth(config, savedConfiguration.externalId) +
-                      `&customerEmail=${user.login}&customerPhoneNumber=${user.phoneHome || 'N/A'}&customerName=${
-                        customer.companyName
-                      } - ${user.firstName} ${user.lastName}`,
-                    {
-                      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
-                    }
-                  )
-                  .pipe(
-                    switchMap(() =>
-                      this.http.post(
-                        `${config.endPoint}/self-service-api/proposal/firm-requests`,
-                        this.constructAPIAuth(config, savedConfiguration.externalId),
-                        {
-                          headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
-                        }
-                      )
-                    )
-                  )
-              )
-            )
+          )
         )
       );
     }
