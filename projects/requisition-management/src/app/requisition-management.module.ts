ecbe3e04820c5d2bd3d3819d16092751751e1a5c
Silke Grueber <SGrueber@intershop.com>
2020-08-03 09:15:51 +0200

    add requisition summary component

--- projects/requisition-management/src/app/requisition-management.module.ts
+++ projects/requisition-management/src/app/requisition-management.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from 'ish-shared/shared.module';
 
+import { RequisitionSummaryComponent } from './components/requisition-summary/requisition-summary.component';
 import { RequisitionsListComponent } from './components/requisitions-list/requisitions-list.component';
 import { ApproverPageComponent } from './pages/approver/approver-page.component';
 import { BuyerPageComponent } from './pages/buyer/buyer-page.component';
@@ -10,7 +11,13 @@ import { RequisitionManagementRoutingModule } from './pages/requisition-manageme
 import { RequisitionManagementStoreModule } from './store/requisition-management-store.module';
 
 @NgModule({
-  declarations: [ApproverPageComponent, BuyerPageComponent, RequisitionDetailPageComponent, RequisitionsListComponent],
+  declarations: [
+    ApproverPageComponent,
+    BuyerPageComponent,
+    RequisitionDetailPageComponent,
+    RequisitionSummaryComponent,
+    RequisitionsListComponent,
+  ],
   imports: [RequisitionManagementRoutingModule, RequisitionManagementStoreModule, SharedModule],
 })
 export class RequisitionManagementModule {}
