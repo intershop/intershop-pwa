40c4c8c29b79d9221b67daae3da91c3f65c8e063
Silke <s.grueber@intershop.de>
2020-08-14 12:53:41 +0200

    feat: approve/reject a requisition

--- projects/requisition-management/src/app/pages/requisition-detail/requisition-detail-page.component.spec.ts
+++ projects/requisition-management/src/app/pages/requisition-detail/requisition-detail-page.component.spec.ts
@@ -3,7 +3,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { TranslateModule } from '@ngx-translate/core';
 import { MockComponent } from 'ng-mocks';
-import { instance, mock } from 'ts-mockito';
+import { of } from 'rxjs';
+import { anyString, instance, mock, when } from 'ts-mockito';
 
 import { AddressComponent } from 'ish-shared/components/address/address/address.component';
 import { BasketCostSummaryComponent } from 'ish-shared/components/basket/basket-cost-summary/basket-cost-summary.component';
@@ -13,8 +14,10 @@ import { InfoBoxComponent } from 'ish-shared/components/common/info-box/info-box
 import { LoadingComponent } from 'ish-shared/components/common/loading/loading.component';
 
 import { RequisitionBuyerApprovalComponent } from '../../components/requisition/requisition-buyer-approval/requisition-buyer-approval.component';
+import { RequisitionRejectDialogComponent } from '../../components/requisition/requisition-reject-dialog/requisition-reject-dialog.component';
 import { RequisitionSummaryComponent } from '../../components/requisition/requisition-summary/requisition-summary.component';
 import { RequisitionManagementFacade } from '../../facades/requisition-management.facade';
+import { RequisitionView } from '../../models/requisition/requisition.model';
 
 import { RequisitionDetailPageComponent } from './requisition-detail-page.component';
 
@@ -37,6 +40,7 @@ describe('Requisition Detail Page Component', () => {
         MockComponent(LineItemListComponent),
         MockComponent(LoadingComponent),
         MockComponent(RequisitionBuyerApprovalComponent),
+        MockComponent(RequisitionRejectDialogComponent),
         MockComponent(RequisitionSummaryComponent),
         RequisitionDetailPageComponent,
       ],
@@ -48,11 +52,51 @@ describe('Requisition Detail Page Component', () => {
     fixture = TestBed.createComponent(RequisitionDetailPageComponent);
     component = fixture.componentInstance;
     element = fixture.nativeElement;
+
+    const requisition = {
+      id: '4711',
+      requisitionNo: '4712',
+      approval: {
+        status: 'Approval Pending',
+        statusCode: 'pending',
+        customerApprovers: [
+          { firstName: 'Jack', lastName: 'Link' },
+          { firstName: 'Bernhhard', lastName: 'Boldner' },
+        ],
+      },
+      user: { firstName: 'Patricia', lastName: 'Miller' },
+      totals: undefined,
+      creationDate: 24324321,
+      lineItemCount: 2,
+      lineItems: undefined,
+    } as RequisitionView;
+
+    when(reqFacade.requisition$(anyString())).thenReturn(of(requisition));
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
     expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+  it('should display only the title if there is no requisition given', () => {
+    when(reqFacade.requisition$).thenReturn();
+
+    fixture.detectChanges();
+    expect(element).toMatchInlineSnapshot(`
+      <div class="float-right">
+        <ul class="share-tools">
+          <li>
+            <a class="link-print" href="javascript:window.print();" rel="nofollow"
+              ><fa-icon ng-reflect-icon="fas,print"></fa-icon
+              ><span class="share-label">account.orderdetails.print_link.text</span></a
+            >
+          </li>
+        </ul>
+      </div>
+      <h1>approval.detailspage.heading</h1>
+      <ish-error-message></ish-error-message>
+    `);
   });
 });
