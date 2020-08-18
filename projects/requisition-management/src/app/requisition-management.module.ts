5addcd1578f5ab6b1e437af2c6c7a96f96cdee4e
Silke Grueber <SGrueber@intershop.com>
2020-08-18 19:18:28 +0200

    feat: display budget bars on buyer approval widget

--- projects/requisition-management/src/app/requisition-management.module.ts
+++ projects/requisition-management/src/app/requisition-management.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from 'ish-shared/shared.module';
 
+import { BudgetBarComponent } from './components/requisition/budget-bar/budget-bar.component';
 import { RequisitionBuyerApprovalComponent } from './components/requisition/requisition-buyer-approval/requisition-buyer-approval.component';
 import { RequisitionRejectDialogComponent } from './components/requisition/requisition-reject-dialog/requisition-reject-dialog.component';
 import { RequisitionSummaryComponent } from './components/requisition/requisition-summary/requisition-summary.component';
@@ -15,6 +16,7 @@ import { RequisitionManagementStoreModule } from './store/requisition-management
 @NgModule({
   declarations: [
     ApproverPageComponent,
+    BudgetBarComponent,
     BuyerPageComponent,
     RequisitionBuyerApprovalComponent,
     RequisitionDetailPageComponent,
