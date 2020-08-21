06583a6497c7a4824070a33ec9c9fc9761d9f865
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-08-21 12:36:18 +0200

    fix(schematics): enable move-component helper for projects

--- schematics/src/helpers/move-component/factory.ts
+++ schematics/src/helpers/move-component/factory.ts
@@ -1,7 +1,6 @@
 import { strings } from '@angular-devkit/core';
 import { Rule, SchematicsException } from '@angular-devkit/schematics';
 import { tsquery } from '@phenomnomnominal/tsquery';
-import { getWorkspace } from '@schematics/angular/utility/workspace';
 import * as ts from 'typescript';
 
 import { readIntoSourceFile } from '../../utils/filesystem';
@@ -106,12 +105,8 @@ export function move(options: Options): Rule {
     };
     console.log('moving', options.from, '\n    to', options.to);
 
-    const workspace = await getWorkspace(host);
-    const project = workspace.projects.get(options.project);
-    const sourceRoot = project.sourceRoot;
-
     host.visit(file => {
-      if (file.startsWith(`/${sourceRoot}/app/`)) {
+      if (file.startsWith(`/src/app/`) || file.startsWith(`/projects/`)) {
         if (file.includes(from + '/')) {
           renames.push([file, replacePath(file)]);
 
