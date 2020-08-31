f62636ac0a57710304a44cb02628d3ad9e5bc277
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-08-31 08:23:42 +0200

    refactor: allow numbers as loading for ngrx reducer helpers (#371)

---
+++ src/app/core/utils/ngrx-creators.spec.ts
@@ -0,0 +1,171 @@
+import { createAction, createReducer } from '@ngrx/store';
+import { noop } from 'rxjs';
+
+import { makeHttpError } from './dev/api-service-utils';
+import { httpError, payload, setErrorOn, setLoadingOn, unsetLoadingAndErrorOn } from './ngrx-creators';
+
+describe('Ngrx Creators', () => {
+  const load = createAction('[Test] Load');
+  const loadSuccess = createAction('[Test API] Load Success', payload<{ entities: unknown[] }>());
+  const loadFail = createAction('[Test API] Load Fail', httpError());
+
+  describe('with boolean loading', () => {
+    const initialState = { loading: false, error: undefined };
+    const reducer = createReducer(
+      initialState,
+      setLoadingOn(load),
+      setErrorOn(loadFail),
+      unsetLoadingAndErrorOn(loadSuccess)
+    );
+
+    describe('on load action', () => {
+      it('should set loading to true when reducing load action', () => {
+        const state = reducer(initialState, load());
+        expect(state).toMatchInlineSnapshot(`
+          Object {
+            "error": undefined,
+            "loading": true,
+          }
+        `);
+      });
+
+      describe('on load success', () => {
+        it('should set loading to false on load success', () => {
+          const state = reducer({ ...initialState, loading: true }, loadSuccess({ entities: [] }));
+          expect(state).toMatchInlineSnapshot(`
+            Object {
+              "error": undefined,
+              "loading": false,
+            }
+          `);
+        });
+      });
+
+      describe('on load fail', () => {
+        it('should set error to error and loading to false on load fail', () => {
+          const state = reducer(
+            { ...initialState, loading: true },
+            loadFail({ error: makeHttpError({ message: 'ERROR' }) })
+          );
+          expect(state).toMatchInlineSnapshot(`
+            Object {
+              "error": Object {
+                "message": "ERROR",
+                "name": "HttpErrorResponse",
+              },
+              "loading": false,
+            }
+          `);
+        });
+      });
+    });
+  });
+
+  describe('with number loading', () => {
+    const initialState = { loading: 0, error: undefined };
+    const reducer = createReducer(
+      initialState,
+      setLoadingOn(load),
+      setErrorOn(loadFail),
+      unsetLoadingAndErrorOn(loadSuccess)
+    );
+
+    describe('on load action', () => {
+      it('should increase loading when reducing load action', () => {
+        const state = reducer(initialState, load());
+        expect(state).toMatchInlineSnapshot(`
+          Object {
+            "error": undefined,
+            "loading": 1,
+          }
+        `);
+      });
+
+      describe('on load success', () => {
+        it('should decrease loading on load success', () => {
+          const state = reducer({ ...initialState, loading: 1 }, loadSuccess({ entities: [] }));
+          expect(state).toMatchInlineSnapshot(`
+            Object {
+              "error": undefined,
+              "loading": 0,
+            }
+          `);
+        });
+      });
+
+      describe('on load fail', () => {
+        it('should set error to error and decrease loading on load fail', () => {
+          const state = reducer(
+            { ...initialState, loading: 1 },
+            loadFail({ error: makeHttpError({ message: 'ERROR' }) })
+          );
+          expect(state).toMatchInlineSnapshot(`
+            Object {
+              "error": Object {
+                "message": "ERROR",
+                "name": "HttpErrorResponse",
+              },
+              "loading": 0,
+            }
+          `);
+        });
+      });
+
+      describe('on second load action', () => {
+        it('should increase loading more when reducing second load action', () => {
+          const state = reducer({ ...initialState, loading: 1 }, load());
+          expect(state).toMatchInlineSnapshot(`
+            Object {
+              "error": undefined,
+              "loading": 2,
+            }
+          `);
+        });
+      });
+    });
+
+    describe('dev warning', () => {
+      let consoleSpy: jest.SpyInstance;
+
+      beforeEach(() => {
+        consoleSpy = jest.spyOn(console, 'warn').mockImplementationOnce(noop);
+      });
+
+      afterEach(() => {
+        consoleSpy.mockClear();
+      });
+
+      describe('on load success', () => {
+        it('should warn when loading would be decreased below 0', () => {
+          const state = reducer({ ...initialState, loading: 0 }, loadSuccess({ entities: [] }));
+          expect(state).toMatchInlineSnapshot(`
+            Object {
+              "error": undefined,
+              "loading": 0,
+            }
+          `);
+          expect(consoleSpy).toHaveBeenCalled();
+        });
+      });
+
+      describe('on load fail', () => {
+        it('should warn when loading would be decreased below 0', () => {
+          const state = reducer(
+            { ...initialState, loading: 0 },
+            loadFail({ error: makeHttpError({ message: 'ERROR' }) })
+          );
+          expect(state).toMatchInlineSnapshot(`
+            Object {
+              "error": Object {
+                "message": "ERROR",
+                "name": "HttpErrorResponse",
+              },
+              "loading": 0,
+            }
+          `);
+          expect(consoleSpy).toHaveBeenCalled();
+        });
+      });
+    });
+  });
+});
--- src/app/core/utils/ngrx-creators.ts
+++ src/app/core/utils/ngrx-creators.ts
@@ -10,10 +10,10 @@ export function httpError<P = {}>() {
   return (args: { error: HttpError } & P) => ({ payload: { ...args } });
 }
 
-export function setLoadingOn<S extends { loading: boolean }>(...actionCreators: ActionCreator[]): On<S> {
+export function setLoadingOn<S extends { loading: boolean | number }>(...actionCreators: ActionCreator[]): On<S> {
   const stateFnc = (state: S) => ({
     ...state,
-    loading: true,
+    loading: typeof state.loading === 'boolean' ? true : state.loading + 1,
   });
   if (actionCreators.length === 1) {
     return on(actionCreators[0], stateFnc);
@@ -22,13 +22,35 @@ export function setLoadingOn<S extends { loading: boolean }>(...actionCreators: 
   }
 }
 
-export function setErrorOn<S extends { loading: boolean; error: HttpError }>(
+function calculateLoading<T extends boolean | number, S extends { loading: T }>(state: S): T {
+  if (typeof state.loading === 'number' && state.loading - 1 < 0) {
+    console.warn('State loading would be decreased below 0.');
+  }
+  return (typeof state.loading === 'boolean' ? false : Math.max((state.loading as number) - 1, 0)) as T;
+}
+
+export function unsetLoadingAndErrorOn<S extends { loading: boolean | number }>(
+  ...actionCreators: ActionCreator[]
+): On<S> {
+  const stateFnc = (state: S) => ({
+    ...state,
+    loading: calculateLoading(state),
+    error: undefined,
+  });
+  if (actionCreators.length === 1) {
+    return on(actionCreators[0], stateFnc);
+  } else {
+    return on(actionCreators[0], ...actionCreators.splice(1), stateFnc);
+  }
+}
+
+export function setErrorOn<S extends { loading: boolean | number; error: HttpError }>(
   ...actionCreators: ActionCreator[]
 ): On<S> {
   const stateFnc = (state: S, action: { payload: { error: HttpError }; type: string }) => ({
     ...state,
     error: action.payload.error,
-    loading: false,
+    loading: calculateLoading(state),
   });
   if (actionCreators.length === 1) {
     return on(actionCreators[0], stateFnc);
