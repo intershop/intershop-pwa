85105ec9ddddee1745a677517a09515b632b3498
Johannes Metzner <jometzner@gmx.net>
2020-09-08 09:29:06 +0200

    Feature/company structure create group (#378)
    * Work on create group page
    
    * Add create-group form component
    
    * Submit form
    
    * Add create new group functionality
    
    * Remove ID form field
    
    * Fix errors from ngx-treeview
    
    * Work on parent selection of create group page
    
    * Rework variable naming

---
+++ projects/organization-management/src/app/components/hierarchies/group-form/group-form.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { HttpError } from 'ish-core/models/http-error/http-error.model';
+import { SelectOption } from 'ish-shared/forms/components/select/select.component';
+
+import { Node, NodeTree } from '../../../models/node/node.model';
+
+@Component({
+  selector: 'ish-group-form',
+  templateUrl: './group-form.component.html',
+  changeDetection: ChangeDetectionStrategy.Default,
+})
+export class GroupFormComponent implements OnChanges {
+  @Input() form: FormGroup;
+  @Input() error: HttpError;
+  @Input() parents: NodeTree;
+  parentOptions: SelectOption[];
+
+  ngOnChanges() {
+    this.parentOptions = this.mapParentOptions(this.parents.nodes);
+  }
+
+  private mapParentOptions(nodeMap: { [id: string]: Node }): SelectOption[] {
+    return Object.keys(nodeMap).map(nodeId => ({ value: nodeId, label: nodeMap[nodeId].name }));
+  }
+}
