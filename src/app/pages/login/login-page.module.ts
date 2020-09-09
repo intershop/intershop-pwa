59c6ecceb9c248254947b609da38ce221354682d
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-09-09 16:38:31 +0200

    refactor: provide login guard handling in lazy module instead of app module

--- src/app/pages/login/login-page.module.ts
+++ src/app/pages/login/login-page.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LoginGuard } from 'ish-core/guards/login.guard';
 import { SharedModule } from 'ish-shared/shared.module';
 
 import { LoginPageComponent } from './login-page.component';
 
-const loginPageRoutes: Routes = [{ path: '', component: LoginPageComponent }];
+const loginPageRoutes: Routes = [
+  {
+    path: '',
+    component: LoginPageComponent,
+    canActivate: [LoginGuard],
+    data: {
+      meta: {
+        title: 'account.login.link',
+        robots: 'noindex, nofollow',
+      },
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(loginPageRoutes), SharedModule],
