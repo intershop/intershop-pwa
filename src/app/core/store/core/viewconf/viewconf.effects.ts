3e55b635da47e1843f791b2e7574f95e8f15b4b4
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-09-10 09:14:37 +0200

    fix: set breadcrumb only on successful navigation

--- src/app/core/store/core/viewconf/viewconf.effects.ts
+++ src/app/core/store/core/viewconf/viewconf.effects.ts
@@ -1,9 +1,10 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
-import { createEffect } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { routerNavigatedAction } from '@ngrx/router-store';
 import { Store, select } from '@ngrx/store';
 import { defer, fromEvent, iif } from 'rxjs';
-import { distinctUntilChanged, map } from 'rxjs/operators';
+import { distinctUntilChanged, map, sample } from 'rxjs/operators';
 
 import { selectRouteData } from 'ish-core/store/core/router';
 import { distinctCompareWith } from 'ish-core/utils/operators';
@@ -13,7 +14,7 @@ import { getBreadcrumbData } from './viewconf.selectors';
 
 @Injectable()
 export class ViewconfEffects {
-  constructor(private store: Store, @Inject(PLATFORM_ID) private platformId: string) {}
+  constructor(private store: Store, private actions$: Actions, @Inject(PLATFORM_ID) private platformId: string) {}
 
   toggleStickyHeader$ = createEffect(() =>
     iif(
@@ -31,6 +32,7 @@ export class ViewconfEffects {
   retrieveBreadcrumbDataFromRouting$ = createEffect(() =>
     this.store.pipe(
       select(selectRouteData('breadcrumbData')),
+      sample(this.actions$.pipe(ofType(routerNavigatedAction))),
       distinctCompareWith(this.store.pipe(select(getBreadcrumbData))),
       map(breadcrumbData => setBreadcrumbData({ breadcrumbData }))
     )
