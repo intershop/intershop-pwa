d43f68835d78cac6b18a50d9a37ce3205e33b729
Johannes Metzner <J.Metzner@intershop.de>
2020-09-11 17:55:42 +0200

    Rebase and migrate to 0.23

--- src/app/extensions/organization-hierarchies/exports/organization-hierarchies-exports.module.ts
+++ src/app/extensions/organization-hierarchies/exports/organization-hierarchies-exports.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FeatureToggleModule } from 'ish-core/feature-toggle.module';
 import { LAZY_FEATURE_MODULE } from 'ish-core/utils/module-loader/module-loader.service';
 
-import { LazyHierarchySwitchComponent } from './hierarchies/lazy-hierarchy-switch/lazy-hierarchy-switch.component';
+import { LazyHierarchySwitchComponent } from './lazy-hierarchy-switch/lazy-hierarchy-switch.component';
 
 @NgModule({
   imports: [FeatureToggleModule],
