7f4721e231680c44d39e2218c366bfbe565c1faa
Johannes Metzner <J.Metzner@intershop.de>
2020-09-15 18:39:17 +0200

    Add further tests (ISREST-1068)

--- projects/organization-management/src/app/pages/hierarchies/hierarchies-page.component.spec.ts
+++ projects/organization-management/src/app/pages/hierarchies/hierarchies-page.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MockComponent } from 'ng-mocks';
 import { TreeviewComponent } from 'ngx-treeview';
 import { of } from 'rxjs';
 import { instance, mock, when } from 'ts-mockito';
+
+import { HttpError } from 'ish-core/models/http-error/http-error.model';
 
 import { OrganizationManagementFacade } from '../../facades/organization-management.facade';
 import { Node, NodeTree } from '../../models/node/node.model';
@@ -16,6 +18,7 @@ describe('Hierarchies Page Component', () => {
   let fixture: ComponentFixture<HierarchiesPageComponent>;
   let element: HTMLElement;
   let organizationManagementFacade: OrganizationManagementFacade;
+  const error = {} as HttpError;
   const rootNode = {
     id: 'root',
     name: 'ROOT',
@@ -46,11 +49,63 @@ describe('Hierarchies Page Component', () => {
     fixture = TestBed.createComponent(HierarchiesPageComponent);
     component = fixture.componentInstance;
     element = fixture.nativeElement;
+    const translate = TestBed.inject(TranslateService);
+    translate.use('en');
+    translate.setTranslation('en', {
+      'account.organization.hierarchies.description': 'Manage the structure of "{{0}}".',
+      'account.organization.hierarchies': 'Company Structure',
+    });
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
     expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+  it('should display organizations root after init', () => {
+    fixture.detectChanges();
+    expect(component.items$).toBeTruthy();
+    expect(element.querySelector('div').textContent).toMatchInlineSnapshot(`"Manage the structure of \\"ROOT\\"."`);
+  });
+
+  it('should not display organizations in case of error', () => {
+    when(organizationManagementFacade.groups$()).thenReturn(of());
+    when(organizationManagementFacade.groupsError$).thenReturn(of(error));
+    fixture.detectChanges();
+    expect(element.querySelector('h1.a')).toBeFalsy();
+    expect(element).toMatchInlineSnapshot(`<h1>Company Structure</h1>`);
+  });
+
+  it('should map node tree data after init', () => {
+    const treeItems = component.mapToTreeItems(nodeTree, ['root']);
+    expect(treeItems).toBeArrayOfSize(1);
+    expect(treeItems[0]).toMatchInlineSnapshot(`
+      Object {
+        "checked": false,
+        "children": Array [
+          Object {
+            "checked": false,
+            "children": Array [],
+            "collapsed": false,
+            "disabled": false,
+            "text": "Child",
+            "value": Object {
+              "id": "child",
+              "name": "Child",
+              "organization": "acme.org",
+            },
+          },
+        ],
+        "collapsed": false,
+        "disabled": false,
+        "text": "ROOT",
+        "value": Object {
+          "id": "root",
+          "name": "ROOT",
+          "organization": "acme.org",
+        },
+      }
+    `);
   });
 });
