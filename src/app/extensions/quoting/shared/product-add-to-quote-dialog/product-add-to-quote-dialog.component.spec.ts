0346ae3adfc21d2bf78b445db777c211a3c10cbe
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-09-22 14:32:55 +0200

    test: improve testing

--- src/app/extensions/quoting/shared/product-add-to-quote-dialog/product-add-to-quote-dialog.component.spec.ts
+++ src/app/extensions/quoting/shared/product-add-to-quote-dialog/product-add-to-quote-dialog.component.spec.ts
@@ -5,10 +5,12 @@ import { MockComponent } from 'ng-mocks';
 import { of } from 'rxjs';
 import { instance, mock, when } from 'ts-mockito';
 
+import { findAllCustomElements } from 'ish-core/utils/dev/html-query-utils';
 import { ErrorMessageComponent } from 'ish-shared/components/common/error-message/error-message.component';
 import { LoadingComponent } from 'ish-shared/components/common/loading/loading.component';
 
 import { QuoteContextFacade } from '../../facades/quote-context.facade';
+import { Quote, QuoteRequest } from '../../models/quoting/quoting.model';
 import { QuoteEditComponent } from '../quote-edit/quote-edit.component';
 import { QuoteInteractionsComponent } from '../quote-interactions/quote-interactions.component';
 import { QuoteViewComponent } from '../quote-view/quote-view.component';
@@ -19,10 +21,10 @@ describe('Product Add To Quote Dialog Component', () => {
   let component: ProductAddToQuoteDialogComponent;
   let fixture: ComponentFixture<ProductAddToQuoteDialogComponent>;
   let element: HTMLElement;
+  let context: QuoteContextFacade;
 
   beforeEach(async () => {
-    const context = mock(QuoteContextFacade);
-    when(context.select('state')).thenReturn(of('New'));
+    context = mock(QuoteContextFacade);
 
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, TranslateModule.forRoot()],
@@ -51,5 +53,50 @@ describe('Product Add To Quote Dialog Component', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
     expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+  it('should display edit component for unsubmitted quote requests', () => {
+    when(context.select('entity')).thenReturn(of({} as QuoteRequest));
+    when(context.select('state')).thenReturn(of('New'));
+
+    fixture.detectChanges();
+
+    expect(findAllCustomElements(element)).toMatchInlineSnapshot(`
+      Array [
+        "ish-error-message",
+        "ish-quote-edit",
+        "ish-quote-interactions",
+      ]
+    `);
+  });
+
+  it('should display view component for submitted quote requests', () => {
+    when(context.select('entity')).thenReturn(of({} as QuoteRequest));
+    when(context.select('state')).thenReturn(of('Submitted'));
+
+    fixture.detectChanges();
+
+    expect(findAllCustomElements(element)).toMatchInlineSnapshot(`
+      Array [
+        "ish-error-message",
+        "ish-quote-view",
+        "ish-quote-interactions",
+      ]
+    `);
+  });
+
+  it('should display view component for quotes', () => {
+    when(context.select('entity')).thenReturn(of({} as Quote));
+    when(context.select('state')).thenReturn(of('Responded'));
+
+    fixture.detectChanges();
+
+    expect(findAllCustomElements(element)).toMatchInlineSnapshot(`
+      Array [
+        "ish-error-message",
+        "ish-quote-view",
+        "ish-quote-interactions",
+      ]
+    `);
   });
 });
