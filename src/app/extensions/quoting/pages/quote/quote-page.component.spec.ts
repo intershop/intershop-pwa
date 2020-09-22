3dfb82e9d7449ba66fcd7850b9f86c64c0592db9
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-09-22 14:00:47 +0200

    test: improve testing

--- src/app/extensions/quoting/pages/quote/quote-page.component.spec.ts
+++ src/app/extensions/quoting/pages/quote/quote-page.component.spec.ts
@@ -2,12 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { MockComponent } from 'ng-mocks';
-import { instance, mock } from 'ts-mockito';
+import { of } from 'rxjs';
+import { instance, mock, when } from 'ts-mockito';
 
+import { findAllCustomElements } from 'ish-core/utils/dev/html-query-utils';
 import { ErrorMessageComponent } from 'ish-shared/components/common/error-message/error-message.component';
 import { LoadingComponent } from 'ish-shared/components/common/loading/loading.component';
 
 import { QuoteContextFacade } from '../../facades/quote-context.facade';
+import { Quote, QuoteRequest } from '../../models/quoting/quoting.model';
 import { QuoteEditComponent } from '../../shared/quote-edit/quote-edit.component';
 import { QuoteInteractionsComponent } from '../../shared/quote-interactions/quote-interactions.component';
 import { QuoteViewComponent } from '../../shared/quote-view/quote-view.component';
@@ -18,8 +21,11 @@ describe('Quote Page Component', () => {
   let component: QuotePageComponent;
   let fixture: ComponentFixture<QuotePageComponent>;
   let element: HTMLElement;
+  let context: QuoteContextFacade;
 
   beforeEach(async () => {
+    context = mock(QuoteContextFacade);
+
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, TranslateModule.forRoot()],
       declarations: [
@@ -32,7 +38,7 @@ describe('Quote Page Component', () => {
       ],
     })
       .overrideComponent(QuotePageComponent, {
-        set: { providers: [{ provide: QuoteContextFacade, useFactory: () => instance(mock(QuoteContextFacade)) }] },
+        set: { providers: [{ provide: QuoteContextFacade, useFactory: () => instance(context) }] },
       })
       .compileComponents();
   });
@@ -47,5 +53,50 @@ describe('Quote Page Component', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
     expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+  it('should display edit component for unsubmitted quote requests', () => {
+    when(context.select('entity')).thenReturn(of({} as QuoteRequest));
+    when(context.select('state')).thenReturn(of('New'));
+
+    fixture.detectChanges();
+
+    expect(findAllCustomElements(element)).toMatchInlineSnapshot(`
+      Array [
+        "ish-error-message",
+        "ish-quote-edit",
+        "ish-quote-interactions",
+      ]
+    `);
+  });
+
+  it('should display view component for submitted quote requests', () => {
+    when(context.select('entity')).thenReturn(of({} as QuoteRequest));
+    when(context.select('state')).thenReturn(of('Submitted'));
+
+    fixture.detectChanges();
+
+    expect(findAllCustomElements(element)).toMatchInlineSnapshot(`
+      Array [
+        "ish-error-message",
+        "ish-quote-view",
+        "ish-quote-interactions",
+      ]
+    `);
+  });
+
+  it('should display view component for quotes', () => {
+    when(context.select('entity')).thenReturn(of({} as Quote));
+    when(context.select('state')).thenReturn(of('Responded'));
+
+    fixture.detectChanges();
+
+    expect(findAllCustomElements(element)).toMatchInlineSnapshot(`
+      Array [
+        "ish-error-message",
+        "ish-quote-view",
+        "ish-quote-interactions",
+      ]
+    `);
   });
 });
