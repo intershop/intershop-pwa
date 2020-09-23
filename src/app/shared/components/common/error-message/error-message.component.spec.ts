9e7d99b616e16c6f009b6bc4b5b0ed36a1996714
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-09-23 10:50:41 +0200

    feat: styled toasts for success and error messages (#356)
    Co-authored-by: Silke Grueber <s.grueber@intershop.com>

--- src/app/shared/components/common/error-message/error-message.component.spec.ts
+++ src/app/shared/components/common/error-message/error-message.component.spec.ts
@@ -1,8 +1,10 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { MockDirective } from 'ng-mocks';
+import { anything, instance, mock, verify } from 'ts-mockito';
 
 import { ServerHtmlDirective } from 'ish-core/directives/server-html.directive';
+import { MessageFacade } from 'ish-core/facades/message.facade';
 import { makeHttpError } from 'ish-core/utils/dev/api-service-utils';
 
 import { ErrorMessageComponent } from './error-message.component';
@@ -11,11 +13,14 @@ describe('Error Message Component', () => {
   let component: ErrorMessageComponent;
   let fixture: ComponentFixture<ErrorMessageComponent>;
   let element: HTMLElement;
+  let messageFacade: MessageFacade;
 
   beforeEach(async(() => {
+    messageFacade = mock(MessageFacade);
     TestBed.configureTestingModule({
       imports: [TranslateModule.forRoot()],
       declarations: [ErrorMessageComponent, MockDirective(ServerHtmlDirective)],
+      providers: [{ provide: MessageFacade, useFactory: () => instance(messageFacade) }],
     }).compileComponents();
   }));
 
@@ -36,9 +41,21 @@ describe('Error Message Component', () => {
     expect(element.querySelector('[role="alert"]')).toBeFalsy();
   });
 
-  it('should render an error if an error occurs', () => {
+  it('should render an error if an error occurs and toast is false', () => {
     component.error = makeHttpError({ message: 'Test Error' });
+    component.toast = false;
+
+    component.ngOnChanges();
     fixture.detectChanges();
     expect(element.querySelector('[role="alert"]')).toBeTruthy();
+  });
+
+  it('should trigger error toast if an error occurs and toast is true', () => {
+    component.error = makeHttpError({ message: 'Test Error' });
+    component.toast = true;
+
+    component.ngOnChanges();
+    fixture.detectChanges();
+    verify(messageFacade.error(anything())).once();
   });
 });
