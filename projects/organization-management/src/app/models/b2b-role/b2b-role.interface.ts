07b6ab9145018503b03eadd08c5d2b88c4865b9e
Silke Grüber <57660644+SGrueber@users.noreply.github.com>
2020-09-30 18:19:46 +0200

    feat: display user role description on create/edit b2b user pages (#393)

--- projects/organization-management/src/app/models/b2b-role/b2b-role.interface.ts
+++ projects/organization-management/src/app/models/b2b-role/b2b-role.interface.ts
@@ -2,5 +2,6 @@ export interface B2bRoleData {
   roleID: string;
   fixed: boolean;
   roleDisplayName: string;
+  roleDescription?: string;
   permissions: { permissionDisplayName: string }[];
 }
--- projects/organization-management/src/app/models/b2b-role/b2b-role.model.ts
+++ projects/organization-management/src/app/models/b2b-role/b2b-role.model.ts
@@ -2,5 +2,6 @@ export interface B2bRole {
   id: string;
   fixed: boolean;
   displayName: string;
+  description?: string;
   permissionDisplayNames: string[];
 }
