80f3b2f373fe85d4877b10a71d60447e74392b32
harsh.gupta@rsystems.com <harsh.gupta@rsystems.com>
2020-10-07 11:09:51 +0530

    feat: initial implementation for Cybersource Credit Card

--- src/app/pages/checkout-payment/payment-cybersource-creditcard/payment-cybersource-creditcard.component.ts
+++ src/app/pages/checkout-payment/payment-cybersource-creditcard/payment-cybersource-creditcard.component.ts
@@ -11,15 +11,12 @@ import {
 } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import b64u from 'b64u';
-import { range } from 'lodash-es';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 import { Attribute } from 'ish-core/models/attribute/attribute.model';
 import { PaymentMethod } from 'ish-core/models/payment-method/payment-method.model';
 import { ScriptLoaderService } from 'ish-core/utils/script-loader/script-loader.service';
-import { SelectOption } from 'ish-shared/forms/components/select/select.component';
-import { markAsDirtyRecursive } from 'ish-shared/forms/utils/form-utils';
 
 // tslint:disable:no-any - allows access to cybersource js functionality
 declare var Flex: any;
@@ -27,27 +24,12 @@ declare var Flex: any;
 @Component({
   selector: 'ish-payment-cybersource-creditcard',
   templateUrl: './payment-cybersource-creditcard.component.html',
-  styleUrls: ['./payment-cybersource-creditcard.component.scss'],
   changeDetection: ChangeDetectionStrategy.Default,
 })
 export class PaymentCybersourceCreditcardComponent implements OnChanges, OnDestroy, OnInit {
   cyberSourceCreditCardForm: FormGroup;
 
-  constructor(protected scriptLoader: ScriptLoaderService, protected cd: ChangeDetectorRef) {
-    this.monthOptions = range(1, 13)
-      .map(n => n.toString().padStart(2, '0'))
-      .map(n => ({ label: n, value: n }));
-
-    const currentYear = new Date().getFullYear();
-    this.yearOptions = range(currentYear, currentYear + 7).map(n => ({
-      label: n.toString(),
-      value: n.toString(),
-    }));
-  }
-
-  monthOptions: SelectOption[];
-  yearOptions: SelectOption[];
-
+  constructor(protected scriptLoader: ScriptLoaderService, protected cd: ChangeDetectorRef) {}
   /**
    * concardis payment method, needed to get configuration parameters
    */
@@ -59,9 +41,10 @@ export class PaymentCybersourceCreditcardComponent implements OnChanges, OnDestr
   @Input() activated = false;
 
   @Output() cancel = new EventEmitter<void>();
-  @Output() submit = new EventEmitter<{ parameters: Attribute<string>[]; saveAllowed: boolean }>();
+  @Output() submit = new EventEmitter<{ parameters: Attribute[]; saveAllowed: boolean }>();
 
-  private destroy$ = new Subject();
+  // tslint:disable-next-line: private-destroy-field
+  protected destroy$ = new Subject();
 
   /**
    * flag to make sure that the init script is executed only once
@@ -70,29 +53,12 @@ export class PaymentCybersourceCreditcardComponent implements OnChanges, OnDestr
 
   microform: any;
 
-  expirationMonthVal: string;
-  expirationYearVal: string;
-
   /**
    * error messages from host
    */
   errorMessage = {
     general: { message: '' },
-    number: { message: '' },
-    securityCode: { message: '' },
   };
-
-  ngOnInit() {
-    this.cyberSourceCreditCardForm = new FormGroup({});
-    this.cyberSourceCreditCardForm.addControl(
-      'expirationMonth',
-      new FormControl('', [Validators.required, Validators.pattern('[0-9]{2}')])
-    );
-    this.cyberSourceCreditCardForm.addControl(
-      'expirationYear',
-      new FormControl('', [Validators.required, Validators.pattern('[0-9]{4}')])
-    );
-  }
 
   /**
    * load concardis script if component is shown
@@ -103,9 +69,9 @@ export class PaymentCybersourceCreditcardComponent implements OnChanges, OnDestr
     }
   }
 
+  // tslint:disable-next-line: rxjs-prefer-angular-takeuntil
   ngOnDestroy() {
     this.destroy$.next();
-    this.destroy$.complete();
   }
 
   /**
@@ -121,10 +87,23 @@ export class PaymentCybersourceCreditcardComponent implements OnChanges, OnDestr
     return parameter.value;
   }
 
+  ngOnInit() {
+    this.cyberSourceCreditCardForm = new FormGroup({});
+    this.cyberSourceCreditCardForm.addControl(
+      'expirationMonth',
+      new FormControl('', [Validators.required, Validators.pattern('[0-9]{2}')])
+    );
+    this.cyberSourceCreditCardForm.addControl(
+      'expirationYear',
+      new FormControl('', [Validators.required, Validators.pattern('[0-9]{2}')])
+    );
+  }
+
+  // tslint:disable-next-line:no-empty
   loadScript() {
     // spell-checker: words flexkey
     // load script only once if component becomes visible
-    if (this.activated) {
+    if (this.activated && !this.scriptLoaded) {
       const flexkeyId = this.getParamValue('flexkeyId', 'checkout.credit_card.flexkeyId.error.notFound');
 
       this.scriptLoaded = true;
@@ -135,93 +114,73 @@ export class PaymentCybersourceCreditcardComponent implements OnChanges, OnDestr
           // the capture context that was requested server-side for this transaction
           const captureContext = flexkeyId;
 
+          // custom styles that will be applied to each field we create using Microform
+          const myStyles = {
+            input: {
+              'font-size': '14px',
+              'font-family': 'helvetica, tahoma, calibri, sans-serif',
+              color: '#555',
+            },
+            ':focus': { color: 'blue' },
+            ':disabled': { cursor: 'not-allowed' },
+            valid: { color: '#3c763d' },
+            invalid: { color: '#a94442' },
+          };
+
           // setup
           const flex = new Flex(captureContext);
-          this.microform = flex.microform();
-          const cardNumber = this.microform.createField('number');
-          const securityCode = this.microform.createField('securityCode');
-
-          cardNumber.load('#number-container');
+          this.microform = flex.microform({ styles: myStyles });
+          const cardnumber = this.microform.createField('number', { placeholder: 'Enter card number' });
+          const securityCode = this.microform.createField('securityCode', { placeholder: '•••' });
+          cardnumber.load('#number-container');
           securityCode.load('#securityCode-container');
         });
     }
   }
 
-  submitCallback(error: { details: { location: string; message: string }[] }, token: string) {
-    this.resetErrors();
-
-    if (error) {
-      // handling error
-      for (const detail of error.details) {
-        switch (detail.location) {
-          case 'number': {
-            this.errorMessage.number.message = 'checkout.credit_card.number.error.invalid';
-            break;
-          }
-          case 'securityCode': {
-            this.errorMessage.securityCode.message = 'checkout.credit_card.cvc.error.invalid';
-            break;
-          }
-          case 'expirationMonth': {
-            this.cyberSourceCreditCardForm.controls.expirationMonth.setErrors({
-              customError: 'checkout.credit_card.expiryMonth.error.invalid',
-            });
-            break;
-          }
-          case 'expirationYear': {
-            this.cyberSourceCreditCardForm.controls.expirationYear.setErrors({
-              customError: 'checkout.credit_card.expiryMonth.error.invalid',
-            });
-            break;
-          }
-        }
-      }
-    } else if (!this.cyberSourceCreditCardForm.invalid) {
-      const tokenSplit = token.split('.');
-      const payloadJson: {
-        data: { number: string; type: string; expirationMonth: string; expirationYear: string };
-        iss: string;
-        exp: string;
-        iat: string;
-        jti: string;
-      } = JSON.parse(b64u.decode(tokenSplit[1]));
-
-      this.submit.emit({
-        parameters: [
-          { name: 'token', value: token },
-          { name: 'tokenExpiryTime', value: payloadJson.exp },
-          { name: 'cardType', value: payloadJson.data.type },
-          { name: 'maskedCardNumber', value: payloadJson.data.number },
-          { name: 'expirationDate', value: `${this.expirationMonthVal}/${this.expirationYearVal}` },
-        ],
-        saveAllowed: false,
-      });
-    }
-    this.cd.detectChanges();
-  }
-  resetErrors() {
-    this.errorMessage.general.message = undefined;
-    this.errorMessage.number.message = undefined;
-    this.errorMessage.securityCode.message = undefined;
-  }
 
   /**
    * submit cybersource payment form
    */
   submitNewPaymentInstrument() {
-    if (this.cyberSourceCreditCardForm.invalid) {
-      markAsDirtyRecursive(this.cyberSourceCreditCardForm);
-    } else {
-      this.expirationMonthVal = this.cyberSourceCreditCardForm.controls.expirationMonth.value;
-      this.expirationYearVal = this.cyberSourceCreditCardForm.controls.expirationYear.value;
 
-      const options = {
-        expirationMonth: this.expirationMonthVal,
-        expirationYear: this.expirationYearVal,
-      };
+    const expirationMonthVal = this.cyberSourceCreditCardForm.controls.expirationMonth.value;
+    const expirationYearVal = this.cyberSourceCreditCardForm.controls.expirationYear.value;
 
-      this.microform.createToken(options, (error: any, token: any) => this.submitCallback(error, token));
-    }
+    const options = {
+      expirationMonth: expirationMonthVal,
+      expirationYear: expirationYearVal,
+    };
+
+    this.microform.createToken(options, (err: any, token: string) => {
+      if (err) {
+        // handle error
+        // tslint:disable-next-line: no-console
+        console.log(err);
+      } else {
+        const tokenSplit: string[] = token.split('.');
+
+        const payloadjson: {
+          data: { number: string; type: string; expirationMonth: string; expirationYear: string };
+          iss: string;
+          exp: string;
+          iat: string;
+          jti: string;
+        } = JSON.parse(b64u.decode(tokenSplit[1]));
+
+        this.submit.emit({
+          parameters: [
+            { name: 'token', value: token },
+            { name: 'tokenExpiryTime', value: payloadjson.exp },
+            { name: 'cardType', value: payloadjson.data.type },
+            { name: 'maskedCardNumber', value: payloadjson.data.number },
+            { name: 'expirationDate', value: `${expirationMonthVal}/${expirationYearVal}` },
+          ],
+          saveAllowed: false,
+        });
+        this.cd.detectChanges();
+      }
+    });
   }
 
   /**
