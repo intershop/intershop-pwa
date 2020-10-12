6217f7eaffc0f2dae2a1cfc4237589345e57cee4
Silke <s.grueber@intershop.de>
2020-10-12 13:03:13 +0200

    refactor: code adjustments after rebase

--- projects/requisition-management/src/app/components/budget-bar/budget-bar.component.spec.ts
+++ projects/requisition-management/src/app/components/budget-bar/budget-bar.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { of } from 'rxjs';
 import { instance, mock, when } from 'ts-mockito';
@@ -13,18 +13,18 @@ describe('Budget Bar Component', () => {
   let fixture: ComponentFixture<BudgetBarComponent>;
   let element: HTMLElement;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
     const accountFacade = mock(AccountFacade);
     when(accountFacade.userPriceDisplayType$).thenReturn(of('gross'));
 
-    TestBed.configureTestingModule({
+    await TestBed.configureTestingModule({
       imports: [TranslateModule.forRoot()],
       declarations: [BudgetBarComponent, PricePipe],
       providers: [{ provide: AccountFacade, useFactory: () => instance(accountFacade) }],
     }) // tslint:disable-next-line: no-any
       .configureCompiler({ preserveWhitespaces: true } as any)
       .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     const translate = TestBed.inject(TranslateService);
@@ -65,7 +65,7 @@ describe('Budget Bar Component', () => {
     fixture.detectChanges();
     expect(element).toMatchInlineSnapshot(`
       <div class="budget-bar-overflow">
-        <div class="overflow-indicator" style="width: 71%;" title="$500.00">
+        <div class="overflow-indicator" style="width: 71%" title="$500.00">
           <span class="overflow-display">71%</span>
         </div>
       </div>
@@ -74,7 +74,7 @@ describe('Budget Bar Component', () => {
           aria-hidden="true"
           class="budget-bar-used bg-danger"
           ng-reflect-ng-class="bg-danger"
-          style="width: 140%;"
+          style="width: 140%"
           title="$700.00"
         >
           $700.00
@@ -98,7 +98,7 @@ describe('Budget Bar Component', () => {
         class="budget-bar-used budget-bar-used-additional border-left bg-danger"
         role="progressbar"
         ng-reflect-ng-class="bg-danger"
-        style="width: 30%;"
+        style="width: 30%"
         title="$300.00"
       >
         <span>$300.00</span>
