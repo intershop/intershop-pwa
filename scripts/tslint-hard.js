9a80b19f46cf044843ce78ab57b54114fc58712b
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-10-12 18:29:23 +0200

    chore: include skipped tests in escalated tslint run

--- scripts/tslint-hard.js
+++ scripts/tslint-hard.js
@@ -4,17 +4,15 @@ const { execSync } = require('child_process');
 
 const tslintJson = parse(fs.readFileSync('./tslint.json', { encoding: 'UTF-8' }));
 
-Object.keys(tslintJson.rules)
-  .filter(key => key !== 'no-disabled-tests')
-  .forEach(key => {
-    if (
-      typeof tslintJson.rules[key] === 'object' &&
-      tslintJson.rules[key].severity &&
-      tslintJson.rules[key].severity.startsWith('warn')
-    ) {
-      tslintJson.rules[key].severity = 'error';
-    }
-  });
+Object.keys(tslintJson.rules).forEach(key => {
+  if (
+    typeof tslintJson.rules[key] === 'object' &&
+    tslintJson.rules[key].severity &&
+    tslintJson.rules[key].severity.startsWith('warn')
+  ) {
+    tslintJson.rules[key].severity = 'error';
+  }
+});
 
 fs.writeFileSync('./tslint.json', stringify(tslintJson, null, 2));
 execSync('npx prettier --write tslint.json');
