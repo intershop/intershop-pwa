01bb078617f7d2c65c252ad63b70201c92890d50
Silke <s.grueber@intershop.de>
2020-10-13 12:56:51 +0200

    feat: consider the order approval service in the account navigation

--- src/app/pages/account/account-navigation/account-navigation.component.spec.ts
+++ src/app/pages/account/account-navigation/account-navigation.component.spec.ts
@@ -2,11 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { MockComponent } from 'ng-mocks';
-import { of } from 'rxjs';
-import { mock, when } from 'ts-mockito';
 
 import { AuthorizationToggleModule } from 'ish-core/authorization-toggle.module';
-import { AccountFacade } from 'ish-core/facades/account.facade';
 import { FeatureToggleModule } from 'ish-core/feature-toggle.module';
 
 import { AccountUserInfoComponent } from '../account-user-info/account-user-info.component';
@@ -17,21 +14,17 @@ describe('Account Navigation Component', () => {
   let component: AccountNavigationComponent;
   let fixture: ComponentFixture<AccountNavigationComponent>;
   let element: HTMLElement;
-  let accountFacadeMock: AccountFacade;
 
   beforeEach(async () => {
-    accountFacadeMock = mock(AccountFacade);
     await TestBed.configureTestingModule({
       declarations: [AccountNavigationComponent, MockComponent(AccountUserInfoComponent)],
       imports: [
-        AuthorizationToggleModule.forTesting('APP_B2B_MANAGE_USERS'),
+        AuthorizationToggleModule.forTesting('APP_B2B_MANAGE_USERS', 'APP_B2B_PURCHASE'),
         FeatureToggleModule.forTesting('quoting', 'orderTemplates'),
         RouterTestingModule,
         TranslateModule.forRoot(),
       ],
     }).compileComponents();
-
-    when(accountFacadeMock.isBusinessCustomer$).thenReturn(of(true));
   });
 
   beforeEach(() => {
@@ -59,5 +52,16 @@ describe('Account Navigation Component', () => {
   it('should display link to user list', () => {
     fixture.detectChanges();
     expect(element.textContent).toContain('account.organization.user_management');
+  });
+
+  it('should not display link to requisition list if order approval service is disabled', () => {
+    fixture.detectChanges();
+    expect(element.textContent).not.toContain('account.requisitions.requisitions');
+  });
+
+  it('should display link to requisition list if order approval service is enabled', () => {
+    component.isOrderApprovalEnabled = true;
+    fixture.detectChanges();
+    expect(element.textContent).toContain('account.requisitions.requisitions');
   });
 });
--- src/app/pages/account/account-navigation/account-navigation.component.ts
+++ src/app/pages/account/account-navigation/account-navigation.component.ts
@@ -20,52 +20,58 @@ interface NavigationItems {
 })
 export class AccountNavigationComponent implements OnInit, OnChanges {
   @Input() deviceType: DeviceType;
+  @Input() isOrderApprovalEnabled: boolean;
 
   isMobileView = false;
-
-  /**
-   * Manages the Account Navigation items.
-   */
-  navigationItems: NavigationItems = {
-    '/account': { localizationKey: 'account.my_account.link' },
-    '/account/orders': { localizationKey: 'account.order_history.link' },
-    '/account/wishlists': {
-      localizationKey: 'account.wishlists.link',
-      feature: 'wishlists',
-      dataTestingId: 'wishlists-link',
-    },
-    '/account/quotes': { localizationKey: 'account.navigation.quotes.link', feature: 'quoting' },
-    '/account/order-templates': {
-      localizationKey: 'account.ordertemplates.link',
-      feature: 'orderTemplates',
-      dataTestingId: 'order-templates-link',
-    },
-    '/account/payment': { localizationKey: 'account.payment.link', dataTestingId: 'payments-link' },
-    '/account/addresses': { localizationKey: 'account.saved_addresses.link', dataTestingId: 'addresses-link' },
-    '/account/profile': { localizationKey: 'account.profile.link' },
-    '/account/organization': {
-      localizationKey: 'account.organization.user_management',
-      permission: 'APP_B2B_MANAGE_USERS',
-    },
-    '/account/requisitions/buyer': {
-      localizationKey: 'account.requisitions.requisitions',
-      permission: 'APP_B2B_PURCHASE',
-    },
-    '/account/requisitions/approver': {
-      localizationKey: 'account.requisitions.approvals',
-      permission: 'APP_B2B_ORDER_APPROVAL',
-    },
-    '/logout': { localizationKey: 'account.navigation.logout.link' },
-  };
+  navigationItems: NavigationItems;
 
   constructor(private router: Router) {}
 
   ngOnInit() {
     this.isMobileView = this.deviceType === 'tablet' || this.deviceType === 'mobile';
+
+    this.setNavigationItems();
   }
 
   ngOnChanges() {
     this.isMobileView = this.deviceType === 'tablet' || this.deviceType === 'mobile';
+  }
+  /**
+   * Manages the Account Navigation items.
+   */
+  setNavigationItems() {
+    this.navigationItems = {
+      '/account': { localizationKey: 'account.my_account.link' },
+      '/account/requisitions/buyer': {
+        localizationKey: 'account.requisitions.requisitions',
+        permission: this.isOrderApprovalEnabled ? 'APP_B2B_PURCHASE' : 'never',
+      },
+      '/account/requisitions/approver': {
+        localizationKey: 'account.requisitions.approvals',
+        permission: this.isOrderApprovalEnabled ? 'APP_B2B_ORDER_APPROVAL' : 'never',
+      },
+      '/account/orders': { localizationKey: 'account.order_history.link' },
+      '/account/wishlists': {
+        localizationKey: 'account.wishlists.link',
+        feature: 'wishlists',
+        dataTestingId: 'wishlists-link',
+      },
+      '/account/order-templates': {
+        localizationKey: 'account.ordertemplates.link',
+        feature: 'orderTemplates',
+        dataTestingId: 'order-templates-link',
+      },
+      '/account/quotes': { localizationKey: 'account.navigation.quotes.link', feature: 'quoting' },
+      '/account/addresses': { localizationKey: 'account.saved_addresses.link', dataTestingId: 'addresses-link' },
+      '/account/payment': { localizationKey: 'account.payment.link', dataTestingId: 'payments-link' },
+      '/account/profile': { localizationKey: 'account.profile.link' },
+      '/account/organization': {
+        localizationKey: 'account.organization.user_management',
+        permission: 'APP_B2B_MANAGE_USERS',
+      },
+
+      '/logout': { localizationKey: 'account.navigation.logout.link' },
+    };
   }
 
   get currentPath() {
