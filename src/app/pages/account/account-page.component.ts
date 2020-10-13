17d2f09b0c35c2ca0b2c0fe8a7f00312328ba27f
Silke <s.grueber@intershop.de>
2020-10-13 12:56:51 +0200

    feat: consider the order approval service in the account navigation

--- src/app/pages/account/account-page.component.ts
+++ src/app/pages/account/account-page.component.ts
@@ -11,10 +11,12 @@ import { DeviceType } from 'ish-core/models/viewtype/viewtype.types';
 })
 export class AccountPageComponent implements OnInit {
   deviceType$: Observable<DeviceType>;
+  isOrderApprovalEnabled$: Observable<boolean>;
 
   constructor(private appFacade: AppFacade) {}
 
   ngOnInit() {
     this.deviceType$ = this.appFacade.deviceType$;
+    this.isOrderApprovalEnabled$ = this.appFacade.orderApprovalEnabled$;
   }
 }
