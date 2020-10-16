80149c117502d436056a2ccbae96e2d118b62ab1
Silke <s.grueber@intershop.de>
2020-10-16 15:12:40 +0200

    fix: bugfixing after ui review

--- projects/requisition-management/src/app/exports/index.ts
+++ projects/requisition-management/src/app/exports/index.ts
@@ -1,5 +1,3 @@
-// tslint:disable: no-barrel-files
-
 export { RequisitionManagementModule } from '../requisition-management.module';
 
 export { RequisitionManagementBreadcrumbService } from '../services/requisition-management-breadcrumb/requisition-management-breadcrumb.service';
