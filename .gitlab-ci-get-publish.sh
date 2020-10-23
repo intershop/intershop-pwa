9e9e780f8b4c1783a20489ca68d043b9a719d45f
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-10-23 08:13:37 +0200

    build: fix GitLab CI redeploy

--- .gitlab-ci-get-publish.sh
+++ .gitlab-ci-get-publish.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 [ -z "$1" ] && echo "instance name required" && exit 1
-internalPort="${2:-4200}"
 
-currentPort="$(docker inspect --format='{{(index (index .NetworkSettings.Ports "$internalPort/tcp") 0).HostPort}}' "$1")"
+currentPort=`docker inspect --format="{{(index (index .NetworkSettings.Ports \"${2:-4200}/tcp\") 0).HostPort}}" "$1"`
 
 if [ -z "$currentPort" ]
 then
   echo -n '--publish-all'
 else
-  echo -n "--publish $currentPort:$internalPort"
+  echo -n "--publish $currentPort:${2:-4200}"
 fi
