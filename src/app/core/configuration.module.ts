31cfbab279ee952dc0e1a16afdfaf9f179619072
Silke <s.grueber@intershop.de>
2020-10-26 19:59:13 +0100

    fix: display error message for duplicate payment instruments on checkout payment page

--- src/app/core/configuration.module.ts
+++ src/app/core/configuration.module.ts
@@ -4,6 +4,7 @@ import { environment } from '../../environments/environment';
 
 import * as injectionKeys from './configurations/injection-keys';
 import { SPECIAL_HTTP_ERROR_HANDLER } from './interceptors/icm-error-mapper.interceptor';
+import { createPaymentErrorHandler } from './utils/http-error/create-payment.error-handler';
 import { editPasswordErrorHandler } from './utils/http-error/edit-password.error-handler';
 import { LoginUserErrorHandler } from './utils/http-error/login-user.error-handler';
 import { requestReminderErrorHandler } from './utils/http-error/request-reminder.error-handler';
@@ -30,6 +31,7 @@ import { updatePasswordErrorHandler } from './utils/http-error/update-password.e
     { provide: SPECIAL_HTTP_ERROR_HANDLER, useClass: LoginUserErrorHandler, multi: true },
     { provide: SPECIAL_HTTP_ERROR_HANDLER, useValue: requestReminderErrorHandler, multi: true },
     { provide: SPECIAL_HTTP_ERROR_HANDLER, useValue: editPasswordErrorHandler, multi: true },
+    { provide: SPECIAL_HTTP_ERROR_HANDLER, useValue: createPaymentErrorHandler, multi: true },
   ],
 })
 export class ConfigurationModule {}
