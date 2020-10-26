2ac2edbd12171ea614f2ae0f92c55f4039a5894d
Silke <s.grueber@intershop.de>
2020-10-26 19:59:13 +0100

    fix: display error message for duplicate payment instruments on checkout payment page

---
+++ src/app/core/utils/http-error/create-payment.error-handler.ts
@@ -0,0 +1,10 @@
+import { SpecialHttpErrorHandler } from 'ish-core/interceptors/icm-error-mapper.interceptor';
+
+export const createPaymentErrorHandler: SpecialHttpErrorHandler = {
+  test: (error, request) => error.url.endsWith('/payments') && request.method === 'POST',
+  map: error => {
+    if (error?.error && error.error.includes('{') && error.error.includes('}')) {
+      return { code: error.error.slice(error.error.indexOf('{') + 1, error.error.lastIndexOf('}')) };
+    }
+  },
+};
