52c39f78e6e9b37268c788ab0c9283af1f54f5d8
Stefan Hauke <s.hauke@intershop.de>
2020-11-02 12:01:18 +0100

    fixup! add View Context state management

--- src/app/core/store/content/viewcontexts/viewcontexts.effects.ts
+++ src/app/core/store/content/viewcontexts/viewcontexts.effects.ts
@@ -3,7 +3,6 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { map, switchMap } from 'rxjs/operators';
 
 import { CMSService } from 'ish-core/services/cms/cms.service';
-import { log } from 'ish-core/utils/dev/operators';
 import { mapErrorToAction, mapToPayload } from 'ish-core/utils/operators';
 
 import {
@@ -22,7 +21,6 @@ export class ViewcontextsEffects {
       mapToPayload(),
       switchMap(({ viewContextId, callParameters }) =>
         this.cmsService.getViewContextContent(viewContextId, callParameters).pipe(
-          log('view context success'),
           map(({ entrypoint, pagelets }) =>
             loadViewContextEntrypointSuccess({ entrypoint, pagelets, viewContextId, callParameters })
           ),
@@ -32,5 +30,3 @@ export class ViewcontextsEffects {
     )
   );
 }
-
-//           map(([newRoles]) => setUserRolesSuccess({ login, roles: newRoles })),
