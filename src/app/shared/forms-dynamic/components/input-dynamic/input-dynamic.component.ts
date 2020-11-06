7f28f027ec164aaf9d5d8b7972505e126bd90d02
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-11-06 14:46:21 +0100

    refactor: improve template related type compatibility

--- src/app/shared/forms-dynamic/components/input-dynamic/input-dynamic.component.ts
+++ src/app/shared/forms-dynamic/components/input-dynamic/input-dynamic.component.ts
@@ -7,4 +7,12 @@ import { FieldType } from '@ngx-formly/core';
   changeDetection: ChangeDetectionStrategy.Default,
 })
 // tslint:disable-next-line: component-creation-test
-export class InputDynamicComponent extends FieldType {}
+export class InputDynamicComponent extends FieldType {
+  castControlName(val: unknown) {
+    return val as string;
+  }
+
+  castErrorMessages(val: unknown) {
+    return val as { [key: string]: string };
+  }
+}
--- src/app/shared/forms-dynamic/components/select-dynamic/select-dynamic.component.ts
+++ src/app/shared/forms-dynamic/components/select-dynamic/select-dynamic.component.ts
@@ -1,10 +1,24 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FieldType } from '@ngx-formly/core';
 
+import { SelectOption } from 'ish-shared/forms/components/select/select.component';
+
 @Component({
   selector: 'ish-select-dynamic',
   templateUrl: './select-dynamic.component.html',
   changeDetection: ChangeDetectionStrategy.Default,
 })
 // tslint:disable-next-line: component-creation-test
-export class SelectDynamicComponent extends FieldType {}
+export class SelectDynamicComponent extends FieldType {
+  castControlName(val: unknown) {
+    return val as string;
+  }
+
+  castErrorMessages(val: unknown) {
+    return val as { [key: string]: string };
+  }
+
+  castOptions(val: unknown) {
+    return val as SelectOption[];
+  }
+}
