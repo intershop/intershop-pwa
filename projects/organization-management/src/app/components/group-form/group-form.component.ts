8750be601f342b6d5169a101090dff61638dad83
Johannes Metzner <J.Metzner@intershop.de>
2020-11-13 16:18:51 +0100

    Introduce KeyValuePipe

--- projects/organization-management/src/app/components/group-form/group-form.component.ts
+++ projects/organization-management/src/app/components/group-form/group-form.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { KeyValue } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 import { HttpError } from 'ish-core/models/http-error/http-error.model';
@@ -9,31 +10,17 @@ import { Node, NodeTree } from '../../models/node/node.model';
 @Component({
   selector: 'ish-group-form',
   templateUrl: './group-form.component.html',
-  changeDetection: ChangeDetectionStrategy.Default,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GroupFormComponent implements OnChanges {
+export class GroupFormComponent {
   @Input() form: FormGroup;
   @Input() error: HttpError;
   @Input() parents: NodeTree;
-  parentOptions: SelectOption[];
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.form || changes.parents) {
-      this.parentOptions = this.mapParentOptions(this.parents.nodes);
-    }
-  }
-
-  private mapParentOptions(nodeMap: { [id: string]: Node }): SelectOption[] {
-    return Object.keys(nodeMap)
-      .map(nodeId => ({ value: nodeId, label: nodeMap[nodeId].name }))
-      .sort((a: SelectOption, b: SelectOption) => {
-        if (a.label < b.label) {
-          return -1;
-        }
-        if (a.label > b.label) {
-          return 1;
-        }
-        return 0;
-      });
+  toOptions(pairs: KeyValue<string, Node>[]): SelectOption[] {
+    return pairs.map(value => ({
+      label: value.value.name,
+      value: value.key,
+    }));
   }
 }
