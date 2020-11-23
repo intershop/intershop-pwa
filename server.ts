98459a0170d00e6b27d1e19b3022dd99605baa60
Danilo Hoffmann <d.hoffmann@intershop.de>
2020-11-23 12:04:01 +0100

    perf: prevent serving files from assets folder with Angular Universal

--- server.ts
+++ server.ts
@@ -131,6 +131,13 @@ export function app() {
       console.log(`SSR ${req.originalUrl}`);
     }
 
+    if (req.originalUrl.startsWith('/assets/')) {
+      if (logging) {
+        console.log(`RES 404 ${req.originalUrl} - cannot serve static assets with Angular Universal`);
+      }
+      return res.sendStatus(404);
+    }
+
     if (req.headers.accept) {
       const accept = req.headers.accept.toLowerCase();
       if (!accept.includes('html') && ['css', 'image', 'json', 'javascript'].some(inc => accept.includes(inc))) {
