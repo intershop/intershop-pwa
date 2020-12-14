78cda823f6589985130139c017ef9785d06f3286
Johannes Metzner <J.Metzner@intershop.de>
2020-12-14 11:29:22 +0100

    Work on state mgmt for hierarche switch extension

--- src/app/extensions/organization-hierarchies/store/group/group.effects.ts
+++ src/app/extensions/organization-hierarchies/store/group/group.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { filter, tap } from 'rxjs/operators';
 
-import { loadGroup } from './group.actions';
+import { loadGroups } from './group.actions';
 
 @Injectable()
 export class GroupEffects {
@@ -10,7 +10,7 @@ export class GroupEffects {
 
   loadGroup$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(loadGroup),
+      ofType(loadGroups),
       // tslint:disable-next-line:no-console
       tap(() => console.log('got loadGroup in GroupEffects.loadGroup$')),
       filter(() => false)
