95729d87b8c9b27e6547556c5d8cce9215fb4688
Johannes Metzner <J.Metzner@intershop.de>
2020-12-17 17:18:13 +0100

    First attempt for store handling

--- src/app/extensions/organization-hierarchies/store/group/group.effects.spec.ts
+++ src/app/extensions/organization-hierarchies/store/group/group.effects.spec.ts
@@ -1,26 +1,51 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Action } from '@ngrx/store';
+import { provideMockStore } from '@ngrx/store/testing';
 import { cold, hot } from 'jest-marbles';
 import { Observable } from 'rxjs';
+import { instance, mock } from 'ts-mockito';
+
+import { Customer } from 'ish-core/models/customer/customer.model';
+import { CustomerStoreModule } from 'ish-core/store/customer/customer-store.module';
+import { getLoggedInCustomer } from 'ish-core/store/customer/user';
+
+import { OrganizationHierarchiesService } from '../../services/organization-hierarchies/organization-hierarchies.service';
 
 import { loadGroups } from './group.actions';
 import { GroupEffects } from './group.effects';
 
+@Component({ template: 'dummy' })
+class DummyComponent {}
+
+const customer = { customerNo: 'patricia' } as Customer;
+
 describe('Group Effects', () => {
   let actions$: Observable<Action>;
   let effects: GroupEffects;
+  let orgServiceMock: OrganizationHierarchiesService;
 
   beforeEach(() => {
+    orgServiceMock = mock(OrganizationHierarchiesService);
     TestBed.configureTestingModule({
-      providers: [GroupEffects, provideMockActions(() => actions$)],
+      declarations: [DummyComponent],
+      imports: [CustomerStoreModule.forTesting('user')],
+      providers: [
+        GroupEffects,
+        provideMockActions(() => actions$),
+        { provide: OrganizationHierarchiesService, useFactory: () => instance(orgServiceMock) },
+        provideMockStore({
+          selectors: [{ selector: getLoggedInCustomer, value: { customer } }],
+        }),
+      ],
     });
 
     effects = TestBed.inject(GroupEffects);
   });
 
-  describe('loadGroup$', () => {
-    it('should not dispatch actions when encountering loadGroup', () => {
+  describe('loadGroups$', () => {
+    it('should not dispatch actions when encountering loadGroups', () => {
       const action = loadGroups();
       actions$ = hot('-a-a-a', { a: action });
       const expected$ = cold('------');
