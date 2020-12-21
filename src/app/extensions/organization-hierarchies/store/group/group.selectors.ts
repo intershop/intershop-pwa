ae6f759b9b794ea1f6ac87cdc2ec9fc628f26f76
Stefan Hauke <s.hauke@intershop.de>
2020-12-21 13:56:05 +0100

    removed currently unused code to get CI pipeline green with no dead code

--- src/app/extensions/organization-hierarchies/store/group/group.selectors.ts
+++ src/app/extensions/organization-hierarchies/store/group/group.selectors.ts
@@ -6,10 +6,6 @@ import { groupAdapter } from './group.reducer';
 
 const getGroupState = createSelector(getOrganizationHierarchiesState, state => state.group);
 
-export const getGroupLoading = createSelector(getGroupState, state => state.loading);
-
-export const getGroupError = createSelector(getGroupState, state => state.error);
-
 export const {
   selectAll: getGroupsOfOrganization,
   selectTotal: getGroupsOfOrganizationCount,
