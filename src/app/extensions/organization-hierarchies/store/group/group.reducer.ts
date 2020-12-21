2bebe55bb479c30eb0c342c49494bc252a743a2c
Stefan Hauke <s.hauke@intershop.de>
2020-12-21 16:51:33 +0100

    fixup! removed currently unused code to get CI pipeline green with no dead code

--- src/app/extensions/organization-hierarchies/store/group/group.reducer.ts
+++ src/app/extensions/organization-hierarchies/store/group/group.reducer.ts
@@ -1,35 +1,22 @@
 import { EntityState, createEntityAdapter } from '@ngrx/entity';
 import { createReducer, on } from '@ngrx/store';
 
-import { HttpError } from 'ish-core/models/http-error/http-error.model';
-import { setErrorOn, setLoadingOn } from 'ish-core/utils/ngrx-creators';
-
 import { OrganizationGroup } from '../../models/organization-group/organization-group.model';
 
-import { loadGroups, loadGroupsFail, loadGroupsSuccess } from './group.actions';
+import { loadGroupsSuccess } from './group.actions';
 
 export const groupAdapter = createEntityAdapter<OrganizationGroup>();
 
-export interface GroupState extends EntityState<OrganizationGroup> {
-  loading: boolean;
-  error: HttpError;
-}
+export interface GroupState extends EntityState<OrganizationGroup> {}
 
-const initialState: GroupState = groupAdapter.getInitialState({
-  loading: false,
-  error: undefined,
-});
+const initialState: GroupState = groupAdapter.getInitialState({});
 
 export const groupReducer = createReducer(
   initialState,
-  setLoadingOn(loadGroups),
-  setErrorOn(loadGroupsFail),
   on(loadGroupsSuccess, (state: GroupState, action) => {
     const { groups } = action.payload;
     return {
       ...groupAdapter.upsertMany(groups, state),
-      loading: false,
-      error: undefined,
     };
   })
 );
