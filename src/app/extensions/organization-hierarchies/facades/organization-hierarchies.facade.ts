25f206fd9d009a1d85e720ec4a560f3e5087426f
Johannes Metzner <jm@jometzner.de>
2021-01-04 16:39:06 +0100

    Dispatch loadGroups action when customer has logged in

--- src/app/extensions/organization-hierarchies/facades/organization-hierarchies.facade.ts
+++ src/app/extensions/organization-hierarchies/facades/organization-hierarchies.facade.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { switchMap, take, tap } from 'rxjs/operators';
 
-import { getGroupsOfOrganization, getGroupsOfOrganizationCount } from '../store/group';
+import { getLoggedInCustomer } from 'ish-core/store/customer/user';
+import { whenTruthy } from 'ish-core/utils/operators';
+
+import { getGroupsOfOrganization, getGroupsOfOrganizationCount, loadGroups } from '../store/group';
 
 // tslint:disable:member-ordering
 @Injectable({ providedIn: 'root' })
@@ -10,5 +15,13 @@ export class OrganizationHierarchiesFacade {
 
   groups$ = this.store.pipe(select(getGroupsOfOrganization));
 
-  groupsCount$ = this.store.pipe(select(getGroupsOfOrganizationCount));
+  groupsCount$(): Observable<number> {
+    const customer$ = this.store.pipe(select(getLoggedInCustomer));
+    return customer$.pipe(
+      whenTruthy(),
+      take(1),
+      tap(() => this.store.dispatch(loadGroups())),
+      switchMap(() => this.store.pipe(select(getGroupsOfOrganizationCount)))
+    );
+  }
 }
