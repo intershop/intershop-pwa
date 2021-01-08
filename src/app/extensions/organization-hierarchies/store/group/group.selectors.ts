84a1f436986567ad87fdcb4f2c5371b6f1048e2d
Johannes Metzner <jm@jometzner.de>
2021-01-08 17:12:28 +0100

    Transmit selected buying group

--- src/app/extensions/organization-hierarchies/store/group/group.selectors.ts
+++ src/app/extensions/organization-hierarchies/store/group/group.selectors.ts
@@ -1,12 +1,22 @@
 import { createSelector } from '@ngrx/store';
 
+import { OrganizationGroup } from '../../models/organization-group/organization-group.model';
 import { getOrganizationHierarchiesState } from '../organization-hierarchies-store';
 
 import { groupAdapter } from './group.reducer';
 
 const getGroupState = createSelector(getOrganizationHierarchiesState, state => state.group);
 
-export const {
-  selectAll: getGroupsOfOrganization,
-  selectTotal: getGroupsOfOrganizationCount,
-} = groupAdapter.getSelectors(getGroupState);
+const { selectAll, selectTotal, selectEntities } = groupAdapter.getSelectors(getGroupState);
+
+export const getGroupsOfOrganization = selectAll;
+
+export const getGroupsOfOrganizationCount = selectTotal;
+
+export const getSelectedGroupId = createSelector(getGroupState, state => state.selected);
+
+export const getSelectedGroupDetails = createSelector(
+  selectEntities,
+  getSelectedGroupId,
+  (entities, id): OrganizationGroup => id && entities[id]
+);
