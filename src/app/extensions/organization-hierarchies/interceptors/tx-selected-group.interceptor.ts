84d769b3f66d65afd4eb89d818ee0b6d4d134f22
Johannes Metzner <jm@jometzner.de>
2021-01-18 19:58:57 +0100

    Get rid of filter ops as they terminate the pipe

--- src/app/extensions/organization-hierarchies/interceptors/tx-selected-group.interceptor.ts
+++ src/app/extensions/organization-hierarchies/interceptors/tx-selected-group.interceptor.ts
@@ -1,34 +1,32 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { concatMap, filter, first, map, switchMap } from 'rxjs/operators';
+import { Observable, iif } from 'rxjs';
+import { concatMap, first, map } from 'rxjs/operators';
 
-import { getFeatures } from 'ish-core/store/core/configuration';
-import { whenTruthy } from 'ish-core/utils/operators';
-
-import { OrganizationHierarchiesFacade } from '../facades/organization-hierarchies.facade';
+import { getSelectedGroupDetails } from '../store/group';
 
 @Injectable()
 export class TxSelectedGroupInterceptor implements HttpInterceptor {
-  constructor(private facade: OrganizationHierarchiesFacade, private store: Store) {}
+  constructor(private store: Store) {}
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return this.store.pipe(
-      // tslint:disable-next-line
-      filter(store => store['organizationHierarchies']),
-      select(getFeatures),
-      whenTruthy(),
-      filter(features => features.includes('organizationHierarchies')),
-      switchMap(() =>
-        this.facade.getSelectedGroup().pipe(
-          map(group =>
-            group?.parentid && !req?.headers.has('BuyingGroupID')
-              ? req.clone({ headers: req.headers.set('BuyingGroupID', group.id) })
-              : req
+      first(),
+      concatMap((store: { organizationHierarchies: unknown }) =>
+        iif(
+          () => !!store.organizationHierarchies,
+          this.store.pipe(
+            select(getSelectedGroupDetails),
+            map(group =>
+              group?.parentid && !req?.headers.has('BuyingGroupID')
+                ? req.clone({ headers: req.headers.set('BuyingGroupID', group.id) })
+                : req
+            ),
+            first(),
+            concatMap(request => next.handle(request))
           ),
-          first(),
-          concatMap(request => next.handle(request))
+          next.handle(req)
         )
       )
     );
