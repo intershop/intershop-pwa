e6ec72d72ffe6384f856611623b3ed2c4bdeec6e
Stefan Hauke <s.hauke@intershop.de>
2021-01-18 20:21:04 +0100

    TEMP: PUNCHOUT TEST DATA + TEST CONFIGURATION

--- src/environments/environment.model.ts
+++ src/environments/environment.model.ts
@@ -148,4 +148,6 @@ export const ENVIRONMENT_DEFAULTS: Environment = {
     allowedCookies: ['cookieConsent', 'apiToken'],
   },
   cookieConsentVersion: 1,
+  // TODO: to no longer test punchout VALIDATE and SEARCH with mock data the next line needs to be removed once the ICM punchout REST API offers this functionality
+  apiMockPaths: ['^customers/OilCorp/punchouts/oci/(validate|search)'],
 };
