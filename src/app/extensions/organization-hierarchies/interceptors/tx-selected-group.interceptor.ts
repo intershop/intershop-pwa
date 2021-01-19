060448d24657d01605bee267155f30d0b55fff3c
Johannes Metzner <jm@jometzner.de>
2021-01-19 13:15:46 +0100

    Transmit path of selected group
    Signed-off-by: Johannes Metzner <jm@jometzner.de>

--- src/app/extensions/organization-hierarchies/interceptors/tx-selected-group.interceptor.ts
+++ src/app/extensions/organization-hierarchies/interceptors/tx-selected-group.interceptor.ts
@@ -1,10 +1,10 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Store, select } from '@ngrx/store';
-import { Observable, iif } from 'rxjs';
-import { concatMap, first, map } from 'rxjs/operators';
+import { Observable, from, iif } from 'rxjs';
+import { concatMap, first, map, reduce, switchMap } from 'rxjs/operators';
 
-import { getSelectedGroupDetails } from '../store/group';
+import { getSelectedGroupPath } from '../store/group';
 
 @Injectable()
 export class TxSelectedGroupInterceptor implements HttpInterceptor {
@@ -17,10 +17,13 @@ export class TxSelectedGroupInterceptor implements HttpInterceptor {
         iif(
           () => !!store.organizationHierarchies,
           this.store.pipe(
-            select(getSelectedGroupDetails),
-            map(group =>
-              group?.parentid && !req?.headers.has('BuyingGroupID')
-                ? req.clone({ headers: req.headers.set('BuyingGroupID', group.id) })
+            select(getSelectedGroupPath),
+            switchMap(groups =>
+              from(groups).pipe(reduce((acc, val, index) => (index > 0 ? `${val.id},${acc}` : val.id), ''))
+            ),
+            map(path =>
+              path?.length > 0 && !req?.headers.has('BuyingGroupID')
+                ? req.clone({ headers: req.headers.set('BuyingGroupID', path) })
                 : req
             ),
             first(),
