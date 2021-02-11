7848596346e5b5edc1e9e5849d38d7d22738a960
Danilo Hoffmann <d.hoffmann@intershop.de>
2021-02-11 20:14:10 +0000

    chore: helper operator for detecting object changes

--- src/app/core/utils/dev/operators.ts
+++ src/app/core/utils/dev/operators.ts
@@ -1,5 +1,5 @@
 import { OperatorFunction } from 'rxjs';
-import { delay, tap } from 'rxjs/operators';
+import { delay, map, pairwise, startWith, tap } from 'rxjs/operators';
 
 export function randomDelay<T>(min = 1000, max = 10000): OperatorFunction<T, T> {
   return source$ => source$.pipe(delay(Math.floor(Math.random() * (max - min + 1)) + min));
@@ -8,4 +8,50 @@ export function randomDelay<T>(min = 1000, max = 10000): OperatorFunction<T, T> 
 export function log<T>(message?: unknown, stringify?: boolean): OperatorFunction<T, T> {
   // tslint:disable-next-line:no-console
   return source$ => source$.pipe(tap(e => console.log(message || '', stringify ? JSON.stringify(e) : e)));
+}
+
+export function logDiff<T>(message?: unknown): OperatorFunction<T, T> {
+  return source$ =>
+    source$.pipe(
+      map(el => el || ({} as T)),
+      startWith({} as T),
+      pairwise(),
+      // tslint:disable-next-line: no-console
+      tap(([a, b]) => console.log(message || '', keyChanges(a, b), { emit: b })),
+      map(([, b]) => b)
+    );
+}
+
+// https://gist.github.com/Yimiprod/7ee176597fef230d1451
+
+function keyChanges<T>(a: T, b: T) {
+  const changes = {};
+
+  function walkObject(base: T, object: T, path = '') {
+    for (const key of Object.keys(base)) {
+      const currentPath = path === '' ? key : `${path}.${key}`;
+
+      if (object[key] === undefined) {
+        changes[currentPath] = '-';
+      }
+    }
+
+    for (const [key, value] of Object.entries(object)) {
+      const currentPath = Array.isArray(object) ? path + `[${key}]` : path === '' ? key : `${path}.${key}`;
+
+      if (base[key] === undefined) {
+        changes[currentPath] = '+';
+      } else if (value !== base[key]) {
+        if (typeof value === 'object' && typeof base[key] === 'object') {
+          walkObject(base[key], value, currentPath);
+        } else {
+          changes[currentPath] = object[key];
+        }
+      }
+    }
+  }
+
+  walkObject(a, b);
+
+  return changes;
 }
