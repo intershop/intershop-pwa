78210d0b839821f91c89abf56187b6548eebc57d
Danilo Hoffmann <hoffmann@evident.nl>
2021-03-06 14:42:50 +0100

    refactor: use product context access directive in product brand component

--- src/app/pages/product/product-brand/product-brand.component.ts
+++ src/app/pages/product/product-brand/product-brand.component.ts
@@ -1,19 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-
-import { ProductContextFacade } from 'ish-core/facades/product-context.facade';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'ish-product-brand',
   templateUrl: './product-brand.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductBrandComponent implements OnInit {
-  manufacturer$: Observable<string>;
-
-  constructor(private context: ProductContextFacade) {}
-
-  ngOnInit() {
-    this.manufacturer$ = this.context.select('product', 'manufacturer');
-  }
-}
+export class ProductBrandComponent {}
