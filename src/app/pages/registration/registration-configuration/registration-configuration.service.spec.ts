780f2f088e6ba797b7a49f5db125df5b3a67cbfc
max.kless@googlemail.com <max.kless@googlemail.com>
2021-03-24 14:55:56 +0100

    refactor: move registration submit to service

--- src/app/pages/registration/registration-configuration/registration-configuration.service.spec.ts
+++ src/app/pages/registration/registration-configuration/registration-configuration.service.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { instance, mock } from 'ts-mockito';
+
+import { AccountFacade } from 'ish-core/facades/account.facade';
 
 import { RegistrationConfigurationService } from './registration-configuration.service';
 
 describe('Registration Configuration Service', () => {
   let registrationConfigurationService: RegistrationConfigurationService;
+  let accountFacade: AccountFacade;
 
   beforeEach(() => {
+    accountFacade = mock(AccountFacade);
     TestBed.configureTestingModule({
-      providers: [RegistrationConfigurationService],
+      providers: [
+        { provide: AccountFacade, useFactory: () => instance(accountFacade) },
+        RegistrationConfigurationService,
+      ],
     });
     registrationConfigurationService = TestBed.inject(RegistrationConfigurationService);
   });
