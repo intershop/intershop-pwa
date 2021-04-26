8ae24b1573e96b055bffba72966a10f90852a1d3
Marcel Eisentraut <meisentraut@intershop.de>
2021-04-26 08:31:36 +0200

    feat: add support for CMS component "Product List (Filter)" (#673)

--- src/app/core/facades/cms.facade.ts
+++ src/app/core/facades/cms.facade.ts
@@ -4,13 +4,16 @@ import { Observable, combineLatest } from 'rxjs';
 import { delay, filter, map, switchMap, tap } from 'rxjs/operators';
 
 import { CallParameters } from 'ish-core/models/call-parameters/call-parameters.model';
+import { CategoryHelper } from 'ish-core/models/category/category.helper';
 import { getContentInclude, loadContentInclude } from 'ish-core/store/content/includes';
 import { getContentPageTree, loadContentPageTree } from 'ish-core/store/content/page-tree';
 import { getContentPagelet } from 'ish-core/store/content/pagelets';
 import { getContentPageLoading, getSelectedContentPage } from 'ish-core/store/content/pages';
+import { getParametersProductList, loadParametersProductListFilter } from 'ish-core/store/content/parameters';
 import { getViewContext, loadViewContextEntrypoint } from 'ish-core/store/content/viewcontexts';
 import { getPGID } from 'ish-core/store/customer/user';
 import { whenTruthy } from 'ish-core/utils/operators';
+import { URLFormParams } from 'ish-core/utils/url-form-params';
 import { SfeAdapterService } from 'ish-shared/cms/sfe-adapter/sfe-adapter.service';
 import { SfeMapper } from 'ish-shared/cms/sfe-adapter/sfe.mapper';
 
@@ -49,5 +52,40 @@ export class CMSFacade {
   contentPageTree$(rootId: string, depth: number) {
     this.store.dispatch(loadContentPageTree({ rootId, depth }));
     return this.store.pipe(select(getContentPageTree(rootId)));
+  }
+
+  parameterProductListFilter$(categoryId?: string, productFilter?: string, scope?: string, amount?: number) {
+    const listConfiguration = this.getProductListConfiguration(categoryId, productFilter, scope, amount);
+    this.store.dispatch(
+      loadParametersProductListFilter({
+        id: listConfiguration.id,
+        searchParameter: listConfiguration.searchParameter,
+        amount,
+      })
+    );
+    return this.store.pipe(select(getParametersProductList(listConfiguration.id)));
+  }
+
+  private getProductListConfiguration(
+    categoryId?: string,
+    productFilter?: string,
+    scope?: string,
+    amount?: number
+  ): { id: string; searchParameter: URLFormParams } {
+    let id = '';
+    const searchParameter: URLFormParams = {};
+
+    id = categoryId ? `${id}@${categoryId}` : id;
+    id = productFilter ? `${id}@${productFilter}` : id;
+    id = scope ? `${id}@${scope}` : id;
+    id = amount ? `${id}@${amount}` : id;
+
+    if (categoryId && scope !== 'Global') {
+      searchParameter.category = [CategoryHelper.getCategoryPath(categoryId)];
+    }
+
+    searchParameter.productFilter = productFilter ? [productFilter] : ['fallback_searchquerydefinition'];
+
+    return { id, searchParameter };
   }
 }
