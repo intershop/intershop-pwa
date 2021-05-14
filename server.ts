d68acce642f8f19a1394f2fe55abea2bc41e0e3e
Johannes Metzner <J.Metzner@intershop.de>
2021-05-14 20:51:43 +0200

    angular/core update

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 // tslint:disable: no-console ish-ordered-imports force-jsdoc-comments
-import 'zone.js/dist/zone-node';
+import 'zone.js/node';
 
 import * as express from 'express';
 import { join } from 'path';
