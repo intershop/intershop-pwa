c0cce86bd22375c775b018cb0f01289c1916baec
Christopher Meyer <christopher_meyer@outlook.de>
2021-05-26 13:05:02 +0200

    feat: simplified & styled hierarchy path component

--- src/app/extensions/organization-hierarchies/shared/hierarchy-path/hierarchy-path.component.ts
+++ src/app/extensions/organization-hierarchies/shared/hierarchy-path/hierarchy-path.component.ts
@@ -1,15 +1,36 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 import { Basket } from 'ish-core/models/basket/basket.model';
+import { Group } from 'ish-core/models/group/group.model';
 import { Order } from 'ish-core/models/order/order.model';
 import { GenerateLazyComponent } from 'ish-core/utils/module-loader/generate-lazy-component.decorator';
 
 @Component({
   selector: 'ish-hierarchy-path',
   templateUrl: './hierarchy-path.component.html',
+  styleUrls: ['hierarchy-path.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 @GenerateLazyComponent()
-export class HierarchyPathComponent {
+export class HierarchyPathComponent implements OnInit {
   @Input() object: Basket | Order;
+
+  path: Group[];
+  leaf: string;
+  root: string;
+  pathElements: Group[] = [];
+
+  ngOnInit(): void {
+    if (this.object) {
+      this.path = this.object.buyingContextInfo.groupPath;
+      if (this.path) {
+        this.leaf = this.path[this.path.length - 1].groupName;
+        this.root = this.path[0].groupName;
+
+        if (this.path.length > 2) {
+          this.pathElements = this.path.slice(1, this.path.length - 1);
+        }
+      }
+    }
+  }
 }
