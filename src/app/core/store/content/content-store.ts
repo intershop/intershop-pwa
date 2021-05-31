baf127c7c15e6f84d82f74e306c0bd42e4d4fc1c
Stefan Hauke <s.hauke@intershop.de>
2021-05-31 17:51:19 +0200

    feat: introduce content parameter state management - initial with filtered product lists (#673)

--- src/app/core/store/content/content-store.ts
+++ src/app/core/store/content/content-store.ts
@@ -4,6 +4,7 @@ import { IncludesState } from './includes/includes.reducer';
 import { PageTreeState } from './page-tree/page-tree.reducer';
 import { PageletsState } from './pagelets/pagelets.reducer';
 import { PagesState } from './pages/pages.reducer';
+import { ParametersState } from './parameters/parameters.reducer';
 import { ViewcontextsState } from './viewcontexts/viewcontexts.reducer';
 
 export interface ContentState {
@@ -12,6 +13,7 @@ export interface ContentState {
   pages: PagesState;
   viewcontexts: ViewcontextsState;
   pagetree: PageTreeState;
+  parameters: ParametersState;
 }
 
 export const getContentState = createFeatureSelector<ContentState>('content');
