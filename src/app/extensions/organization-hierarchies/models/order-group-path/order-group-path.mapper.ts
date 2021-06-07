79db8f56d62afd76a3c6bed9ce542278ea508d68
SKoch <S.Koch@intershop.de>
2021-06-07 16:49:16 +0200

    IS-32738: remove empty lines

--- src/app/extensions/organization-hierarchies/models/order-group-path/order-group-path.mapper.ts
+++ src/app/extensions/organization-hierarchies/models/order-group-path/order-group-path.mapper.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 
-
-
 import { OrderGroupPathBaseData } from './order-group-path.interface';
 import { OrderGroupPath } from './order-group-path.model';
 
