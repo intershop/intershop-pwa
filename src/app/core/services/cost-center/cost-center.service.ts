f11cb97e22b10d84313aa7ed0d2a2cebdc6d256a
Carola Bratsch <cbratsch@intershop.de>
2021-08-04 07:35:11 +0200

    fix service

--- src/app/core/services/cost-center/cost-center.service.ts
+++ src/app/core/services/cost-center/cost-center.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { concatMap, map, take } from 'rxjs/operators';
+import { switchMap, withLatestFrom } from 'rxjs/operators';
 
 import { CostCenter } from 'ish-core/models/cost-center/cost-center.model';
 import { ApiService } from 'ish-core/services/api/api.service';
-import { getLoggedInCustomer } from 'ish-core/store/customer/user';
+import { getLoggedInCustomer, getLoggedInUser } from 'ish-core/store/customer/user';
 
 @Injectable({ providedIn: 'root' })
 export class CostCenterService {
@@ -13,10 +13,12 @@ export class CostCenterService {
 
   getCostCenterForBusinessUser(): Observable<CostCenter> {
     return this.store.pipe(
-      select(getLoggedInCustomer),
-      map(customer => customer?.customerNo || '-'),
-      take(1),
-      concatMap(customerNo => this.apiService.get<CostCenter>(`customers/${customerNo}/users/-/costcenters`))
+      withLatestFrom(this.store.pipe(select(getLoggedInUser)), this.store.pipe(select(getLoggedInCustomer))),
+      switchMap(([, user, customer]) =>
+        this.apiService.get<CostCenter>(
+          `customers/${customer.customerNo}/users/${encodeURIComponent(user.login)}/costcenters`
+        )
+      )
     );
   }
 }
