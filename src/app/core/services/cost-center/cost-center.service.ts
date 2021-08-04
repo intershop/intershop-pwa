101cbdfff0cc7727c89dcc9ea538eb95e4a66f7b
Carola Bratsch <cbratsch@intershop.de>
2021-08-04 09:40:33 +0200

    fix service

--- src/app/core/services/cost-center/cost-center.service.ts
+++ src/app/core/services/cost-center/cost-center.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { switchMap, withLatestFrom } from 'rxjs/operators';
+import { Observable, combineLatest } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 import { CostCenter } from 'ish-core/models/cost-center/cost-center.model';
 import { ApiService } from 'ish-core/services/api/api.service';
 import { getLoggedInCustomer, getLoggedInUser } from 'ish-core/store/customer/user';
+import { log } from 'ish-core/utils/dev/operators';
+import { whenTruthy } from 'ish-core/utils/operators';
 
 @Injectable({ providedIn: 'root' })
 export class CostCenterService {
   constructor(private apiService: ApiService, private store: Store) {}
 
   getCostCenterForBusinessUser(): Observable<CostCenter> {
-    return this.store.pipe(
-      withLatestFrom(this.store.pipe(select(getLoggedInUser)), this.store.pipe(select(getLoggedInCustomer))),
-      switchMap(([, user, customer]) =>
+    return combineLatest([
+      this.store.pipe(select(getLoggedInCustomer), whenTruthy()),
+      this.store.pipe(select(getLoggedInUser), whenTruthy()),
+    ]).pipe(
+      log(),
+      switchMap(([customer, user]) =>
         this.apiService.get<CostCenter>(
           `customers/${customer.customerNo}/users/${encodeURIComponent(user.login)}/costcenters`
         )
