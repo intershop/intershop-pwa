2df12e6a22e0041ba16bc4b86a3f4c44681df437
Silke <s.grueber@intershop.de>
2021-11-19 11:54:17 +0100

    feat: formly - data type of the input addon text extended by Observable<string>

--- src/app/shared/formly/wrappers/input-addon-wrapper/input-addon-wrapper.component.ts
+++ src/app/shared/formly/wrappers/input-addon-wrapper/input-addon-wrapper.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FieldWrapper } from '@ngx-formly/core';
+import { of } from 'rxjs';
 
 /**
  * Wrapper to add input-addons to fields, using bootstrap styling.
  *
- *  @templateOption **addonRight** - object of type ``{ text: string }`` that will be used to render an input addon to the right of the field.
- *  @templateOption **addonLeft** - object of type ``{ text: string }`` that will be used to render an input addon to the left of the field.
+ *  @templateOption **addonRight** - object of type ``{ text: string }`` or ``{ text: Observable<string> }`` that will be used to render an input addon to the right of the field.
+ *  @templateOption **addonLeft** - object of type ``{ text: string }`` or ``{ text: Observable<string> }`` that will be used to render an input addon to the left of the field.
  *
  */
 @Component({
@@ -13,4 +14,18 @@ import { FieldWrapper } from '@ngx-formly/core';
   templateUrl: './input-addon-wrapper.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InputAddonWrapperComponent extends FieldWrapper {}
+export class InputAddonWrapperComponent extends FieldWrapper {
+  get addonLeftText() {
+    if (!this.to.addonLeft?.text) {
+      return;
+    }
+    return typeof this.to.addonLeft.text === 'string' ? of(this.to.addonLeft.text) : this.to.addonLeft.text;
+  }
+
+  get addonRightText() {
+    if (!this.to.addonRight?.text) {
+      return;
+    }
+    return typeof this.to.addonRight.text === 'string' ? of(this.to.addonRight.text) : this.to.addonRight.text;
+  }
+}
