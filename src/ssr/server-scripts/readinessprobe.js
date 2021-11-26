33b2fa2b9d64696b2e5f996297cc62c4063511e7
Marcel Eisentraut <meisentraut@intershop.de>
2021-11-26 09:16:44 +0100

    refactor: readiness-probe

--- src/ssr/server-scripts/readinessprobe.js
+++ src/ssr/server-scripts/readinessprobe.js
@@ -1,17 +1,21 @@
-function formatBytes(bytes) {
+function memoryLimit() {
   const regex = /(\d+)([a-zA-Z])/m;
-  [, number, size] = bytes.match(regex);
+  [, max, size] = (process.env.SSR_MAX_MEM || '400M').match(regex);
+
+  const limit = parseFloat(process.env.MEM_READY_LIMIT) || 0.8;
 
   const k = 1000;
   const sizes = ['B', 'K', 'M', 'G'];
 
   const i = sizes.findIndex(el => el === size);
 
-  return parseFloat(parseFloat(number) * Math.pow(k, i));
+  return parseFloat(parseFloat(limit * max) * Math.pow(k, i));
 }
 
 const pm2 = require('pm2');
 let ports = require('./ecosystem-ports.json');
+
+const limit = memoryLimit();
 
 if (process.env.ACTIVE_THEMES) {
   const active = process.env.ACTIVE_THEMES.split(',');
@@ -24,18 +28,12 @@ pm2.connect(err1 => {
   if (!err1) {
     pm2.list((err2, list) => {
       if (!err2) {
-        let tooMuchMemoryCounter = 0;
-        Object.entries(ports).forEach(([theme]) => {
-          list
-            .filter(el => el.name === theme)
-            .filter(
-              el =>
-                el.monit?.memory >=
-                (process.env.MAX_MEMORY_READY ? formatBytes(process.env.MAX_MEMORY_READY) : formatBytes('400M'))
-            )
-            .map(() => tooMuchMemoryCounter++);
-        });
-        tooMuchMemoryCounter != 0 ? process.exit(1) : process.exit(0);
+        let memoryExceeds = 0;
+        Object.entries(ports).map(
+          ([theme]) =>
+            (memoryExceeds += list.filter(el => el.name === theme).filter(el => el.monit?.memory >= limit).length)
+        );
+        process.exit(memoryExceeds > 0 ? 1 : 0);
       } else {
         console.log('pm2 list error:', err2);
         process.exit(1);
