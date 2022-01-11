940b47906c321018ff69b3b498f696d81567123b
Harsh Gupta <Harsh.Gupta@rsystems.com>
2022-01-11 08:31:23 +0530

    feat: refactoring code for bank group detail.

--- src/app/pages/checkout-payment/payment-payone-bank-group-detail/payment-payone-bank-group-detail.component.ts
+++ src/app/pages/checkout-payment/payment-payone-bank-group-detail/payment-payone-bank-group-detail.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
+import { AttributeHelper } from 'ish-core/models/attribute/attribute.helper';
 import { PaymentInstrument } from 'ish-core/models/payment-instrument/payment-instrument.model';
 import { PaymentMethod } from 'ish-core/models/payment-method/payment-method.model';
 
@@ -9,11 +10,13 @@ import { PaymentMethod } from 'ish-core/models/payment-method/payment-method.mod
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PaymentPayoneBankGroupDetailComponent implements OnInit {
-  constructor(protected cd: ChangeDetectorRef) {}
   /**
    * payone payment method, needed to get configuration parameters
    */
   @Input() paymentMethod: PaymentMethod;
+  /**
+   * payone payment instrument, needed for currently selected instrument.
+   */
   @Input() paymentInstrument: PaymentInstrument;
 
   bankGroupName: string;
@@ -25,22 +28,17 @@ export class PaymentPayoneBankGroupDetailComponent implements OnInit {
   /**
    * function to get localized bankGroup name
    */
-  protected getBankGroupName(): string {
-    if (this.paymentInstrument && this.paymentInstrument.parameters) {
-      // fetching bankGroup code from payment instrument
-      const bankGroupCodeAttr = this.paymentInstrument.parameters.find(attribute => attribute.name === 'bankGroupCode');
-      let bankGroupCodeValue;
-      if (bankGroupCodeAttr) {
-        bankGroupCodeValue = bankGroupCodeAttr.value ? bankGroupCodeAttr.value.toString() : undefined;
-      }
-      // fetching corresponding bankGroup name from bankGroup map
-      if (bankGroupCodeValue) {
-        for (const bankGroupMap of this.paymentMethod.hostedPaymentPageParameters) {
-          if (bankGroupMap.name === bankGroupCodeValue) {
-            return bankGroupMap.value;
-          }
-        }
-      }
-    }
+  private getBankGroupName(): string {
+    // fetching bankGroup code from payment instrument
+    const bankGroupCodeValue = AttributeHelper.getAttributeValueByAttributeName<string>(
+      this.paymentInstrument?.parameters,
+      'bankGroupCode'
+    );
+
+    // fetching corresponding bankGroup name from bankGroup map
+    const bankGroupMap = bankGroupCodeValue
+      ? this.paymentMethod.hostedPaymentPageParameters.find(param => param.name === bankGroupCodeValue)
+      : undefined;
+    return bankGroupMap?.value;
   }
 }
