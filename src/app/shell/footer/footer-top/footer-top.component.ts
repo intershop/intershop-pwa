d14acdc7b861c50214bdfef8fac3bf282fbf6aca
MoritzRS <moritz.r.schulz@gmail.com>
2022-02-03 13:26:43 +0100

    feat: introduce Footer Top component that integrates the Store Locator

---
+++ src/app/shell/footer/footer-top/footer-top.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'ish-footer-top',
+  templateUrl: './footer-top.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterTopComponent {}
