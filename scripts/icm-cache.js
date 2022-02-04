59d2b7500ca986baa6872959fde8de8b9078c85e
Stefan Hauke <s.hauke@intershop.de>
2022-02-04 11:23:48 +0100

    docs: local ICM cache for (performance) testing purposes - add integration documentation

--- scripts/icm-cache.js
+++ scripts/icm-cache.js
@@ -2,9 +2,57 @@
  * Local ICM Cache
  *
  * Proxy tool for caching ICM responses
+ * Required dependency https://github.com/addisonj/node-cacher
  *
- * Set environment variable ICM_BASE_URL directed to an ICM installation when starting the tool.
- * Set ICM_BASE_URL of your PWA to http://localhost:10000.
+ *
+ * FOR TESTING
+ *
+ * To run the PWA with this local caching set the ICM_BASE_URL of your PWA to "http://localhost:10000".
+ * - in one shell run: ICM_BASE_URL=http://localhost:10000 npm run start
+ * Set environment variable ICM_BASE_URL directed to an ICM installation when starting the cache tool.
+ * - in a second shell run: ICM_BASE_URL=<ICM> node scripts/icm-cache.js
+ *
+ *
+ * FOR INTEGRATION
+ *
+ * Move the 'icm-cache.js' file to the 'src/ssr/server-scripts' folder and install the dependency.
+ *
+ * An environment variable LOCAL_ICM_CACHE could be used to enable/disable caching ICM responses in a deployed SSR container.
+ * The following code snippets to integrate the local ICM cache could be used.
+ *
+ * Code snippet to adapt the 'build-ecosystem.js':
+-----
+const LOCAL_ICM_CACHE = /^(on|1|true|yes)$/i.test(process.env.LOCAL_ICM_CACHE);
+const localICMCacheActive = LOCAL_ICM_CACHE && !!process.env.ICM_BASE_URL;
+if (LOCAL_ICM_CACHE && !process.env.ICM_BASE_URL) {
+  console.warn('LOCAL_ICM_CACHE cannot be used without setting ICM_BASE_URL');
+}
+if (localICMCacheActive) {
+  content += `
+  - script: dist/icm-cache.js
+    name: icm-cache
+    time: true
+`;
+}
+-----
+ *
+ * Code snippet to adapt the 'Object.entries(ports).forEach(([theme, port])' part of 'build-ecosystem.js':
+-----
+if (localICMCacheActive) {
+  content += `      ICM_BASE_URL_SSR: http://localhost:10000
+    `;
+}
+-----
+ *
+ * Code snippet to adapt 'src\app\core\store\core\configuration\configuration.selectors.ts':
+-----
+const ssrBaseUrl = typeof process !== 'undefined' && process.env.ICM_BASE_URL_SSR;
+
+export const getICMServerURL = createSelector(getConfigurationState, state =>
+  state.baseURL && state.server ? `${ssrBaseUrl || state.baseURL}/${state.server}` : undefined
+);
+-----
+ *
  */
 
 const express = require('express');
