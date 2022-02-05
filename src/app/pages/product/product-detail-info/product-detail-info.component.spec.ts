3b9f9d1af49525bc3836ae8c45b77eb368b11465
suschneider <57661483+suschneider@users.noreply.github.com>
2022-02-05 22:54:20 +0100

    feat: replace product detail info accordion with tabs (#994)

--- src/app/pages/product/product-detail-info/product-detail-info.component.spec.ts
+++ src/app/pages/product/product-detail-info/product-detail-info.component.spec.ts
@@ -1,39 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 import { MockComponent } from 'ng-mocks';
 import { instance, mock } from 'ts-mockito';
 
 import { ProductContextFacade } from 'ish-core/facades/product-context.facade';
-import { AccordionItemComponent } from 'ish-shared/components/common/accordion-item/accordion-item.component';
-import { AccordionComponent } from 'ish-shared/components/common/accordion/accordion.component';
 import { ProductAttachmentsComponent } from 'ish-shared/components/product/product-attachments/product-attachments.component';
 import { ProductAttributesComponent } from 'ish-shared/components/product/product-attributes/product-attributes.component';
 import { ProductShipmentComponent } from 'ish-shared/components/product/product-shipment/product-shipment.component';
 
-import { ProductDetailInfoAccordionComponent } from './product-detail-info-accordion.component';
+import { ProductDetailInfoComponent } from './product-detail-info.component';
 
-describe('Product Detail Info Accordion Component', () => {
-  let component: ProductDetailInfoAccordionComponent;
-  let fixture: ComponentFixture<ProductDetailInfoAccordionComponent>;
+describe('Product Detail Info Component', () => {
+  let component: ProductDetailInfoComponent;
+  let fixture: ComponentFixture<ProductDetailInfoComponent>;
   let element: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TranslateModule.forRoot()],
+      imports: [NgbNavModule, TranslateModule.forRoot()],
       declarations: [
-        MockComponent(AccordionComponent),
-        MockComponent(AccordionItemComponent),
         MockComponent(ProductAttachmentsComponent),
         MockComponent(ProductAttributesComponent),
         MockComponent(ProductShipmentComponent),
-        ProductDetailInfoAccordionComponent,
+        ProductDetailInfoComponent,
       ],
       providers: [{ provide: ProductContextFacade, useFactory: () => instance(mock(ProductContextFacade)) }],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProductDetailInfoAccordionComponent);
+    fixture = TestBed.createComponent(ProductDetailInfoComponent);
     component = fixture.componentInstance;
     element = fixture.nativeElement;
   });
