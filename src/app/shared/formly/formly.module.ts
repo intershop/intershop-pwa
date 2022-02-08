5e5d37e2d39395aa14ad4c911d3453dd5e006747
max.kless@googlemail.com <max.kless@googlemail.com>
2022-02-08 17:48:32 +0100

    fix: remove formly stuff

--- src/app/shared/formly/formly.module.ts
+++ src/app/shared/formly/formly.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { FieldType, FormlyModule as FormlyBaseModule } from '@ngx-formly/core';
 
 import { ExtensionsModule } from './extensions/extensions.module';
-import { CaptchaFieldComponent } from './types/captcha-field/captcha-field.component';
-import { SelectFieldComponent } from './types/select-field/select-field.component';
-import { TextInputFieldComponent } from './types/text-input-field/text-input-field.component';
-import { TextareaFieldComponent } from './types/textarea-field/textarea-field.component';
 import { TypesModule } from './types/types.module';
 import { ValidationMessageComponent } from './wrappers/components/validation-message/validation-message.component';
 import { WrappersModule } from './wrappers/wrappers.module';
@@ -24,13 +20,6 @@ import { WrappersModule } from './wrappers/wrappers.module';
     TypesModule,
     WrappersModule,
   ],
-  exports: [
-    CaptchaFieldComponent,
-    FormlyBaseModule,
-    SelectFieldComponent,
-    TextareaFieldComponent,
-    TextInputFieldComponent,
-    ValidationMessageComponent,
-  ],
+  exports: [FormlyBaseModule, ValidationMessageComponent],
 })
 export class FormlyModule {}
