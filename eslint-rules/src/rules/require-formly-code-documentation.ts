af1066274179cf44f481bbe2f7dc5f33787a84e8
max.kless@googlemail.com <max.kless@googlemail.com>
2022-02-17 17:18:28 +0100

    ci: add require formly documentation rule

---
+++ eslint-rules/src/rules/require-formly-code-documentation.ts
@@ -0,0 +1,66 @@
+import { AST_NODE_TYPES, TSESLint, TSESTree } from '@typescript-eslint/experimental-utils';
+
+import { getClosestAncestorByKind } from '../helpers';
+
+export const requireFormlyCodeDocumentationRule: TSESLint.RuleModule<string, []> = {
+  meta: {
+    messages: {
+      missingDocumentationError: `Missing documentation for {{ artifactName }}. \n Please provide documentation for all Formly types, wrappers and extensions.`,
+    },
+    type: 'problem',
+    schema: [],
+  },
+  create: context => {
+    function hasPrecedingComment(node: TSESTree.ClassDeclaration) {
+      return (
+        context.getSourceCode().getCommentsBefore(node)?.length > 0 ||
+        context.getSourceCode().getCommentsBefore(node.decorators?.[0])?.length > 0
+      );
+    }
+    if (!context.getFilename().includes('formly')) {
+      return {};
+    }
+    return {
+      ClassDeclaration(node) {
+        if (isFormlyArtifactClass(node) && !hasPrecedingComment(node)) {
+          context.report({
+            node,
+            messageId: 'missingDocumentationError',
+            data: {
+              artifactName: node.id.name,
+            },
+          });
+        }
+      },
+      'ExportNamedDeclaration Identifier'(node: TSESTree.Identifier) {
+        if (
+          node.typeAnnotation?.typeAnnotation?.type === AST_NODE_TYPES.TSTypeReference &&
+          node.typeAnnotation.typeAnnotation.typeName.type === AST_NODE_TYPES.Identifier &&
+          node.typeAnnotation.typeAnnotation.typeName.name === 'FormlyExtension' &&
+          context
+            .getSourceCode()
+            .getCommentsBefore(getClosestAncestorByKind(context, AST_NODE_TYPES.ExportNamedDeclaration)).length === 0
+        ) {
+          context.report({
+            node,
+            messageId: 'missingDocumentationError',
+            data: {
+              artifactName: node.name,
+            },
+          });
+        }
+      },
+    };
+  },
+};
+
+function isFormlyArtifactClass(node: TSESTree.ClassDeclaration): boolean {
+  return (
+    ['FieldType', 'FieldWrapper'].some(
+      superClass => node.superClass?.type === AST_NODE_TYPES.Identifier && node.superClass?.name === superClass
+    ) ||
+    node.implements?.some(
+      impl => impl.expression.type === AST_NODE_TYPES.Identifier && impl.expression.name === 'FormlyExtension'
+    )
+  );
+}
