9dc33634a9b9abafc2f7442dfa5c1be24c2a1f3f
Danilo Hoffmann <dhhyi@aol.com>
2022-02-21 21:46:51 +0100

    chore: lint-hard with auto restore of configs

--- scripts/eslint-hard.js
+++ scripts/eslint-hard.js
@@ -1,21 +1,75 @@
 const fs = require('fs');
 const { parse, stringify } = require('comment-json');
 const { execSync } = require('child_process');
+const glob = require('glob');
 
-const eslintJson = parse(fs.readFileSync('./.eslintrc.json', { encoding: 'UTF-8' }));
+const savedConfigs = {};
 
-eslintJson.overrides.forEach((overrideObject, index) => {
-  Object.keys(overrideObject.rules)
-    .filter(k => k !== 'jest/no-disabled-tests')
-    .forEach(key => {
-      let ruleInfo = eslintJson.overrides[index].rules[key];
-      if (typeof ruleInfo === 'string' && ruleInfo === 'warn') {
-        eslintJson.overrides[index].rules[key] = 'error';
-      } else if (ruleInfo?.[0] === 'warn') {
-        eslintJson.overrides[index].rules[key][0] = 'error';
-      }
-    });
+function restoreConfigs() {
+  Object.entries(savedConfigs).forEach(([key, config]) => {
+    console.log('restoring', key);
+    fs.writeFileSync(key, config);
+  });
+}
+
+// https://stackoverflow.com/questions/10021373/what-is-the-windows-equivalent-of-process-onsigint-in-node-js/14861513#14861513
+if (process.platform === 'win32') {
+  var rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on('SIGINT', function () {
+    process.emit('SIGINT');
+  });
+}
+
+process.on('SIGINT', function () {
+  restoreConfigs();
+  process.exit(1);
 });
 
-fs.writeFileSync('./.eslintrc.json', stringify(eslintJson, null, 2));
-execSync('npx prettier --write .eslintrc.json');
+glob.sync('**/.eslintrc.json', { ignore: ['node_modules/**'] }).forEach(eslintConfigPath => {
+  const eslintConfig = fs.readFileSync(eslintConfigPath, { encoding: 'UTF-8' });
+
+  const eslintJson = parse(eslintConfig);
+  if (eslintJson.overrides) {
+    console.log('modifying', eslintConfigPath);
+    savedConfigs[eslintConfigPath] = eslintConfig;
+
+    eslintJson.overrides.forEach((overrideObject, index) => {
+      Object.keys(overrideObject.rules)
+        .filter(k => k !== 'jest/no-disabled-tests')
+        .forEach(key => {
+          let ruleInfo = eslintJson.overrides[index].rules[key];
+          if (typeof ruleInfo === 'string' && ruleInfo === 'warn') {
+            eslintJson.overrides[index].rules[key] = 'error';
+          } else if (ruleInfo?.[0] === 'warn') {
+            eslintJson.overrides[index].rules[key][0] = 'error';
+          }
+        });
+    });
+
+    fs.writeFileSync(eslintConfigPath, stringify(eslintJson, null, 2));
+  }
+});
+
+let command = 'npm run lint';
+
+if (process.argv.length > 2) {
+  command = 'npx eslint ' + process.argv.slice(2).join(' ');
+}
+
+let foundError = false;
+
+try {
+  execSync(command, { stdio: 'inherit' });
+} catch (error) {
+  foundError = true;
+} finally {
+  restoreConfigs();
+}
+
+if (foundError) {
+  process.exit(1);
+}
