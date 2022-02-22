8261c87ba82c88a6d472bb115aacc0fa7ca48e4d
max.kless@googlemail.com <max.kless@googlemail.com>
2022-02-22 18:43:31 +0100

    fix: remove windows workaround logic

--- scripts/eslint-hard.js
+++ scripts/eslint-hard.js
@@ -12,18 +12,6 @@ function restoreConfigs() {
   });
 }
 
-// https://stackoverflow.com/questions/10021373/what-is-the-windows-equivalent-of-process-onsigint-in-node-js/14861513#14861513
-if (process.platform === 'win32') {
-  var rl = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  rl.on('SIGINT', function () {
-    process.emit('SIGINT');
-  });
-}
-
 process.on('SIGINT', function () {
   restoreConfigs();
   process.exit(1);
