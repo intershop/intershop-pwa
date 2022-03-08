ecbb5453db0104efacd57d5285a5c7df6581af65
Marcel Eisentraut <meisentraut@intershop.de>
2022-03-08 15:02:56 +0100

    refactor: change import of wrappers.module.ts

--- src/libs/shared/ui/formly/wrappers/wrappers.module.ts
+++ src/libs/shared/ui/formly/wrappers/wrappers.module.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ComponentsModule } from '@intershop-pwa/formly/components/components.module';
 import { FormlyModule as FormlyBaseModule } from '@ngx-formly/core';
 import { TranslateModule } from '@ngx-translate/core';
-
-import { ComponentsModule } from '../components/components.module';
 
 import { DescriptionWrapperComponent } from './description-wrapper/description-wrapper.component';
 import { HorizontalCheckboxWrapperComponent } from './horizontal-checkbox-wrapper/horizontal-checkbox-wrapper.component';
