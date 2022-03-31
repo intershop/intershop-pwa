3bd57e53ac1b8f27cabd271e36e282f8cc846f2f
Harsh Gupta <Harsh.Gupta@rsystems.com>
2022-03-31 17:29:42 +0530

    calling payone rest endpoints

--- src/app/pages/checkout-payment/payment-payone-directdebit-manage-mandate/payment-payone-directdebit-manage-mandate.component.ts
+++ src/app/pages/checkout-payment/payment-payone-directdebit-manage-mandate/payment-payone-directdebit-manage-mandate.component.ts
@@ -1,15 +1,8 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  Input,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subject, takeUntil } from 'rxjs';
 
+import { CheckoutFacade } from 'ish-core/facades/checkout.facade';
 import { Basket } from 'ish-core/models/basket/basket.model';
 import { PaymentInstrument } from 'ish-core/models/payment-instrument/payment-instrument.model';
 import { PaymentMethod } from 'ish-core/models/payment-method/payment-method.model';
@@ -21,20 +14,22 @@ import { ScriptLoaderService } from 'ish-core/utils/script-loader/script-loader.
   templateUrl: './payment-payone-directdebit-manage-mandate.component.html',
   changeDetection: ChangeDetectionStrategy.Default,
 })
-export class PaymentPayoneDirectdebitManageMandateComponent implements OnChanges, OnDestroy, OnInit {
+export class PaymentPayoneDirectdebitManageMandateComponent implements OnDestroy, OnInit {
   @Input() paymentInstrument: PaymentInstrument;
   @Input() paymentMethod: PaymentMethod;
   @Input() basket: Basket;
 
   payoneManageMandateForm: FormGroup;
   mandateText = '';
-  mandateId = '';
-  mandateError = false;
+  isMandateStatusPending = true;
+  paymentId = '';
+  basketId = '';
 
   constructor(
     private apiService: ApiService,
     protected scriptLoader: ScriptLoaderService,
-    protected cd: ChangeDetectorRef
+    protected cd: ChangeDetectorRef,
+    private checkoutFacade: CheckoutFacade
   ) {}
 
   // eslint-disable-next-line ish-custom-rules/private-destroy-field
@@ -44,21 +39,17 @@ export class PaymentPayoneDirectdebitManageMandateComponent implements OnChanges
    * initialize parameter form on init
    */
   ngOnInit() {
+    this.paymentId = this.basket.payment.id;
+    this.basketId = this.basket.id;
     this.payoneManageMandateForm = new FormGroup({});
+
     this.payoneManageMandateForm.addControl(
-      'acceptMandate',
+      `AcceptSEPAMandate_${this.paymentId}`,
       new FormControl('', [Validators.required, Validators.pattern('true')])
     );
-  }
 
-  /**
-   * load payone script if component is shown
-   */
-  ngOnChanges() {
-    if (this.paymentMethod) {
-      this.mandateError = false;
-      this.manageMandate();
-    }
+    // calling method to check mandate status.
+    this.manageMandate();
   }
 
   ngOnDestroy() {
@@ -67,8 +58,8 @@ export class PaymentPayoneDirectdebitManageMandateComponent implements OnChanges
   }
 
   /**
-   * gets a parameter value from payment method
-   * sets the general error message (key) if the parameter is not available
+   * gets a parameter value from payment method.
+   * sets the general error message (key) if the parameter is not available.
    */
   protected getParamValue(name: string, errorMessage: string): string {
     const parameter = this.paymentMethod.hostedPaymentPageParameters?.find(param => param.name === name);
@@ -79,33 +70,73 @@ export class PaymentPayoneDirectdebitManageMandateComponent implements OnChanges
     return parameter.value;
   }
 
+  /**
+   * This method is to call ICM Payone REST endpoint to manage mandate.
+   */
   manageMandate() {
-    // calling Payone client API for manage mandate
-    if (this.paymentInstrument.parameters?.find(attribute => attribute.name === 'IBAN')) {
-      const ibanAttr = this.paymentInstrument.parameters.find(attribute => attribute.name === 'IBAN');
+    // TODO: create url dynamically and replace localhost hard coded. Passing paymentId and BasketId parameters.
+    const mandateURL = `http://localhost:81/INTERSHOP/rest/WFS/inSPIRED-inTRONICS-Site/-/payone/mandate/manage?paymentId=${this.paymentId}&basketId=${this.basketId}`;
 
-      const ibanValue = ibanAttr.value ? ibanAttr.value.toString() : undefined;
-      const paymentId = this.basket.payment.id;
-      const basketId = this.basket.id;
-      if (ibanValue) {
-        const mandateURL = `http://localhost:81/INTERSHOP/rest/WFS/inSPIRED-inTRONICS-Site/-/payone/mandate/manage?paymentId=${paymentId}&basketId=${basketId}`;
+    // TODO: make facade to call api service.
+    this.apiService
+      .get<{ mandateId: string; mandateStatus: string; mandateText: string }>(mandateURL)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((response: { mandateId: string; mandateStatus: string; mandateText: string }) =>
+        this.mandateResponseCallback(response)
+      );
+  }
 
-        this.apiService
-          .get<JSON>(mandateURL)
-          .pipe(takeUntil(this.destroy$))
-          .subscribe((response: any) => this.mandateResponseCallback(response));
-      }
+  /**
+   * This method is to process response from manage mandate rest api.
+   */
+  mandateResponseCallback(response: { mandateId: string; mandateStatus: string; mandateText: string }) {
+    // setting mandate text when mandate status is pending.
+    if (response.mandateStatus === 'pending') {
+      this.isMandateStatusPending = true;
+      this.mandateText = response.mandateText;
+      this.cd.detectChanges();
+    }
+    // showing download link when mandate state is active.
+    if (response.mandateStatus === 'active') {
+      this.isMandateStatusPending = false;
+      this.cd.detectChanges();
     }
   }
 
-  mandateResponseCallback(response: { mandateId: string; mandateStatus: string; mandateText: string }) {
-    console.log(response);
-    this.mandateId = response.mandateId;
-    if (response.mandateStatus === 'pending') {
-      // TODO: set checkbox label as mandateText and show checkbox to accept mandate
-      this.mandateText = response.mandateText;
-      // this.cd.detectChanges();
-      console.log(this.mandateId + decodeURI(this.mandateText));
+  /**
+   * This method is to call ICM Payone REST endpoint for download mandate.
+   * This is called on click of download link.
+   */
+  downloadMandate() {
+    // TODO: create url dynamically and replace localhost hard coded. Passing paymentId and BasketId parameters.
+    const mandateURL = `http://localhost:81/INTERSHOP/rest/WFS/inSPIRED-inTRONICS-Site/-/payone/mandate/download?paymentId=${this.paymentId}&basketId=${this.basketId}`;
+
+    // TODO: make facade to call api service. Also find way to handle pdf format response.
+    this.apiService
+      .get<'arraybuffer'>(mandateURL)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: 'arraybuffer') => {
+        var file = new Blob([data], { type: 'application/pdf' });
+        var fileURL = URL.createObjectURL(file);
+        window.open(fileURL);
+      });
+  }
+
+  /**
+   * this method is to set basket custom attribute on click of accept mandate checkbox.
+   */
+  onCheckChange(event: any) {
+    // this is true when checkbox is checked.
+    if (event.target.checked) {
+      // TODO: checked state of checkbox is lost after page gets refreshed due this call.
+      // set basket custom attribute on check of accept mandate checkbox.
+      this.checkoutFacade.setBasketCustomAttribute({
+        name: `AcceptSEPAMandate_${this.paymentId}`,
+        value: 'on',
+      });
+    } else {
+      // delete basket custom attribute on uncheck of accept mandate checkbox.
+      this.checkoutFacade.deleteBasketCustomAttribute(`AcceptSEPAMandate_${this.paymentId}`);
     }
   }
 }
