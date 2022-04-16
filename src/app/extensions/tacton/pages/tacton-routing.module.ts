5eef7590f815d48b73ef849d75a6ee98005c5eaf
Danilo Hoffmann <dhhyi@aol.com>
2022-04-16 11:24:18 +0200

    fix: add missing feature guards to feature routing modules

--- src/app/extensions/tacton/pages/tacton-routing.module.ts
+++ src/app/extensions/tacton/pages/tacton-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { FeatureToggleGuard } from 'ish-core/feature-toggle.module';
 import { AuthGuard } from 'ish-core/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'configure',
     loadChildren: () => import('./configure/configure-page.module').then(m => m.ConfigurePageModule),
-    canActivate: [AuthGuard],
+    canActivate: [FeatureToggleGuard, AuthGuard],
     data: {
+      feature: 'tacton',
       queryParams: {
         messageKey: 'tacton',
       },
