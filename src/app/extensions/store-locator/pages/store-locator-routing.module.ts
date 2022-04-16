cf572746b1ec5541a23f4a854228fb824c4409c6
Danilo Hoffmann <dhhyi@aol.com>
2022-04-16 11:24:18 +0200

    fix: add missing feature guards to feature routing modules

--- src/app/extensions/store-locator/pages/store-locator-routing.module.ts
+++ src/app/extensions/store-locator/pages/store-locator-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { FeatureToggleGuard } from 'ish-core/feature-toggle.module';
+
 import { STORE_MAP_ICON_CONFIGURATION } from '../services/stores-map/stores-map.service';
 
 const routes: Routes = [
   {
     path: 'store-finder',
     loadChildren: () => import('./store-locator/store-locator-page.module').then(m => m.StoreLocatorPageModule),
+    canActivate: [FeatureToggleGuard],
+    data: { feature: 'storeLocator' },
   },
 ];
 
