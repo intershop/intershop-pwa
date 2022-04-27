780faf7dc6d55ff04fd5ac4fb46012d28d910058
Andreas Fiedler <AFiedler@intershop.com>
2022-04-27 15:07:10 +0200

    Add product information to matomo.

--- src/app/pages/product/product-detail/product-detail.component.ts
+++ src/app/pages/product/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatomoTracker } from '@ngx-matomo/tracker';
+import { Observable, Subject, switchMap, takeUntil } from 'rxjs';
 
 import { ProductContextFacade } from 'ish-core/facades/product-context.facade';
 import { ProductView } from 'ish-core/models/product-view/product-view.model';
@@ -10,10 +11,26 @@ import { ProductView } from 'ish-core/models/product-view/product-view.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductDetailComponent implements OnInit {
+  private destroy = new Subject<void>();
   product$: Observable<ProductView>;
 
-  constructor(private context: ProductContextFacade) {}
+  constructor(private context: ProductContextFacade, private readonly tracker: MatomoTracker) {}
   ngOnInit() {
     this.product$ = this.context.select('product');
+    this.product$
+      .pipe(
+        switchMap(async product => {
+          this.tracker.setEcommerceView(product.sku, product.name, 'hackathon', 0.0);
+          this.tracker.trackPageView();
+        }),
+        takeUntil(this.destroy)
+      )
+      // eslint-disable-next-line rxjs/no-ignored-subscribe
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy.next();
+    this.destroy.complete();
   }
 }
