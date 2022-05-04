feb6be846409d5e159f53affa524d234f47e0ae2
Danilo Hoffmann <dhhyi@aol.com>
2022-05-04 15:13:23 +0200

    chore: pin jest at version 27

--- scripts/upgrade-pwa.js
+++ scripts/upgrade-pwa.js
@@ -6,6 +6,7 @@ const pinned = {
   '@ng-bootstrap/ng-bootstrap': '11', // 12 requires Bootstrap 5
   '@types/node': '16', // LTS
   '@cspell/dict-de-de': '1.1.32', // later versions use the GPL license
+  jest: '27',
 };
 
 // <HELPERS>
