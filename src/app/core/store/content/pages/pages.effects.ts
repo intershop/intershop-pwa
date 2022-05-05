b2de79ab74794718983334cc55bc81270027b3ec
Marcel Eisentraut <meisentraut@intershop.de>
2022-05-05 11:41:18 +0200

    fix: errored content pages should return a page not found page

--- src/app/core/store/content/pages/pages.effects.ts
+++ src/app/core/store/content/pages/pages.effects.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store, select } from '@ngrx/store';
-import { map, mergeMap } from 'rxjs/operators';
+import { from } from 'rxjs';
+import { concatMap, map, mergeMap } from 'rxjs/operators';
 
 import { CMSService } from 'ish-core/services/cms/cms.service';
 import { selectRouteParam } from 'ish-core/store/core/router';
 import { setBreadcrumbData } from 'ish-core/store/core/viewconf';
+import { HttpStatusCodeService } from 'ish-core/utils/http-status-code/http-status-code.service';
 import { mapErrorToAction, mapToPayloadProperty, whenTruthy } from 'ish-core/utils/operators';
 
 import { loadContentPage, loadContentPageFail, loadContentPageSuccess } from './pages.actions';
@@ -13,7 +15,12 @@ import { getBreadcrumbForContentPage } from './pages.selectors';
 
 @Injectable()
 export class PagesEffects {
-  constructor(private actions$: Actions, private store: Store, private cmsService: CMSService) {}
+  constructor(
+    private actions$: Actions,
+    private store: Store,
+    private cmsService: CMSService,
+    private httpStatusCodeService: HttpStatusCodeService
+  ) {}
 
   loadContentPage$ = createEffect(() =>
     this.actions$.pipe(
@@ -25,6 +32,15 @@ export class PagesEffects {
           .pipe(map(loadContentPageSuccess), mapErrorToAction(loadContentPageFail))
       )
     )
+  );
+
+  redirectIfErrorInContentPage$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(loadContentPageFail),
+        concatMap(() => from(this.httpStatusCodeService.setStatus(404)))
+      ),
+    { dispatch: false }
   );
 
   selectedContentPage$ = createEffect(() =>
