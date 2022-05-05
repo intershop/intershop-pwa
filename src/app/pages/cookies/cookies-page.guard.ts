2e8c34e17afb9bf1d8344fbed23eccddeafed0ea
Marcel Eisentraut <meisentraut@intershop.de>
2022-05-05 10:45:52 +0200

    refactor: ssr should return loading spinner on entering the /cookie route

--- src/app/pages/cookies/cookies-page.guard.ts
+++ src/app/pages/cookies/cookies-page.guard.ts
@@ -1,4 +1,4 @@
-import { isPlatformBrowser } from '@angular/common';
+import { isPlatformServer } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { CanActivate, NavigationEnd, Router } from '@angular/router';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
@@ -17,31 +17,32 @@ export class CookiesPageGuard implements CanActivate {
   ) {}
 
   async canActivate() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.currentDialog = this.modalService.open(CookiesModalComponent, {
-        centered: true,
-        size: 'lg',
-        backdrop: 'static',
-      });
+    if (isPlatformServer(this.platformId)) {
+      return this.router.parseUrl('/loading');
+    }
 
-      const cookiesModalComponent = this.currentDialog.componentInstance as CookiesModalComponent;
+    this.currentDialog = this.modalService.open(CookiesModalComponent, {
+      centered: true,
+      size: 'lg',
+      backdrop: 'static',
+    });
 
-      // dialog closed
-      cookiesModalComponent.closeModal.pipe(first()).subscribe(() => {
+    const cookiesModalComponent = this.currentDialog.componentInstance as CookiesModalComponent;
+
+    // dialog closed
+    cookiesModalComponent.closeModal.pipe(first()).subscribe(() => {
+      this.currentDialog.dismiss();
+    });
+
+    // navigated away with link on dialog
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        first()
+      )
+      .subscribe(() => {
         this.currentDialog.dismiss();
       });
-
-      // navigated away with link on dialog
-      this.router.events
-        .pipe(
-          filter(event => event instanceof NavigationEnd),
-          first()
-        )
-        .subscribe(() => {
-          this.currentDialog.dismiss();
-        });
-      return false;
-    }
-    return true;
+    return false;
   }
 }
