7564b01dd5a06acce5e117b90533f82c3019733e
Danilo Hoffmann <dhhyi@aol.com>
2022-05-23 17:52:54 +0200

    perf(test): local jest speedup with custom haste map (#1119)

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+const { pathsToModuleNameMapper } = require('ts-jest');
+
+const tsConfig = require('comment-json').parse(fs.readFileSync('./tsconfig.json', { encoding: 'utf-8' }));
+
 const esModules = ['lodash-es/.*', 'swiper', 'ssr-window', 'dom7', '.*\\.mjs$'];
 
 module.exports = {
@@ -12,15 +18,14 @@ module.exports = {
   roots: ['src', 'projects'],
   setupFilesAfterEnv: ['<rootDir>/src/setupJest.ts'],
   transformIgnorePatterns: [`node_modules/(?!${esModules.join('|')})`],
-  moduleNameMapper: {
-    '^ish-(.*)$': '<rootDir>/src/app/$1',
-    '^organization-management$': '<rootDir>/projects/organization-management/src/app/exports',
-    '^requisition-management$': '<rootDir>/projects/requisition-management/src/app/exports',
-  },
+  moduleNameMapper: pathsToModuleNameMapper(tsConfig.compilerOptions.paths, { prefix: '<rootDir>' }),
   snapshotSerializers: [
     './src/jest-serializer/AngularHTMLSerializer.js',
     './src/jest-serializer/CategoryTreeSerializer.js',
     './src/jest-serializer/NgrxActionSerializer.js',
     './src/jest-serializer/NgrxActionArraySerializer.js',
   ],
+  haste: {
+    hasteMapModulePath: path.join(__dirname, 'jest.config.haste-map.js'),
+  },
 };
