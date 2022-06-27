bad228294bf3288718c3b01be91c14eaaf8e33ce
Stefan Hauke <s.hauke@intershop.de>
2022-06-27 12:58:55 +0200

    switched from 'let' to 'const'

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,12 +2,12 @@ declare module 'express-http-proxy';
 
 declare module 'express-robots-txt';
 
-declare let PRODUCTION_MODE: boolean;
+declare const PRODUCTION_MODE: boolean;
 
-declare let SERVICE_WORKER: boolean;
+declare const SERVICE_WORKER: boolean;
 
-declare let NGRX_RUNTIME_CHECKS: boolean;
+declare const NGRX_RUNTIME_CHECKS: boolean;
 
-declare let PWA_VERSION: string;
+declare const PWA_VERSION: string;
 
-declare let THEME: string;
+declare const THEME: string;
