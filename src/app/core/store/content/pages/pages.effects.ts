401b3939a0f16bed240fca907067e827f4b82bd6
Marcel Eisentraut <meisentraut@intershop.de>
2022-06-29 15:16:26 +0200

    fix: content page not found when reloading a page with an authenticated user (#1198)
    (cherry picked from commit 1e7af60ddbbee59a7836f399eab82338316a4b84)

--- src/app/core/store/content/pages/pages.effects.ts
+++ src/app/core/store/content/pages/pages.effects.ts
@@ -7,8 +7,14 @@ import { concatMap, map, mergeMap } from 'rxjs/operators';
 import { CMSService } from 'ish-core/services/cms/cms.service';
 import { selectRouteParam } from 'ish-core/store/core/router';
 import { setBreadcrumbData } from 'ish-core/store/core/viewconf';
+import { personalizationStatusDetermined } from 'ish-core/store/customer/user';
 import { HttpStatusCodeService } from 'ish-core/utils/http-status-code/http-status-code.service';
-import { mapErrorToAction, mapToPayloadProperty, whenTruthy } from 'ish-core/utils/operators';
+import {
+  mapErrorToAction,
+  mapToPayloadProperty,
+  useCombinedObservableOnAction,
+  whenTruthy,
+} from 'ish-core/utils/operators';
 
 import { loadContentPage, loadContentPageFail, loadContentPageSuccess } from './pages.actions';
 import { getBreadcrumbForContentPage } from './pages.selectors';
@@ -24,7 +30,7 @@ export class PagesEffects {
 
   loadContentPage$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(loadContentPage),
+      useCombinedObservableOnAction(this.actions$.pipe(ofType(loadContentPage)), personalizationStatusDetermined),
       mapToPayloadProperty('contentPageId'),
       mergeMap(contentPageId =>
         this.cmsService
