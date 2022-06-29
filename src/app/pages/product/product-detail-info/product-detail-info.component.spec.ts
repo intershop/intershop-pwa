8a0a265cf01e1d2104aaf72b6df6a3d5304c0621
Silke <s.grueber@intershop.de>
2022-06-29 13:18:29 +0200

    fix: don't display the review tab on product master variation page (#1193)
    (cherry picked from commit c70b0ffa27ac6d065bb681705193784162c5071c)

--- src/app/pages/product/product-detail-info/product-detail-info.component.spec.ts
+++ src/app/pages/product/product-detail-info/product-detail-info.component.spec.ts
@@ -1,7 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { instance, mock } from 'ts-mockito';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
+import { instance, mock, when } from 'ts-mockito';
 
 import { ProductContextFacade } from 'ish-core/facades/product-context.facade';
+import { FeatureToggleModule } from 'ish-core/feature-toggle.module';
+import { ProductView } from 'ish-core/models/product-view/product-view.model';
 
 import { ProductDetailInfoComponent } from './product-detail-info.component';
 
@@ -9,11 +14,17 @@ describe('Product Detail Info Component', () => {
   let component: ProductDetailInfoComponent;
   let fixture: ComponentFixture<ProductDetailInfoComponent>;
   let element: HTMLElement;
+  let context: ProductContextFacade;
 
   beforeEach(async () => {
+    context = mock(ProductContextFacade);
+    when(context.select('product')).thenReturn(of({ sku: '123' } as ProductView));
+    when(context.select('variationCount')).thenReturn(of(0));
+
     await TestBed.configureTestingModule({
+      imports: [FeatureToggleModule.forTesting('rating'), NgbNavModule, TranslateModule.forRoot()],
       declarations: [ProductDetailInfoComponent],
-      providers: [{ provide: ProductContextFacade, useFactory: () => instance(mock(ProductContextFacade)) }],
+      providers: [{ provide: ProductContextFacade, useFactory: () => instance(context) }],
     }).compileComponents();
   });
 
@@ -27,5 +38,20 @@ describe('Product Detail Info Component', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
     expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+  it('should display the product description tab with status active', () => {
+    fixture.detectChanges();
+
+    expect(element.querySelectorAll('ul.nav-tabs li')).toHaveLength(2);
+    expect(element.querySelector('ul.nav-tabs li[data-testing-id=product-description-tab] a.active')).toBeTruthy();
+  });
+
+  it('should not display the product rating tab for variation master', () => {
+    when(context.select('variationCount')).thenReturn(of(2));
+
+    fixture.detectChanges();
+
+    expect(element.querySelectorAll('ul.nav-tabs li')).toHaveLength(1);
   });
 });
--- src/app/pages/product/product-detail-info/product-detail-info.component.ts
+++ src/app/pages/product/product-detail-info/product-detail-info.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 import { ProductContextDisplayProperties, ProductContextFacade } from 'ish-core/facades/product-context.facade';
 import { ProductView } from 'ish-core/models/product-view/product-view.model';
@@ -11,12 +11,14 @@ import { ProductView } from 'ish-core/models/product-view/product-view.model';
 })
 export class ProductDetailInfoComponent implements OnInit {
   product$: Observable<ProductView>;
+  isVariationMaster$: Observable<boolean>;
   active = 'DESCRIPTION';
 
   constructor(private context: ProductContextFacade) {}
 
   ngOnInit() {
     this.product$ = this.context.select('product');
+    this.isVariationMaster$ = this.context.select('variationCount').pipe(map(variationCount => !!variationCount));
   }
 
   configuration$(key: keyof ProductContextDisplayProperties) {
