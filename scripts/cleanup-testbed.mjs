c15f5758ffd3a251431e9713638b507accc51116
max.kless@googlemail.com <max.kless@googlemail.com>
2022-07-14 11:52:50 +0200

    fix: repair cleanup testbed

--- scripts/cleanup-testbed.mjs
+++ scripts/cleanup-testbed.mjs
@@ -1,5 +1,5 @@
 import { execSync, spawnSync } from 'child_process';
-import { existsSync, statSync } from 'fs';
+import { copyFileSync, existsSync, statSync } from 'fs';
 import glob from 'glob';
 import minimatch from 'minimatch';
 import runAll from 'npm-run-all';
@@ -92,13 +92,20 @@ if (args.length === 0) {
   console.log('searching for tests in project');
   files = glob.sync(jestPathPattern, { ignore: ['node_modules/**'] });
 } else if (args.length === 1) {
-  if (!minimatch(normalize(args[0]), jestProjects)) {
+  const normalizedPath = normalize(args[0]);
+  let statRes;
+  try {
+    statRes = statSync(normalizedPath);
+  } catch (err) {
+    console.warn(`File "${normalizedPath}" doesn't exist.`);
+  }
+  if (!minimatch(normalizedPath, jestProjects)) {
     console.log('using', args[0], 'as git-rev');
     files = findTests(spawnSync('git', ['--no-pager', 'diff', args[0], '--name-only']).stdout.toString().split('\n'));
-  } else if (statSync(normalize(args[0])).isDirectory()) {
+  } else if (statRes?.isDirectory()) {
     console.log('using', args[0], 'as folder');
     files = glob.sync(join(args[0], jestPattern));
-  } else if (statSync(normalize(args[0])).isFile()) {
+  } else if (statRes?.isFile()) {
     files = findTests(args);
   } else {
     console.error('cannot interpret argument as git revision, folder or test file');
@@ -184,7 +191,7 @@ if (!files.length) {
 
       // write snapshot file of copy
       if (existsSync(fileSnapshotPath) && !existsSync(copySnapshotPath)) {
-        execSync(`npx jest -i ${copyPath}`, { stdio: 'ignore' });
+        copyFileSync(fileSnapshotPath, copySnapshotPath);
       }
 
       for (const node of copy.forEachDescendantAsArray()) {
