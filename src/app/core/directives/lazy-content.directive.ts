0247b3ec28c1c8e0a941c44a9be53a0f6c9b470c
Marcel Eisentraut <meisentraut@intershop.de>
2022-07-14 15:07:01 +0200

    refactor: define type of TemplateRef

--- src/app/core/directives/lazy-content.directive.ts
+++ src/app/core/directives/lazy-content.directive.ts
@@ -4,5 +4,5 @@ import { Directive, TemplateRef } from '@angular/core';
   selector: '[ishLazyContent]',
 })
 export class LazyContentDirective {
-  constructor(public template: TemplateRef<any>) {}
+  constructor(public template: TemplateRef<unknown>) {}
 }
