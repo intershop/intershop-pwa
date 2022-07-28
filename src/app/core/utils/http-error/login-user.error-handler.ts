881bc2cd0da60489b60e849761969b22f5eed53e
Johannes Metzner <jm@jometzner.de>
2022-07-28 18:11:00 +0200

    fix: missing error when using wrong credentials

--- src/app/core/utils/http-error/login-user.error-handler.ts
+++ src/app/core/utils/http-error/login-user.error-handler.ts
@@ -1,10 +1,9 @@
-import { HttpErrorResponse, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 
 import { USER_REGISTRATION_LOGIN_TYPE } from 'ish-core/configurations/injection-keys';
 import { SpecialHttpErrorHandler } from 'ish-core/interceptors/icm-error-mapper.interceptor';
 import { HttpError } from 'ish-core/models/http-error/http-error.model';
-import { ApiService } from 'ish-core/services/api/api.service';
 
 /* eslint-disable @typescript-eslint/ban-types */
 
@@ -12,12 +11,8 @@ import { ApiService } from 'ish-core/services/api/api.service';
 export class LoginUserErrorHandler implements SpecialHttpErrorHandler {
   constructor(@Inject(USER_REGISTRATION_LOGIN_TYPE) public loginType: string) {}
 
-  test(error: HttpErrorResponse, request: HttpRequest<unknown>): boolean {
-    return (
-      request.headers.has(ApiService.AUTHORIZATION_HEADER_KEY) &&
-      error.status === 401 &&
-      error.url.includes('customers/-')
-    );
+  test(error: HttpErrorResponse): boolean {
+    return error.status === 401 && error.url.includes('token');
   }
   map(): Partial<HttpError> {
     if (this.loginType === 'email') {
