3957b148180b0b813d46b029c0f918a3552919c7
Tristan Boulière <108473857+tbouliere-datasolution@users.noreply.github.com>
2022-08-01 17:30:03 +0200

    feat: translate cms configuration parameter URL based on type (#385, #1223)
    * translate ICM shared file system reference to URL in configuration parameter based on parameter type ("ImageFileRef" and "FileRef") instead of naming convention
    
    Co-authored-by: Johannes Metzner <jm@jometzner.de>

--- src/app/core/models/content-configuration-parameter/content-configuration-parameter.interface.ts
+++ src/app/core/models/content-configuration-parameter/content-configuration-parameter.interface.ts
@@ -1,4 +1,5 @@
 export interface ContentConfigurationParameterData {
   definitionQualifiedName: string;
   value: string | object;
+  type: string;
 }
--- src/app/core/models/content-configuration-parameter/content-configuration-parameter.mapper.ts
+++ src/app/core/models/content-configuration-parameter/content-configuration-parameter.mapper.ts
@@ -24,34 +24,42 @@ export class ContentConfigurationParameterMapper {
 
     if (data) {
       configurationParameters = Object.entries(data)
-        .map(([key, value]) => ({ [key]: value.value }))
+        .map(([key, value]) => ({ [key]: this.postProcessData(value) }))
         .reduce((acc, val) => ({ ...acc, ...val }));
     }
-
-    this.postProcessFileReferences(configurationParameters);
 
     return configurationParameters;
   }
 
+  private resolveStaticURL(value: string): string {
+    if (value.startsWith('http')) {
+      return value;
+    }
+
+    if (!value.includes(':/')) {
+      return value;
+    }
+
+    const split = value.split(':');
+
+    return encodeURI(`${this.staticURL}/${split[0]}/${this.lang}${split[1]}`);
+  }
+
   /**
-   * TODO: make this dependant on the type of the configuration parameter once the CMS REST API provides this information
-   * for now filter all configuration parameters that start with 'Image' or 'Video'
-   * and where the value does not start with 'http' but contains ':/'
-   + if the filter matches convert the CMS REST API value in a full server URL to the configured file
+   * TODO: Make this method use name-based plugin mechanism to delegate post processing of
+   * configuration parameter data to specific handler.
    */
-  postProcessFileReferences(data: ContentConfigurationParameters): ContentConfigurationParameters {
-    Object.keys(data)
-      .filter(
-        key =>
-          (key.startsWith('Image') || key.startsWith('Video')) &&
-          data[key] &&
-          !data[key].toString().startsWith('http') &&
-          data[key].toString().includes(':/')
-      )
-      .forEach(key => {
-        const split = data[key].toString().split(':');
-        data[key] = encodeURI(`${this.staticURL}/${split[0]}/${this.lang}${split[1]}`);
-      });
-    return data;
+  private postProcessData(data: ContentConfigurationParameterData): string | object | number {
+    switch (data.type) {
+      case 'bc_pmc:types.pagelet2-ImageFileRef':
+      case 'bc_pmc:types.pagelet2-FileRef':
+        if (Array.isArray(data.value)) {
+          return data.value.map(x => this.resolveStaticURL(x));
+        } else {
+          return this.resolveStaticURL(data.value.toString());
+        }
+      default:
+        return data.value;
+    }
   }
 }
