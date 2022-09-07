934b7070c4c6d0e972afa0e24cf356e04211b094
Silke <s.grueber@intershop.de>
2022-09-07 10:12:56 +0200

    feat: add specific header component for error pages
    * current difference to the simple header: logo link with reloading link instead of router link

--- src/app/pages/error/error-page.module.ts
+++ src/app/pages/error/error-page.module.ts
@@ -8,7 +8,7 @@ import { ErrorComponent } from './error/error.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 
 const errorPageRoutes: Routes = [
-  { path: '', component: ErrorPageComponent, data: { wrapperClass: 'errorpage', headerType: 'simple' } },
+  { path: '', component: ErrorPageComponent, data: { wrapperClass: 'errorpage', headerType: 'error' } },
 ];
 
 @NgModule({
--- src/app/shell/header/header/header.component.spec.ts
+++ src/app/shell/header/header/header.component.spec.ts
@@ -7,7 +7,9 @@ import { instance, mock, when } from 'ts-mockito';
 import { AppFacade } from 'ish-core/facades/app.facade';
 import { findAllCustomElements } from 'ish-core/utils/dev/html-query-utils';
 import { BackToTopComponent } from 'ish-shell/header/back-to-top/back-to-top.component';
+import { HeaderCheckoutComponent } from 'ish-shell/header/header-checkout/header-checkout.component';
 import { HeaderDefaultComponent } from 'ish-shell/header/header-default/header-default.component';
+import { HeaderErrorComponent } from 'ish-shell/header/header-error/header-error.component';
 import { HeaderSimpleComponent } from 'ish-shell/header/header-simple/header-simple.component';
 
 import { HeaderComponent } from './header.component';
@@ -27,7 +29,9 @@ describe('Header Component', () => {
       declarations: [
         HeaderComponent,
         MockComponent(BackToTopComponent),
+        MockComponent(HeaderCheckoutComponent),
         MockComponent(HeaderDefaultComponent),
+        MockComponent(HeaderErrorComponent),
         MockComponent(HeaderSimpleComponent),
       ],
       providers: [{ provide: AppFacade, useFactory: () => instance(appFacade) }],
@@ -62,6 +66,28 @@ describe('Header Component', () => {
     expect(findAllCustomElements(element)).toMatchInlineSnapshot(`
       Array [
         "ish-header-simple",
+        "ish-back-to-top",
+      ]
+    `);
+  });
+
+  it('should render error header component if set', () => {
+    when(appFacade.headerType$).thenReturn(of('error'));
+    fixture.detectChanges();
+    expect(findAllCustomElements(element)).toMatchInlineSnapshot(`
+      Array [
+        "ish-header-error",
+        "ish-back-to-top",
+      ]
+    `);
+  });
+
+  it('should render checkout header component if set', () => {
+    when(appFacade.headerType$).thenReturn(of('checkout'));
+    fixture.detectChanges();
+    expect(findAllCustomElements(element)).toMatchInlineSnapshot(`
+      Array [
+        "ish-header-checkout",
         "ish-back-to-top",
       ]
     `);
