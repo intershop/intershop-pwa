765effbc0a299529528d333ebec32926bd38ec42
Tristan BOULIERE <tbouliere@datasolution.fr>
2022-09-30 11:52:20 +0000

    feat: add configurable product attributes
    An attempt to make product defaut easily overridable was mande on ticket ##1098
    However, this change
    only affects the product listing.
    It is not possible to easily add an attribute globally on all the
    products of the site (and the api rest on the backoffice side is quite rigid).
    This modification
    introduces a service which allows to solve this problem more easily and also to make this
    configuration parameterizable at the level of an extension by using the \"FeatureToggleService\".
    
    BREAKING CHANGES:
    products-list-attribute.ts no longer provide default export

--- src/app/core/services/products/products-list-attributes.ts
+++
@@ -1,1 +0,0 @@
-export default 'sku,availability,manufacturer,image,minOrderQuantity,maxOrderQuantity,stepOrderQuantity,inStock,promotions,packingUnit,mastered,productMaster,productMasterSKU,roundedAverageRating,retailSet,defaultCategory';
