ff711bae476f6c75c8d2f66ca1a3860969d70f1c
Marcel Eisentraut <meisentraut@intershop.de>
2023-01-09 17:16:34 +0100

    fix: reset correct product context infos when switching prorduct from retail-set to a single product

--- src/app/core/facades/product-context.facade.ts
+++ src/app/core/facades/product-context.facade.ts
@@ -242,11 +242,18 @@ export class ProductContextFacade extends RxState<ProductContext> implements OnD
 
     this.connect(
       'hasQuantityError',
-      this.select('children').pipe(
-        map(children => Object.values(children)),
-        skipWhile(children => !children?.length),
-        map(children => !children.length || children.some(child => child.hasQuantityError)),
-        distinctUntilChanged()
+      this.select('sku').pipe(
+        whenTruthy(),
+        distinctUntilChanged(),
+        switchMap(() =>
+          this.select('children').pipe(
+            map(children => Object.values(children)),
+            debounceTime(300),
+            skipWhile(children => !children?.length),
+            map(children => !children.length || children.some(child => child.hasQuantityError)),
+            distinctUntilChanged()
+          )
+        )
       )
     );
 
@@ -272,20 +279,27 @@ export class ProductContextFacade extends RxState<ProductContext> implements OnD
 
     this.connect(
       'quantity',
-      this.select('children').pipe(
-        map(children => Object.values(children)),
-        skipWhile(children => !children?.length),
-        map(children =>
-          children.reduce(
-            (sum, child) =>
-              sum +
-              (Number.isInteger(child.quantity) && !child.hasQuantityError && !child.hasProductError
-                ? child.quantity
-                : 0),
-            0
+      this.select('sku').pipe(
+        whenTruthy(),
+        distinctUntilChanged(),
+        switchMap(() =>
+          this.select('children').pipe(
+            map(children => Object.values(children)),
+            debounceTime(300),
+            skipWhile(children => !children?.length),
+            map(children =>
+              children.reduce(
+                (sum, child) =>
+                  sum +
+                  (Number.isInteger(child.quantity) && !child.hasQuantityError && !child.hasProductError
+                    ? child.quantity
+                    : 0),
+                0
+              )
+            ),
+            distinctUntilChanged()
           )
-        ),
-        distinctUntilChanged()
+        )
       )
     );
 
@@ -299,11 +313,18 @@ export class ProductContextFacade extends RxState<ProductContext> implements OnD
 
     this.connect(
       'hasProductError',
-      this.select('children').pipe(
-        map(children => Object.values(children)),
-        skipWhile(children => !children?.length),
-        map(children => !children.length || children.some(child => child.hasProductError)),
-        distinctUntilChanged()
+      this.select('sku').pipe(
+        whenTruthy(),
+        distinctUntilChanged(),
+        switchMap(() =>
+          this.select('children').pipe(
+            map(children => Object.values(children)),
+            debounceTime(300),
+            skipWhile(children => !children?.length),
+            map(children => !children.length || children.some(child => child.hasProductError)),
+            distinctUntilChanged()
+          )
+        )
       )
     );
 
