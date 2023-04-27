dd9621ccd410b8f2950fe1471339e1a0252300c7
Stefan Hauke <s.hauke@intershop.de>
2023-04-27 18:02:43 +0200

    fix: adapt 'addGlobalGuard' function to work with functional guards
    * needed to fix Hybrid Approach to successfully register 'hybridRedirectGuard'

--- src/app/core/utils/routing.ts
+++ src/app/core/utils/routing.ts
@@ -1,19 +1,19 @@
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateChildFn, CanActivateFn, Router } from '@angular/router';
 
 export function addGlobalGuard(
   router: Router,
-  guard: CanActivateFn,
+  guard: CanActivateFn | CanActivateChildFn,
   config: { canActivate: boolean; canActivateChild: boolean } = { canActivate: true, canActivateChild: true }
 ) {
   router.config.forEach(route => {
-    if (config.canActivate && guard.prototype.canActivate) {
+    if (config.canActivate) {
       if (route.canActivate) {
         route.canActivate.push(guard);
       } else {
         route.canActivate = [guard];
       }
     }
-    if (config.canActivateChild && guard.prototype.canActivateChild) {
+    if (config.canActivateChild) {
       if (route.canActivateChild) {
         route.canActivateChild.push(guard);
       } else {
