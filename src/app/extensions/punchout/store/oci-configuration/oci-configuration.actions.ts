6c6cecb6382eb4e4129011b20c4c976f041be85e
Dilara Gueler <D.Gueler@intershop.de>
2023-05-09 10:14:25 +0200

    feat: actions are created for the oci configuration

---
+++ src/app/extensions/punchout/store/oci-configuration/oci-configuration.actions.ts
@@ -0,0 +1,23 @@
+import { createAction } from '@ngrx/store';
+
+import { httpError, payload } from 'ish-core/utils/ngrx-creators';
+
+import { OciConfiguration } from '../../models/oci-configuration/oci-configuration.model';
+
+export const loadOciConfigurations = createAction('[Punchout] Load Oci Configurations');
+
+export const loadOciConfigurationsFail = createAction('[Punchout API] Load Oci Configurations Fail', httpError());
+
+export const loadOciConfigurationsSuccess = createAction(
+  '[Punchout API] Load Oci Configurations Success',
+  payload<{ ociConfigurations: OciConfiguration[] }>()
+);
+
+export const updateOciConfiguration = createAction('[Punchout] Update Oci Configurations');
+
+export const updateOciConfigurationFail = createAction('[Punchout API] Update Oci Configurations Fail', httpError());
+
+export const updateOciConfigurationSuccess = createAction(
+  '[Punchout API] Update Oci Configurations Success',
+  payload<{ ociConfiguration: OciConfiguration }>()
+);
