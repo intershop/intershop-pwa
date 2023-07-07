31028be76ed05efd3ea16ea7038cc606e804b323
Marcel Eisentraut <meisentraut@intershop.de>
2023-07-07 16:19:59 +0200

    fix: /localization call should wait until all server configurations are applied (#1457)

--- src/app/core/utils/translate/icm-translate-loader.ts
+++ src/app/core/utils/translate/icm-translate-loader.ts
@@ -1,9 +1,11 @@
 import { Inject, Injectable, InjectionToken } from '@angular/core';
 import { TransferState, makeStateKey } from '@angular/platform-browser';
+import { Actions, ofType } from '@ngrx/effects';
+import { routerNavigationAction } from '@ngrx/router-store';
 import { TranslateLoader } from '@ngx-translate/core';
 import { memoize } from 'lodash-es';
 import { combineLatest, defer, from, iif, of } from 'rxjs';
-import { catchError, map, shareReplay, tap } from 'rxjs/operators';
+import { catchError, first, map, shareReplay, switchMap, tap } from 'rxjs/operators';
 
 import { LocalizationsService } from 'ish-core/services/localizations/localizations.service';
 import { InjectSingle } from 'ish-core/utils/injection';
@@ -20,6 +22,7 @@ export const LOCAL_TRANSLATIONS = new InjectionToken<LocalTranslations>('transla
 @Injectable()
 export class ICMTranslateLoader implements TranslateLoader {
   constructor(
+    private actions$: Actions,
     private transferState: TransferState,
     private localizations: LocalizationsService,
     @Inject(LOCAL_TRANSLATIONS) private localTranslations: InjectSingle<typeof LOCAL_TRANSLATIONS>
@@ -33,10 +36,17 @@ export class ICMTranslateLoader implements TranslateLoader {
     const server$ = iif(
       () => !SSR && this.transferState.hasKey(SSR_TRANSLATIONS),
       of(this.transferState.get(SSR_TRANSLATIONS, {})),
-      this.localizations.getServerTranslations(lang).pipe(
-        tap(data => {
-          this.transferState.set(SSR_TRANSLATIONS, data);
-        })
+      // the localization call should wait until all server supplied configuration parameter are applied to the state
+      this.actions$.pipe(
+        ofType(routerNavigationAction),
+        first(),
+        switchMap(() =>
+          this.localizations.getServerTranslations(lang).pipe(
+            tap(data => {
+              this.transferState.set(SSR_TRANSLATIONS, data);
+            })
+          )
+        )
       )
     );
     return combineLatest([local$, server$]).pipe(
