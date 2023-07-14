09538094f02448b720b97dfe0948a9005a828685
Andreas Steinmann <asteinmann@intershop.com>
2023-07-14 12:44:06 +0200

    refactor: use regexp to remove the trailing slash

--- src/app/core/services/designview/designview.service.ts
+++ src/app/core/services/designview/designview.service.ts
@@ -14,9 +14,7 @@ interface StorefrontEditingMessage {
 
 @Injectable({ providedIn: 'root' })
 export class DesignviewService {
-  private iapBaseURL = environment.iapBaseURL?.endsWith('/')
-    ? environment.iapBaseURL.substring(0, environment.iapBaseURL.length - 1)
-    : environment.iapBaseURL;
+  private iapBaseURL = environment.iapBaseURL?.replace(/\/$/, '');
   private allowedHostMessageTypes = ['dv-clientRefresh'];
   private hostMessagesSubject$ = new Subject<StorefrontEditingMessage>();
 
