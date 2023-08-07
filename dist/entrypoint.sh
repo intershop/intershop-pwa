743f6eb721e46a9ff2817b756d5c7a437b00d2dd
Danilo Hoffmann <dhhyi@aol.com>
2023-08-07 10:36:33 +0200

    fix: properly handle system signals in pwa docker container

--- dist/entrypoint.sh
+++ dist/entrypoint.sh
@@ -2,18 +2,14 @@
 
 set -e
 
-trap 'echo recieved INT; exit 1' SIGINT
-trap 'echo recieved TERM; exit 0' SIGTERM
-trap 'echo recieved KILL; exit 1' SIGKILL
-
 if [ -z "$*" ]
 then
-  # use 'node dist/<theme>/run-standalone'
+  # use 'exec node dist/<theme>/run-standalone'
   # instead of pm2 to fallback to running
   # a single theme only
 
   node dist/build-ecosystem.js
-  pm2-runtime dist/ecosystem.yml
+  exec pm2-runtime dist/ecosystem.yml
 else
   exec "$@"
 fi
