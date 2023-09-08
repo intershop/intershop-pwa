b7e4bcc30c04ac522ca1944ad502d52e5d3efef6
LucasHengelhaupt <l.hengelhaupt@intershop.com>
2023-09-08 14:02:50 +0200

    reverted refactor

--- src/app/core/store/customer/basket/basket.effects.ts
+++ src/app/core/store/customer/basket/basket.effects.ts
@@ -79,21 +79,19 @@ export class BasketEffects {
   /**
    * The load basket effect.
    */
-  loadBasket$ =
-    !SSR &&
-    createEffect(() =>
-      this.actions$.pipe(
-        ofType(loadBasket),
-        mergeMap(() =>
-          window.sessionStorage.getItem('basket-id')
-            ? of(loadBasketWithId({ basketId: window.sessionStorage.getItem('basket-id') }))
-            : this.basketService.getBasket().pipe(
-                map(basket => loadBasketSuccess({ basket })),
-                mapErrorToAction(loadBasketFail)
-              )
-        )
+  loadBasket$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadBasket),
+      mergeMap(() =>
+        !SSR && window.sessionStorage.getItem('basket-id')
+          ? of(loadBasketWithId({ basketId: window.sessionStorage.getItem('basket-id') }))
+          : this.basketService.getBasket().pipe(
+              map(basket => loadBasketSuccess({ basket })),
+              mapErrorToAction(loadBasketFail)
+            )
       )
-    );
+    )
+  );
 
   /**
    * Loads a basket with the given id.
