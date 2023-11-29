d18cde7d8e93c0cbe868bd85f73399de81174969
Danilo Hoffmann <dhhyi@aol.com>
2023-11-29 16:00:04 +0100

    feat: use server setting for message-to-merchant checkout field (#1542)
    BREAKING CHANGES: Removed `messageToMerchant` feature toggle since the feature is controlled via back office configuration in ICM 11 (see [Migrations / From 5.0 to 5.1](https://github.com/intershop/intershop-pwa/blob/develop/docs/guides/migrations.md#from-50-to-51) for more details and ICM 7.10 compatibility).

--- src/environments/environment.b2b.ts
+++ src/environments/environment.b2b.ts
@@ -13,7 +13,6 @@ export const environment: Environment = {
     'businessCustomerRegistration',
     'costCenters',
     'maps',
-    'messageToMerchant',
     'punchout',
     'quickorder',
     'quoting',
