90222bf7822b58bab7efd1a8d851e1e9b8d7a6f1
Danilo Hoffmann <dhhyi@aol.com>
2023-12-06 16:13:28 +0100

    refactor: type-safe omit helper

--- src/app/core/utils/functions.ts
+++ src/app/core/utils/functions.ts
@@ -49,11 +49,14 @@ export function mergeDeep(target: any, source: any): any {
 /**
  * Returns a copy of the object composed of the own and inherited enumerable property paths of the object that are not omitted.
  */
-export function omit<T>(from: T, ...keys: (keyof T | string)[]) {
+export function omit<T extends object, K extends (string | number | symbol)[]>(
+  from: T,
+  ...keys: K
+): Omit<T, K[number]> {
   return !!from && !Array.isArray(from)
-    ? Object.entries(from)
+    ? (Object.entries(from)
         .filter(([key]) => !keys.includes(key))
-        .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {})
+        .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {}) as T)
     : from;
 }
 
