c3e24e10da6f64814ce04787d57b9dc7bfd946c1
Danilo Hoffmann <dhhyi@aol.com>
2023-12-13 15:08:48 +0100

    feat: display and edit custom fields on basket

--- src/app/core/models/line-item-update/line-item-update.model.ts
+++ src/app/core/models/line-item-update/line-item-update.model.ts
@@ -1,6 +1,9 @@
+import { CustomFields } from 'ish-core/models/custom-field/custom-field.model';
+
 export interface LineItemUpdate {
   itemId: string;
   quantity?: number;
   sku?: string;
   unit?: string;
+  customFields?: CustomFields;
 }
--- src/app/shared/components/line-item/line-item-edit/line-item-edit.component.ts
+++ src/app/shared/components/line-item/line-item-edit/line-item-edit.component.ts
@@ -1,10 +1,33 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  Self,
+  ViewChild,
+} from '@angular/core';
+import { UntypedFormGroup } from '@angular/forms';
+import { RxState } from '@rx-angular/state';
+import { combineLatest, take } from 'rxjs';
 
+import { AppFacade } from 'ish-core/facades/app.facade';
 import { ProductContextFacade } from 'ish-core/facades/product-context.facade';
+import { CustomFieldsComponentInput } from 'ish-core/models/custom-field/custom-field.model';
 import { LineItemUpdate } from 'ish-core/models/line-item-update/line-item-update.model';
-import { ProductView } from 'ish-core/models/product-view/product-view.model';
+import { LineItemView } from 'ish-core/models/line-item/line-item.model';
+import { ProductHelper } from 'ish-core/models/product/product.model';
 import { ModalDialogComponent } from 'ish-shared/components/common/modal-dialog/modal-dialog.component';
+
+interface ComponentState {
+  loading: boolean;
+  visible: boolean;
+  variationEditable: boolean;
+  lineItem: Partial<Pick<LineItemView, 'id' | 'quantity' | 'productSKU' | 'customFields' | 'singleBasePrice'>>;
+  customFields: CustomFieldsComponentInput[];
+}
 
 /**
  * The Line Item Edit Component displays an edit-link and edit-dialog.
@@ -13,34 +36,81 @@ import { ModalDialogComponent } from 'ish-shared/components/common/modal-dialog/
   selector: 'ish-line-item-edit',
   templateUrl: './line-item-edit.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [ProductContextFacade],
 })
-export class LineItemEditComponent implements OnInit {
+export class LineItemEditComponent extends RxState<ComponentState> implements OnInit, AfterViewInit {
   @ViewChild('modalDialog') modalDialogRef: ModalDialogComponent<unknown>;
 
-  @Input({ required: true }) itemId: string;
+  @Input({ required: true }) set lineItem(lineItem: ComponentState['lineItem']) {
+    this.set({ lineItem });
+    this.resetContext();
+  }
+
   @Output() updateItem = new EventEmitter<LineItemUpdate>();
 
-  product$: Observable<ProductView>;
+  customFieldsForm = new UntypedFormGroup({});
 
-  constructor(private context: ProductContextFacade) {}
+  constructor(@Self() private context: ProductContextFacade, private appFacade: AppFacade) {
+    super();
+  }
 
   ngOnInit() {
-    this.product$ = this.context.select('product');
+    this.connect(
+      'variationEditable',
+      combineLatest([
+        this.appFacade.serverSetting$<boolean>('preferences.ChannelPreferences.EnableAdvancedVariationHandling'),
+        this.context.select('product'),
+      ]),
+      (_, [advancedVariationHandlingEnabled, product]) =>
+        ProductHelper.isVariationProduct(product) && !advancedVariationHandlingEnabled
+    );
+
+    this.connect(
+      'visible',
+      combineLatest([this.select('variationEditable'), this.appFacade.customFieldsForScope$('BasketLineItem')]),
+      (_, [variationEditable, customFields]) => variationEditable || customFields.length > 0
+    );
+
+    this.connect('loading', this.context.select('loading'));
+
+    this.connect(
+      'customFields',
+      combineLatest([this.appFacade.customFieldsForScope$('BasketLineItem'), this.select('lineItem', 'customFields')]),
+      (_, [customFields, customFieldsData]) =>
+        customFields.map(field => ({
+          name: field.name,
+          editable: field.editable,
+          value: customFieldsData[field.name],
+        }))
+    );
+  }
+
+  ngAfterViewInit(): void {
+    this.context.hold(this.context.select('product'), product => {
+      this.modalDialogRef.options.confirmDisabled = !product.available;
+      this.modalDialogRef.options.titleText = product.name;
+    });
+  }
+
+  private resetContext() {
+    const lineItem = this.get('lineItem');
+    this.context.set({ quantity: lineItem.quantity.value, sku: lineItem.productSKU });
   }
 
   show() {
-    this.modalDialogRef.show();
+    this.resetContext();
 
-    this.context.hold(this.context.select('product'), product => {
-      this.modalDialogRef.options.confirmDisabled = !product.available;
-    });
+    this.context.hold(this.modalDialogRef.confirmed.pipe(take(1)), () => {
+      const customFields = this.customFieldsForm.value;
 
-    this.context.hold(this.modalDialogRef.confirmed, () =>
       this.updateItem.emit({
-        itemId: this.itemId,
+        itemId: this.get('lineItem').id,
         sku: this.context.get('sku'),
         quantity: this.context.get('quantity'),
-      })
-    );
+        customFields: Object.keys(customFields).length > 0 ? customFields : undefined,
+      });
+    });
+
+    this.modalDialogRef.show();
   }
 }
