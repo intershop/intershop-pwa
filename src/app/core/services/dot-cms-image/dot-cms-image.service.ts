3cb115c88a6154e450691ad2e5b1d60b6b333fd6
Dilara Gueler <D.Gueler@intershop.de>
2024-01-02 17:15:42 +0100

    chore: apiService used instead of http

--- src/app/core/services/dot-cms-image/dot-cms-image.service.ts
+++ src/app/core/services/dot-cms-image/dot-cms-image.service.ts
@@ -1,21 +1,21 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 
 import { DotCmsImageData } from 'ish-core/models/dot-cms-image/dot-cms-image.interface';
 import { DotCmsImageMapper } from 'ish-core/models/dot-cms-image/dot-cms-image.mapper';
 import { DotCmsImage } from 'ish-core/models/dot-cms-image/dot-cms-image.model';
+import { ApiService } from 'ish-core/services/api/api.service';
 
 @Injectable({ providedIn: 'root' })
 export class DotCmsImageService {
   private apiUrl =
-    'https://local.dotcms.site:8443/api/content/render/false/query/+contentType:dotAsset%20+(conhost:48190c8c-42c4-46af-8d1a-0cd5db894797%20conhost:SYSTEM_HOST)%20+languageId:1%20+deleted:false%20+working:true%20+variant:default/orderby/modDate%20desc';
+    'https://local.dotcms.site:8443/api/content/render/false/query/+contentType:dotAsset%20+(conhost:48190c8c-42c4-46af-8d1a-0cd5db894797%20conhost:SYSTEM_HOST)%20+languageId:1%20+deleted:false%20+working:true%20+variant:default/orderby/modDate%20desc/api/images';
 
-  constructor(private imageMapper: DotCmsImageMapper, private http: HttpClient) {}
+  constructor(private apiService: ApiService, private imageMapper: DotCmsImageMapper) {}
 
   getDotCmsImage(): Observable<DotCmsImage[]> {
-    return this.http
-      .get<{ contentlets: DotCmsImageData[] }>(`${this.apiUrl}/api/images`)
+    return this.apiService
+      .get<{ contentlets: DotCmsImageData[] }>(`${this.apiUrl}`)
       .pipe(map(response => response.contentlets.map(image => this.imageMapper.fromData(image))));
   }
 }
