c99e1d0666ff5dda455391ba684f14e18a2d8a11
Stefan Hauke <s.hauke@intershop.de>
2024-01-16 11:32:15 +0100

    feat: introduce `htmlEncode` pipe to encode HTML code to be rendered as string (#1575)
    * add documentation note

--- src/app/core/pipes.module.ts
+++ src/app/core/pipes.module.ts
@@ -5,6 +5,7 @@ import { PricePipe } from './models/price/price.pipe';
 import { DatePipe } from './pipes/date.pipe';
 import { FeatureTogglePipe } from './pipes/feature-toggle.pipe';
 import { HighlightPipe } from './pipes/highlight.pipe';
+import { HtmlEncodePipe } from './pipes/html-encode.pipe';
 import { MakeHrefPipe } from './pipes/make-href.pipe';
 import { SanitizePipe } from './pipes/sanitize.pipe';
 import { ServerSettingPipe } from './pipes/server-setting.pipe';
@@ -19,6 +20,7 @@ const pipes = [
   DatePipe,
   FeatureTogglePipe,
   HighlightPipe,
+  HtmlEncodePipe,
   MakeHrefPipe,
   PricePipe,
   ProductRoutePipe,
