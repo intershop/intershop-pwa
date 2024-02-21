8088584340ef7308eaf45224c853affaee59472d
Silke <s.grueber@intershop.de>
2024-02-21 09:11:14 +0100

    fixup! fix: load only the newsletter if this feature is configured

--- src/app/pages/account-profile/fetch-user-newsletter.guard.ts
+++ src/app/pages/account-profile/fetch-user-newsletter.guard.ts
@@ -1,5 +1,5 @@
 import { inject } from '@angular/core';
-import { Observable, map, of } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 import { AccountFacade } from 'ish-core/facades/account.facade';
 
@@ -10,6 +10,4 @@ export function fetchUserNewsletterGuard(): boolean | Observable<boolean> {
   const accountFacade = inject(AccountFacade);
 
   return accountFacade.loadNewsletterSubscription().pipe(map(() => true));
-
-  return of(true);
 }
