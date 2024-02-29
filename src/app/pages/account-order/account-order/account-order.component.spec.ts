d1fb5685ed70e599b7e6ff5eea6854757db2327c
LucasHengelhaupt <l.hengelhaupt@intershop.com>
2024-02-29 16:31:01 +0100

    test: fixed jest

--- src/app/pages/account-order/account-order/account-order.component.spec.ts
+++ src/app/pages/account-order/account-order/account-order.component.spec.ts
@@ -2,7 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { TranslateModule } from '@ngx-translate/core';
 import { MockComponent, MockPipe } from 'ng-mocks';
+import { of } from 'rxjs';
+import { instance, mock, when } from 'ts-mockito';
 
+import { CheckoutFacade } from 'ish-core/facades/checkout.facade';
+import { ShoppingFacade } from 'ish-core/facades/shopping.facade';
 import { FeatureToggleModule } from 'ish-core/feature-toggle.module';
 import { DatePipe } from 'ish-core/pipes/date.pipe';
 import { ServerSettingPipe } from 'ish-core/pipes/server-setting.pipe';
@@ -22,6 +26,9 @@ describe('Account Order Component', () => {
   let element: HTMLElement;
 
   beforeEach(async () => {
+    const checkoutFacadeMock = mock(CheckoutFacade);
+    when(checkoutFacadeMock.basketLoading$).thenReturn(of(false));
+
     await TestBed.configureTestingModule({
       declarations: [
         AccountOrderComponent,
@@ -36,6 +43,10 @@ describe('Account Order Component', () => {
         MockPipe(ServerSettingPipe, path => path === 'shipping.messageToMerchant'),
       ],
       imports: [FeatureToggleModule.forTesting(), TranslateModule.forRoot()],
+      providers: [
+        { provide: CheckoutFacade, useFactory: () => instance(checkoutFacadeMock) },
+        { provide: ShoppingFacade, useFactory: () => instance(mock(ShoppingFacade)) },
+      ],
     }).compileComponents();
   });
 
@@ -75,5 +86,10 @@ describe('Account Order Component', () => {
   it('should display the order list link after creation', () => {
     fixture.detectChanges();
     expect(element.querySelector('[data-testing-id="orders-link"]')).toBeTruthy();
+  });
+
+  it('should display the create cart from order button after creation', () => {
+    fixture.detectChanges();
+    expect(element.querySelector('[data-testing-id="create-cart-from-order"]')).toBeTruthy();
   });
 });
