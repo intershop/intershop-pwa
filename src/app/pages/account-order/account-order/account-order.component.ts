f80d0c0cd5027ea482f76a8b4ae9000e2d8369be
LucasHengelhaupt <l.hengelhaupt@intershop.com>
2024-02-29 16:30:48 +0100

    feat: added loading spinner

--- src/app/pages/account-order/account-order/account-order.component.ts
+++ src/app/pages/account-order/account-order/account-order.component.ts
@@ -1,7 +1,11 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, Input, OnInit, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { BehaviorSubject, Observable, combineLatest, map, startWith } from 'rxjs';
 
+import { CheckoutFacade } from 'ish-core/facades/checkout.facade';
 import { ShoppingFacade } from 'ish-core/facades/shopping.facade';
 import { Order } from 'ish-core/models/order/order.model';
+import { whenFalsy } from 'ish-core/utils/operators';
 
 /**
  * The Order Page Component displays the details of an order.
@@ -14,14 +18,39 @@ import { Order } from 'ish-core/models/order/order.model';
   templateUrl: './account-order.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AccountOrderComponent {
+export class AccountOrderComponent implements OnInit {
   @Input({ required: true }) order: Order;
 
-  constructor(private shoppingFacade: ShoppingFacade) {}
+  basketLoading$: Observable<boolean>;
+
+  buttonDisabled$: Observable<boolean>;
+
+  constructor(private checkoutFacade: CheckoutFacade, private shoppingFacade: ShoppingFacade) {}
+
+  // fires 'true' after add To Cart is clicked and basket is loading
+  displaySpinner$ = new BehaviorSubject(false);
+
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.basketLoading$ = this.checkoutFacade.basketLoading$;
+
+    // update emitted to display spinning animation
+    this.basketLoading$.pipe(whenFalsy(), takeUntilDestroyed(this.destroyRef)).subscribe(this.displaySpinner$); // false
+
+    const loading$ = this.displaySpinner$.pipe(startWith(false));
+
+    // disable button when spinning, in case of an error or basket loading
+    this.buttonDisabled$ = combineLatest([loading$, this.basketLoading$]).pipe(
+      map(conditions => conditions.some(c => c))
+    );
+  }
 
   addToBasket() {
     this.order.lineItems.forEach(lineItem => {
       this.shoppingFacade.addProductToBasket(lineItem.productSKU, lineItem.quantity.value);
     });
+
+    this.displaySpinner$.next(true);
   }
 }
