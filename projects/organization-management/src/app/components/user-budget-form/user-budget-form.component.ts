5fdc907c77dfa181c539490c0ed79eb03464028a
Silke <s.grueber@intershop.de>
2024-04-15 12:49:13 +0200

    fix: enable localized number inputs for budgets

--- projects/organization-management/src/app/components/user-budget-form/user-budget-form.component.ts
+++ projects/organization-management/src/app/components/user-budget-form/user-budget-form.component.ts
@@ -5,7 +5,6 @@ import { FormlyFieldConfig } from '@ngx-formly/core';
 
 import { AppFacade } from 'ish-core/facades/app.facade';
 import { whenTruthy } from 'ish-core/utils/operators';
-import { SpecialValidators } from 'ish-shared/forms/validators/special-validators';
 
 import { UserBudget } from '../../models/user-budget/user-budget.model';
 
@@ -88,14 +87,7 @@ export class UserBudgetFormComponent implements OnInit {
               addonLeft: {
                 text: this.appFacade.currencySymbol$(this.model.currency),
               },
-            },
-            validators: {
-              validation: [SpecialValidators.moneyAmount],
-            },
-            validation: {
-              messages: {
-                moneyAmount: 'account.user.new.OrderLimit.error.valid',
-              },
+              mask: 'separator.2',
             },
           },
           {
@@ -114,14 +106,7 @@ export class UserBudgetFormComponent implements OnInit {
                   addonLeft: {
                     text: this.appFacade.currencySymbol$(this.model.currency),
                   },
-                },
-                validators: {
-                  validation: [SpecialValidators.moneyAmount],
-                },
-                validation: {
-                  messages: {
-                    moneyAmount: 'account.user.new.Budget.error.valid',
-                  },
+                  mask: 'separator.2',
                 },
               },
               {
