ef76a619fd27b1a63da22e8b4a8b187aad2e94ac
Danilo Hoffmann <dhhyi@aol.com>
2024-05-30 08:26:19 +0200

    refactor: rework account-profile-user component

--- src/app/pages/account-profile-user/account-profile-user/account-profile-user.component.ts
+++ src/app/pages/account-profile-user/account-profile-user/account-profile-user.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig } from '@ngx-formly/core';
+import { RxState } from '@rx-angular/state';
 import { pick } from 'lodash-es';
-import { Observable, concatMap, map, of, withLatestFrom } from 'rxjs';
+import { concatMap, map, of, withLatestFrom } from 'rxjs';
 
 import { AccountFacade } from 'ish-core/facades/account.facade';
 import { AppFacade } from 'ish-core/facades/app.facade';
@@ -10,6 +11,12 @@ import { HttpError } from 'ish-core/models/http-error/http-error.model';
 import { User } from 'ish-core/models/user/user.model';
 import { FieldLibrary } from 'ish-shared/formly/field-library/field-library';
 import { markAsDirtyRecursive } from 'ish-shared/forms/utils/form-utils';
+
+interface ComponentState {
+  currentUser: User;
+  model: Partial<User>;
+  fields: FormlyFieldConfig[];
+}
 
 /**
  * The Account Profile User Page Component displays a form for changing the user's profile data
@@ -20,8 +27,10 @@ import { markAsDirtyRecursive } from 'ish-shared/forms/utils/form-utils';
   templateUrl: './account-profile-user.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AccountProfileUserComponent implements OnInit {
-  @Input({ required: true }) currentUser$: Observable<User>;
+export class AccountProfileUserComponent extends RxState<ComponentState> implements OnInit {
+  @Input({ required: true }) set currentUser(val: User) {
+    this.set('currentUser', () => val);
+  }
   @Input() error: HttpError;
 
   @Output() updateUserProfile = new EventEmitter<User>();
@@ -29,35 +38,41 @@ export class AccountProfileUserComponent implements OnInit {
   private submitted = false;
 
   accountProfileUserForm = new FormGroup({});
-  model$: Observable<Partial<User>>;
-  fields$: Observable<FormlyFieldConfig[]>;
 
-  constructor(private fieldLibrary: FieldLibrary, private accountFacade: AccountFacade, private appFacade: AppFacade) {}
+  constructor(private fieldLibrary: FieldLibrary, private accountFacade: AccountFacade, private appFacade: AppFacade) {
+    super();
+  }
 
   ngOnInit() {
     // only retrieves and sets the newsletter-subscription-status in the model if the server-setting for it is enabled
-    this.model$ = this.currentUser$.pipe(
-      map(user => pick(user, 'title', 'firstName', 'lastName', 'phoneHome')),
-      withLatestFrom(this.appFacade.serverSetting$<boolean>('marketing.newsletterSubscriptionEnabled')),
-      concatMap(([userAttributes, newsletterSubscriptionEnabled]) =>
-        this.accountFacade.subscribedToNewsletter$.pipe(
-          map(subscribedToNewsletter =>
-            newsletterSubscriptionEnabled
-              ? {
-                  ...userAttributes,
-                  newsletter: subscribedToNewsletter,
-                }
-              : userAttributes
+    this.connect(
+      'model',
+      this.select('currentUser').pipe(
+        map(user => pick(user, 'title', 'firstName', 'lastName', 'phoneHome')),
+        withLatestFrom(this.appFacade.serverSetting$<boolean>('marketing.newsletterSubscriptionEnabled')),
+        concatMap(([userAttributes, newsletterSubscriptionEnabled]) =>
+          this.accountFacade.subscribedToNewsletter$.pipe(
+            map(subscribedToNewsletter =>
+              newsletterSubscriptionEnabled
+                ? {
+                    ...userAttributes,
+                    newsletter: subscribedToNewsletter,
+                  }
+                : userAttributes
+            )
           )
         )
       )
     );
 
     // only displays the newsletter-subscription-field if the server-setting for the newsletter is enabled
-    this.fields$ = of(this.fieldLibrary.getConfigurationGroup('personalInfo')).pipe(
-      withLatestFrom(this.appFacade.serverSetting$<boolean>('marketing.newsletterSubscriptionEnabled')),
-      map(([fields, newsletterSubscriptionEnabled]) =>
-        newsletterSubscriptionEnabled ? [...fields, this.newsletterCheckboxField()] : fields
+    this.connect(
+      'fields',
+      of(this.fieldLibrary.getConfigurationGroup('personalInfo')).pipe(
+        withLatestFrom(this.appFacade.serverSetting$<boolean>('marketing.newsletterSubscriptionEnabled')),
+        map(([fields, newsletterSubscriptionEnabled]) =>
+          newsletterSubscriptionEnabled ? [...fields, this.newsletterCheckboxField()] : fields
+        )
       )
     );
   }
@@ -75,7 +90,7 @@ export class AccountProfileUserComponent implements OnInit {
   /**
    * Submits form and throws update event when form is valid
    */
-  submitForm(currentUser: User) {
+  submitForm() {
     if (this.accountProfileUserForm.invalid) {
       this.submitted = true;
       markAsDirtyRecursive(this.accountProfileUserForm);
@@ -87,7 +102,7 @@ export class AccountProfileUserComponent implements OnInit {
       this.accountFacade.updateNewsletterSubscription(subscribedToNewsletter);
     }
 
-    this.updateUserProfile.emit({ ...currentUser, ...this.accountProfileUserForm.value });
+    this.updateUserProfile.emit({ ...this.get('currentUser'), ...this.accountProfileUserForm.value });
   }
 
   get buttonDisabled() {
