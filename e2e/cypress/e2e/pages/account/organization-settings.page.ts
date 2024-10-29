8ac1bfbc9479645047fbba3becc8b7976aff5abf
skoch <S.Koch@intershop.de>
2024-10-29 07:39:25 +0100

    feat: fix cypress failure (#99165)

--- e2e/cypress/e2e/pages/account/organization-settings.page.ts
+++ e2e/cypress/e2e/pages/account/organization-settings.page.ts
@@ -4,4 +4,16 @@ export class OrganizationSettingsPage {
   readonly tag = 'ish-organization-settings-page';
 
   readonly header = new HeaderModule();
+
+  editCompanyDetails() {
+    cy.get('[data-testing-id="edit-company"]').click();
+  }
+
+  get companyName() {
+    return cy.get(this.tag).find('[data-testing-id="company-field"]');
+  }
+
+  get taxationId() {
+    return cy.get(this.tag).find('[data-testing-id="taxation-id-field"]');
+  }
 }
