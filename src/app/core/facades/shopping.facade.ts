9859e4dd40432417409fc1ed2f77d30104073ebb
Andreas Steinmann <asteinmann@intershop.com>
2025-02-24 11:53:46 +0100

    refactor: always check for changes in input field when searching

--- src/app/core/facades/shopping.facade.ts
+++ src/app/core/facades/shopping.facade.ts
@@ -258,7 +258,6 @@ export class ShoppingFacade {
     return searchTerm.pipe(
       debounceTime(400),
       filter(term => term.length > 2),
-      distinctUntilChanged(),
       tap(term => this.store.dispatch(suggestSearch({ searchTerm: term }))),
       switchMap(() => this.store.pipe(select(getSuggestSearchResults)))
     );
