073055729c995e45cc54705c2700a426d3ab4391
Stefan Hauke <shauke@intershop.de>
2025-03-18 09:11:45 +0100

    docs: update Hybrid Approach documentation for ICM 11+ and PWA 5.x

--- src/environments/environment.model.ts
+++ src/environments/environment.model.ts
@@ -157,6 +157,7 @@ export const ENVIRONMENT_DEFAULTS: Omit<Environment, 'icmChannel'> = {
   icmServerStatic: 'INTERSHOP/static/WFS',
   icmServerWeb: 'INTERSHOP/web/WFS',
   icmApplication: '-',
+  hybridApplication: '-',
 
   /* FEATURE TOGGLES */
   features: [
