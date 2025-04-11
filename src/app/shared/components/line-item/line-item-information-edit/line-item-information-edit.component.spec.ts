e88eeb7e8954584fd0d934a3449f7d2d989a4f39
Silke <s.grueber@intershop.de>
2025-04-11 13:26:45 +0200

    fix: issues after rebasing

--- src/app/shared/components/line-item/line-item-information-edit/line-item-information-edit.component.spec.ts
+++ src/app/shared/components/line-item/line-item-information-edit/line-item-information-edit.component.spec.ts
@@ -1,24 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { instance, mock } from 'ts-mockito';
 
+import { AppFacade } from 'ish-core/facades/app.facade';
 import { ProductContextFacade } from 'ish-core/facades/product-context.facade';
 
-import { LineItemEditComponent } from './line-item-edit.component';
+import { LineItemInformationEditComponent } from './line-item-information-edit.component';
 
-describe('Line Item Edit Component', () => {
-  let component: LineItemEditComponent;
-  let fixture: ComponentFixture<LineItemEditComponent>;
+describe('Line Item Information Edit Component', () => {
+  let component: LineItemInformationEditComponent;
+  let fixture: ComponentFixture<LineItemInformationEditComponent>;
   let element: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LineItemEditComponent],
-      providers: [{ provide: ProductContextFacade, useFactory: () => instance(mock(ProductContextFacade)) }],
+      imports: [TranslateModule.forRoot()],
+      declarations: [LineItemInformationEditComponent],
+      providers: [
+        { provide: AppFacade, useFactory: () => instance(mock(AppFacade)) },
+        { provide: ProductContextFacade, useFactory: () => instance(mock(ProductContextFacade)) },
+      ],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LineItemEditComponent);
+    fixture = TestBed.createComponent(LineItemInformationEditComponent);
     component = fixture.componentInstance;
     element = fixture.nativeElement;
   });
