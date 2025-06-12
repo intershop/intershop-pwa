a9697ce5617be9beb7f1ec1c401142e73d12771e
LucasHengelhaupt <lucas.hengelhaupt@gmail.com>
2025-06-12 14:03:25 +0200

    feat(accessibility): handle focus change after navigation (#1831, #1841)

--- src/app/core/store/core/router/router.effects.ts
+++ src/app/core/store/core/router/router.effects.ts
@@ -1,10 +1,9 @@
 import { ApplicationRef, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { routerNavigatedAction } from '@ngrx/router-store';
-import { EMPTY, Observable } from 'rxjs';
+import { EMPTY } from 'rxjs';
 import { delay, switchMap, take } from 'rxjs/operators';
 
-import { log } from 'ish-core/utils/dev/operators';
 import { DomService } from 'ish-core/utils/dom/dom.service';
 import { whenTruthy } from 'ish-core/utils/operators';
 
@@ -20,23 +19,21 @@ export class RouterEffects {
           ofType(routerNavigatedAction),
           delay(50),
           switchMap(() => {
-            const h1Element = this.domService.querySelector('h1');
-
             // focus h1 immediately if it is already available
-            if (h1Element) {
-              console.log('h1 found, setting focus:', h1Element);
-              this.domService.setAttribute(h1Element, 'tabindex', '-1');
-              h1Element.focus();
-
+            if (this.setFocusToHeading()) {
+              console.log('h1 found immediately');
               return EMPTY;
             }
 
-            // If no h1 found, wait for the app to be stable and try again
+            // if no h1 found, wait for the app to be stable and try again
             return this.appRef.isStable.pipe(
-              log('app is stable'),
               whenTruthy(),
               take(1),
-              switchMap(() => this.setFocusToHeading())
+              switchMap(() => {
+                this.setFocusToHeading();
+                console.log('h1 found after stable');
+                return EMPTY;
+              })
             );
           })
         ),
@@ -44,16 +41,20 @@ export class RouterEffects {
     );
 
   /**
-   * Sets focus to the first h1 element on the page after navigation when the application is stable.
+   * Sets focus to the first h1 element on the page.
+   * @returns true if an h1 element was found and focused, otherwise false
    */
-  private setFocusToHeading(): Observable<void> {
+  private setFocusToHeading(): boolean {
     const h1Element = this.domService.querySelector('h1');
 
     if (h1Element) {
+      // necessary to set tabindex, otherwise the element cannot be focused
       this.domService.setAttribute(h1Element, 'tabindex', '-1');
       h1Element.focus();
+
+      return true;
     }
 
-    return EMPTY;
+    return false;
   }
 }
