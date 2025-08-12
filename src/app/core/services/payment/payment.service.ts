080046ef32f158bcff40fa2e7a989ffc82c0dc41
Stefan Hauke <shauke@intershop.de>
2025-08-12 09:07:54 +0200

    style: apply ESLint plugin (@typescript-eslint) update changes
    * `ng lint --fix`
    * some issued needed manual fixing

--- src/app/core/services/payment/payment.service.ts
+++ src/app/core/services/payment/payment.service.ts
@@ -169,7 +169,7 @@ export class PaymentService {
    * @returns                 The payment instrument id.
    */
   private sendRedirectUrlsIfRequired(pm: PaymentMethodBaseData, paymentInstrument: string): Observable<string> {
-    if (!pm?.capabilities || !pm.capabilities.some(data => ['RedirectBeforeCheckout'].includes(data))) {
+    if (!pm?.capabilities?.some(data => ['RedirectBeforeCheckout'].includes(data))) {
       return of(paymentInstrument);
       // send redirect urls if there is a redirect required
     } else {
