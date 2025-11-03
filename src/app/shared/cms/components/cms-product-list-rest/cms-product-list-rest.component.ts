5632cd67d1d23ff938a915b650408debd31f067a
Dorina Kraft <D.Kraft@intershop.de>
2025-11-03 15:45:04 +0100

    feat: predefined endpoints for CMS Product List  (REST) component
    * CMS _Product List (REST)_ rendering component was enhanced to support predefined endpoints for ICM (`icm://`) and SPARQUE (`sparque://`).
    * e.g. `icm://products?searchTerm=notebook&attrs=sku&amount=6`
    * e.g. `sparque://recommendations?strategyString=recommendation_best_sold&count=6`
    * instead of using absolute REST endpoints to a fixed ICM/SPARQUE backend URL

--- src/app/shared/cms/components/cms-product-list-rest/cms-product-list-rest.component.ts
+++ src/app/shared/cms/components/cms-product-list-rest/cms-product-list-rest.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable ish-custom-rules/no-intelligence-in-artifacts */
 // eslint-disable-next-line ish-custom-rules/ban-imports-file-pattern
 import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
@@ -5,8 +6,21 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { ContentPageletView } from 'ish-core/models/content-view/content-view.model';
+import { ApiService } from 'ish-core/services/api/api.service';
+import { SparqueApiService } from 'ish-core/services/sparque-api/sparque-api.service';
 import { CMSComponent } from 'ish-shared/cms/models/cms-component/cms-component.model';
 
+/**
+ * CMS component that displays a product list by fetching product SKUs from a REST endpoint.
+ *
+ * This component supports multiple API protocols:
+ * - `icm://` - Uses the standard ICM API service
+ * - `sparque://` - Uses the Sparque API service with v2 API version
+ * - Standard HTTP URLs - Uses Angular's HttpClient
+ *
+ * The component can optionally apply a mapper function to transform the REST response
+ * and limit the number of products displayed.
+ */
 @Component({
   selector: 'ish-cms-product-list-rest',
   templateUrl: './cms-product-list-rest.component.html',
@@ -17,7 +31,11 @@ export class CMSProductListRestComponent implements CMSComponent, OnChanges {
 
   productSKUs$: Observable<string[]>;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private apiService: ApiService,
+    private sparqueApiService: SparqueApiService
+  ) {}
 
   ngOnChanges() {
     if (this.pagelet.hasParam('ProductsRestEndpoint')) {
@@ -26,23 +44,41 @@ export class CMSProductListRestComponent implements CMSComponent, OnChanges {
   }
 
   private getProductSKUs$(): Observable<string[]> {
-    return this.httpClient.get<unknown>(this.pagelet.stringParam('ProductsRestEndpoint')).pipe(
-      map(data => {
-        let skus: string[] = [];
+    // determine which service to use based on the protocol prefix of the endpoint URL
+    if (this.pagelet.stringParam('ProductsRestEndpoint').startsWith('icm://')) {
+      // use standard ICM API service for 'icm' protocol
+      return this.apiService
+        .get(this.pagelet.stringParam('ProductsRestEndpoint').replace('icm://', ''))
+        .pipe(map(data => this.mapProductData(data)));
+    } else if (this.pagelet.stringParam('ProductsRestEndpoint').startsWith('sparque://')) {
+      // use Sparque API service for 'sparque' protocol (use hardcoded apiVersion 'v2')
+      return this.sparqueApiService
+        .get(this.pagelet.stringParam('ProductsRestEndpoint').replace('sparque://', ''), 'v2')
+        .pipe(map(data => this.mapProductData(data)));
+    } else {
+      // default to standard HTTP client for other URLs
+      return this.httpClient
+        .get<unknown>(this.pagelet.stringParam('ProductsRestEndpoint'))
+        .pipe(map(data => this.mapProductData(data)));
+    }
+  }
 
-        // if the REST response is not already an Array of SKUs
-        // a given mapper function can be applied to the REST 'data' to map the data to an Array of SKUs
-        skus = this.pagelet.hasParam('ProductsRestResponseMapper')
-          ? Function('data', `"use strict"; return ${this.pagelet.stringParam('ProductsRestResponseMapper')}`)(data)
-          : data;
+  private mapProductData(data: unknown): string[] {
+    {
+      let skus: string[] = [];
 
-        // limit the number of rendered products
-        if (this.pagelet.hasParam('MaxNumberOfProducts')) {
-          skus = skus.splice(0, this.pagelet.numberParam('MaxNumberOfProducts'));
-        }
+      // if the REST response is not already an Array of SKUs
+      // a given mapper function can be applied to the REST 'data' to map the data to an Array of SKUs
+      skus = this.pagelet.hasParam('ProductsRestResponseMapper')
+        ? Function('data', `"use strict"; return ${this.pagelet.stringParam('ProductsRestResponseMapper')}`)(data)
+        : data;
 
-        return skus;
-      })
-    );
+      // limit the number of rendered products
+      if (this.pagelet.hasParam('MaxNumberOfProducts')) {
+        skus = skus.splice(0, this.pagelet.numberParam('MaxNumberOfProducts'));
+      }
+
+      return skus;
+    }
   }
 }
