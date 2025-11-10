778bf2bd21d81aff4eb7569430ba8821e7cf53ec
Tim Berk <timberk3@googlemail.com>
2025-11-10 18:20:38 +0100

    feat: introduce CSV import handler for reusability and adapt quick order (#1799)

--- src/app/extensions/quickorder/shared/quickorder-csv-form/quickorder-csv-form.component.spec.ts
+++ src/app/extensions/quickorder/shared/quickorder-csv-form/quickorder-csv-form.component.spec.ts
@@ -1,9 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { instance, mock } from 'ts-mockito';
+import { anyNumber, anyString, instance, mock, verify } from 'ts-mockito';
 
 import { ShoppingFacade } from 'ish-core/facades/shopping.facade';
+import { CsvImportData } from 'ish-core/utils/csv/csv.import-handler';
 
 import { QuickorderCsvFormComponent } from './quickorder-csv-form.component';
 
@@ -11,12 +12,14 @@ describe('Quickorder Csv Form Component', () => {
   let component: QuickorderCsvFormComponent;
   let fixture: ComponentFixture<QuickorderCsvFormComponent>;
   let element: HTMLElement;
+  let shoppingFacadeMock: ShoppingFacade;
 
   beforeEach(async () => {
+    shoppingFacadeMock = mock(ShoppingFacade);
     await TestBed.configureTestingModule({
       declarations: [QuickorderCsvFormComponent],
       imports: [ReactiveFormsModule, TranslateModule.forRoot()],
-      providers: [{ provide: ShoppingFacade, useFactory: () => instance(mock(ShoppingFacade)) }],
+      providers: [{ provide: ShoppingFacade, useValue: instance(shoppingFacadeMock) }],
     }).compileComponents();
   });
 
@@ -30,5 +33,76 @@ describe('Quickorder Csv Form Component', () => {
     expect(component).toBeTruthy();
     expect(element).toBeTruthy();
     expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+  it('should parse CSV correctly', () => {
+    const csvData: CsvImportData = {
+      headers: ['Product ID', 'Quantity'],
+      data: ['12345,10', '67890,5'],
+    };
+
+    component.parseCsvData(csvData);
+    expect(component.productsFromCsv).toHaveLength(2);
+
+    const firstProduct = component.productsFromCsv[0];
+    expect(firstProduct.sku).toEqual('12345');
+    expect(firstProduct.quantity).toEqual(10);
+    const secondProduct = component.productsFromCsv[1];
+    expect(secondProduct.sku).toEqual('67890');
+    expect(secondProduct.quantity).toEqual(5);
+  });
+
+  it('should handle empty csv', () => {
+    const csvData: CsvImportData = {
+      headers: ['Product ID', 'Quantity'],
+      data: [''],
+    };
+
+    component.parseCsvData(csvData);
+    expect(component.productsFromCsv).toBeTruthy();
+    expect(component.productsFromCsv).toHaveLength(0);
+  });
+
+  it('should call addProductToBasket on submit', () => {
+    fixture.detectChanges();
+
+    const csvData: CsvImportData = {
+      headers: ['Product ID', 'Quantity'],
+      data: ['12345,10'],
+    };
+
+    component.parseCsvData(csvData);
+    component.addCsvToCart();
+
+    verify(shoppingFacadeMock.addProductToBasket('12345', 10)).once();
+  });
+
+  it('should not call addroductToBasket when parsedProducts is empty on submit', () => {
+    fixture.detectChanges();
+
+    const csvData: CsvImportData = {
+      headers: ['Product ID', 'Quantity'],
+      data: [''],
+    };
+
+    component.parseCsvData(csvData);
+    component.addCsvToCart();
+
+    verify(shoppingFacadeMock.addProductToBasket(anyString(), anyNumber())).never();
+  });
+
+  it('should reset CSV product array and fileInput', () => {
+    const csvData: CsvImportData = {
+      headers: ['Product ID', 'Quantity'],
+      data: ['12345,10', '67890,5'],
+    };
+
+    component.parseCsvData(csvData);
+    expect(component.productsFromCsv).toHaveLength(2);
+    fixture.detectChanges();
+
+    component.resetInput();
+    expect(component.productsFromCsv).toHaveLength(0);
+    expect(component.fileInput.nativeElement.value).toBeEmpty();
   });
 });
--- src/app/extensions/quickorder/shared/quickorder-csv-form/quickorder-csv-form.component.ts
+++ src/app/extensions/quickorder/shared/quickorder-csv-form/quickorder-csv-form.component.ts
@@ -1,10 +1,19 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  inject,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { ShoppingFacade } from 'ish-core/facades/shopping.facade';
 import { SkuQuantityType } from 'ish-core/models/product/product.model';
-
-declare type CsvStatusType = 'Default' | 'ValidFormat' | 'InvalidFormat' | 'IncorrectInput';
+import { CsvImportData, CsvImportHandler, CsvImportStatus } from 'ish-core/utils/csv/csv.import-handler';
 
 @Component({
   selector: 'ish-quickorder-csv-form',
@@ -13,83 +22,82 @@ declare type CsvStatusType = 'Default' | 'ValidFormat' | 'InvalidFormat' | 'Inco
 })
 export class QuickorderCsvFormComponent implements OnInit {
   csvForm: FormGroup;
-  private productsFromCsv: SkuQuantityType[] = [];
-  status: CsvStatusType;
+  status: CsvImportStatus = 'Default';
+  // not-dead-code
+  productsFromCsv: SkuQuantityType[] = [];
+  private quickOrderHeaders: string[] = ['Product ID', 'Quantity'];
 
-  constructor(private qf: FormBuilder, private cdRef: ChangeDetectorRef, private shoppingFacade: ShoppingFacade) {}
+  private readonly destroyRef = inject(DestroyRef);
 
-  ngOnInit() {
-    this.csvForm = this.qf.group({
-      csvFile: ['', Validators.required],
+  @ViewChild('fileInput') fileInput: ElementRef<HTMLInputElement>;
+
+  constructor(private shoppingFacade: ShoppingFacade, private cdRef: ChangeDetectorRef, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.productsFromCsv = [];
+    this.csvForm = this.fb.group({
+      csvFile: [undefined],
     });
-
-    this.status = 'Default';
   }
 
-  uploadListener(target: EventTarget): void {
-    const files = (target as HTMLInputElement).files;
-    this.status = 'Default';
-
-    if (this.isValidCSVFile(files[0])) {
-      const reader = new FileReader();
-      reader.readAsText(files[0]);
-
-      reader.onload = () => {
-        const csvData = reader.result;
-        const csvRecordsArray = (csvData as string).split(/\r\n|\n/);
-        this.productsFromCsv = this.getDataRecordsArrayFromCSVFile(csvRecordsArray);
-      };
-
-      reader.onloadend = () => {
-        this.status =
-          this.productsFromCsv.filter(p => p.sku !== '' && p.quantity !== undefined).length !== 0
-            ? 'ValidFormat'
-            : 'IncorrectInput';
-
-        this.cdRef.markForCheck();
-      };
-    } else {
-      this.status = 'InvalidFormat';
+  onFileChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const file = input.files?.[0];
+    if (!file) {
+      return;
     }
+
+    CsvImportHandler.processCsvFile(file, this.quickOrderHeaders)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: fileContent => {
+          this.parseCsvData(fileContent);
+          this.cdRef.markForCheck();
+        },
+        error: error => {
+          this.status = error as CsvImportStatus;
+          this.cdRef.markForCheck();
+        },
+      });
   }
 
-  private isValidCSVFile(file: File) {
-    return file.name.endsWith('.csv');
-  }
-
-  private getDataRecordsArrayFromCSVFile(csvRecordsArray: string[]): SkuQuantityType[] {
+  // not-dead-code
+  parseCsvData(csvData: CsvImportData) {
     try {
-      return csvRecordsArray
-        .filter(r => !!r)
-        .map(record => record.split(/[,;]/))
-        .map(record => ({
-          sku: record[0].trim(),
-          quantity: +record[1].trim(),
+      this.productsFromCsv = csvData.data
+        .map(line => line.split(','))
+        .map(columns => ({
+          sku: columns[0].trim(),
+          quantity: +columns[1].trim(),
         }))
-        .filter(record => !isNaN(record.quantity));
+        .filter(record => record.sku && !isNaN(record.quantity));
+      this.status = 'Valid';
     } catch (error) {
-      this.status = 'IncorrectInput';
-      return [];
+      this.status = 'InvalidData';
+      this.productsFromCsv = [];
     }
   }
 
   addCsvToCart() {
-    if (this.status === 'ValidFormat') {
-      if (this.productsFromCsv.length > 0) {
-        this.productsFromCsv.forEach(product => {
-          this.shoppingFacade.addProductToBasket(product.sku, product.quantity);
-        });
-      }
-      this.resetCsvProductArray();
+    if (this.productsFromCsv.length > 0) {
+      this.productsFromCsv.forEach(product => {
+        this.shoppingFacade.addProductToBasket(product.sku, product.quantity);
+      });
+    }
+    this.resetInput();
+  }
+
+  resetInput() {
+    this.productsFromCsv = [];
+    this.status = 'Default';
+    this.csvForm.reset();
+
+    if (this.fileInput) {
+      this.fileInput.nativeElement.value = '';
     }
   }
 
-  resetCsvProductArray() {
-    this.productsFromCsv = [];
-    this.status = 'Default';
-  }
-
   get isCsvDisabled() {
-    return this.status !== 'ValidFormat';
+    return this.status !== 'Valid';
   }
 }
