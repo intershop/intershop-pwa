b92d8403d6db755b1cc29fcb2394e4bdcfa2c9f6
Frederik Gießke <FGiesske@intershop.com>
2025-11-19 16:16:09 +0100

    feat: better pwa - design view interactions

--- src/app/core/store/content/design-view/design-view.reducer.ts
+++ src/app/core/store/content/design-view/design-view.reducer.ts
@@ -4,10 +4,14 @@ import { designViewActions } from './design-view.actions';
 
 export interface DesignViewState {
   selectedPageletId: string;
+  previewedPageletId: string;
+  scrollToPageletId: string;
 }
 
 const initialState: DesignViewState = {
   selectedPageletId: undefined,
+  previewedPageletId: undefined,
+  scrollToPageletId: undefined,
 };
 
 export const designViewReducer = createReducer(
@@ -18,6 +22,22 @@ export const designViewReducer = createReducer(
     (state, action): DesignViewState => ({
       ...state,
       selectedPageletId: action.payload.pageletId,
+    })
+  ),
+
+  on(
+    designViewActions.previewPagelet,
+    (state, action): DesignViewState => ({
+      ...state,
+      previewedPageletId: action.payload.pageletId,
+    })
+  ),
+
+  on(
+    designViewActions.scrollToPagelet,
+    (state, action): DesignViewState => ({
+      ...state,
+      scrollToPageletId: action.payload.pageletId,
     })
   )
 );
