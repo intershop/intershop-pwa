2fe2c48a8a171b3632bbf4c75b5727484ef0a76c
sgrueber <S.Grueber@intershop.de>
2026-01-02 13:51:22 +0100

    fix: variations display on variation master page

--- e2e/cypress/e2e/specs/shopping/variation-handling.b2b.e2e-spec.ts
+++ e2e/cypress/e2e/specs/shopping/variation-handling.b2b.e2e-spec.ts
@@ -38,9 +38,7 @@ describe('Variation Handling B2B', () => {
       at(ProductDetailPage, page => page.gotoMasterProduct());
       at(ProductDetailPage, page => {
         page.sku.should('have.text', _.masterSKU);
-        // flaky test - has to be enabled again
-        // eslint-disable-next-line etc/no-commented-out-code
-        // page.variations.numberOfItems.should('equal', _.numberOfVariations);
+        page.variations.numberOfItems.should('equal', _.numberOfVariations);
       });
     });
   });
@@ -52,9 +50,7 @@ describe('Variation Handling B2B', () => {
       at(ProductDetailPage, page => {
         page.sku.should('have.text', _.masterSKU);
         page.infoNav('Details').should('be.visible');
-        // flaky test - has to be enabled again
-        // eslint-disable-next-line etc/no-commented-out-code
-        // page.variations.numberOfItems.should('equal', _.numberOfVariations);
+        page.variations.numberOfItems.should('equal', _.numberOfVariations);
       });
     });
   });
