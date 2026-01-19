d9b7501a74f78c8ccf0fac6388d21d770f639f42
Steffen Moldenhauer <54577793+smoldenhauer-ish@users.noreply.github.com>
2026-01-19 09:22:21 +0100

    fix: use current locale in static filter navigation image URLs (#1999)

--- src/app/core/models/filter-navigation/filter-navigation.mapper.spec.ts
+++ src/app/core/models/filter-navigation/filter-navigation.mapper.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { provideMockStore } from '@ngrx/store/testing';
 
-import { getICMStaticURL } from 'ish-core/store/core/configuration';
+import { getCurrentLocale, getICMStaticURL } from 'ish-core/store/core/configuration';
 
 import { FilterNavigationData } from './filter-navigation.interface';
 import { FilterNavigationMapper } from './filter-navigation.mapper';
@@ -12,7 +12,14 @@ describe('Filter Navigation Mapper', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideMockStore({ selectors: [{ selector: getICMStaticURL, value: 'http://www.example.org' }] })],
+      providers: [
+        provideMockStore({
+          selectors: [
+            { selector: getICMStaticURL, value: 'http://www.example.org/INTERSHOP/static/WFS/Test-Channel-Site/rest' },
+            { selector: getCurrentLocale, value: 'de_DE' },
+          ],
+        }),
+      ],
     });
     mapper = TestBed.inject(FilterNavigationMapper);
   });
@@ -32,7 +39,9 @@ describe('Filter Navigation Mapper', () => {
 
     it('should parse objects when elements exists with facets', () => {
       const data = {
-        elements: [{ filterEntries: [{ name: 'testName', link: { uri: '/filters/uri?SearchParameter=param' } }] }],
+        elements: [
+          { filterEntries: [{ name: 'testName', link: { uri: '/productfilters/uri?FilterName=FilterValue' } }] },
+        ],
       } as FilterNavigationData;
 
       const model = mapper.fromData(data);
@@ -41,8 +50,8 @@ describe('Filter Navigation Mapper', () => {
       expect(model.filter[0].facets).toHaveLength(1);
       expect(model.filter[0].facets[0].searchParameter).toMatchInlineSnapshot(`
         {
-          "SearchParameter": [
-            "param",
+          "FilterName": [
+            "FilterValue",
           ],
           "category": undefined,
         }
@@ -55,6 +64,53 @@ describe('Filter Navigation Mapper', () => {
       expect(model.filter).toBeTruthy();
       expect(model.filter).toHaveLength(1);
       expect(model.filter[0].facets).toHaveLength(0);
+    });
+
+    it('should use "-" as fallback when currentLocale is undefined for image filter values', () => {
+      const data = {
+        elements: [
+          {
+            filterEntries: [
+              {
+                name: 'testImage',
+                link: { uri: '/productfilters/uri?FilterName=FilterValue' },
+                mappedType: 'image',
+                mappedValue: 'image-unit:/subfolder/image.jpg',
+              },
+            ],
+          },
+        ],
+      } as FilterNavigationData;
+
+      // Override the private currentLocale property to undefined
+      (mapper as unknown as { currentLocale: string }).currentLocale = undefined;
+
+      const model = mapper.fromData(data);
+      expect(model.filter[0].facets[0].mappedValue).toBe(
+        'url(http://www.example.org/INTERSHOP/static/WFS/Test-Channel-Site/rest/image-unit/-/subfolder/image.jpg)'
+      );
+    });
+
+    it('should use currentLocale when available for image filter values', () => {
+      const data = {
+        elements: [
+          {
+            filterEntries: [
+              {
+                name: 'testImage',
+                link: { uri: '/productfilters/uri?FilterName=FilterValue' },
+                mappedType: 'image',
+                mappedValue: 'image-unit:/subfolder/image.jpg',
+              },
+            ],
+          },
+        ],
+      } as FilterNavigationData;
+
+      const model = mapper.fromData(data);
+      expect(model.filter[0].facets[0].mappedValue).toBe(
+        'url(http://www.example.org/INTERSHOP/static/WFS/Test-Channel-Site/rest/image-unit/de_DE/subfolder/image.jpg)'
+      );
     });
   });
 });
