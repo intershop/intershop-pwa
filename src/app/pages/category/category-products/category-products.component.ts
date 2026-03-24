bd49d2cf5f5a2ffda6834c737b6134b557804a4a
Tim Berk <timberk3@googlemail.com>
2026-03-24 14:26:49 +0100

    fix: update initial collapsed state and adjust product image styles for better responsiveness

--- src/app/pages/category/category-products/category-products.component.ts
+++ src/app/pages/category/category-products/category-products.component.ts
@@ -32,7 +32,7 @@ export class CategoryProductsComponent implements OnInit, OnChanges {
   @Input({ required: true }) category: CategoryView;
   @Input() deviceType: DeviceType;
 
-  isCollapsed = false;
+  isCollapsed = true;
 
   ngOnInit() {
     this.isCollapsed = this.deviceType === 'mobile';
