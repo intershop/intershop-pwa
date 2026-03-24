4dcc86c635c22211ecd8d45cba1a94902bf03397
Tim Berk <timberk3@googlemail.com>
2026-03-24 13:13:53 +0100

    Reapply "temp: added lazy loading product-carousel component to remove swipermodule out of initial bundle"
    This reverts commit 572f52e9570431895fe8142d7eee8c33fd5c7c6b.

--- src/app/shared/components/product/products-list/products-list.component.ts
+++ src/app/shared/components/product/products-list/products-list.component.ts
@@ -1,33 +1,25 @@
 import { AsyncPipe, NgClass } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Inject, Input, OnChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
 import { isEqual } from 'lodash-es';
 import { Observable, combineLatest, of } from 'rxjs';
 import { distinctUntilChanged, map } from 'rxjs/operators';
-import SwiperCore, { Navigation, Pagination, SwiperOptions, A11y } from 'swiper';
-import { SwiperModule } from 'swiper/angular';
 
-import {
-  LARGE_BREAKPOINT_WIDTH,
-  MEDIUM_BREAKPOINT_WIDTH,
-  SMALL_BREAKPOINT_WIDTH,
-} from 'ish-core/configurations/injection-keys';
 import { ProductContextDirective } from 'ish-core/directives/product-context.directive';
 import { ProductContextDisplayProperties } from 'ish-core/facades/product-context.facade';
 import { ShoppingFacade } from 'ish-core/facades/shopping.facade';
-import { InjectSingle } from 'ish-core/utils/injection';
 import {
   ProductItemComponent,
   ProductItemDisplayType,
 } from 'ish-shared/components/product/product-item/product-item.component';
 
-SwiperCore.use([Pagination, Navigation, A11y]);
+import { ProductsListCarouselComponent } from './products-list-carousel/products-list-carousel.component';
 
 @Component({
   selector: 'ish-products-list',
   templateUrl: './products-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [AsyncPipe, ProductItemComponent, NgClass, ProductContextDirective, SwiperModule],
+  imports: [AsyncPipe, ProductItemComponent, NgClass, ProductContextDirective, ProductsListCarouselComponent],
 })
 export class ProductsListComponent implements OnChanges {
   @Input({ required: true }) productSKUs: string[];
@@ -38,120 +30,13 @@ export class ProductsListComponent implements OnChanges {
   @Input() listItemConfiguration: Partial<ProductContextDisplayProperties>;
 
   productSKUs$: Observable<string[]>;
-
-  /**
-   * track already fetched SKUs
-   */
-  private fetchedSKUs = new Set<string>();
-
-  /**
-   * configuration of swiper carousel
-   * https://swiperjs.com/swiper-api
-   */
-  swiperConfig: SwiperOptions;
-
-  constructor(
-    @Inject(SMALL_BREAKPOINT_WIDTH) private smallBreakpointWidth: InjectSingle<typeof SMALL_BREAKPOINT_WIDTH>,
-    @Inject(MEDIUM_BREAKPOINT_WIDTH) private mediumBreakpointWidth: InjectSingle<typeof MEDIUM_BREAKPOINT_WIDTH>,
-    @Inject(LARGE_BREAKPOINT_WIDTH) private largeBreakpointWidth: InjectSingle<typeof LARGE_BREAKPOINT_WIDTH>,
-    private shoppingFacade: ShoppingFacade
-  ) {
-    this.swiperConfig = {
-      watchSlidesProgress: true,
-      direction: 'horizontal',
-      navigation: true,
-      pagination: {
-        clickable: true,
-      },
-      observer: true,
-      observeParents: true,
-    };
-  }
+  constructor(private shoppingFacade: ShoppingFacade) {}
 
   ngOnChanges(): void {
-    this.configureSlides(this.slideItems);
-
     // remove all SKUs from the productSKUs Array that are also contained in the failed products Array
     this.productSKUs$ = combineLatest([of(this.productSKUs), this.shoppingFacade.failedProducts$]).pipe(
       distinctUntilChanged<[string[], string[]]>(isEqual),
       map(([skus, failed]) => skus.filter(x => !failed.includes(x)))
     );
-  }
-
-  lazyFetch(fetch: boolean, sku: string): boolean {
-    if (fetch) {
-      this.fetchedSKUs.add(sku);
-    }
-    return this.fetchedSKUs.has(sku);
-  }
-
-  /**
-   * Configure Swiper slidesPerView/slidesPerGroup settings
-   * with breakpoint responsive design considerations based on the given slide items.
-   *
-   * @param slideItems The amount of slide items that should be rendered if enough screen space is available.
-   */
-  private configureSlides(slideItems: number) {
-    switch (slideItems) {
-      case 1: {
-        this.swiperConfig.breakpoints = {
-          0: {
-            slidesPerView: 1,
-            slidesPerGroup: 1,
-          },
-        };
-        break;
-      }
-      case 2: {
-        this.swiperConfig.breakpoints = {
-          0: {
-            slidesPerView: 1,
-            slidesPerGroup: 1,
-          },
-          [this.smallBreakpointWidth]: {
-            slidesPerView: 2,
-            slidesPerGroup: 2,
-          },
-        };
-        break;
-      }
-      case 3: {
-        this.swiperConfig.breakpoints = {
-          0: {
-            slidesPerView: 1,
-            slidesPerGroup: 1,
-          },
-          [this.smallBreakpointWidth]: {
-            slidesPerView: 2,
-            slidesPerGroup: 2,
-          },
-          [this.mediumBreakpointWidth]: {
-            slidesPerView: 3,
-            slidesPerGroup: 3,
-          },
-        };
-        break;
-      }
-      default: {
-        this.swiperConfig.breakpoints = {
-          0: {
-            slidesPerView: 1,
-            slidesPerGroup: 1,
-          },
-          [this.smallBreakpointWidth]: {
-            slidesPerView: 2,
-            slidesPerGroup: 2,
-          },
-          [this.mediumBreakpointWidth]: {
-            slidesPerView: 3,
-            slidesPerGroup: 3,
-          },
-          [this.largeBreakpointWidth]: {
-            slidesPerView: 4,
-            slidesPerGroup: 4,
-          },
-        };
-      }
-    }
   }
 }
