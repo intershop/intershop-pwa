2c0686c98d6a5caacfc278bdcd0ec9a707e9aa51
Tim Berk <timberk3@googlemail.com>
2026-03-25 13:28:00 +0100

    temp: homepage cls fix

--- src/app/shared/cms/components/content-pagelet/content-pagelet.component.ts
+++ src/app/shared/cms/components/content-pagelet/content-pagelet.component.ts
@@ -23,7 +23,6 @@ import { whenTruthy } from 'ish-core/utils/operators';
 import { ContentDesignViewWrapperComponent } from 'ish-shared/cms/components/content-design-view-wrapper/content-design-view-wrapper.component';
 import { CMSComponentProvider, CMS_COMPONENT } from 'ish-shared/cms/configurations/injection-keys';
 import { CMSComponent } from 'ish-shared/cms/models/cms-component/cms-component.model';
-import { LoadingComponent } from 'ish-shared/components/common/loading/loading.component';
 
 /**
  * The Content Pagelet Component renders the pagelet for the given 'pageletId'.
@@ -39,19 +38,15 @@ import { LoadingComponent } from 'ish-shared/components/common/loading/loading.c
   styleUrls: ['./content-pagelet.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [ContentDesignViewWrapperComponent, LoadingComponent],
+  imports: [ContentDesignViewWrapperComponent],
 })
 export class ContentPageletComponent implements OnChanges, OnInit {
-  private static readonly FEATURED_PRODUCTS_PAGELET_PREFIX = 'pwa_featured_products';
-
   /**
    * The Id of the Pagelet that is to be rendered.
    */
   @Input() pageletId: string;
 
   @ViewChild('cmsOutlet', { read: ViewContainerRef, static: true }) cmsOutlet: ViewContainerRef;
-
-  pageletRendered = false;
 
   private pageletId$ = new ReplaySubject<string>(1);
   private destroyRef = inject(DestroyRef);
@@ -67,13 +62,11 @@ export class ContentPageletComponent implements OnChanges, OnInit {
       )
       .subscribe(pagelet => {
         this.mapComponent(pagelet);
-        this.pageletRendered = true;
         this.cdRef.markForCheck();
       });
   }
 
   ngOnChanges() {
-    this.pageletRendered = false;
     this.pageletId$.next(this.pageletId);
   }
 
@@ -103,13 +96,5 @@ export class ContentPageletComponent implements OnChanges, OnInit {
     if (instance.ngOnChanges) {
       instance.ngOnChanges({ pagelet: new SimpleChange(undefined, pagelet, true) });
     }
-  }
-
-  get shouldShowPlaceholder(): boolean {
-    return !this.pageletRendered && this.hasFeaturedProductsPlaceholder(this.pageletId);
-  }
-
-  private hasFeaturedProductsPlaceholder(pageletId: string): boolean {
-    return pageletId?.startsWith(ContentPageletComponent.FEATURED_PRODUCTS_PAGELET_PREFIX);
   }
 }
