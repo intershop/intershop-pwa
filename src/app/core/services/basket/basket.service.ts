0f7596cd6725e58535e3c9b19e17a362657e0490
Susanne Schneider <s.schneider@intershop.de>
2026-03-27 12:45:49 +0100

    chore: update to Prettier 3 and Stylelint 17 (#2053)
    * add `@prettier/sync` to make Jest work together with Prettier 3
    * add migration note

--- src/app/core/services/basket/basket.service.ts
+++ src/app/core/services/basket/basket.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 import { HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
