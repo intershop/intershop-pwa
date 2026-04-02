00827fb29c90855076ee802e8874621cf5702f78
Stefan Hauke <shauke@intershop.de>
2026-04-02 11:35:59 +0200

    feat: display inventory supplier stock at product detail if available (#2051)
    * inspired by warehouse example from training example
    * works if e.g. IOM returns supplier stock via REST API

--- src/app/core/models/product-inventory/product-inventory.model.ts
+++ src/app/core/models/product-inventory/product-inventory.model.ts
@@ -2,4 +2,12 @@ export interface ProductInventory {
   sku: string;
   inStock: boolean;
   availableStock?: number;
+  supplierStock?: SupplierStock[];
+}
+
+export interface SupplierStock {
+  id: string; // supplier id
+  displayName: string; // supplier display name
+  inStock: boolean;
+  availableStock?: number;
 }
