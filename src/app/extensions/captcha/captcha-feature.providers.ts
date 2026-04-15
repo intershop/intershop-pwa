cfb91c77f970fc56e2e5da9c51a220de6864586b
Tim Berk <timberk3@googlemail.com>
2026-04-15 10:27:49 +0200

    fix: captcha v3 error on b2c

--- src/app/extensions/captcha/captcha-feature.providers.ts
+++ src/app/extensions/captcha/captcha-feature.providers.ts
@@ -1,12 +1,18 @@
 import { EnvironmentProviders, Provider } from '@angular/core';
+import { RECAPTCHA_V3_SITE_KEY } from 'ng-recaptcha-2';
 
-import { SitekeyProviderService } from './exports/sitekey-provider/sitekey-provider.service';
+import { SitekeyProviderService, getSynchronizedSiteKey } from './exports/sitekey-provider/sitekey-provider.service';
 
 export function provideCaptchaFeature(): (Provider | EnvironmentProviders)[] {
   return [
     {
       provide: SitekeyProviderService,
       useClass: SitekeyProviderService,
+    },
+    {
+      provide: RECAPTCHA_V3_SITE_KEY,
+      useFactory: getSynchronizedSiteKey,
+      deps: [SitekeyProviderService],
     },
   ];
 }
