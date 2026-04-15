b501b2d9d92485f08231c39336fdbcdaeedcbc9e
Tim Berk <timberk3@googlemail.com>
2026-04-15 13:42:12 +0200

    fix: add missing formly config for rating feature

--- src/app/extensions/rating/rating-feature.providers.ts
+++ src/app/extensions/rating/rating-feature.providers.ts
@@ -1,9 +1,23 @@
 import { EnvironmentProviders, Provider } from '@angular/core';
+import { ConfigOption, provideFormlyConfig } from '@ngx-formly/core';
 
 import { LAZY_FEATURE_MODULE } from 'ish-core/utils/module-loader/module-loader.service';
 
+import { RatingStarsFieldComponent } from './formly/rating-stars-field/rating-stars-field.component';
+
+const ratingFormlyConfig: ConfigOption = {
+  types: [
+    {
+      name: 'ish-rating-stars-field',
+      component: RatingStarsFieldComponent,
+      wrappers: ['form-field-horizontal', 'validation'],
+    },
+  ],
+};
+
 export function provideRatingFeature(): (Provider | EnvironmentProviders)[] {
   return [
+    provideFormlyConfig(ratingFormlyConfig),
     {
       provide: LAZY_FEATURE_MODULE,
       useValue: {
@@ -16,4 +30,3 @@ export function provideRatingFeature(): (Provider | EnvironmentProviders)[] {
 }
 
 export const RATING_FEATURE_PROVIDERS = provideRatingFeature();
-
