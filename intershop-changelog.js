89be8f18a0ed0ff15b922ee294cf9108e70765b6
Stefan Hauke <SHauke@intershop.com>
2026-05-22 15:53:31 +0200

    deps: migration from deprecated `conventional-changelog-cli` to current `conventional-changelog` (#2095)
    * used to generate the `CHANGELOG.md` file entries for new releases
    * `intershop-changelog.js` config script migrated by Copilot

--- intershop-changelog.js
+++ intershop-changelog.js
@@ -1,107 +1,91 @@
 'use strict';
-var compareFunc = require('compare-func');
-var Q = require('q');
-var readFile = Q.denodeify(require('fs').readFile);
-var resolve = require('path').resolve;
+var fs = require('fs');
+var path = require('path');
 
-var parserOpts = {
-  headerPattern: /^(\w*)(?:\((.*)\))?: (.*)$/,
-  headerCorrespondence: ['type', 'scope', 'subject'],
-  noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
-  revertPattern: /^revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
-  revertCorrespondence: ['header', 'hash'],
-};
+module.exports = {
+  parser: {
+    headerPattern: /^(\w*)(?:\((.*)\))?: (.*)$/,
+    headerCorrespondence: ['type', 'scope', 'subject'],
+    noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+    revertPattern: /^revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
+    revertCorrespondence: ['header', 'hash'],
+  },
+  writer: {
+    transform: function (commit) {
+      var noBreakingChanges = true;
+      var diff = {};
 
-var writerOpts = {
-  transform: function (commit) {
-    var noBreakingChanges = true;
-    var issues = [];
-
-    commit.notes.forEach(note => {
-      note.title = 'BREAKING CHANGES';
-      noBreakingChanges = false;
-    });
-
-    if (commit.type === 'feat') {
-      commit.type = 'Features';
-    } else if (commit.type === 'fix') {
-      commit.type = 'Bug Fixes';
-    } else if (commit.type === 'perf') {
-      commit.type = 'Performance Improvements';
-    } else if (commit.type === 'docs') {
-      commit.type = 'Documentation';
-    } else if (commit.type === 'deps') {
-      commit.type = 'Dependencies';
-    } else if (noBreakingChanges) {
-      return;
-    } else if (commit.type === 'style') {
-      commit.type = 'Styles';
-    } else if (commit.type === 'refactor') {
-      commit.type = 'Code Refactoring';
-    } else if (commit.type === 'revert') {
-      commit.type = 'Reverts';
-    } else if (commit.type === 'test') {
-      commit.type = 'Tests';
-    } else if (commit.type === 'chore') {
-      commit.type = 'Chores';
-    }
-
-    if (commit.scope === '*') {
-      commit.scope = '';
-    }
-
-    if (typeof commit.hash === 'string') {
-      commit.hash = commit.hash.substring(0, 7);
-    }
-
-    // remove references that already appear in the subject
-    commit.references = commit.references.filter(reference => {
-      if (issues.indexOf(reference.issue) === -1) {
-        return true;
+      if (commit.notes && commit.notes.length > 0) {
+        diff.notes = commit.notes.map(note => Object.assign({}, note, { title: 'BREAKING CHANGES' }));
+        noBreakingChanges = false;
       }
 
-      return false;
-    });
+      if (commit.type === 'feat') {
+        diff.type = 'Features';
+      } else if (commit.type === 'fix') {
+        diff.type = 'Bug Fixes';
+      } else if (commit.type === 'perf') {
+        diff.type = 'Performance Improvements';
+      } else if (commit.type === 'docs') {
+        diff.type = 'Documentation';
+      } else if (commit.type === 'deps') {
+        diff.type = 'Dependencies';
+      } else if (noBreakingChanges) {
+        return null;
+      } else if (commit.type === 'refactor') {
+        diff.type = 'Code Refactoring';
+      } else if (commit.type === 'style') {
+        diff.type = 'Styles';
+      } else if (commit.type === 'test') {
+        diff.type = 'Tests';
+      } else if (commit.type === 'revert') {
+        diff.type = 'Reverts';
+      } else if (commit.type === 'chore') {
+        diff.type = 'Chores';
+      }
 
-    return commit;
+      if (commit.scope === '*') {
+        diff.scope = '';
+      }
+
+      if (typeof commit.hash === 'string') {
+        diff.hash = commit.hash.substring(0, 7);
+      }
+
+      // remove references that already appear in the subject
+      var issues = [];
+      diff.references = commit.references.filter(reference => issues.indexOf(reference.issue) === -1);
+
+      return Object.assign({}, commit, diff);
+    },
+    groupBy: 'type',
+    commitGroupsSort: function (arg1, arg2) {
+      var order = [
+        'Features',
+        'Bug Fixes',
+        'Performance Improvements',
+        'Documentation',
+        'Dependencies',
+        'Code Refactoring',
+        'Styles',
+        'Tests',
+        'Reverts',
+        'Chores',
+      ];
+      if (order.indexOf(arg1.title) < order.indexOf(arg2.title)) {
+        return -1;
+      }
+      if (order.indexOf(arg1.title) > order.indexOf(arg2.title)) {
+        return 1;
+      }
+      return 0;
+    },
+    commitsSort: ['scope'],
+    noteGroupsSort: 'title',
+    notesSort: 'text',
+    mainTemplate: fs.readFileSync(path.resolve(__dirname, 'templates/template.hbs'), 'utf-8'),
+    headerPartial: fs.readFileSync(path.resolve(__dirname, 'templates/header.hbs'), 'utf-8'),
+    commitPartial: fs.readFileSync(path.resolve(__dirname, 'templates/commit.hbs'), 'utf-8'),
+    footerPartial: fs.readFileSync(path.resolve(__dirname, 'templates/footer.hbs'), 'utf-8'),
   },
-  groupBy: 'type',
-  commitGroupsSort: function (arg1, arg2) {
-    var order = [
-      'Features',
-      'Bug Fixes',
-      'Performance Improvements',
-      'Documentation',
-      'Dependencies',
-      'Code Refactoring',
-    ];
-    if (order.indexOf(arg1.title) < order.indexOf(arg2.title)) {
-      return -1;
-    }
-    if (order.indexOf(arg1.title) > order.indexOf(arg2.title)) {
-      return 1;
-    }
-    return 0;
-  },
-  commitsSort: ['scope'],
-  noteGroupsSort: 'title',
-  notesSort: compareFunc,
 };
-
-module.exports = Q.all([
-  readFile(resolve(__dirname, 'templates/template.hbs'), 'utf-8'),
-  readFile(resolve(__dirname, 'templates/header.hbs'), 'utf-8'),
-  readFile(resolve(__dirname, 'templates/commit.hbs'), 'utf-8'),
-  readFile(resolve(__dirname, 'templates/footer.hbs'), 'utf-8'),
-]).spread((template, header, commit, footer) => {
-  writerOpts.mainTemplate = template;
-  writerOpts.headerPartial = header;
-  writerOpts.commitPartial = commit;
-  writerOpts.footerPartial = footer;
-
-  return {
-    gitRawCommitsOpts: { merges: null },
-    parserOpts: parserOpts,
-    writerOpts: writerOpts,
-  };
-});
