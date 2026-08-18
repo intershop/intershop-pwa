9df1ba43cf36ee91961c88cde22844e1ac402b65
suschneider <57661483+suschneider@users.noreply.github.com>
2026-08-18 16:05:23 +0200

    fix: quantity for adding retail set products to order template (#2160)

--- src/app/extensions/order-templates/shared/product-add-to-order-template/product-add-to-order-template.component.ts
+++ src/app/extensions/order-templates/shared/product-add-to-order-template/product-add-to-order-template.component.ts
@@ -6,6 +6,7 @@ import { take } from 'rxjs/operators';
 
 import { AccountFacade } from 'ish-core/facades/account.facade';
 import { ProductContextFacade } from 'ish-core/facades/product-context.facade';
+import { ProductHelper } from 'ish-core/models/product/product.model';
 import { GenerateLazyComponent } from 'ish-core/utils/module-loader/generate-lazy-component.decorator';
 
 import { OrderTemplatesFacade } from '../../facades/order-templates.facade';
@@ -65,18 +66,12 @@ export class ProductAddToOrderTemplateComponent implements OnInit {
   }
 
   addProductToOrderTemplate(orderTemplate: { id: string; title: string }) {
+    const product = this.context.get('product');
+    const quantity = ProductHelper.isRetailSet(product) ? 1 : this.context.get('quantity');
     if (!orderTemplate.id) {
-      this.orderTemplatesFacade.addProductToNewOrderTemplate(
-        orderTemplate.title,
-        this.context.get('sku'),
-        this.context.get('quantity')
-      );
+      this.orderTemplatesFacade.addProductToNewOrderTemplate(orderTemplate.title, this.context.get('sku'), quantity);
     } else {
-      this.orderTemplatesFacade.addProductToOrderTemplate(
-        orderTemplate.id,
-        this.context.get('sku'),
-        this.context.get('quantity')
-      );
+      this.orderTemplatesFacade.addProductToOrderTemplate(orderTemplate.id, this.context.get('sku'), quantity);
     }
   }
 }
