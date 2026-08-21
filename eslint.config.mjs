399fbdfa7aa209f27e287e5c988d49df78aa7b97
Andreas Steinmann <asteinmann@intershop.com>
2026-08-21 15:41:21 +0200

    chore: tag UCP integration touchpoints with @ucp-integration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -38,7 +38,7 @@ export default defineConfig([
     'eslint-rules/dist/**/*',
     'schematics/dist/**/*',
     'schematics/src/utils/lint-fix-mock.js',
-    'ucp/**/*',
+    'ucp/**/*', // @ucp-integration
   ]),
   // TypeScript recommended + stylistic configs from typescript-eslint
   ...[...tseslint.configs.recommended, ...tseslint.configs.stylistic].map(config => ({
