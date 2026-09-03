222bfdb85d7953348cec4c232bb18d0c5f482ee3
Tim Berk <timberk3@googlemail.com>
2026-09-03 15:00:23 +0200

    fix: support arbitrary configurations in esbuild build scripts

--- scripts/build-multi-pwa.js
+++ scripts/build-multi-pwa.js
@@ -2,7 +2,11 @@ const { mkdirSync, readFileSync, writeFileSync } = require('fs');
 const { sync: spawnSync } = require('cross-spawn');
 
 const packageJson = JSON.parse(readFileSync('package.json', { encoding: 'utf-8' }));
-const activeThemes = (process.env.npm_config_active_themes || packageJson.config['active-themes'])
+const activeThemes = (
+  process.env.ACTIVE_THEMES ||
+  process.env.npm_config_active_themes ||
+  packageJson.config['active-themes']
+)
   .split(',')
   .map(theme => theme.trim())
   .filter(Boolean);
@@ -17,15 +21,20 @@ if (!activeThemes.length) {
 const ports = {};
 
 activeThemes.forEach((theme, index) => {
-  const buildScript = clientOnly ? `build:client:${theme}` : `build:${theme}`;
-  if (!packageJson.scripts[buildScript]) {
-    console.error(`Missing npm script "${buildScript}".`);
-    process.exit(1);
+  const result = spawnSync(
+    'node',
+    [
+      'scripts/build-pwa.js',
+      ...(clientOnly ? ['client'] : []),
+      `--configuration=${theme}`,
+      ...buildArguments,
+      `--output-path=dist/${theme}`,
+    ],
+    { stdio: 'inherit' }
+  );
+  if (result.error) {
+    throw result.error;
   }
-
-  const result = spawnSync('npm', ['run', buildScript, '--', ...buildArguments, `--output-path=dist/${theme}`], {
-    stdio: 'inherit',
-  });
   if (result.status !== 0) {
     process.exit(result.status);
   }
@@ -41,4 +50,7 @@ mkdirSync('dist', { recursive: true });
 writeFileSync('dist/ecosystem-ports.json', JSON.stringify(ports, undefined, 2));
 
 const runtimeBuild = spawnSync('npm', ['run', 'build:ssr-runtime'], { stdio: 'inherit' });
-process.exit(runtimeBuild.status);
+if (runtimeBuild.error) {
+  throw runtimeBuild.error;
+}
+process.exit(runtimeBuild.status ?? 1);
